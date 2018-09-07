ae14b36503bb0cab7c04353b4cc08a7084b22a71
Jakob Engelbrecht Olesen <jakob@basher.dk>
2018-09-07 14:31:46 +0200

    Add Card component

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'kirby-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent implements OnInit {
+  @Input() title: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
