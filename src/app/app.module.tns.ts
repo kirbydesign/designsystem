abfb0234720e5afb38bb771b9586ea06b938c9df
andersbloch <jb4692@jyskebank.dk>
2018-09-08 18:07:36 +0200

    Nativescript card, etc. added.

--- src/app/app.module.tns.ts
+++ src/app/app.module.tns.ts
@@ -4,6 +4,7 @@ import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import {CardComponent} from './card/card.component';
 
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
@@ -16,6 +17,7 @@ import { HomeComponent } from './home/home.component';
   declarations: [
     AppComponent,
     HomeComponent,
+    CardComponent,
   ],
   imports: [
     NativeScriptModule,
