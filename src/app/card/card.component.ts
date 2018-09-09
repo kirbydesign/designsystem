64858e744a7b46d5c0f4fca254c29cceaa476ff2
Jakob Engelbrecht Olesen <jakob@basher.dk>
2018-09-09 13:29:33 +0200

    Added subtitle property

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CardComponent implements OnInit {
   @Input() title: string;
+  @Input() subtitle: string;
 
   constructor() { }
 
