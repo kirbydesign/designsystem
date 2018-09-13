9a691029e1b6539588e0b3263fbda759c9bd0403
Tommy Mathiesen <tommy.rolchau.mathiesen@jyskebank.dk>
2018-09-13 10:43:01 +0200

    Stuff

--- src/app/page/header/header.component.ts
+++ src/app/page/header/header.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  items:string[] = ['Design', 'Components'];
+
   constructor() { }
 
   ngOnInit() {
