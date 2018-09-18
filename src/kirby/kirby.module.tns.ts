2f9928f21096d75367411c2a2cec630e09bd0f34
Jakob Engelbrecht Olesen <jakob@basher.dk>
2018-09-18 14:41:37 +0200

    Moved declarations to common file

--- src/kirby/kirby.module.tns.ts
+++ src/kirby/kirby.module.tns.ts
@@ -1,15 +1,7 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptCommonModule } from 'nativescript-angular/common';
 
-import {CardComponent} from './components/card/card.component';
-import {ButtonComponent} from './components/button/button.component';
-import {ListComponent} from './components/list/list.component';
-
-const declarations = [
-  CardComponent,
-  ButtonComponent,
-  ListComponent,
-];
+import {declarations} from './kirby.common';
 
 @NgModule({
   imports: [NativeScriptCommonModule],
--- src/kirby/kirby.module.ts
+++ src/kirby/kirby.module.ts
@@ -1,16 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import {CardComponent} from './components/card/card.component';
-import {ButtonComponent} from './components/button/button.component';
-import {ListComponent} from './components/list/list.component';
-
-const declarations = [
-  CardComponent,
-  ButtonComponent,
-  ListComponent,
-];
-
+import {declarations} from './kirby.common';
 
 @NgModule({
   imports: [CommonModule],
