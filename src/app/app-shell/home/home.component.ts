70e6e27c4870c305d75d4ca523ad4974a87f6225
Tommy Mathiesen <tommy.rolchau.mathiesen@jyskebank.dk>
2018-09-20 15:26:37 +0200

    :sparkles: Added first draft of the app-shell

--- src/app/app-shell/home/home.component.ts
+++ src/app/app-shell/home/home.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  isMenuOpen = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onToggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
   }
 
 }
