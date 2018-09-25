c5360ae587f318d6fdeebd5540e42237e1b510c1
Jakob Engelbrecht Olesen <jakob@basher.dk>
2018-09-25 15:21:15 +0200

    Replace css classes with input properties

--- src/kirby/components/button/button.component.ts
+++ src/kirby/components/button/button.component.ts
@@ -7,11 +7,21 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
   @Input() label: string;
+  @Input() shape?: 'round';
+  @Input() expand?: 'full' | 'block';
   @Output() action = new EventEmitter<number>();
+  buttonCssClasses: {};
   private clickCount = 0;
+
   constructor() { }
 
   ngOnInit() {
+    const { expand, shape } = this;
+    this.buttonCssClasses = {
+      'round': shape === 'round',
+      'block': expand === 'block',
+      'full': expand === 'full',
+    };
   }
 
   handleClick() {
