e4253076357891a1841a10a99f85a6abf6d9abcf
Jakob Engelbrecht Olesen <jakob@basher.dk>
2018-10-04 12:33:16 +0200

    Added npm publishing

---
+++ src/kirby/index.ts
@@ -0,0 +1,5 @@
+export { KirbyModule } from './kirby.module';
+export { CardComponent } from './components/card/card.component';
+export { ButtonComponent } from './components/button/button.component';
+export { ListComponent } from './components/list/list.component';
+export { IconComponent } from './components/icon/icon.component';
