acf9d6c76080be09921bfa1b11e3e42cf4e366f7
Tommy Rolchau Mathiesen <tommy.rolchau.mathiesen@jyskebank.dk>
2018-10-18 08:47:18 +0200

    :sparkles: Modal now handles tabbing

--- src/app/examples/modal-example/modal-example.component.ts
+++ src/app/examples/modal-example/modal-example.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalExampleComponent implements OnInit {
 
+  isLightModalOpen = false;
+  isDarkModalOpen = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  openLightModal() {
+    this.isLightModalOpen = true;
+    console.log('Opening light modal...');
+  }
+
+  openDarkModal() {
+    this.isDarkModalOpen = true;
+    console.log('Opening dark modal...');
+  }
+
+  closeDarkModal() {
+    this.isDarkModalOpen = false;
+    console.log('Closing dark modal...do stuff');
+  }
+
+  closeLightModal() {
+    this.isLightModalOpen = false;
+    console.log('Closing light modal...do stuff');
   }
 
 }
--- src/kirby/components/modal/modal.component.ts
+++ src/kirby/components/modal/modal.component.ts
@@ -1,24 +1,138 @@
-import { Component, OnInit, Input, HostListener } from '@angular/core';
+import {  Component,
+          OnInit,
+          OnDestroy,
+          Input,
+          Output,
+          EventEmitter,
+          ViewChild,
+          ElementRef,
+          AfterViewChecked,
+        } from '@angular/core';
 
 @Component({
   selector: 'kirby-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss']
 })
-export class ModalComponent implements OnInit {
-
+export class ModalComponent implements OnInit, OnDestroy, AfterViewChecked {
   // State of the modal - called from another component, e.g. a button
-  @Input() open = false;
+  private _isOpen = false;
+  // Theme option - e.g. dark, light or whatever theme there is
+  @Input() theme?: string;
+  modalCssClasses: {};
+  // Output closing state
+  @Output() closingModal = new EventEmitter();
+  // Pointer to modal DOM element
+  private activeModal: ElementRef;
+  private isFocus: boolean;
+  // Remember which element had focus before opening the modal
+  private oldFocus: HTMLElement;
 
-  ngOnInit() {
+  @ViewChild('activeModal') set content(content: ElementRef) {
+    this.activeModal = content;
   }
 
-  @HostListener('window:keyup', ['$event'])
-  handleKeyup(evt: KeyboardEvent) {
+  ngOnInit() {
+    const { theme } = this;
+    this.modalCssClasses = {
+      ['theme--' + theme] : theme
+    };
+    this.keydownHandler = this.keydownHandler.bind(this);
+    this.keyupHandler = this.keyupHandler.bind(this);
+  }
+
+  ngOnDestroy() {
+    document.removeEventListener('keydown', this.keydownHandler, false);
+    document.removeEventListener('keyup', this.keyupHandler, false);
+  }
+
+  ngAfterViewChecked() {
+    if (this.activeModal !== undefined && !this.isFocus) {
+      this.oldFocus = <HTMLElement>document.activeElement;
+      const focusableElms: HTMLElement[] = this._getFocusableChildren(this.activeModal.nativeElement);
+
+      if (focusableElms.length) {
+        focusableElms[0].focus();
+      }
+
+      this.isFocus = true;
+      console.log('AfterViewChecked called - elm: ', this.activeModal);
+    }
+  }
+
+  @Input()
+  set isOpen(value: boolean) {
+    this._isOpen = value;
+    if (this._isOpen) {
+      document.addEventListener('keydown', this.keydownHandler, false);
+      document.addEventListener('keyup', this.keyupHandler, false);
+    } else {
+      document.removeEventListener('keydown', this.keydownHandler, false);
+      document.removeEventListener('keyup', this.keyupHandler, false);
+    }
+  }
+
+  get isOpen(): boolean {
+    return this._isOpen;
+  }
+
+  closeModal() {
+    this._isOpen = false;
+    this.isFocus = false;
+    this.closingModal.emit();
+    this.oldFocus.focus();
+  }
+
+  trapTabKey(evt) {
+    const elm = this.activeModal.nativeElement;
+    const focusableChildren = this._getFocusableChildren(elm);
+    const focusedItemIndex = focusableChildren.indexOf(<HTMLElement>document.activeElement);
+
+    if (evt.shiftKey && focusedItemIndex === 0) {
+      (<HTMLElement>focusableChildren[focusableChildren.length - 1]).focus();
+      event.preventDefault();
+    } else if (!evt.shiftKey && focusedItemIndex === focusableChildren.length - 1) {
+      (<HTMLElement>focusableChildren[0]).focus();
+      event.preventDefault();
+    }
+  }
+
+  _getFocusableChildren(elm: HTMLElement): HTMLElement[] {
+    const focusableElements = [
+        'a[href]',
+        'area[href]',
+        'input:not([disabled])',
+        'select:not([disabled])',
+        'textarea:not([disabled])',
+        'button:not([disabled])',
+        'iframe',
+        'object',
+        'embed',
+        '[contenteditable]',
+        '[tabindex]:not([tabindex^="-"])'];
+    const focusElements: HTMLElement[] = Array.from(elm.querySelectorAll(focusableElements.join()));
+    return focusElements.filter((child: HTMLElement) => !!(child.offsetWidth || child.offsetHeight || child.getClientRects().length));
+  }
+
+  keyupHandler(evt: KeyboardEvent) {
     // Check for ESCAPE key press
-    if (evt.keyCode === 27) {
-      this.open = !this.open;
-      console.log("Escape - Closing modal...");
+    if (evt.keyCode === 27 && this._isOpen) {
+      // Don't close modal if the active element is an input field.
+      if (document.activeElement.nodeName.toLowerCase() === 'input' &&
+          (<HTMLInputElement>document.activeElement).type !== 'checkbox' &&
+          (<HTMLInputElement>document.activeElement).type !== 'radio') {
+        return false;
+      }
+      this.closeModal();
+    }
+  }
+
+  keydownHandler(evt: KeyboardEvent) {
+    // Check for TAB key press
+    if (evt.keyCode === 9 && this._isOpen) {
+      console.log('Handle tabbing...');
+      // Make sure we don't tab outside for the Modal
+      this.trapTabKey(evt);
     }
   }
 }
