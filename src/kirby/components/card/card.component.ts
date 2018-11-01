781f2965c9a9e8a6af6ca16d6feb7155da1e5467
Tommy Rolchau Mathiesen <tommy.rolchau.mathiesen@jyskebank.dk>
2018-11-01 15:57:42 +0100

    :construction: Temp commit

--- src/kirby/components/card/card.component.ts
+++ src/kirby/components/card/card.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input, HostBinding } from '@angular/core';
+import { Component, OnInit, Input, HostBinding, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'kirby-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
+  // encapsulation: ViewEncapsulation.None
 })
 export class CardComponent implements OnInit {
   @Input() title: string;
