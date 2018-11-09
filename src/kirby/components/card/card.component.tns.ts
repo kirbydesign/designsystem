b8be56bbc95d6051151e06c33ffd9c3803d582b5
DK7N07 Søren Krogh Neigaard <dk7n07@jyskebank.dk>
2018-11-09 13:37:18 +0100

    Added shadow to card

--- src/kirby/components/card/card.component.tns.ts
+++ src/kirby/components/card/card.component.tns.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit, Input, HostBinding, ViewEncapsulation } from '@angular/core';
+import { Color } from 'tns-core-modules/color';
 import { screen } from 'platform';
 import { EventData } from 'tns-core-modules/data/observable/observable';
 import { FlexboxLayout } from 'tns-core-modules/ui/layouts/flexbox-layout/flexbox-layout';
 
 const screenScale = screen.mainScreen.scale;
+declare const CGSizeMake: any;
+declare const android: any;
 
 @Component({
   selector: 'kirby-card',
@@ -15,6 +18,8 @@ export class CardComponent implements OnInit {
   @Input() subtitle: string;
 
   cardSizeClass = '';
+  private shadowColor: Color = new Color('#1c1c1c');
+  private shadowOffset = 2.0;// TODO: Get value from common configuration
 
   constructor() { }
 
@@ -27,7 +32,6 @@ export class CardComponent implements OnInit {
     // A timeout is crap, but try without, fail you will
     setTimeout(() => {
       // TODO: Get breakpoints from somewhere
-      const heightDP = stack.getMeasuredHeight() / screenScale;
       const widthDP = stack.getMeasuredWidth() / screenScale;
       if (widthDP >= 400) {
         this.cardSizeClass = 'card-large';
@@ -37,6 +41,33 @@ export class CardComponent implements OnInit {
         this.cardSizeClass = 'card-small';
       }
     }, 100);
+    this.addShadow(args);
+  }
+
+  addShadow(args: EventData) {
+    const tnsView = <any>args.object;
+    if (tnsView.android) {
+      tnsView.eachChildView((child) => {
+        if (child instanceof FlexboxLayout) {
+          const bgColor = child.style.backgroundColor;
+          const androidView = child.android;
+          const shape = new android.graphics.drawable.GradientDrawable();
+          shape.setShape(android.graphics.drawable.GradientDrawable.RECTANGLE);
+          shape.setColor(android.graphics.Color.parseColor(bgColor + ''));
+          shape.setCornerRadius(16.0);// TODO: Get value from common configuration
+          androidView.setBackgroundDrawable(shape);
+          androidView.setElevation(16);// TODO: Get value from common configuration
+          return true;
+        }
+      });
+    } else if (tnsView.ios) {
+      const iosView = tnsView.ios;
+      iosView.layer.shadowColor = this.shadowColor.ios.CGColor;
+      iosView.layer.shadowOffset = CGSizeMake(0, this.shadowOffset);
+      iosView.layer.shadowOpacity = 0.5;// TODO: Get value from common configuration
+      iosView.layer.shadowRadius = 6.0;// TODO: Get value from common configuration
+      return;
+    }
   }
 
 }
