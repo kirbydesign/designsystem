cf9745fc1f349f80784add3f60242135466d724e
Jakob Engelbrecht Olesen <jakob@basher.dk>
2018-11-19 15:28:05 +0100

    Fix routerLink on button

--- src/kirby/kirby.module.ts
+++ src/kirby/kirby.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import {declarations} from './kirby.common';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   declarations: declarations,
   exports: declarations
 })
