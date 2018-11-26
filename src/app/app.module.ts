0513a8d1f8a8332ee9b6d697557ff1c9f242d1a7
Jack Ord <jrn@bankdata.dk>
2018-11-26 10:46:14 +0100

    Cleaning whitespace and commas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -21,7 +22,7 @@ import { KirbyModule } from '../kirby/kirby.module';
         BrowserModule,
         AppRoutingModule,
         HttpClientModule,
-        KirbyModule
+        KirbyModule,
     ],
 
     providers: [],
--- src/app/examples/examples.common.ts
+++ src/app/examples/examples.common.ts
@@ -1,5 +1,5 @@
-import {ButtonExampleComponent} from './button-example/button-example.component';
-import {CardExampleComponent} from './card-example/card-example.component';
+import { ButtonExampleComponent } from './button-example/button-example.component';
+import { CardExampleComponent } from './card-example/card-example.component';
 import { ListExampleComponent } from './list-example/list-example.component';
 import { ChartExampleComponent } from './chart-example/chart-example.component';
 
