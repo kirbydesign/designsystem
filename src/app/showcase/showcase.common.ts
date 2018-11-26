6d0d7487031d26454ec0ad9c2c3f992f5c2df581
Jack Ord <jrn@bankdata.dk>
2018-11-26 10:49:19 +0100

    More cleaning...

--- src/app/showcase/showcase.common.ts
+++ src/app/showcase/showcase.common.ts
@@ -1,9 +1,9 @@
-import {ExamplesModule} from '../examples/examples.module';
-import {ShowcaseRoutingModule} from './showcase-routing.module';
-import {ShowcaseComponent} from './showcase.component';
-import {ButtonShowcaseComponent} from './button-showcase/button-showcase.component';
-import {CardShowcaseComponent} from './card-showcase/card-showcase.component';
-import {ListShowcaseComponent} from './list-showcase/list-showcase.component';
+import { ExamplesModule } from '../examples/examples.module';
+import { ShowcaseRoutingModule } from './showcase-routing.module';
+import { ShowcaseComponent } from './showcase.component';
+import { ButtonShowcaseComponent } from './button-showcase/button-showcase.component';
+import { CardShowcaseComponent } from './card-showcase/card-showcase.component';
+import { ListShowcaseComponent } from './list-showcase/list-showcase.component';
 import { ChartShowcaseComponent } from './chart-showcase/chart-showcase.component';
 
 export const COMPONENT_IMPORTS: any[] = [
