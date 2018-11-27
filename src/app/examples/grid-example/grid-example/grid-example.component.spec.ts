e3e57645223439915f8aca4f8d59312bddcfd0f0
DK7N07 Søren Krogh Neigaard <dk7n07@jyskebank.dk>
2018-11-27 22:02:31 +0100

    Gaaah

--- src/app/examples/grid-example/grid-example/grid-example.component.spec.ts
+++ src/app/examples/grid-example/grid-example/grid-example.component.spec.ts
@@ -2,7 +2,6 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GridExampleComponent } from './grid-example.component';
 import { KirbyModule } from '../../../../kirby/kirby.module';
-import { GridCardConfiguration } from '../../../../kirby/components/grid/grid-card-configuration';
 
 describe('GridExampleComponent', () => {
   let component: GridExampleComponent;
@@ -11,7 +10,7 @@ describe('GridExampleComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ GridExampleComponent ],
-      imports: [KirbyModule, GridCardConfiguration]
+      imports: [KirbyModule]
     })
     .compileComponents();
   }));
