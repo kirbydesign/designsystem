d51fcd8ca79c5d7ae0a89c25eb77f2f7254639da
Tommy R. Mathiesen <tommy.rolchau.mathiesen@jyskebank.dk>
2018-12-03 12:09:37 +0100

    Added grid-showcase component

---
+++ src/app/showcase/grid-showcase/grid-showcase.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'kirby-grid-showcase',
+  templateUrl: './grid-showcase.component.html',
+  styleUrls: ['./grid-showcase.component.scss']
+})
+export class GridShowcaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
