0fa21988539a5f6d0a4067d5b5d743bdac872ef2
Peter Ring <peter.ring21@gmail.com>
2018-12-18 08:43:53 +0100

    changed data for items to be more suitable

--- src/app/examples/list-example/list-example.component.ts
+++ src/app/examples/list-example/list-example.component.ts
@@ -7,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListExampleComponent implements OnInit {
 
+  // myItems = [
+  //   {title: 'Netflix', amount: -79.95, detail: 'Peter'},
+  //   {title: 'McDonnalds Århus', amount: -20, detail: 'Jack'},
+  //   {title: 'Super Brugsen', amount: -180.65, detail: 'Morten'}
+  // ];
+
   myItems = [
-    {title: 'Rema1000', amount: 1002.34, detail: 'Peter'},
-    {title: 'Føtex', amount: 20, detail: 'Jack'},
-    {title: 'Brugsen', amount: 78455468.76, detail: 'Morten'}
+    {title: 'Vestas Wind Systems', subTitle: '20 stk', amount: '18.309 DKK', detail: 225},
+    {title: 'A.P. Møller-Mærsk', subTitle: '2 stk', amount: '14.019 DKK', detail: -354},
+    {title: 'Novo Nordisk A/S B', subTitle: '18 stk', amount: '7560 DKK', detail: 171}
   ];
 
   constructor() { }
