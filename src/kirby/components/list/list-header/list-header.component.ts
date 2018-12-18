ae99af3f138fdb15ee44aed62f2eec5cd48248d0
Peter Ring <peter.ring21@gmail.com>
2018-12-18 09:36:40 +0100

    created list header component

---
+++ src/kirby/components/list/list-header/list-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'kirby-list-header',
+  templateUrl: './list-header.component.html',
+  styleUrls: ['./list-header.component.scss']
+})
+export class ListHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
