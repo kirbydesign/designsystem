6d8a18a772f8d8931236f14a522356fa657f9377
Peter Gull <petergull@Peters-MacBook-Pro.local>
2019-02-06 12:42:05 +0100

    changed example a bit

--- src/app/examples/list-example/list-example.component.ts
+++ src/app/examples/list-example/list-example.component.ts
@@ -9,7 +9,7 @@ export class ListExampleComponent implements OnInit {
 
   myItems = [
     {title: 'Vestas Wind Systems', subTitle: '20 stk', amount: '18.309 DKK', detail: 225},
-    {title: 'A.P. Møller-Mærsk', subTitle: '2 stk', amount: '14.019 DKK', detail: -354},
+    {title: 'A.P. Møller-Mærsk', subTitle: '2 stk', amount: '14.019 DKK'},
     {title: 'Novo Nordisk A/S B', subTitle: '18 stk', amount: '7560 DKK', detail: 171},
     {title: 'Danske Bank A/S', subTitle: '10 stk', amount: '1560 DKK', detail: -171},
   ];
