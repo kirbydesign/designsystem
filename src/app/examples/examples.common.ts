3dbada085ec1e45167f00f1246faf05fc1632382
Peter Gull <petergull@Peters-MacBook-Pro.local>
2019-02-20 08:39:12 +0100

    component declarations added

--- src/app/examples/examples.common.ts
+++ src/app/examples/examples.common.ts
@@ -5,6 +5,9 @@ import { GridExampleComponent } from './grid-example/grid-example.component';
 import { ListExampleComponent } from './list-example/list-example.component';
 import { ChartExampleComponent } from './chart-example/chart-example.component';
 import { FontsExampleComponent } from './fonts-example/fonts-example.component';
+import { ListSectionExampleComponent } from './list-example/tns-list-examples/list-section-example/list-section-example.component';
+// tslint:disable-next-line:max-line-length
+import { ListCustomCellLinesExampleComponent } from './list-example/tns-list-examples/list-custom-cell-lines-example/list-custom-cell-lines-example.component';
 
 export const COMPONENT_DECLARATIONS: any[] = [
   ButtonExampleComponent,
@@ -13,5 +16,7 @@ export const COMPONENT_DECLARATIONS: any[] = [
   GridExampleComponent,
   AvatarExampleComponent,
   ChartExampleComponent,
-  FontsExampleComponent
+  FontsExampleComponent,
+  ListSectionExampleComponent,
+  ListCustomCellLinesExampleComponent
 ];
