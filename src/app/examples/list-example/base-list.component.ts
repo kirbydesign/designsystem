f511a34293bde35403fc391651b9578ec27d1275
Carsten Kring <jb6274@jyskebank.dk>
2019-02-22 15:14:27 +0100

    Load on demand with configuration inside list.component.ts. Now moving it to example code.

--- src/app/examples/list-example/base-list.component.ts
+++ src/app/examples/list-example/base-list.component.ts
@@ -1,12 +1,20 @@
-export class BaseListComponent {
-    myItems = [
-        {title: 'Vestas Wind Systems', subTitle: '20 stk', amount: '18.309 DKK', detail: 225},
-        {title: 'A.P. Møller-Mærsk', subTitle: '2 stk', amount: '14.019 DKK', detail: 0},
-        {title: 'Novo Nordisk A/S B', subTitle: '18 stk', amount: '7560 DKK', detail: 171},
-        {title: 'Danske Bank A/S', subTitle: '10 stk', amount: '1560 DKK', detail: -171},
-    ];
+import { ObservableArray } from 'tns-core-modules/data/observable-array';
 
-    onItemSelect(item: any) {
-        alert(`You have clicked the row [${item.title} ${item.amount}]`);
-    }
+export class BaseListComponent {
+
+  private myItems: ObservableArray<any>;
+
+  constructor() {
+
+    this.myItems = new ObservableArray();
+
+    this.myItems.push({title: 'Vestas Wind Systems', subTitle: '20 stk', amount: '18.309 DKK', detail: 225});
+    this.myItems.push({title: 'A.P. Møller-Mærsk', subTitle: '2 stk', amount: '14.019 DKK', detail: 0});
+    this.myItems.push({title: 'Novo Nordisk A/S B', subTitle: '18 stk', amount: '7560 DKK', detail: 171});
+    this.myItems.push({title: 'Danske Bank A/S', subTitle: '10 stk', amount: '1560 DKK', detail: -171});
+  }
+
+  onItemSelect(item: any) {
+    alert(`You have clicked the row [${item.title} ${item.amount}]`);
+  }
 }
--- src/kirby/components/list/list.component.ts
+++ src/kirby/components/list/list.component.ts
@@ -10,6 +10,7 @@ import {
   QueryList,
   TemplateRef
 } from '@angular/core';
+import { LoadOnDemandListViewEventData, RadListView } from 'nativescript-ui-listview/ui-listview.common';
 
 @Directive({
   selector: '[kirbyListItem]'
@@ -38,6 +39,7 @@ export class ListSectionHeaderDirective {}
 })
 export class ListComponent implements OnInit {
 
+  @Input() myLoadOnDemandMode: string;
   @Input() items: any[];
   @Input() getSectionName?: (item: any) => string;
   @Output() itemSelect = new EventEmitter<any>();
@@ -61,6 +63,7 @@ export class ListComponent implements OnInit {
       console.warn('kirbyListItem is deprecated and will be removed in future versions of Kirby');
     }
     this.isSelectable = this.itemSelect.observers.length > 0;
+    this.myLoadOnDemandMode = 'Auto';
   }
 
   onItemClick(row: any): void {
@@ -77,5 +80,28 @@ export class ListComponent implements OnInit {
 
   rowNumberForListView(headerTemplate: any): string {
     return headerTemplate ? '1' : '0';
+  }
+
+  myLoadOnDemandModeMethod(): string {
+    return this.myLoadOnDemandMode;
+  }
+
+  public addMoreItemsFromSource(chunkSize: number) {
+    const newItems = [...this.items].splice(0, chunkSize);
+    this.items.push(newItems);
+  }
+
+  onLoadMoreItemsRequested(args: LoadOnDemandListViewEventData) {
+    const that = new WeakRef(this);
+    const listView: RadListView = args.object;
+    if (this.items.length > 0) {
+      setTimeout(function () {
+        that.get().addMoreItemsFromSource(4);
+        listView.notifyLoadOnDemandFinished();
+      }, 1500);
+    } else {
+      args.returnValue = false;
+      listView.notifyLoadOnDemandFinished(true);
+    }
   }
 }
