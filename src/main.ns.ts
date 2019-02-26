41b9a470e5422951380e266b2b2ec3d22c6ea546
Jakob Engelbrecht Olesen <jakob@basher.dk>
2019-02-26 22:04:58 +0100

    :fire: Removing hot changes (handled internally by {N} 5.2)

--- src/main.ns.ts
+++ src/main.ns.ts
@@ -1,25 +1,8 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import { platformNativeScriptDynamic } from 'nativescript-angular/platform';
-import { AppOptions } from 'nativescript-angular/platform-common';
 
 import { AppModule } from './app/app.module';
 import './dev/livesync-navigation';
- const options: AppOptions = {};
-if (module['hot']) {
-    const hmrUpdate = require('nativescript-dev-webpack/hmr').hmrUpdate;
-     options.hmrOptions = {
-        moduleTypeFactory: () => AppModule,
-        livesyncCallback: (platformReboot) => {
-            console.log('HMR: Sync...');
-            hmrUpdate();
-            setTimeout(platformReboot, 0);
-        },
-    };
-    hmrUpdate();
-     // Path to your app module.
-    // You might have to change it if your module is in a different place.
-    module['hot'].accept(['./app/app.module']);
-}
 
 // A traditional NativeScript application starts by initializing global objects,
 // setting up global CSS rules, creating, and navigating to the main page.
@@ -27,4 +10,4 @@ if (module['hot']) {
 // A NativeScript Angular app needs to make both paradigms work together,
 // so we provide a wrapper platform object, platformNativeScriptDynamic,
 // that sets up a NativeScript application and can bootstrap the Angular framework.
-platformNativeScriptDynamic(options).bootstrapModule(AppModule);
+platformNativeScriptDynamic().bootstrapModule(AppModule);
