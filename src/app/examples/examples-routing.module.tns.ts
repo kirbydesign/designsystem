f9560e785f21eca9b58b44e0441cd91feac67471
SilverGull <peter.gull@gmail.com>
2019-02-26 14:08:56 +0100

    empty web views removed and native only components prefixed with .tns.*

--- src/app/examples/examples-routing.module.tns.ts
+++ src/app/examples/examples-routing.module.tns.ts
@@ -7,6 +7,12 @@ import { ExamplesModule } from './examples.module';
 import { NativeScriptDoughnutChartExampleComponent } from './nativescript-only/doughnut-chart-example/doughnut-chart-example.component.tns-only';
 // tslint:disable-next-line:max-line-length
 import { NativeScriptLineChartExampleComponent } from './nativescript-only/line-chart-example/line-chart-example.component.tns-only';
+// tslint:disable-next-line
+import { ListSectionExampleComponent } from './list-example/tns-list-examples/list-section-example/list-section-example.component.tns';
+// tslint:disable-next-line
+import { ListCustomCellLinesExampleComponent } from './list-example/tns-list-examples/list-custom-cell-lines-example/list-custom-cell-lines-example.component.tns';
+// tslint:disable-next-line
+import { ListItemExampleComponent } from './list-example/tns-list-examples/list-item-example/list-item-example.component.tns';
 
 const nativeScriptRoutes = [...routes,
   {
@@ -21,7 +27,24 @@ const nativeScriptRoutes = [...routes,
         component: NativeScriptLineChartExampleComponent
       }
     ]
-  }
+  },
+  {
+    path: 'list',
+    children: [
+      {
+        path: 'section',
+        component: ListSectionExampleComponent
+      },
+      {
+        path: 'custom',
+        component: ListCustomCellLinesExampleComponent
+      },
+      {
+        path: 'original',
+        component: ListItemExampleComponent
+      }
+    ]
+  },
 ];
 
 @NgModule({
