13b978d2e2005609e55f92589aef35bfbb8613d2
Peter Ring <peter.ring21@gmail.com>
2019-03-04 09:08:24 +0100

    added example html to showcase

--- src/app/showcase/progress-bar-showcase/progress-bar-showcase.component.ts
+++ src/app/showcase/progress-bar-showcase/progress-bar-showcase.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var require: any;
 
 @Component({
   selector: 'kirby-progress-bar-showcase',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./progress-bar-showcase.component.scss']
 })
 export class ProgressBarShowcaseComponent implements OnInit {
-
+  exampleHtml: string = require('../../examples/progress-bar-example/progress-bar-example.component.html');
   constructor() { }
 
   ngOnInit() {
