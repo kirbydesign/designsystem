38f4e7d75161f3b80a6a3014563573efd71b460c
Lars Larsen <lal@jyskebank.dk>
2019-03-11 15:06:30 +0100

    feat(chart): added gauge chart

--- src/kirby/components/chart/chart.component.ts
+++ src/kirby/components/chart/chart.component.ts
@@ -4,6 +4,7 @@ import { Options } from 'highcharts';
 import { ChartHelper } from './chart-helper';
 import { DonutOptions, DONUT_OPTIONS } from './options/donut';
 import { AreaSplineOptions, AREASPLINE_OPTIONS } from './options/areaspline';
+import { ACTIVITYGAUGE_OPTIONS, ActivityGaugeOptions } from './options/activitygauge';
 import { ChartType } from './chart-type';
 
 @Component({
@@ -14,6 +15,7 @@ import { ChartType } from './chart-type';
     ChartHelper,
     { provide: DONUT_OPTIONS, useValue: DonutOptions },
     { provide: AREASPLINE_OPTIONS, useValue: AreaSplineOptions },
+    { provide: ACTIVITYGAUGE_OPTIONS, useValue: ActivityGaugeOptions },
   ],
 })
 export class ChartComponent implements OnInit, OnChanges {
@@ -23,13 +25,13 @@ export class ChartComponent implements OnInit, OnChanges {
   @Input() description = '';
   @Input() showDataLabels = true;
   @ViewChild('chartContainer') chartContainer: ElementRef;
-
   options: Options = {};
 
   constructor(
     private chartHelper: ChartHelper,
     @Inject(DONUT_OPTIONS) public donutOptions: Options,
-    @Inject(AREASPLINE_OPTIONS) public areasplineOptions: Options
+    @Inject(AREASPLINE_OPTIONS) public areasplineOptions: Options,
+    @Inject(ACTIVITYGAUGE_OPTIONS) public activitygaugeOptions: Options
   ) {}
 
   ngOnInit() {
@@ -62,6 +64,11 @@ export class ChartComponent implements OnInit, OnChanges {
         this.options.chart.type = this.type;
         break;
       }
+      case ChartType.ACTIVITYGAUGE: {
+        this.options = this.activitygaugeOptions;
+        this.options.chart.type = this.type;
+        break;
+      }
     }
   }
 
@@ -89,6 +96,33 @@ export class ChartComponent implements OnInit, OnChanges {
               data: this.data as Array<Highcharts.SeriesAreasplineDataOptions>,
             },
           ];
+          break;
+        }
+        case ChartType.ACTIVITYGAUGE: {
+          const data = this.data[0];
+
+          this.options.title.text = data.title;
+          this.options.subtitle.text = data.subtitle;
+
+          if (data.paneBackgroundColor) {
+            this.options.pane.background = [
+              {
+                ...this.options.pane.background[0],
+                backgroundColor: data.paneBackgroundColor,
+              },
+            ];
+          }
+          if (data.color) {
+            this.options.title.style.color = data.color;
+            this.options.subtitle.style.color = data.color;
+          }
+          this.options.series = [
+            {
+              type: 'solidgauge',
+              data: data.series as Array<Highcharts.SeriesGaugeDataOptions>,
+            },
+          ];
+
           break;
         }
       }
