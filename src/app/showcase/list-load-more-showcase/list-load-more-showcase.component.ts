9cdbc3db6d833247f195fca677bef93add14fe9b
jonas jensen <jonasjensen@jonass-MacBook-Air.local>
2019-03-18 08:29:14 +0100

    enhanced the load more showcase.

--- src/app/showcase/list-load-more-showcase/list-load-more-showcase.component.ts
+++ src/app/showcase/list-load-more-showcase/list-load-more-showcase.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+declare var require: any;
 
 @Component({
   selector: 'kirby-list-load-more-showcase',
   templateUrl: './list-load-more-showcase.component.html',
   styleUrls: ['./list-load-more-showcase.component.scss'],
 })
-export class ListLoadMoreShowcaseComponent {}
+export class ListLoadMoreShowcaseComponent {
+  exampleHtml: string = require('../../examples/list-load-more-example/list-load-more-example.component.html');
+}
