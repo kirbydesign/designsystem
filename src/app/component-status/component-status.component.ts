b8f31b4270d92a420f9f01d3f5518c750ce903f3
Jakob Engelbrecht Olesen <jakob@basher.dk>
2019-03-18 23:08:12 +0100

    :sparkles: Add search bar to Component Status

--- src/app/component-status/component-status.component.ts
+++ src/app/component-status/component-status.component.ts
@@ -1,16 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { componentStatusItems, ComponentStatusItem } from './component-status-items';
-
 @Component({
   selector: 'kirby-component-status',
   templateUrl: './component-status.component.html',
   styleUrls: ['./component-status.component.scss'],
 })
 export class ComponentStatusComponent implements OnInit {
-  items: ComponentStatusItem[] = componentStatusItems;
+  items$: Observable<ComponentStatusItem[]>;
+  searchTerm$ = new BehaviorSubject<string>('');
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.items$ = this.searchTerm$.pipe(
+      map((searchTerm) => this.filterItems(componentStatusItems, searchTerm))
+    );
+  }
+
+  private filterItems(items: ComponentStatusItem[], searchTerm: string): ComponentStatusItem[] {
+    const regex = new RegExp(searchTerm, 'i');
+    return items.filter((item) => this.matchItem(item, regex));
+  }
+
+  private matchItem(item: ComponentStatusItem, searchTerm: RegExp): boolean {
+    return (
+      searchTerm.test(item.component) ||
+      this.matchAliases(item.aliases, searchTerm) ||
+      this.matchChildComponents(item.children, searchTerm)
+    );
+  }
+
+  private matchAliases(aliases: string[], searchTerm: RegExp): boolean {
+    if (Array.isArray(aliases)) {
+      return aliases.some((alias) => searchTerm.test(alias));
+    }
+    return false;
+  }
+
+  private matchChildComponents(children: ComponentStatusItem[], searchTerm: RegExp): boolean {
+    if (Array.isArray(children)) {
+      return children.filter((item) => this.matchItem(item, searchTerm)).length > 0;
+    }
+    return false;
+  }
 }
