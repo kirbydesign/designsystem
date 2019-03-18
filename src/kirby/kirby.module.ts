7294253fbe4cc7c4da3749b1d7ad9c088fd85f1a
Jakob Engelbrecht <jakob@basher.dk>
2019-03-18 19:42:08 +0100

    :heavy_plus_sign: Add Ionic (closes #136) (#158)
    * :heavy_plus_sign: Add Ionic
    
    * :rotating_light: Fix lint
    
    * :package: :heavy_plus_sign: Add Ionic to Kirby package dependencies
    
    * :memo: Add installation section regarding Ionic

--- src/kirby/kirby.module.ts
+++ src/kirby/kirby.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
 
 import { declarations } from './kirby.common';
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, IonicModule.forRoot()],
   declarations: declarations,
   exports: declarations,
 })
