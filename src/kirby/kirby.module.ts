ca05c4de8e5873f715f4b96831f8f8ba7cdcd1b3
Jakob Engelbrecht Olesen <jakob@basher.dk>
2019-03-18 09:17:28 +0100

    :heavy_plus_sign: Add Ionic

--- src/kirby/kirby.module.ts
+++ src/kirby/kirby.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
 
 import { declarations } from './kirby.common';
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, IonicModule.forRoot()],
   declarations: declarations,
   exports: declarations,
 })
-export class KirbyModule {}
+export class KirbyModule { }
