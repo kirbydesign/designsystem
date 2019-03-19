37a8dbdbf0259e973942083bdefda6173bbe405a
jonas jensen <jonasjensen@jonass-MacBook-Air.local>
2019-03-19 14:38:44 +0100

    small fix

--- src/kirby/components/list/helpers/list-helper.ts
+++ src/kirby/components/list/helpers/list-helper.ts
@@ -3,7 +3,7 @@ import { LoadOnDemandEventData } from '../list.event';
 import { ListComponent } from './../list.component';
 
 export class ListHelper {
-  onLoadOnDemand(component: ListComponent, args: LoadOnDemandEventData): void {
+  onLoadOnDemand(component: ListComponent, _args: LoadOnDemandEventData): void {
     if (component.isLoadOnDemandEnabled && !component.isLoading) {
       component.isLoading = true;
       component.loadOnDemand.emit({
