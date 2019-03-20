dfc575318fd872248aca0d4b2b319d8a1682ba28
Anders Skarby <anders@skarby.info>
2019-03-20 15:33:33 +0100

    Github issue #160, now able to render kirby-list with rounded corners (defaults to "no rounded corners", which makes it backwards compatible).

--- src/kirby/components/list/list.component.spec.ts
+++ src/kirby/components/list/list.component.spec.ts
@@ -1,21 +1,51 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 import { GroupByPipe } from './pipes/group-by.pipe';
-import { ListComponent } from './list.component';
+import { ListComponent, ListShape } from './list.component';
+import { ListSectionHeaderComponent } from './list-section-header/list-section-header.component';
+import { ListCellComponent } from './list-cell/list-cell.component';
+
+@Component({
+  template: `
+    <kirby-list [getSectionName]="sectionNameResolver" [items]="items" [shape]="shape">
+      <kirby-list-section-header
+        *kirbyListSectionHeader="let section"
+        [title]="section"
+      ></kirby-list-section-header>
+      <kirby-list-cell *kirbyListCell="let item">{{ item }}</kirby-list-cell>
+    </kirby-list>
+  `,
+})
+class ListHostComponent {
+  sectionNameResolver: (item: any) => string;
+  items: any[];
+  shape: ListShape;
+}
 
 describe('ListComponent', () => {
+  let testHost: ListHostComponent;
   let component: ListComponent;
-  let fixture: ComponentFixture<ListComponent>;
+  let fixture: ComponentFixture<ListHostComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ListComponent, GroupByPipe],
+      declarations: [
+        ListHostComponent,
+        ListComponent,
+        ListSectionHeaderComponent,
+        ListCellComponent,
+        GroupByPipe,
+      ],
     }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ListComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(ListHostComponent);
+    testHost = fixture.componentInstance;
+
+    component = fixture.debugElement.query(By.directive(ListComponent)).componentInstance;
     fixture.detectChanges();
   });
 
@@ -47,11 +77,6 @@ describe('ListComponent', () => {
   });
 
   describe('sections', () => {
-    beforeEach(() => {
-      fixture = TestBed.createComponent(ListComponent);
-      component = fixture.componentInstance;
-    });
-
     it('should be disabled if no section callback is defined', () => {
       fixture.detectChanges();
 
@@ -59,7 +84,7 @@ describe('ListComponent', () => {
     });
 
     it('should be enabled if a section callback is defined', () => {
-      component.getSectionName = (item: any) => 'this is a test';
+      testHost.sectionNameResolver = () => 'this is a test';
 
       fixture.detectChanges();
 
@@ -67,7 +92,7 @@ describe('ListComponent', () => {
     });
 
     it('should render one li element for each item, if the list is not sectioned', () => {
-      component.items = [1, 2, 3];
+      testHost.items = [1, 2, 3];
 
       fixture.detectChanges();
 
@@ -79,15 +104,111 @@ describe('ListComponent', () => {
 
     it('should render one li element for each item and one for each section, if sections are enabled', () => {
       const sections = ['section 1', 'section 2', 'section 3'];
-      component.items = [0, 1, 2];
-      component.getSectionName = (item: any): string => sections[item];
+      testHost.items = [0, 1, 2];
+      testHost.sectionNameResolver = (item: any): string => sections[item];
 
       fixture.detectChanges();
 
       const rootElement: HTMLElement = fixture.debugElement.nativeElement;
-      const liElements = rootElement.querySelectorAll('li');
+      const sectionElements = rootElement.querySelectorAll('li.section-header');
+      const rowElements = rootElement.querySelectorAll('li.row');
 
-      expect(liElements.length).toEqual(component.items.length + sections.length);
+      expect(sectionElements.length).toEqual(sections.length);
+      expect(rowElements.length).toEqual(component.items.length);
+    });
+  });
+
+  describe('boundary styling', () => {
+    describe('in non-sectioned list', () => {
+      beforeEach(() => {
+        testHost.items = [1, 2, 3];
+        fixture.detectChanges();
+      });
+
+      it('should apply "top"-class to first row', () => {
+        const rootElement: HTMLElement = fixture.debugElement.nativeElement;
+        const topLiElement = rootElement.querySelector('li.row:first-of-type');
+        expect(topLiElement.className).toContain('top');
+      });
+
+      it('should not apply "top"- nor "bottom"-class to rows in the middle', () => {
+        const rootElement: HTMLElement = fixture.debugElement.nativeElement;
+        const middleElement = rootElement.querySelector('li.row:nth-child(2)');
+        expect(middleElement.className).not.toContain('top');
+        expect(middleElement.className).not.toContain('bottom');
+      });
+
+      it('should apply "bottom"-class to last row ', () => {
+        const rootElement: HTMLElement = fixture.debugElement.nativeElement;
+        const bottomLiElement = rootElement.querySelector('li.row:last-of-type');
+        expect(bottomLiElement.className).toContain('bottom');
+      });
+
+      it('should be able to apply both "top"- and "bottom" classes when there is only a single row', () => {
+        testHost.items = [1];
+        fixture.detectChanges();
+
+        const rootElement: HTMLElement = fixture.debugElement.nativeElement;
+        const liElement = rootElement.querySelector('li');
+        expect(liElement.className).toContain('top');
+        expect(liElement.className).toContain('bottom');
+      });
+    });
+
+    describe('in sectioned list', () => {
+      beforeEach(() => {
+        testHost.sectionNameResolver = (item: number) => (item % 2 === 0 ? 'even' : 'odd');
+        testHost.items = [1, 2, 3, 4];
+        fixture.detectChanges();
+      });
+
+      it('should apply "top"-class to first row of sections', () => {
+        const rootElement: HTMLElement = fixture.debugElement.nativeElement;
+        const liElements = rootElement.querySelectorAll('li.row');
+        expect(liElements[0].className).toContain('top');
+        expect(liElements[1].className).not.toContain('top');
+        expect(liElements[2].className).toContain('top');
+        expect(liElements[3].className).not.toContain('top');
+      });
+
+      it('should apply "bottom"-class to last row of sections', () => {
+        const rootElement: HTMLElement = fixture.debugElement.nativeElement;
+        const liElements = rootElement.querySelectorAll('li.row');
+        expect(liElements[0].className).not.toContain('bottom');
+        expect(liElements[1].className).toContain('bottom');
+        expect(liElements[2].className).not.toContain('bottom');
+        expect(liElements[3].className).toContain('bottom');
+      });
+
+      it('should be able to apply both "top"- and "bottom" classes when there is only a single row in the section', () => {
+        testHost.items = [1, 2];
+        fixture.detectChanges();
+
+        const rootElement: HTMLElement = fixture.debugElement.nativeElement;
+        const liElements = rootElement.querySelectorAll('li.row');
+        expect(liElements[0].className).toContain('top');
+        expect(liElements[0].className).toContain('bottom');
+        expect(liElements[1].className).toContain('top');
+        expect(liElements[1].className).toContain('bottom');
+      });
+    });
+  });
+
+  describe('shape styling', () => {
+    beforeEach(() => {
+      testHost.items = [1];
+    });
+
+    ['square', 'rounded'].forEach((shape: ListShape) => {
+      it(`should propogate '${shape}'-shape to "li.row"-elements`, () => {
+        testHost.shape = shape;
+        fixture.detectChanges();
+
+        const rootElement: HTMLElement = fixture.debugElement.nativeElement;
+        const liElement = rootElement.querySelector('li.row');
+
+        expect(liElement.className).toContain(shape);
+      });
     });
   });
 });
--- src/kirby/components/list/list.component.ts
+++ src/kirby/components/list/list.component.ts
@@ -5,9 +5,11 @@ import {
   Directive,
   EventEmitter,
   Input,
+  OnChanges,
   OnInit,
   Output,
   QueryList,
+  SimpleChanges,
   TemplateRef,
 } from '@angular/core';
 
@@ -31,35 +33,63 @@ export class ListCellDirective {}
 })
 export class ListSectionHeaderDirective {}
 
+export type ListShape = 'square' | 'rounded';
+
 @Component({
   selector: 'kirby-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss'],
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnInit, OnChanges {
+  /**
+   * Provide items for the list to render. Items must be provided in the order you expect them to be rendered.
+   */
   @Input() items: any[];
+
+  /**
+   * Callback to determine name of section. Sections will be ordered alphabetically.
+   */
   @Input() getSectionName?: (item: any) => string;
+
+  /**
+   * Determine outline shape of:
+   * - list, if {@link #isSectionsEnabled} is `false`
+   * - section, if {@link #isSectionsEnabled} is `true`
+   *
+   * `square` means **without** rounded corners, `rounded` means **with** rounded corners.
+   */
+  @Input() shape: ListShape = 'square';
+
+  /**
+   * Emitting event when an item is selected (tab'ed on mobile, clicked on web)
+   */
   @Output() itemSelect = new EventEmitter<any>();
 
   // The first element that matches ListItemDirective. As a structural directive it unfolds into a template. This is a reference to that.
   @ContentChild(ListItemDirective, { read: TemplateRef }) listItemTemplate;
-  @ContentChild(ListHeaderDirective, { read: TemplateRef }) listHeaderTemplate;
+  @ContentChild(ListHeaderDirective, { read: TemplateRef }) headerTemplate;
   @ContentChild(ListSectionHeaderDirective, { read: TemplateRef }) sectionHeaderTemplate;
   @ContentChildren(ListCellDirective, { read: TemplateRef }) listCellTemplates: QueryList<any>;
 
   isSectionsEnabled: boolean;
   isSelectable: boolean;
 
+  private itemsSortedBySection: any[];
+  private itemClasses: string[];
+
   constructor() {}
 
   ngOnInit() {
-    if (this.getSectionName) {
-      this.isSectionsEnabled = true;
-    }
     if (this.listItemTemplate) {
       console.warn('kirbyListItem is deprecated and will be removed in future versions of Kirby');
     }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const { items } = changes;
+    this.isSectionsEnabled = !!this.getSectionName;
     this.isSelectable = this.itemSelect.observers.length > 0;
+    this.determineClasses((items && items.currentValue) || []);
   }
 
   onItemClick(row: any): void {
@@ -68,5 +98,66 @@ export class ListComponent implements OnInit {
 
   onItemTap(selectedItem: any): void {
     this.itemSelect.emit(selectedItem);
+  }
+
+  getClasses(current: any) {
+    const index = this.itemsSortedBySection.indexOf(current);
+    return this.itemClasses[index] || '';
+  }
+
+  private determineClasses(items: any[]) {
+    if (items) {
+      if (this.isSectionsEnabled) {
+        // If sections are enabled, sort items by section
+        const bySection = (a, b) => this.getSectionName(a).localeCompare(this.getSectionName(b));
+        this.itemsSortedBySection = [...items].sort(bySection);
+      } else {
+        // No sections? No problem... consider all items to be in one giant section
+        this.itemsSortedBySection = [...items];
+      }
+
+      // Calculate classes for each item
+      this.itemClasses = this.itemsSortedBySection.map((current) =>
+        this.getClassesForItem(current)
+      );
+    } else {
+      // No items, make sure not to have any classes
+      this.itemsSortedBySection = [];
+      this.itemClasses = [];
+    }
+  }
+
+  private getClassesForItem(current) {
+    // Determine index of (current) item and if it's first / list in list
+    const index = this.itemsSortedBySection.indexOf(current);
+    const isFirst = index === 0;
+    const isLast = index === this.items.length - 1;
+
+    // Determine if there's an item before and / or after the (current) item
+    const previous = isFirst ? null : this.itemsSortedBySection[index - 1];
+    const next = isLast ? null : this.itemsSortedBySection[index + 1];
+
+    // Determine if the (current) item is the first and / or last item of a section
+    let isTopRowInSection = false;
+    let isBottomRowInSection = false;
+    if (this.isSectionsEnabled) {
+      const currentSectionName = this.getSectionName(current);
+      const previousSectionName = previous ? this.getSectionName(previous) : null;
+      const nextSectionName = next ? this.getSectionName(next) : null;
+      isTopRowInSection = currentSectionName !== previousSectionName;
+      isBottomRowInSection = currentSectionName !== nextSectionName;
+    }
+
+    const classes = [this.shape as string];
+    if (this.isSelectable) {
+      classes.push('selectable');
+    }
+    if (isFirst || isTopRowInSection) {
+      classes.push('top');
+    }
+    if (isLast || isBottomRowInSection) {
+      classes.push('bottom');
+    }
+    return classes.join(' ');
   }
 }
