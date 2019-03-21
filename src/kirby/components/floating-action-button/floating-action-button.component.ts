abf4c472206aefea6287ececf112ed4594fcc0e1
Martin Metaksov <mmmtrifork@gmail.com>
2019-03-21 16:11:24 +0100

    implementation of the html fab

--- src/kirby/components/floating-action-button/floating-action-button.component.ts
+++ src/kirby/components/floating-action-button/floating-action-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'kirby-floating-action-button',
@@ -6,22 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./floating-action-button.component.scss'],
 })
 
-// TODO: add shadows
-// TODO: dynamically add image/icon
-// TODO: add same properties as ionic floating action button, as described in the docs
-// TODO: add unit tests for everything (example/component/showcase)
-// TODO: add documentation
-// TODO: make sure that proper formatting and TS Linting is in place 
-// TODO: create pull-request
-
 export class FloatingActionButtonComponent implements OnInit {
-  iconSrc: string = "~/assets/icons/add/add@3x.png";
+
+  @Input() href?: string;
+  @Input() isTranslucent?: boolean = false;
+  @Input() isDisabled?: boolean = false;
+  @Input() type?: 'button' | 'reset' | 'submit' = 'button';
+  @Input() iconSrc?: string = "/assets/icons/add/add@3x.png";
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
-  onTap(): void {
-    console.log('tapped on the floating action button');
-  }
 }
