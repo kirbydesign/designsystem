e5340c5c82c931e85c75cc56f60cc931a6094456
Jonas Jensen <46445487+jonasjensen-brolstark@users.noreply.github.com>
2019-03-22 09:30:52 +0100

    Kirby list load more function (#147)
    * Load on demand with configuration inside list.component.ts. Now moving it to example code.
    
    * Load on demand with bound callback method.
    
    * Now returning promise with boolean to tell if there is more to load.
    
    * Setting returnValue to that of the promise.
    
    * Now with load on demand template into RadListView.
    
    * Height on load more template.
    
    * Removing "load more" on template. See details on https://jira.bdpnet.dk/browse/RMA-270
    
    * Showing "nore more items" when there is nothing more to load.
    
    * Injecting string for "No more items"
    
    * Styling text for no more items.
    
    * Moved example to own component and cleaned Kirby list interface.
    
    * load more transactions in action bar
    
    * example work
    
    * splitted the list component into a tns version also.
    
    * made tests for list-load-more service.
    
    * oops forgot fdescribe.
    
    * Added infinite scroll directive.
    
    * injected windown into infinite scroll directive.
    
    * started tests.
    
    * fixed stuff
    
    * centered spinner and no more items text
    
    * indicating which file i am working in
    
    * Introduced the windowRef to abstract browser window away.
    
    * added tests for infinite scroll directive.
    
    * opps
    
    * Aligned examples on native and web for load more.
    
    * fixed list load more service.
    
    * made windowRef service only available on web.
    
    * unit test loadmoreservice
    
    * unit test loadmoreservice
    
    * centered no-more-items-text on native
    
    * fixed list component tests.
    
    * console log on onLoadMore error
    
    * showcase loadmore waits on first loadmore
    
    * showcase loadmore number of loaded items
    
    * The load more callback now return new items.
    
    * fixed unit tests and removed unused service.
    
    * small clean-up
    
    * fixed package-lock
    
    * More clean up.
    
    * Review comments.
    
    * fixed issues with headers natively.
    
    * renamed list loadmore to list load more.
    
    * fixed linting error.
    
    * Fixed windowRef inject issue in aot mode.
    
    * Rewrote loadMore callback to eventemitter
    
    * enhanced the load more showcase.
    
    * introduced isLoadMoreEnabled property in the list component.
    
    * Changed load more event. User now has to push items to list.
    
    * doc changes + rename of variable.
    
    * oops forgot the tests.
    
    * function signature clean-up
    
    * Moved examples and rename to load on demand
    
    * removed promised from examples.
    
    * renamed event and callback functions.
    
    * Created ListHelper.
    
    * small fix
    
    * fixed AOT build error.
    
    * minor fix.
    
    * Update src/app/examples/list/list-example.component.tns.html
    
    Co-Authored-By: jonasjensen-brolstark <46445487+jonasjensen-brolstark@users.noreply.github.com>
    
    * review comments.

--- src/app/showcase/showcase.routes.ts
+++ src/app/showcase/showcase.routes.ts
@@ -5,6 +5,7 @@ import { ButtonShowcaseComponent } from './button-showcase/button-showcase.compo
 import { CardShowcaseComponent } from './card-showcase/card-showcase.component';
 import { GridShowcaseComponent } from './grid-showcase/grid-showcase.component';
 import { ListShowcaseComponent } from './list-showcase/list-showcase.component';
+import { ListLoadOnDemandShowcaseComponent } from './list-load-on-demand-showcase/list-load-on-demand-showcase.component';
 import { ShowcaseComponent } from './showcase.component';
 import { ChartShowcaseComponent } from './chart-showcase/chart-showcase.component';
 import { DoughnutChartShowcaseComponent } from './doughnut-chart-showcase/doughnut-chart-showcase.component';
@@ -41,6 +42,10 @@ export const routes: Routes = [
       {
         path: 'list',
         component: ListShowcaseComponent,
+      },
+      {
+        path: 'list-load-on-demand',
+        component: ListLoadOnDemandShowcaseComponent,
       },
       {
         path: 'grid',
