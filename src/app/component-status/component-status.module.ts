b7d09e724597fe1408e00643c2cef3aaaea7ec92
Jakob Engelbrecht Olesen <jakob@basher.dk>
2019-03-24 12:40:04 +0100

    :lipstick: Add status icons

--- src/app/component-status/component-status.module.ts
+++ src/app/component-status/component-status.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
 
 import { ComponentStatusComponent } from './component-status.component';
 
@@ -13,6 +14,7 @@ import { ComponentStatusComponent } from './component-status.component';
         component: ComponentStatusComponent,
       },
     ]),
+    IonicModule.forRoot(),
   ],
   declarations: [ComponentStatusComponent],
   exports: [ComponentStatusComponent],
