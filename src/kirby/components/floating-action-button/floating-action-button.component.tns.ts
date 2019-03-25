f227d41571cfeefbd955e04240f1ca6c3dbaab78
Martin Metaksov <mmmtrifork@gmail.com>
2019-03-25 12:04:43 +0100

    fixed colors

--- src/kirby/components/floating-action-button/floating-action-button.component.tns.ts
+++ src/kirby/components/floating-action-button/floating-action-button.component.tns.ts
@@ -1,4 +1,4 @@
-import { Component, NgZone, Input, Output, EventEmitter } from '@angular/core';
+import { Component, NgZone, Input } from '@angular/core';
 import { OrientationChangedEventData } from 'tns-core-modules/application';
 import { ContentView } from 'tns-core-modules/ui/content-view';
 import { EventData, View } from 'tns-core-modules/ui/page/page';
@@ -10,15 +10,14 @@ const FLOATING_ACTION_BUTTON_COMPONENT_SELECTOR = 'kirby-floating-action-button'
 
 declare const CGSizeMake: any;
 declare const android: any;
+declare var require: any;
+const style: any = require('sass-extract-loader!./floating-action-button.component.scss');
 
 @Component({
   selector: FLOATING_ACTION_BUTTON_COMPONENT_SELECTOR,
   templateUrl: './floating-action-button.component.html',
   styleUrls: ['./floating-action-button.component.scss'],
 })
-
-// TODO: add unit tests for everything (example/component/showcase)
-// TODO: create pull-request
 export class FloatingActionButtonComponent extends ContentView {
   @Input() iconSrc?: string = '~/assets/icons/add/add@3x.png';
   @Input() hasShadow?: boolean = true;
@@ -52,20 +51,23 @@ export class FloatingActionButtonComponent extends ContentView {
       let nativeView = this.view.android;
       var shape = new android.graphics.drawable.GradientDrawable();
       shape.setShape(android.graphics.drawable.GradientDrawable.OVAL);
-      // Shadow color requested by UX/UI designer
-      // shape.setColor(android.graphics.Color.parseColor('#004d32'));
-      // shape.setAlpha(255);
-      // A brigther shadow color that prevents the darker borders around the shape on android
-      shape.setColor(android.graphics.Color.parseColor('#00ffa4'));
+      shape.setColor(android.graphics.Color.parseColor(this.getThemeColor('kirby-primary-shade')));
+      // shape.setColor(android.graphics.Color.parseColor('#00ffa4')); // A brigther shadow color that prevents the darker borders around the shape on android
       nativeView.setBackgroundDrawable(shape);
       nativeView.setElevation(15);
     } else if (this.view.ios) {
       let nativeView = this.view.ios;
-      nativeView.layer.shadowColor = new Color('#004d32').ios.CGColor;
+      nativeView.layer.shadowColor = new Color(
+        this.getThemeColor('kirby-primary-shade')
+      ).ios.CGColor;
       nativeView.layer.shadowOffset = CGSizeMake(0, 2.0);
       nativeView.layer.shadowOpacity = 0.3;
       nativeView.layer.shadowRadius = 5.0;
     }
+  }
+
+  getThemeColor(name: string) {
+    return style.global['$kirby-colors'].value[name].value.hex;
   }
 }
 
