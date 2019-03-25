e52dbf448fd24cbcdc21619159c045e74a0bc2fe
Jakob Engelbrecht Olesen <jakob@basher.dk>
2019-03-25 14:00:22 +0100

    Revert ":sparkles: :book: Get Component Status from GitHub"

--- src/app/component-status/component-status.component.ts
+++ src/app/component-status/component-status.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, BehaviorSubject, merge, Subscription, forkJoin } from 'rxjs';
-import { map, first } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { environment } from '~/environments/environment';
 
@@ -27,16 +26,24 @@ export class ComponentStatusComponent implements OnInit {
     environment.githubBaseUrl +
     '/issues/new?labels=component&template=component-request.md&title=%5BCOMPONENT%5D+';
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   ngOnInit() {
-    this.sortedItems = this.sortItems(componentStatusItems);
+    this.sortedItems = componentStatusItems.sort(
+      (a: ComponentStatusItem, b: ComponentStatusItem) => {
+        let order = this.sortByStatus(a, b);
+        if (order === 0) {
+          order = this.sortByPriority(a, b);
+          if (order === 0) {
+            order = this.sortByComponentName(a, b);
+          }
+        }
+        return order;
+      }
+    );
     this.items$ = this.searchTerm$.pipe(
       map((searchTerm) => this.filterItems(this.sortedItems, searchTerm))
     );
-    setTimeout(() => {
-      this.getCurrentGithubStatus(this.sortedItems);
-    }, 1000);
   }
 
   public isUnderConsiderationOrNotPlanned(item: ComponentStatusItem) {
@@ -50,19 +57,6 @@ export class ComponentStatusComponent implements OnInit {
 
   public getGithubIssueUrl(githubIssueNo: number) {
     return environment.githubBaseUrl + '/issues/' + githubIssueNo;
-  }
-
-  private sortItems(items: ComponentStatusItem[]) {
-    return items.sort((a: ComponentStatusItem, b: ComponentStatusItem) => {
-      let order = this.sortByStatus(a, b);
-      if (order === 0) {
-        order = this.sortByPriority(a, b);
-        if (order === 0) {
-          order = this.sortByComponentName(a, b);
-        }
-      }
-      return order;
-    });
   }
 
   private sortByStatus(a: ComponentStatusItem, b: ComponentStatusItem) {
@@ -116,89 +110,4 @@ export class ComponentStatusComponent implements OnInit {
     }
     return false;
   }
-
-  private getGithubIssues() {
-    const options = {
-      headers: new HttpHeaders({
-        Authorization: 'token ' + environment.oauth.githubToken1 + environment.oauth.githubToken2,
-      }),
-    };
-    const url = environment.githubApi + '/repos/kirbydesign/designsystem/issues?labels=component';
-    return this.http.get(url, options);
-  }
-
-  private getCurrentGithubStatus(items: ComponentStatusItem[]) {
-    const flattenedItems = this.flattenItems(items);
-    console.time('Get Github Status');
-    this.getGithubProjectStatus()
-      .pipe(first())
-      .subscribe((issues) => {
-        issues.forEach((issue) => {
-          // console.log('issue.number:', issue.number);
-          flattenedItems
-            .filter((item) => item.code.githubIssueNo === issue.number)
-            .forEach((item) => {
-              item.code.status = issue.status;
-              item.code.livestatus = true;
-            });
-        });
-        console.timeEnd('Get Github Status');
-        this.sortedItems = this.sortItems(componentStatusItems);
-        this.searchTerm$.next(this.searchTerm$.value);
-      });
-  }
-
-  private flattenItems(items: ComponentStatusItem[]) {
-    const concat = (x, y) => x.concat(y);
-    const flatMap = (arr, f) => arr.map(f).reduce(concat, []);
-    return flatMap(items, (item) => [item, ...(item.children ? item.children : [])]);
-  }
-
-  private getGithubProjectCards(columnId: number, status: ItemCodeStatus) {
-    const options = {
-      headers: new HttpHeaders({
-        Authorization: 'token ' + environment.oauth.githubToken1 + environment.oauth.githubToken2,
-        Accept: 'application/vnd.github.inertia-preview+json',
-      }),
-    };
-    const url = environment.githubApi + '/projects/columns/' + columnId + '/cards';
-    return this.http.get<GithubCard[]>(url, options).pipe(
-      map((cards) => {
-        return cards.map((card) => {
-          let githubIssueNo = 0;
-          const matches = card.content_url.match(/issues\/(\d+)$/);
-          if (matches.length === 2) {
-            githubIssueNo = +matches[1];
-          }
-          return <GithubIssue>{
-            number: githubIssueNo,
-            status,
-          };
-        });
-      })
-    );
-  }
-
-  private getGithubProjectStatus() {
-    const todoColumnId = 4590936;
-    const inProgressColumnId = 4590937;
-    const doneColumnId = 4590938;
-    return forkJoin(
-      this.getGithubProjectCards(todoColumnId, ItemCodeStatus.planned),
-      this.getGithubProjectCards(inProgressColumnId, ItemCodeStatus.inProgress),
-      this.getGithubProjectCards(doneColumnId, ItemCodeStatus.ready)
-    ).pipe(
-      map(([cards1, cards2, cards3]) => {
-        return [...cards1, ...cards2, ...cards3];
-      })
-    );
-  }
-}
-
-interface GithubCard {
-  content_url: string;
-}
-interface GithubIssue {
-  number: number;
-  status: ItemCodeStatus;
 }
