d5a09c5aeb169d41d0703b6e05db86d7af9a8127
Tommy R. Mathiesen <tommy.rolchau.mathiesen@jyskebank.dk>
2019-03-26 14:20:40 +0100

    Removed unused file - fixed lint

--- src/app/examples/badge-example/badge-example.component.ts
+++ src/app/examples/badge-example/badge-example.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'kirby-badge-example',
   templateUrl: './badge-example.component.html',
-  styleUrls: ['./badge-example.component.scss']
+  styleUrls: ['./badge-example.component.scss'],
 })
 export class BadgeExampleComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
