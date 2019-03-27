b96444b9bb6534190a4a35c43f2f29331bf471b5
Smajo Zukanovic <szu@trifork.com>
2019-03-27 13:37:14 +0100

    ionic font added, nativescript icon component added

--- src/app/showcase/icon-showcase/icon-showcase.component.ts
+++ src/app/showcase/icon-showcase/icon-showcase.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+// import { ShowcaseProperty } from '~/app/shared/showcase-properties/showcase-property';
+
 declare var require: any;
 
 @Component({
@@ -8,4 +10,27 @@ declare var require: any;
 })
 export class IconShowcaseComponent {
   exampleHtml: string = require('../../examples/icon-example/icon-example.component.html');
+
+  /*
+  properties: ShowcaseProperty[] = [
+    {
+      name: 'hasShadow',
+      description: 'Determines whether the button will have a shadow or not.',
+      defaultValue: 'true',
+      inputValues: ['boolean'],
+    },
+    {
+      name: 'disabled',
+      description: 'Determines whether the button will be disabled or not.',
+      defaultValue: 'false',
+      inputValues: ['boolean'],
+    },
+    {
+      name: 'iconSrc',
+      description: 'Points to the source of the icon, which will be used.',
+      defaultValue: `'/assets/icons/add/add@3x.png'`,
+      inputValues: ['string'],
+    },
+  ];
+  */
 }
--- src/kirby/components/icon/icon.component.ts
+++ src/kirby/components/icon/icon.component.ts
@@ -3,9 +3,57 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'kirby-icon',
   templateUrl: './icon.component.html',
+  styleUrls: ['./icon.component.scss'],
 })
 export class IconComponent {
-  @Input() name? = 'happy';
-  @Input() size? = 'small';
-  @Input() src?;
+  @Input() src;
+  @Input() size = 'small';
+  @Input() name:
+    | 'add'
+    | 'close'
+    | 'cog'
+    | 'swap'
+    | 'move'
+    | 'log-out'
+    | 'more'
+    | 'arrow-back'
+    | 'help'
+    | 'attach'
+    | 'search'
+    | 'checkbox-outline'
+    | 'checkbox'
+    | 'menu'
+    | 'person' = 'cog';
+
+  icons = [
+    { code: 0xf102, name: 'add' },
+    { code: 0xf2c0, name: 'close' },
+    { code: 0xf2cf, name: 'cog' },
+    { code: 0xf389, name: 'swap' },
+    { code: 0xf331, name: 'move' },
+    { code: 0xf359, name: 'log-out' },
+    { code: 0xf1c9, name: 'more' },
+    { code: 0xf27d, name: 'arrow-back' },
+    { code: 0xf30b, name: 'help' },
+    { code: 0xf28e, name: 'attach' },
+    { code: 0xf375, name: 'search' },
+    { code: 0xf2b8, name: 'checkbox-outline' },
+    { code: 0xf2b9, name: 'checkbox' },
+    { code: 0xf32a, name: 'menu' },
+    { code: 0xf345, name: 'person' },
+  ];
+
+  getIonicIconName(name: string): string {
+    const icon = this.searchIcons(name);
+    return icon !== undefined ? icon.name : 'cog';
+  }
+
+  getCharCode(name: string): string {
+    const icon = this.searchIcons(name);
+    return icon !== undefined ? String.fromCharCode(icon.code) : String.fromCharCode(0xf2cf);
+  }
+
+  private searchIcons(name: string): any {
+    return this.icons.find((icon) => icon.name === name);
+  }
 }
