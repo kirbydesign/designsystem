8fa74c89f39532a51ebecf49599eb7d85582ef14
Martin Metaksov <mmmtrifork@gmail.com>
2019-03-27 11:48:07 +0100

    added menu links for the new examples

--- src/app/showcase/fullscreen-modal-showcase/fullscreen-modal-showcase.component.ts
+++ src/app/showcase/fullscreen-modal-showcase/fullscreen-modal-showcase.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+declare var require: any;
 
 @Component({
   selector: 'kirby-fullscreen-modal-showcase',
   templateUrl: './fullscreen-modal-showcase.component.html',
   styleUrls: ['./fullscreen-modal-showcase.component.scss'],
 })
-export class FullscreenModalShowcaseComponent {}
+export class FullscreenModalShowcaseComponent {
+  exampleHtml: string = require('../../examples/fullscreen-modal-example/fullscreen-modal-example.component.html');
+}
