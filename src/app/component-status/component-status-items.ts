e2d3a6d9ab7651e3ea5b558ecba734943c068e6e
Smajo Zukanovic <szu@trifork.com>
2019-03-28 11:04:15 +0100

    cookbook url added

--- src/app/component-status/component-status-items.ts
+++ src/app/component-status/component-status-items.ts
@@ -246,7 +246,8 @@ export const componentStatusItems: ComponentStatusItem[] = [
     },
     code: {
       version: 0.5,
-      status: ItemCodeStatus.planned,
+      status: ItemCodeStatus.inProgress,
+      cookbookUrl: 'showcase/icon',
       githubIssueNo: 170,
     },
   },
