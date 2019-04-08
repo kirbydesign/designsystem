a749706867573e93f1b56680e6320ba76a1b1668
Martin Metaksov <mmmtrifork@gmail.com>
2019-04-08 16:04:13 +0200

    added documentation for the modal component

--- src/app/showcase/modal-showcase/modal-showcase.component.ts
+++ src/app/showcase/modal-showcase/modal-showcase.component.ts
@@ -11,11 +11,36 @@ declare var require: any;
 export class ModalShowcaseComponent {
   exampleHtml: string = require('../../examples/modal-example/modal-example.component.html');
   properties: ShowcaseProperty[] = [
-    // {
-    //   name: 'showShadow',
-    //   description: 'Determines whether the button will have a shadow or not.',
-    //   defaultValue: 'true',
-    //   inputValues: ['boolean'],
-    // },
+    {
+      name: 'title',
+      description: 'The header of the modal',
+      defaultValue: '',
+      inputValues: ['string'],
+    },
+    {
+      name: 'titleHorizontalAlignment',
+      description: 'Indicates the title horizontal alignment',
+      defaultValue: 'center',
+      inputValues: ['left', 'center'],
+    },
+    {
+      name: 'closeIcon',
+      description: 'The icon that will appear on the default close modal button',
+      defaultValue: 'close',
+      inputValues: ['close', 'arrow'],
+    },
+    {
+      name: 'dim',
+      description: 'Decides the dimness of the background once the modal is open',
+      defaultValue: '0.5',
+      inputValues: ['number (0..1)'],
+    },
+    {
+      name: 'component',
+      description:
+        'The component class which will be rendered inside the modal. It is recommended that your nested components implement the EmbeddedModal interface',
+      defaultValue: '',
+      inputValues: ['Component'],
+    },
   ];
 }
