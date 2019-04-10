bfdcf7c5e7d18e2276c0b02733c3fbf076b84d80
Peter Ring <peter.ring21@gmail.com>
2019-04-10 14:09:26 +0200

    added styling inputs

--- src/kirby/components/checkbox/checkbox.component.ts
+++ src/kirby/components/checkbox/checkbox.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'kirby-checkbox',
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkbox.component.scss']
 })
 export class CheckboxComponent implements OnInit {
+  @Input() size: string;
+  @Input() color: string;
+  @Input() shape: string;
+
+  classes: string[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.classes.push("checkbox");
+    this.classes.push(this.size || "");
+    this.classes.push(this.color || "");
+    this.classes.push(this.shape || "");
   }
 
 }
