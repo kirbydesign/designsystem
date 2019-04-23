309d8676fa74d58acde8c9a4ab0ea7fa89f2d245
Peter Ring <peter.ring21@gmail.com>
2019-04-23 10:00:56 +0200

    added input "checked"

--- src/kirby/components/checkbox/checkbox.component.ts
+++ src/kirby/components/checkbox/checkbox.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./checkbox.component.scss']
 })
 export class CheckboxComponent implements OnInit {
+  @Input() checked: boolean;
   @Input() size: string;
   @Input() color: string;
   @Input() shape: string;
