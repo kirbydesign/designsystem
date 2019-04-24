2538147db691f0afdecf438beb21c54a285d9c68
Martin Metaksov <mmmtrifork@gmail.com>
2019-04-24 13:53:59 +0200

    multiple tab-functionality fixes for keyboard users

--- src/kirby/components/modal/modal.component.ts
+++ src/kirby/components/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { NavParams } from '@ionic/angular';
 
 import { ModalConfig } from './config/modal-config';
@@ -9,14 +9,30 @@ import { IModalController } from './services/modal.controller.interface';
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss'],
 })
-export class ModalComponent {
+export class ModalComponent implements AfterViewInit {
+  @ViewChild('modalCloseButtonWrapper') modalCloseButtonWrapper: ElementRef;
   config: ModalConfig;
 
   constructor(private params: NavParams, private modalController: IModalController) {
     this.config = ModalConfigHelper.processOptionalValues(this.params.get('config'));
   }
 
+  ngAfterViewInit(): void {
+    const el = this.modalCloseButtonWrapper.nativeElement;
+    setTimeout(() => {
+      el.focus();
+      el.blur();
+    }, 50);
+  }
+
   dismissModal(): void {
     this.modalController.hideModal();
+  }
+
+  keyupModal(e: any) {
+    e.preventDefault();
+    if (e.keyCode === 32 || e.keyCode === 13) {
+      this.dismissModal();
+    }
   }
 }
