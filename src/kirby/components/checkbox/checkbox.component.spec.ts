c8fe08413648776a91eced826216de79e26fb902
Peter Ring <peter.ring21@gmail.com>
2019-04-30 13:02:22 +0200

    new tests on creating the right template

--- src/kirby/components/checkbox/checkbox.component.spec.ts
+++ src/kirby/components/checkbox/checkbox.component.spec.ts
@@ -4,7 +4,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MockComponent } from 'ng-mocks';
 import * as ionic from '@ionic/angular';
 import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { DebugElement, SimpleChange } from '@angular/core';
 
 import { CheckboxComponent } from './checkbox.component';
 
@@ -37,6 +37,32 @@ describe('CheckboxComponent', () => {
 
       expect(component.checkedChange.emit).toHaveBeenCalledTimes(1);
       expect(component.checkedChange.emit).toHaveBeenCalledWith(true);
+    });
+  });
+
+  describe('DOM: render templates', () => {
+    it('should find the ion-checkbox element based on css class .square', () => {
+      component.shape = 'square';
+      component.ngOnChanges({
+        shape: new SimpleChange(null, component.shape, true),
+      });
+      fixture.detectChanges();
+      const checkboxDe: DebugElement = fixture.debugElement;
+      const ionCheckboxDe = checkboxDe.query(By.css('.square'));
+      const ionCheckbox: HTMLElement = ionCheckboxDe.nativeElement;
+      expect(ionCheckbox).not.toBe(null);
+    });
+
+    it('should find the ion-checkbox element based on css class .circle', () => {
+      component.shape = 'circle';
+      component.ngOnChanges({
+        shape: new SimpleChange(null, component.shape, true),
+      });
+      fixture.detectChanges();
+      const checkboxDe: DebugElement = fixture.debugElement;
+      const ionCheckboxDe = checkboxDe.query(By.css('.circle'));
+      const ionCheckbox: HTMLElement = ionCheckboxDe.nativeElement;
+      expect(ionCheckbox).not.toBe(null);
     });
   });
 });
