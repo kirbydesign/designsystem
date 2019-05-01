ad378bf3fb7248bcefc5474ee6e6d8f3ba343f22
Alexander Hafstad <alexander@hafstad.dk>
2019-05-01 15:56:18 +0200

    feat(webfont-generator): add src and dest arguments for script

--- tools/webfonts-generator/index.js
+++ tools/webfonts-generator/index.js
@@ -2,33 +2,38 @@ const fs = require('fs');
 const webfontsGenerator = require('webfonts-generator');
 
 /*
- * Where are the svg's located
+ * Get required arguments, and init
  */
-const src = `${__dirname}/../../src/assets/icons/sprites`;
-/*
- * Where should the generated webfont be located
- */
-const dest = 'src/assets/fonts/iconfont';
+const src = getNamedArg('src');
+const dest = getNamedArg('dest');
+
+if(src && dest) {
+  const icons = locateIcons(src);
+  generateWebfont(icons, dest);
+}
 
 /*
- * Read all svg's from src folder
+ * Get all svg's from src folder
  */
-fs.readdir(src, (err, files) => {
-  const icons = files.filter(file => {
-    return file.endsWith('.svg');
-  }).map(file => {
-    return `${src}/${file}`;
-  });
-  generateWebfont(icons);
-});
+function locateIcons(src) {
+  try {
+    return fs.readdirSync(src).filter(file => {
+      return file.endsWith('.svg');
+    }).map(file => {
+      return `${src}/${file}`;
+    });
+  } catch (e) {
+    console.log(e.message);
+  }
+}
 
 /*
  * Generate webfont
  */
-function generateWebfont(icons) {
+function generateWebfont(icons, dest) {
   webfontsGenerator({
     files: icons,
-    dest: `${__dirname}/../../${dest}`,
+    dest,
     html: true,
     htmlTemplate: `${__dirname}/html.hbs`
   }, function(error) {
@@ -38,6 +43,16 @@ function generateWebfont(icons) {
       console.log(`Done! Webfont is located here: ${dest}`);
     }
   });
+}
+
+function getNamedArg(name, optional = false) {
+  const result = process.argv.filter((arg) => {
+    return arg.startsWith(`${name}=`);
+  }).map(arg => arg.replace(`${name}=`, ''))[0];
+
+  if(!result && !optional) console.log(`Argument: "${name}" is required`);
+
+  return result;
 }
 
 
