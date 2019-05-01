e06e21ac86135dc8ef619c14b7e58c2a1c7780cb
Martin Metaksov <mmmtrifork@gmail.com>
2019-05-01 15:11:03 +0200

    fix for a minor bug when clicking on the close icon

--- src/kirby/components/modal/modal.component.ts
+++ src/kirby/components/modal/modal.component.ts
@@ -34,7 +34,8 @@ export class ModalComponent implements AfterViewInit {
   onModalDismiss(e: any) {
     // Handle key press, due to:
     // https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/button_role#Required_JavaScript_Features
-    if (e && e.keyCode !== 32 && e.keyCode !== 13) {
+    if (e && e.keyCode && e.keyCode !== 32 && e.keyCode !== 13) {
+      console.log(`interrupting dismiss, keycode: ${e.keyCode}`);
       return;
     }
     this.modalController.hideModal();
--- src/kirby/components/modal/services/modal.controller.ts
+++ src/kirby/components/modal/services/modal.controller.ts
@@ -29,6 +29,8 @@ export class ModalController implements IModalController {
     modalCloseEvent.then((data) => {
       this.forgetTopModal();
       if (onCloseModal) {
+        // Since Ionic wraps the return value in an object, which contains data as a property, we need to return data.data
+        // We don't expect this on native, hence we return just data
         onCloseModal(typeof data === 'object' && 'data' in data ? data.data : data);
       }
     });
