a9503409771242a5951b68f33f36ef6c2760d54b
Jakob Engelbrecht Olesen <jakob@basher.dk>
2019-05-08 14:46:01 +0200

    :sparkles: Map code viewer language to supported types

--- src/app/shared/code-viewer/code-viewer.component.ts
+++ src/app/shared/code-viewer/code-viewer.component.ts
@@ -1,15 +1,24 @@
 import { highlightAll } from 'prismjs';
-
-import { Component, AfterViewChecked, Input } from '@angular/core';
+import { Component, AfterViewChecked, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'kirby-code-viewer,kirby-html-viewer',
   templateUrl: './code-viewer.component.html',
   styleUrls: ['./code-viewer.component.scss'],
 })
-export class CodeViewerComponent implements AfterViewChecked {
-  @Input() language: 'html' | 'css' | 'scss' | 'js' | 'ts';
+export class CodeViewerComponent implements OnChanges, AfterViewChecked {
+  @Input() language: 'html' | 'css' | 'scss' | 'js' | 'ts' | 'typescript';
   code: string;
+  languageTitle: string;
+  supportedLanguages = {
+    html: 'html',
+    css: 'css',
+    scss: 'css',
+    js: 'js',
+    ts: 'js',
+    typescript: 'js',
+  };
+  codeViewerLanguage: string;
 
   @Input()
   set html(value: string) {
@@ -39,6 +48,12 @@ export class CodeViewerComponent implements AfterViewChecked {
   set ts(value: string) {
     this.code = value;
     this.language = 'js'; // TODO: 'ts'
+  }
+
+  ngOnChanges(_: SimpleChanges): void {
+    this.languageTitle = this.language.toUpperCase();
+    const supportedLanguage = this.supportedLanguages[this.language];
+    this.codeViewerLanguage = `language-${supportedLanguage}`;
   }
 
   ngAfterViewChecked() {
