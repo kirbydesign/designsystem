0c1c9f9be4d3ad93f0a05f784a1e16a4708b2471
Jakob Engelbrecht Olesen <jakob@basher.dk>
2019-05-08 14:44:40 +0200

    :bug: Fix tests

--- src/app/showcase/button-showcase/button-showcase.component.spec.ts
+++ src/app/showcase/button-showcase/button-showcase.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { ExamplesModule } from '../../examples/examples.module';
-import { HtmlViewerComponent } from '../../shared/html-viewer/html-viewer.component';
+import { CodeViewerComponent } from '../../shared/code-viewer/code-viewer.component';
 import { ButtonShowcaseComponent } from './button-showcase.component';
 
 describe('ButtonShowcaseComponent', () => {
@@ -12,7 +12,7 @@ describe('ButtonShowcaseComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [ExamplesModule, RouterTestingModule],
-      declarations: [ButtonShowcaseComponent, HtmlViewerComponent],
+      declarations: [ButtonShowcaseComponent, CodeViewerComponent],
     }).compileComponents();
   }));
 
