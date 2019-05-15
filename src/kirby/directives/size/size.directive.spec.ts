0520b34a441ec56f4c8e97d5152d072cbbdfd070
Jonas Jensen <jonas.jensen@broelstaerk.dk>
2019-05-15 12:03:35 +0200

    added tests.

--- src/kirby/directives/size/size.directive.spec.ts
+++ src/kirby/directives/size/size.directive.spec.ts
@@ -9,4 +9,25 @@ describe('Directive: Size', () => {
     const directive = new SizeDirective();
     expect(directive).toBeTruthy();
   });
+
+  it(`should be small size if input is 'small'`, () => {
+    const directive = new SizeDirective();
+    directive.size = 'small';
+
+    expect(directive.isSmallSize).toBeTruthy();
+  });
+
+  it(`should be medium size if input is 'medium'`, () => {
+    const directive = new SizeDirective();
+    directive.size = 'medium';
+
+    expect(directive.isMediumSize).toBeTruthy();
+  });
+
+  it(`should be large size if input is 'large'`, () => {
+    const directive = new SizeDirective();
+    directive.size = 'large';
+
+    expect(directive.isLargeSize).toBeTruthy();
+  });
 });
