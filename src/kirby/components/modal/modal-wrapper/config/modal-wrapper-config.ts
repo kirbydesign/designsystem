9e2990bd67113015b6292410e58f1233d722b72b
Alexander Hafstad <alexander@hafstad.dk>
2019-05-15 17:09:21 +0200

    refactor(modal): remove 'undefined' from componentProps

--- src/kirby/components/modal/modal-wrapper/config/modal-wrapper-config.ts
+++ src/kirby/components/modal/modal-wrapper/config/modal-wrapper-config.ts
@@ -4,5 +4,5 @@ export interface ModalWrapperConfig {
   closeIconName?: 'close' | 'arrow-back';
   dim?: number;
   component: any;
-  componentProps?: undefined | { [key: string]: any };
+  componentProps?: { [key: string]: any };
 }
