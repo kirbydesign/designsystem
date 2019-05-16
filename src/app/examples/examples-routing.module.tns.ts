1f559d03f69baf223c05f4e8ffebb6c6b86286d7
Anders Skarby <anders@skarby.info>
2019-05-16 14:00:05 +0200

    Rounded corners on kirby-list (issue #160) (#162)
    * Github issue #160, now able to render kirby-list with rounded corners (defaults to "no rounded corners", which makes it backwards compatible).
    
    * Github issue #160, Had accidentally renamed a template variable.
    
    * Merged master to branch (fixed merge conflicts)
    
    * Merged master to branch (fixed merge conflicts)
    
    * Issue #160 - now styling with CSS pseudo classes on web (almost) exclusively.
    
    * Attempting to pages for the list examples (in the showcase) - aligning with web and {N}. Creating nested menu-structure for list examples, and interface to adjust styling properties for list.
    
    * Reverted changes to navigation (adding sub-items to list), will have multiple examples in a single List-page, but allow to switch between them (show a single example, at a time)
    
    * Just want this to be offloaded of this feature branch. Then I'll "attack" the revisited showcase as a separate issue.
    
    * Incorporated feedback from PR (from @jakobe)
    
    * Native section example matches web
    
    * minor css and ts improvements
    
    * #160, implemented feedback from PR.
    
    * #160, fixed linting errors.
    
    * Update src/app/examples/list/list-custom-cell-lines-example/list-custom-cell-lines-example.component.tns.html
    
    Co-Authored-By: Jakob Engelbrecht <jakob@basher.dk>
    
    * Update src/app/examples/list/list-custom-cell-lines-example/list-custom-cell-lines-example.component.tns.html
    
    Co-Authored-By: Jakob Engelbrecht <jakob@basher.dk>
    
    * #160, fixed more feedback from the PR.
    
    * Fixed feedback from PR.
    
    * Fixed feedback from PR (removed dependency upon NativeScriptFormsModule).
    
    * Fixed feedback from PR (removed dependency upon Ionic from AppModule).

--- src/app/examples/examples-routing.module.tns.ts
+++ src/app/examples/examples-routing.module.tns.ts
@@ -7,9 +7,9 @@ import { routes } from './examples.routes';
 import { ExamplesModule } from './examples.module';
 import { NativeScriptDoughnutChartExampleComponent } from './nativescript-only/doughnut-chart-example/doughnut-chart-example.component.tns-only';
 import { NativeScriptLineChartExampleComponent } from './nativescript-only/line-chart-example/line-chart-example.component.tns-only';
-import { ListSectionExampleComponent } from './list/tns-list-examples/list-section-example/list-section-example.component.tns';
-import { ListCustomCellLinesExampleComponent } from './list/tns-list-examples/list-custom-cell-lines-example/list-custom-cell-lines-example.component.tns';
-import { ListItemExampleComponent } from './list/tns-list-examples/list-item-example/list-item-example.component.tns';
+import { ListSectionExampleComponent } from './nativescript-only/list-section-example/list-section-example.component.tns';
+import { ListCustomCellLinesExampleComponent } from './list/list-custom-cell-lines-example/list-custom-cell-lines-example.component.tns';
+import { ListItemExampleComponent } from './list/list-item-example/list-item-example.component.tns';
 import { ListLoadOnDemandExampleComponent } from './list/load-on-demand/list-load-on-demand-example.component';
 
 const nativeScriptRoutes = [
