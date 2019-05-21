67d17d38f8655f89d6bd2b808a838837857a470c
Jakob Engelbrecht Olesen <jakob@basher.dk>
2019-05-21 19:46:24 +0200

    :sparkles: Add CardThemecolorExampleComponent

---
+++ src/app/examples/card/card-themecolor-example/card-themecolor-example.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'kirby-card-themecolor-example',
+  templateUrl: './card-themecolor-example.component.html',
+  styleUrls: ['./card-themecolor-example.component.scss']
+})
+export class CardThemecolorExampleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
