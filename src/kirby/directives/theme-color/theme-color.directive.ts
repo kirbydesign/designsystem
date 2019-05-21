663fa21dece8c42ef24b51ce6458bb7e1b457a2c
Jakob Engelbrecht Olesen <jakob@basher.dk>
2019-05-21 19:41:51 +0200

    :sparkles: Add themeColor directive

---
+++ src/kirby/directives/theme-color/theme-color.directive.ts
@@ -0,0 +1,39 @@
+import { Input, HostBinding, Directive } from '@angular/core';
+
+import { ColorType } from './../../helpers/color-type';
+
+@Directive({
+  // tslint:disable-next-line:directive-selector
+  selector: 'kirby-card[themeColor]',
+})
+export class ThemeColorDirective {
+  @HostBinding('class.primary')
+  private isPrimary: boolean;
+  @HostBinding('class.secondary')
+  private isSecondary: boolean;
+  @HostBinding('class.tertiary')
+  private isTertiary: boolean;
+  @HostBinding('class.success')
+  private isSuccess: boolean;
+  @HostBinding('class.warning')
+  private isWarning: boolean;
+  @HostBinding('class.danger')
+  private isDanger: boolean;
+  @HostBinding('class.light')
+  private isLight: boolean;
+  @HostBinding('class.medium')
+  private isMedium: boolean;
+  @HostBinding('class.dark')
+  private isDark: boolean;
+  @Input() set themeColor(value: ColorType) {
+    this.isPrimary = value === 'primary';
+    this.isSecondary = value === 'secondary';
+    this.isTertiary = value === 'tertiary';
+    this.isSuccess = value === 'success';
+    this.isWarning = value === 'warning';
+    this.isDanger = value === 'danger';
+    this.isLight = value === 'light';
+    this.isMedium = value === 'medium';
+    this.isDark = value === 'dark';
+  }
+}
