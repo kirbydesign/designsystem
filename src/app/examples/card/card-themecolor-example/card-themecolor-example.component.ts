ac69d763f1b9bc86705ab2d2b7e5bf7bbea74fcf
Jakob Engelbrecht Olesen <jakob@basher.dk>
2019-05-21 19:49:07 +0200

    :rotating_light: Fix lint

--- src/app/examples/card/card-themecolor-example/card-themecolor-example.component.ts
+++ src/app/examples/card/card-themecolor-example/card-themecolor-example.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'kirby-card-themecolor-example',
   templateUrl: './card-themecolor-example.component.html',
-  styleUrls: ['./card-themecolor-example.component.scss']
+  styleUrls: ['./card-themecolor-example.component.scss'],
 })
-export class CardThemecolorExampleComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class CardThemecolorExampleComponent {}
