9a01eb897541ee2b3ce287a3fc82ada287ad9c8d
Christian Ludemann <ext.christian.lude@ef.com>
2019-05-28 07:18:38 +0200

    chore(slide-button): setup showcase properties

--- src/app/showcase/slide-button-showcase/slide-button-showcase.component.ts
+++ src/app/showcase/slide-button-showcase/slide-button-showcase.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
 
+import { ShowcaseProperty } from '~/app/shared/showcase-properties/showcase-property';
+
 @Component({
   selector: 'kirby-slide-button-showcase',
   templateUrl: './slide-button-showcase.component.html',
   styleUrls: ['./slide-button-showcase.component.scss'],
 })
-export class SlideButtonShowcaseComponent {}
+export class SlideButtonShowcaseComponent {
+  exampleHtml: string = require('../../examples/slide-button-example/slide-button-example.component.html');
+  properties: ShowcaseProperty[] = [
+    {
+      name: 'expand',
+      description:
+        'If the button needs to expand to full width of its parent container, then use expand.',
+      defaultValue: 'null',
+      inputValues: ['fullWidth'],
+    },
+  ];
+}
