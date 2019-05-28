605d3c54eb24e2321cf93733cf978649c504ca27
Jakob Engelbrecht <jakob@basher.dk>
2019-05-28 09:03:48 +0200

    :bug: :lipstick: Fix modal height on {N} (#323)
    * :bug: :lipstick: Fix modal height on {N}
    
    * :green_apple: :lipstick: Add default padding and margin

--- src/kirby/components/modal/modal-wrapper/modal-wrapper.component.tns.ts
+++ src/kirby/components/modal/modal-wrapper/modal-wrapper.component.tns.ts
@@ -1,6 +1,9 @@
-import { Component, Injector } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
 import { ModalDialogParams } from 'nativescript-angular';
 import { ContentView, ShownModallyData, View } from 'tns-core-modules/ui/content-view';
+import { screen } from 'tns-core-modules/platform';
+import * as application from 'tns-core-modules/application';
+declare const android;
 
 import { ModalConfig } from './config/modal-config';
 import { COMPONENT_PROPS, ModalConfigHelper } from './config/modal-config.helper';
@@ -10,10 +13,12 @@ import { IModalController } from '../services/modal.controller.interface';
   templateUrl: './modal-wrapper.component.html',
   styleUrls: ['./modal-wrapper.component.scss'],
 })
-export class ModalWrapperComponent extends ContentView {
+export class ModalWrapperComponent extends ContentView implements OnInit {
   config: ModalConfig;
   view: View;
   componentPropsInjector: Injector;
+  outerPaddingTop = 32;
+  innerMarginBottom = 0;
 
   constructor(
     private modalController: IModalController,
@@ -27,6 +32,23 @@ export class ModalWrapperComponent extends ContentView {
       parent: injector,
     });
     this.modalController.register({ close: this.params.closeCallback });
+  }
+
+  ngOnInit(): void {
+    this.setAndroidPaddingAndMargin();
+  }
+
+  setAndroidPaddingAndMargin() {
+    if (application.android) {
+      const additionalDP = 12;
+      this.outerPaddingTop = this.getStatusBarHeight() + additionalDP;
+      const navBarHeight = this.getNavBarHeight();
+      if (navBarHeight > 0) {
+        this.innerMarginBottom = navBarHeight - additionalDP;
+      } else {
+        this.innerMarginBottom = additionalDP;
+      }
+    }
   }
 
   onShowingModally(args: ShownModallyData): void {
@@ -65,5 +87,54 @@ export class ModalWrapperComponent extends ContentView {
         });
     }
     // TODO: by default iOS slides up, however it slides together with the dimmed background, hence needs fixing
+  }
+
+  getStatusBarHeight() {
+    var result = 0;
+    if (application.android) {
+      var resourceId = application.android.context
+        .getResources()
+        .getIdentifier('status_bar_height', 'dimen', 'android');
+      if (resourceId) {
+        const screenScale = screen.mainScreen.scale;
+        const pixelValue = application.android.context.getResources().getDimension(resourceId);
+        result = pixelValue / screenScale;
+      }
+    }
+    return result;
+  }
+
+  getNavBarHeight() {
+    if (application.android) {
+      let navBarHeight = 0;
+      let windowManager = application.android.context.getSystemService(
+        android.content.Context.WINDOW_SERVICE
+      );
+      const defaultDisplay = windowManager.getDefaultDisplay();
+
+      const realDisplayMetrics = new android.util.DisplayMetrics();
+      defaultDisplay.getRealMetrics(realDisplayMetrics);
+
+      const realHeight = realDisplayMetrics.heightPixels;
+      const realWidth = realDisplayMetrics.widthPixels;
+
+      const displayMetrics = new android.util.DisplayMetrics();
+      defaultDisplay.getMetrics(displayMetrics);
+
+      const displayHeight = displayMetrics.heightPixels;
+      const displayWidth = displayMetrics.widthPixels;
+
+      if (realHeight - displayHeight > 0) {
+        // Portrait
+        navBarHeight = realHeight - displayHeight;
+      } else if (realWidth - displayWidth > 0) {
+        // Landscape
+        navBarHeight = realWidth - displayWidth;
+      }
+
+      // Convert to device independent pixels and return
+      return navBarHeight / application.android.context.getResources().getDisplayMetrics().density;
+    }
+    return 0;
   }
 }
