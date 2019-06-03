c7184aceb59411421d3740dda360ab493bdc6306
Christian Ludemann <ext.christian.lude@ef.com>
2019-06-03 20:35:14 +0200

    feat(slide-button): make sure only drag when tap on thumbnail

--- src/kirby/components/slide-button/slide-button.component.tns.ts
+++ src/kirby/components/slide-button/slide-button.component.tns.ts
@@ -1,10 +1,16 @@
 import { Component, OnInit, ViewChild, ElementRef, ChangeDetectionStrategy } from '@angular/core';
-import { PanGestureEventData } from 'tns-core-modules/ui/gestures/gestures';
+import { PanGestureEventData, TouchGestureEventData } from 'tns-core-modules/ui/gestures/gestures';
 import { GridLayout } from 'tns-core-modules/ui/layouts/grid-layout';
 import { Button } from 'tns-core-modules/ui/button';
 
 import { SlideButtonCommon } from './slide-button.common';
 export const SLIDE_BUTTON_SELECTOR = 'kirby-slide-button';
+
+enum PanStates {
+  down = 1,
+  move = 2,
+  up = 3,
+}
 
 @Component({
   selector: SLIDE_BUTTON_SELECTOR,
@@ -16,6 +22,7 @@ export class SlideButtonComponent extends SlideButtonCommon implements OnInit {
   @ViewChild('slideTextRef') slideTextRef: ElementRef;
   @ViewChild('slideThumbnailRef') slideThumbnailRef: ElementRef;
   slideThumbnailElm: Button;
+  virtualPanXPosition = 0; // this is used to get the tap x position when dragging out of sliding frame
   prevDeltaX: number;
   prevDeltaY: number;
   isDraggedToEnd: boolean;
@@ -43,11 +50,11 @@ export class SlideButtonComponent extends SlideButtonCommon implements OnInit {
   }
 
   onPan(args: PanGestureEventData) {
-    if (args.state === 1) {
+    if (args.state === PanStates.down) {
       this.onPanDown();
-    } else if (args.state === 2) {
+    } else if (args.state === PanStates.move) {
       this.onDrag(args);
-    } else if (args.state === 3) {
+    } else if (args.state === PanStates.up) {
       this.onPanUp();
     }
   }
@@ -69,31 +76,46 @@ export class SlideButtonComponent extends SlideButtonCommon implements OnInit {
         duration: this.slideResetTime,
       });
       this.slidePct = 0;
+      this.virtualPanXPosition = 0;
     }
   }
 
   private onDrag(args: PanGestureEventData) {
-    this.slideThumbnailElm.translateX += args.deltaX - this.prevDeltaX;
+    const newXPosition = this.calculateNewXPosition(args);
+    const edgeXMarginAdjusted = this.getEdgeX();
+    const isSliderAtEnd =
+      this.virtualPanXPosition >= edgeXMarginAdjusted || newXPosition >= edgeXMarginAdjusted;
+
+    const edgeXPct = (this.slideThumbnailElm.translateX / edgeXMarginAdjusted) * 100;
+    this.slidePct = edgeXPct > 100 ? 100 : edgeXPct;
+    if (this.slideThumbnailElm.translateX < 0 || this.virtualPanXPosition < 0) {
+      this.resetSlideButton();
+    } else if (isSliderAtEnd) {
+      this.slideThumbnailElm.translateX = edgeXMarginAdjusted;
+      this.isDraggedToEnd = true;
+    } else {
+      this.slideThumbnailElm.translateX = newXPosition;
+      this.isDraggedToEnd = false;
+    }
+  }
+
+  private calculateNewXPosition(args: PanGestureEventData) {
+    const slideMovementInCurrentFrame = args.deltaX - this.prevDeltaX;
+    this.virtualPanXPosition += slideMovementInCurrentFrame;
     this.prevDeltaX = args.deltaX;
     this.prevDeltaY = args.deltaY;
+    const newXPosition = this.slideThumbnailElm.translateX + slideMovementInCurrentFrame;
+    return newXPosition;
+  }
+
+  private getEdgeX() {
     const convDpToPixelsFactor =
       (this.slideThumbnailElm.width as number) / this.slideThumbnailElm.getMeasuredWidth();
     const edgeX =
       (this.itemContainer.getMeasuredWidth() - this.slideThumbnailElm.getMeasuredWidth()) *
       convDpToPixelsFactor;
-
     const edgeXMarginAdjusted = edgeX - +this.slideThumbnailElm.margin * 2; // adjusted for thumbnail margin
-
-    const edgeXPct = (this.slideThumbnailElm.translateX / edgeXMarginAdjusted) * 100;
-    this.slidePct = edgeXPct > 100 ? 100 : edgeXPct;
-    if (this.slideThumbnailElm.translateX < 0) {
-      this.resetSlideButton();
-    } else if (this.slideThumbnailElm.translateX >= edgeXMarginAdjusted) {
-      this.slideThumbnailElm.translateX = edgeXMarginAdjusted;
-      this.isDraggedToEnd = true;
-    } else {
-      this.isDraggedToEnd = false;
-    }
+    return edgeXMarginAdjusted;
   }
 
   private resetSlideButton() {
@@ -104,5 +126,6 @@ export class SlideButtonComponent extends SlideButtonCommon implements OnInit {
     this.prevDeltaX = 0;
     this.prevDeltaY = 0;
     this.slidePct = 0;
+    this.virtualPanXPosition = 0;
   }
 }
