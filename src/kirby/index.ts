3d9481afddca9bd42ec4974fd4a170b4bd9bece3
Anders Skarby <anders@skarby.info>
2019-06-06 08:55:52 +0200

    Moved list dependencies to separate barrel file, fixed linting errors in kirby.module.tns.ts

--- src/kirby/index.ts
+++ src/kirby/index.ts
@@ -6,20 +6,6 @@ export { CardHeaderComponent } from './components/card/card-header/card-header.c
 export { GridComponent } from './components/grid/grid.component';
 export { IconComponent } from './components/icon/icon.component';
 export { CustomIconNameDirective } from './components/icon/custom-icon-name.directive';
-export { InfiniteScrollDirective } from './components/list/directives/infinite-scroll.directive';
-export { ListComponent } from './components/list/list.component';
-export { ListCellComponent } from './components/list/list-cell/list-cell.component';
-export { ListCellLineComponent } from './components/list/list-cell-line/list-cell-line.component';
-export { ListFlexItemComponent } from './components/list/list-flex-item/list-flex-item.component';
-export { ListHeaderComponent } from './components/list/list-header/list-header.component';
-export { ListItemComponent } from './components/list/list-item/list-item.component';
-export {
-  ListSectionHeaderComponent,
-} from './components/list/list-section-header/list-section-header.component';
-export { ListFlexItemDirective } from './components/list/list.directive';
-export { ListHeaderDirective } from './components/list/list.directive';
-export { ListItemDirective } from './components/list/list.directive';
-export { ListSectionHeaderDirective } from './components/list/list.directive';
 export { KirbyModule } from './kirby.module';
 export { SpinnerComponent } from './components/spinner/spinner.component';
 export {
@@ -27,7 +13,6 @@ export {
 } from './components/segmented-control/segmented-control.component';
 export { SegmentItem } from './components/segmented-control/segment-item';
 export { BadgeComponent } from './components/badge/badge.component';
-export { COMPONENT_PROPS } from './components/modal/modal-wrapper/config/modal-config.helper';
 export { CustomIconSettings, CUSTOM_FONT_SETTINGS } from './components/icon/custom-icon-settings';
 export { GridCardConfiguration } from './components/grid/grid-card-configuration';
 export {
