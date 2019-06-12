4fd2ec9e4193213af9fe3d0aba5840876b8d94ed
Alexander Hafstad <alexander@hafstad.dk>
2019-06-12 16:58:58 +0200

    feat(icon): update example

--- src/app/examples/icon-example/icon-example.component.ts
+++ src/app/examples/icon-example/icon-example.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+
 import { icons } from '@kirbydesign/designsystem/components/icon/icon.component';
 import { CustomIcon } from '@kirbydesign/designsystem/components/icon/custom-icon-settings';
+import { Sizes } from '@kirbydesign/designsystem/directives/size/size.directive';
 
 @Component({
   selector: 'kirby-icon-example',
@@ -9,4 +11,7 @@ import { CustomIcon } from '@kirbydesign/designsystem/components/icon/custom-ico
 })
 export class IconExampleComponent {
   icons: CustomIcon[] = icons;
+  sizes = Sizes;
+
+  defaultIconsCodeExample = `<kirby-icon name="NAME"></kirby-icon>`;
 }
