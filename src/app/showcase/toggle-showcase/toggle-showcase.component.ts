bd824648788431fb8cef9d2f253dc3b7760cb2fc
Smajo Zukanovic <szu@trifork.com>
2019-06-13 09:29:11 +0200

    ion-toggle added

--- src/app/showcase/toggle-showcase/toggle-showcase.component.ts
+++ src/app/showcase/toggle-showcase/toggle-showcase.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { ShowcaseProperty } from './../../shared/showcase-properties/showcase-property';
+declare var require: any;
 
 @Component({
   selector: 'kirby-toggle-showcase',
   templateUrl: './toggle-showcase.component.html',
-  styleUrls: ['./toggle-showcase.component.scss']
+  styleUrls: ['./toggle-showcase.component.scss'],
 })
-export class ToggleShowcaseComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ToggleShowcaseComponent {
+  exampleHtml: string = require('../../examples/toggle-example/toggle-example.component.html');
+  properties: ShowcaseProperty[] = [
+    {
+      name: 'checked',
+      description: 'If true, the toggle is selected.',
+      defaultValue: 'false',
+      inputValues: ['true', 'false'],
+    },
+    {
+      name: 'themeColor',
+      description: 'Sets which color the button should use from the theme palette.',
+      defaultValue: 'primary',
+      inputValues: ['primary', 'secondary'],
+    },
+  ];
 }
--- src/kirby/components/toggle/toggle.component.ts
+++ src/kirby/components/toggle/toggle.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'kirby-toggle',
   templateUrl: './toggle.component.html',
-  styleUrls: ['./toggle.component.scss']
+  styleUrls: ['./toggle.component.scss'],
 })
-export class ToggleComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class ToggleComponent {}
