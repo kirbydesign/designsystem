d8861c4c2c30f92293274512a959392af4c7d3b8
Jakob Engelbrecht Olesen <jakob@basher.dk>
2019-06-21 12:31:39 +0200

    :pencil: Configurable calendar example

--- src/app/examples/calendar-example/calendar-card-example.component.ts
+++ src/app/examples/calendar-example/calendar-card-example.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'kirby-calendar-card-example',
@@ -7,29 +7,12 @@ import { Component } from '@angular/core';
 })
 export class CalendarCardExampleComponent {
   selectedDate: Date;
-  disableDatesArray: Array<Date> = new Array();
-
-  constructor() {
-    const today = new Date();
-    var date1 = new Date();
-    date1.setDate(today.getDate() + 3);
-    var date2 = new Date();
-    date2.setDate(today.getDate() + 5);
-    var date3 = new Date();
-    date3.setDate(today.getDate() + 7);
-    var date4 = new Date();
-    date4.setDate(today.getDate() + 10);
-    var date5 = new Date();
-    date5.setDate(today.getDate() + 15);
-    var date6 = new Date();
-    date6.setDate(today.getDate() + 25);
-    var date7 = new Date();
-    date7.setDate(today.getDate() + 28);
-    var date8 = new Date();
-    date8.setDate(today.getDate() + 35);
-
-    this.disableDatesArray.push(date1, date2, date3, date4, date5, date6, date7, date8);
-  }
+  @Input() disableWeekends = false;
+  @Input() disablePastDates = false;
+  @Input() disableFutureDates = false;
+  @Input() minDate: Date;
+  @Input() maxDate: Date;
+  @Input() disabledDates: Date[];
 
   onDateChange(selectedDate: Date) {
     this.selectedDate = selectedDate;
--- src/app/showcase/calendar-showcase/calendar-showcase.component.ts
+++ src/app/showcase/calendar-showcase/calendar-showcase.component.ts
@@ -10,33 +10,87 @@ declare var require: any;
   styleUrls: ['./calendar-showcase.component.scss'],
 })
 export class CalendarShowcaseComponent {
+  disableWeekends = false;
+  disablePastDates = false;
+  disableFutureDates = false;
+  setMinDate = false;
+  setMaxDate = false;
+  setdisabledDates = false;
+  minDate: Date;
+  maxDate: Date;
+  disabledDates: Array<Date> = new Array();
+
+  constructor() {
+    const today = new Date();
+
+    this.minDate = new Date();
+    this.minDate.setDate(today.getDate() - 60);
+    this.maxDate = new Date();
+    this.maxDate.setDate(today.getDate() + 60);
+
+    const date1 = new Date();
+    date1.setDate(today.getDate() + 3);
+    const date2 = new Date();
+    date2.setDate(today.getDate() + 5);
+    const date3 = new Date();
+    date3.setDate(today.getDate() + 7);
+    const date4 = new Date();
+    date4.setDate(today.getDate() + 10);
+    const date5 = new Date();
+    date5.setDate(today.getDate() + 15);
+    const date6 = new Date();
+    date6.setDate(today.getDate() + 25);
+    const date7 = new Date();
+    date7.setDate(today.getDate() + 28);
+    const date8 = new Date();
+    date8.setDate(today.getDate() + 35);
+    this.disabledDates.push(date1, date2, date3, date4, date5, date6, date7, date8);
+  }
+
   exampleHtml: string = require('../../examples/calendar-example/calendar-example.component.html');
   exampleWithCardHtml: string = require('../../examples/calendar-example/calendar-card-example.component.html');
   properties: ShowcaseProperty[] = [
     {
+      name: 'selectedDate',
+      description: '(Optional) Returns/sets the selected date.',
+      defaultValue: 'null',
+      inputValues: ['Date'],
+    },
+    {
       name: 'disableWeekends',
-      description: 'Disables selection of weekends.',
+      description: '(Optional) Disables selection of weekends.',
       defaultValue: 'false',
       inputValues: ['true | false'],
     },
     {
       name: 'disablePastDates',
-      description: 'Determines if dates in the past can be selected.',
-      defaultValue: 'true',
+      description: '(Optional) Disables selection of dates earlier then the current date.',
+      defaultValue: 'false',
       inputValues: ['true | false'],
     },
     {
-      name: 'disableDates',
-      description:
-        'Array of dates that you want to disable inside the calendar. Date format is YYYY-MM-DD.',
-      defaultValue: 'null',
-      inputValues: ['Date[]'],
+      name: 'disableFutureDates',
+      description: '(Optional) Disables selection of dates later than the current date.',
+      defaultValue: 'false',
+      inputValues: ['true | false'],
     },
     {
-      name: 'currentDate',
-      description: 'Date that you want to select inside the calendar. Date format is YYYY-MM-DD.',
+      name: 'minDate',
+      description: '(Optional) The earliest date that should be selectable.',
       defaultValue: 'null',
       inputValues: ['Date'],
+    },
+    {
+      name: 'maxDate',
+      description: '(Optional) The latest date that should be selectable.',
+      defaultValue: 'null',
+      inputValues: ['Date'],
+    },
+    {
+      name: 'disabledDates',
+      description: '(Optional) Array of dates that should not be selectable.',
+      defaultValue: 'null',
+      inputValues: ['Date[]'],
     },
   ];
 }
