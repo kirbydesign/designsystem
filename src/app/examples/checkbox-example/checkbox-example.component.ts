e0e03d422dd826cfc44fb73c60156ffe7c334994
Jakob Engelbrecht Olesen <jakob@basher.dk>
2019-06-21 10:34:29 +0200

    :pencil: Add checkbox event handler

--- src/app/examples/checkbox-example/checkbox-example.component.ts
+++ src/app/examples/checkbox-example/checkbox-example.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'kirby-checkbox-example',
   templateUrl: './checkbox-example.component.html',
   styleUrls: ['./checkbox-example.component.scss'],
 })
-export class CheckboxExampleComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
+export class CheckboxExampleComponent {
+  checkedChange(checked: boolean) {
+    console.log('Checked:', checked);
+  }
 }
