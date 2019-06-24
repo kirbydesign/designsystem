00a31263fe2518c0dc931f5d27a369a0a79879ab
Alexander Hafstad <alexander@hafstad.dk>
2019-06-24 21:44:56 +0200

    fix(icon): change back icon.component.ts

--- src/kirby/components/icon/icon.component.ts
+++ src/kirby/components/icon/icon.component.ts
@@ -1,9 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'kirby-icon',
   templateUrl: './icon.component.html',
   styleUrls: ['./icon.component.scss'],
+  // Using host property decorator is fine for static values:
+  // tslint:disable-next-line:use-host-property-decorator
+  host: {
+    class: 'kirby-icon',
+  },
 })
 export class IconComponent {
   static DEFAULT_ICON_CODE = 0xf2cf;
@@ -26,9 +31,6 @@ export class IconComponent {
     | 'menu'
     | 'person' = IconComponent.DEFAULT_ICON_NAME;
 
-  @Input() size: 'small' | 'large' = 'small';
-  @Input() colortype: 'primary' | 'secondary' | 'tertiary' | 'alert' | 'success' | 'danger';
-
   getIonicIconName(name: string): string {
     const icon = iconsCharCodeMap[name];
     return icon !== undefined ? name : IconComponent.DEFAULT_ICON_NAME;
@@ -43,20 +45,20 @@ export class IconComponent {
 }
 // tslint:disable:prettier
 export const iconsCharCodeMap = {
-  add: 0xf102,
-  close: 0xf2c0,
-  cog: 0xf2cf,
-  swap: 0xf389,
-  move: 0xf331,
+  'add': 0xf102,
+  'close': 0xf2c0,
+  'cog': 0xf2cf,
+  'swap': 0xf389,
+  'move': 0xf331,
   'log-out': 0xf359,
-  more: 0xf1c9,
+  'more': 0xf1c9,
   'arrow-back': 0xf27d,
-  help: 0xf30b,
-  attach: 0xf28e,
-  search: 0xf375,
+  'help': 0xf30b,
+  'attach': 0xf28e,
+  'search': 0xf375,
   'checkbox-outline': 0xf2b8,
-  checkbox: 0xf2b9,
-  menu: 0xf32a,
-  person: 0xf345,
+  'checkbox': 0xf2b9,
+  'menu': 0xf32a,
+  'person': 0xf345,
 };
 // tslint:enable:prettier
