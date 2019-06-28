e3298c2215e4c43ea192903f8ed1ab2faef82c7d
Alexander Hafstad <alexander@hafstad.dk>
2019-06-28 13:39:41 +0200

    feat(icon): add general kirby icons and documentation how to update them (#366)
    * feat(icon): add general kirby icons and documentation how to update them
    
    * refactor(icon): rename 'fortune' icon to 'moneybag'
    
    * refactor(icons): change svg-files to have english naming
    
    * Update src/app/examples/icon-example/icon-example.component.ts
    
    refactor(icon-example): remove unused property
    
    Co-Authored-By: Jakob Engelbrecht <jakob@basher.dk>
    
    * refactor(icon): rename 'custom*' to just icon as they aren't custom anymore
    
    * refactor(icon): renaming, and inline fromCharCode
    
    * :truck: Move icons to Kirby folder
    
    * Revert
    
    * :fire: Remove unused sprites
    
    * :pencil: Update readme
    
    * :pencil: Update readme
    
    * refactor(icons): remove title and desc
    
    * feat(icons): add colors
    
    * :bug: Fix font folder
    
    * fix(icon): specs
    
    * fix(icons): specs
    
    * chore: add svg's to test assets angular.json

--- src/app/examples/icon-example/icon-example.component.ts
+++ src/app/examples/icon-example/icon-example.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
 
+import { icons } from '@kirbydesign/designsystem/components/icon/icon.component';
+import { Icon } from '@kirbydesign/designsystem/components/icon/icon-settings';
+import { Sizes } from '@kirbydesign/designsystem/directives/size/size.directive';
+
 @Component({
   selector: 'kirby-icon-example',
   templateUrl: './icon-example.component.html',
   styleUrls: ['./icon-example.component.scss'],
 })
-export class IconExampleComponent {}
+export class IconExampleComponent {
+  icons: Icon[] = icons;
+  sizes = Sizes;
+  color: string;
+
+  changeColor(color: string) {
+    this.color = color;
+  }
+}
