bbdae531b241b97549ed124a77c7d982cfdea08b
Morten Bjerg Gregersen <mgr@bankdata.dk>
2019-07-01 09:33:41 +0200

    Inline example variables

--- src/app/examples/empty-state-example/empty-state-example.component.ts
+++ src/app/examples/empty-state-example/empty-state-example.component.ts
@@ -5,9 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './empty-state-example.component.html',
   styleUrls: ['./empty-state-example.component.scss'],
 })
-export class EmptyStateExampleComponent {
-  iconName = 'help';
-  title = 'No items';
-  text = "You don't have any items. Call support to add some items to your account.";
-  buttonText = 'Call support';
-}
+export class EmptyStateExampleComponent {}
