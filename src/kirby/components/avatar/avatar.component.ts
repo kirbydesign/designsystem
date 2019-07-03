e0784963d4448e7d535467f59b0bde69c21ad601
Jonas Jensen <jonas.jensen@broelstaerk.dk>
2019-07-03 09:47:06 +0200

    :sparkles: Made web version of avatar with icon

--- src/kirby/components/avatar/avatar.component.ts
+++ src/kirby/components/avatar/avatar.component.ts
@@ -4,11 +4,6 @@ import { Component, Input, OnInit } from '@angular/core';
   selector: 'kirby-avatar',
   templateUrl: './avatar.component.html',
   styleUrls: ['./avatar.component.scss'],
-  // Using host property decorator is fine for static values:
-  // tslint:disable-next-line:use-host-property-decorator
-  host: {
-    class: 'kirby-avatar',
-  },
 })
 export class AvatarComponent implements OnInit {
   @Input() imageSrc: string;
