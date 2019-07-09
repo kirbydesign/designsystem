8346f5ae90038648bd18246cf89ffee05a50b036
Alexander Hafstad <alexander@hafstad.dk>
2019-07-09 12:01:51 +0200

    chore(package.json): change version of zone.js and reverted back test.ts

--- src/test.ts
+++ src/test.ts
@@ -1,12 +1,11 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
+import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
-
-import 'zone.js/dist/zone-testing';
 
 // Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
 declare const __karma__: any;
