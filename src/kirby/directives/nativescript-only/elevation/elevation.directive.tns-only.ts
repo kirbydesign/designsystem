77f59baf3091a915915ae3e448a5ebcec6b245ce
Jakob Engelbrecht Olesen <jakob@basher.dk>
2019-07-15 13:52:09 +0200

    :reycle: Replace timeout with loaded event

--- src/kirby/directives/nativescript-only/elevation/elevation.directive.tns-only.ts
+++ src/kirby/directives/nativescript-only/elevation/elevation.directive.tns-only.ts
@@ -1,5 +1,5 @@
 import { Directive, OnInit, Input, ElementRef, Renderer2 } from '@angular/core';
-import { isIOS } from 'tns-core-modules/ui/page/page';
+import { isAndroid, isIOS } from 'tns-core-modules/platform';
 
 import { ScssHelper } from '../../../scss/scss-helper';
 
@@ -8,31 +8,33 @@ export class NativeScriptElevationDirective implements OnInit {
   @Input() kirbyElevation?: 2 | 4 | 8;
   @Input() iosMasksToBounds?: boolean;
 
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit(): void {
     if (!this.kirbyElevation) {
       return;
     }
-    this.awaitNativeElementInitAndApplyElevation();
-  }
-
-  // Due to a timing issue, the native element is not available initially:
-  // https://github.com/NativeScript/nativescript-angular/issues/848
-  private awaitNativeElementInitAndApplyElevation() {
-    setTimeout(() => {
-      if (this.el.nativeElement.ios || this.el.nativeElement.android) {
+    // Due to a timing issue, the native element is not available initially on Android:
+    // https://github.com/NativeScript/nativescript-angular/issues/848
+    if (isAndroid) {
+      this.elementRef.nativeElement.on('loaded', () => {
         this.applyElevation();
-      } else {
-        // continue recursion until the native element gets initialized
-        this.awaitNativeElementInitAndApplyElevation();
-      }
-    }, 5);
+      });
+    } else {
+      this.applyElevation();
+    }
   }
 
   private applyElevation() {
-    if (isIOS) {
-      const iosView = this.el.nativeElement.ios;
+    if (isAndroid) {
+      // In android we need a container wrapping the element which will have the shadow
+      const androidChildView = this.elementRef.nativeElement.firstChild;
+      if (!androidChildView) {
+        return;
+      }
+      this.renderer.setStyle(androidChildView, `android-elevation`, `${this.kirbyElevation}`);
+    } else if (isIOS) {
+      const iosView = this.elementRef.nativeElement.ios;
 
       // if true, masksToBounds prevents scrolling bugs on list rows
       iosView.layer.masksToBounds = this.iosMasksToBounds;
@@ -41,13 +43,6 @@ export class NativeScriptElevationDirective implements OnInit {
       // source: https://github.com/Especializa/nativescript-ng-shadow/blob/master/src/common/shadow.ts
       iosView.layer.shadowOffset = CGSizeMake(0, 0.54 * this.kirbyElevation - 0.14);
       iosView.layer.shadowOpacity = 0.006 * this.kirbyElevation + 0.25;
-    } else {
-      // In android we need a container wrapping the element which will have the shadow
-      const androidChildView = this.el.nativeElement.firstChild;
-      if (!androidChildView) {
-        return;
-      }
-      this.renderer.setStyle(androidChildView, `android-elevation`, `${this.kirbyElevation}`);
     }
   }
 }
