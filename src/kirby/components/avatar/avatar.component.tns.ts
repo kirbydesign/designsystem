bafe1082b4f496c3fd042ba4dae4f42455397974
Christian Ludemann <ext.christian.lude@ef.com>
2019-07-22 13:57:12 +0200

    feat(avatar): show text in avatar + include paths for web scss

--- src/kirby/components/avatar/avatar.component.tns.ts
+++ src/kirby/components/avatar/avatar.component.tns.ts
@@ -1,7 +1,16 @@
-import { Component, Input, AfterViewInit, ContentChild, AfterContentInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  AfterViewInit,
+  ContentChild,
+  AfterContentInit,
+  ChangeDetectionStrategy,
+} from '@angular/core';
 import { registerElement } from 'nativescript-angular';
 import { EventData, View } from 'tns-core-modules/ui/core/view/view';
 import { ContentView } from 'tns-core-modules/ui/content-view';
+
+import { Avatar } from './avatar.interface';
 
 declare const CGSizeMake: any;
 declare const android: any;
@@ -12,13 +21,14 @@ const AVATAR_COMPONENT_SELECTOR = 'kirby-avatar';
   selector: AVATAR_COMPONENT_SELECTOR,
   templateUrl: './avatar.component.html',
   styleUrls: ['./avatar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   // Using host property decorator is fine for static values:
   // tslint:disable-next-line:use-host-property-decorator
   host: {
     class: 'kirby-avatar',
   },
 })
-export class AvatarComponent extends ContentView {
+export class AvatarComponent extends ContentView implements Avatar {
   @Input() imageSrc: string;
   @Input() altText: string;
   @Input() text: string;
