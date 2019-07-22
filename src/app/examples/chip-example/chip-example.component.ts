1294b6ef82c25c48872191f9acdd9695034750f0
Christian Ludemann <ext.christian.lude@ef.com>
2019-07-22 15:40:55 +0200

    feat(chip): change colors depending on theme for web

--- src/app/examples/chip-example/chip-example.component.ts
+++ src/app/examples/chip-example/chip-example.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { ThemeColor } from '@kirbydesign/designsystem/helpers/theme-color.type';
 
 @Component({
   selector: 'kirby-chip-example',
@@ -7,5 +9,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChipExampleComponent implements OnInit {
   constructor() {}
+  @Input() themeColor: ThemeColor | '' = '';
   ngOnInit() {}
 }
