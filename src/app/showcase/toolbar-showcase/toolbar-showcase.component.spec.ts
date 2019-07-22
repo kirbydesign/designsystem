f46ff78ea7c585b86b62027185f084d146005dce
Peter Ring <peter.ring21@gmail.com>
2019-07-22 10:56:30 +0200

    created showcase component

---
+++ src/app/showcase/toolbar-showcase/toolbar-showcase.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { ToolbarShowcaseComponent } from './toolbar-showcase.component';
+
+describe('ToolbarShowcaseComponent', () => {
+  let component: ToolbarShowcaseComponent;
+  let fixture: ComponentFixture<ToolbarShowcaseComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ToolbarShowcaseComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ToolbarShowcaseComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/showcase/toolbar-showcase/toolbar-showcase.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'kirby-toolbar-showcase',
+  templateUrl: './toolbar-showcase.component.html',
+  styleUrls: ['./toolbar-showcase.component.css']
+})
+export class ToolbarShowcaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
