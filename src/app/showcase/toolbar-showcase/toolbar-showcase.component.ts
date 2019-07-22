d56ce233df47453efa6d8eca5100d5e8c1866de1
Peter Ring <peter.ring21@gmail.com>
2019-07-22 11:51:52 +0200

    updated showcase to display example

--- src/app/showcase/toolbar-showcase/toolbar-showcase.component.ts
+++ src/app/showcase/toolbar-showcase/toolbar-showcase.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'kirby-toolbar-showcase',
   templateUrl: './toolbar-showcase.component.html',
-  styleUrls: ['./toolbar-showcase.component.css']
+  styleUrls: ['./toolbar-showcase.component.css'],
 })
 export class ToolbarShowcaseComponent implements OnInit {
+  exampleHtml: string = require('../../examples/toolbar-example/toolbar-example.component.html');
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
