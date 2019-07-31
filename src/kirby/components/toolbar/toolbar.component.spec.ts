a75dfdb0d29fa8133d4e88c03669a2a9c0fa2ce2
Peter Ring <peter.ring21@gmail.com>
2019-07-31 09:14:31 +0200

    created test for input and output

--- src/kirby/components/toolbar/toolbar.component.spec.ts
+++ src/kirby/components/toolbar/toolbar.component.spec.ts
@@ -1,27 +1,70 @@
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { Spectator, createTestComponentFactory } from '@netbasal/spectator';
+import { MockComponent } from 'ng-mocks';
+import * as ionic from '@ionic/angular';
 
 import { ToolbarComponent } from './toolbar.component';
 
-describe('ToolbarComponent', () => {
-  let component: ToolbarComponent;
-  let fixture: ComponentFixture<ToolbarComponent>;
+import { IconComponent } from '../icon/icon.component';
+import { IonTitle } from '@ionic/angular';
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ToolbarComponent],
-    }).compileComponents();
-  }));
+fdescribe('ToolbarComponent', () => {
+  const title = 'title';
+  const showBackButton = true;
+
+  let spectator: Spectator<ToolbarComponent>;
+
+  const createHost = createTestComponentFactory({
+    component: ToolbarComponent,
+    declarations: [
+      MockComponent(ionic.IonToolbar),
+      MockComponent(ionic.IonTitle),
+      MockComponent(IconComponent),
+    ],
+  });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ToolbarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    spectator = createHost({ title, showBackButton });
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(spectator.component).toBeTruthy();
+  });
+
+  describe('event: back', () => {
+    it('should be called once', () => {
+      spyOn(spectator.component.backButtonSelected, 'emit');
+
+      spectator.component.onBackButtonSelected();
+
+      expect(spectator.component.backButtonSelected.emit).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('Inputs', () => {
+    it('should set the text in ion-title', () => {
+      const titleChange = 'test';
+
+      spectator.setInput('title', titleChange);
+
+      expect(spectator.query('ion-title').textContent).toContain(titleChange);
+    });
+
+    it('should trigger rendering of the back button', () => {
+      const show = true;
+
+      spectator.setInput('showBackButton', show);
+
+      expect(spectator.query('.toolbar-item-left')).toBeTruthy();
+    });
+
+    it('should remove the back button from the DOM', () => {
+      const show = false;
+
+      spectator.setInput('showBackButton', show);
+
+      expect(spectator.query('.toolbar-item-left')).not.toBeTruthy();
+    });
   });
 });
