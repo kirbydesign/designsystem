ddfe704b9b95d9e4d5c11935595ef323b7b7719c
Peter Ring <peter.ring21@gmail.com>
2019-07-31 09:45:49 +0200

    added functionality to change color

--- src/app/examples/toolbar-example/toolbar-example.component.ts
+++ src/app/examples/toolbar-example/toolbar-example.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Color, ColorHelper } from '@kirbydesign/designsystem/helpers/color-helper';
+
 @Component({
   selector: 'kirby-toolbar-example',
   templateUrl: './toolbar-example.component.html',
   styleUrls: ['./toolbar-example.component.scss'],
 })
-export class ToolbarExampleComponent implements OnInit {
-  constructor() {}
+export class ToolbarExampleComponent {
+  color: Color;
+  colors: Color[] = ColorHelper.getMainColors();
 
-  ngOnInit() {}
+  changeColor(color: Color) {
+    this.color = color;
+    console.log('color: ', color.name);
+  }
 
   backButtonSelected(): void {
     alert('backbutton clicked!');
