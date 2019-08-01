47be86c6939c36e13626a9279a72b74d372e0b14
Alexander Hafstad <alexander@hafstad.dk>
2019-08-01 15:36:53 +0200

    fix(github): fix failing spec

--- src/app/shared/github/github.service.spec.ts
+++ src/app/shared/github/github.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
 import { GithubService } from './github.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('GithubService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    })
+  );
 
   it('should be created', () => {
     const service: GithubService = TestBed.get(GithubService);
