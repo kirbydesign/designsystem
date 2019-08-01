94a8fc1870b0be7275f6f17325f9ce6225b160c4
Alexander Hafstad <alexander@hafstad.dk>
2019-08-01 15:37:21 +0200

    fix(github): linting

--- src/app/shared/github/github.service.spec.ts
+++ src/app/shared/github/github.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { GithubService } from './github.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('GithubService', () => {
   beforeEach(() =>
