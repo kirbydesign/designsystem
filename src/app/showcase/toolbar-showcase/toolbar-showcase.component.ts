d08de5f3e4e56cff0da538cc430dd77809ed09c8
Peter Ring <peter.ring21@gmail.com>
2019-08-14 14:39:42 +0200

    added description for events on showcase component

--- src/app/showcase/toolbar-showcase/toolbar-showcase.component.ts
+++ src/app/showcase/toolbar-showcase/toolbar-showcase.component.ts
@@ -11,11 +11,11 @@ export class ToolbarShowcaseComponent implements OnInit {
       <kirby-toolbar title="TITLE"></kirby-toolbar>
       <!-- themeColor -->
       <kirby-toolbar themeColor="primary"></kirby-toolbar>
-      <!-- Size -->
-      <kirby-toolbar hideBackButton="false"></kirby-toolbar>
-      <!-- Secondary and end elements -->
-      <kirby-toolbar>
-        <kirby-icon secondary size="md"></kirby-icon>
+      <!-- back button -->
+      <kirby-toolbar hideBackButton="false" (back)="backButtonSelected()"></kirby-toolbar>
+      <!-- Middle and end elements -->
+      <kirby-toolbar (middleSelected)="foo()" (endSelected)="bar()">
+        <kirby-icon middle size="md"></kirby-icon>
         <kirby-icon end size="md" name="more"></kirby-icon>
       </kirby-toolbar>
     `;
