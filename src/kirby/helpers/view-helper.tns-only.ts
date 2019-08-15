646e2c5b03e8845691f7a984b4d28cd76402f35a
Martin Metaksov <mmmtrifork@gmail.com>
2019-08-15 09:21:11 +0200

    fixed typo

--- src/kirby/helpers/view-helper.tns-only.ts
+++ src/kirby/helpers/view-helper.tns-only.ts
@@ -2,9 +2,10 @@ import { View } from 'tns-core-modules/ui/content-view';
 
 export class ViewHelper {
   /**
-   * Used to safely invoke a function after a view is loaded.
-   * @param view - A view which needs to be loaded before invoking the function.
-   * @param func - A function which will be executed view is loaded.
+   * Used to safely invoke a function after a view is loaded. Prevents memory
+   * leaks and issues when interacting with a view that may not yet be loaded.
+   * @param view - A view which needs to be loaded before invoking a function.
+   * @param func - A function which will be executed once the view is loaded.
    */
   public static invokeOnViewLoaded(view: View, func: Function): void {
     if (view.isLoaded) {
