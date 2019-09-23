9079afbd62c6b7c195a656ad4c16273b67b5804d
Jakob Engelbrecht Olesen <jakob@basher.dk>
2019-09-23 12:39:02 +0200

    :sparkles: Add FAB sheet component

---
+++ src/kirby/components/fab-sheet/fab-sheet.component.spec.ts
@@ -0,0 +1,29 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockComponents } from 'ng-mocks';
+import * as ionic from '@ionic/angular';
+
+import { FabSheetComponent } from './fab-sheet.component';
+
+describe('FabSheetComponent', () => {
+  let component: FabSheetComponent;
+  let fixture: ComponentFixture<FabSheetComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        FabSheetComponent,
+        MockComponents(ionic.IonFab, ionic.IonFabButton, ionic.IonFabList),
+      ],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FabSheetComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
