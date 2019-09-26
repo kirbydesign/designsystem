9ad1fe3ca1055154e1c844974817140fbcb20eef
Jakob Engelbrecht Olesen <jakob@basher.dk>
2019-09-26 23:04:11 +0200

    :recycle: :pencil: Clean up Form Field stand-alone examples

--- src/app/examples/form-field-example/form-field-example.component.ts
+++ src/app/examples/form-field-example/form-field-example.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'kirby-form-field-example',
   templateUrl: './form-field-example.component.html',
   styleUrls: ['./form-field-example.component.scss'],
 })
-export class FormFieldExampleComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
-}
+export class FormFieldExampleComponent {}
