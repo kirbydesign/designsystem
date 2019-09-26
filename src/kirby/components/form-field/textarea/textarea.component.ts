49bc969b128682832213bd0660eccf2d38f2dfec
Jakob Engelbrecht Olesen <jakob@basher.dk>
2019-09-26 22:49:46 +0200

    :recycle: Refactor textarea component

--- src/kirby/components/form-field/textarea/textarea.component.ts
+++ src/kirby/components/form-field/textarea/textarea.component.ts
@@ -1,81 +1,23 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  EventEmitter,
-  forwardRef,
-  Input,
-  Output,
-} from '@angular/core';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
-
-export const CUSTOM_TEXTAREA_CONTROL_VALUE_ACCESSOR: any = {
-  provide: NG_VALUE_ACCESSOR,
-  useExisting: forwardRef(() => TextareaComponent),
-  multi: true,
-};
+import { ChangeDetectionStrategy, Component, Input, HostBinding } from '@angular/core';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [CUSTOM_TEXTAREA_CONTROL_VALUE_ACCESSOR],
-  selector: 'kirby-textarea',
+  // tslint:disable-next-line:component-selector
+  selector: 'textarea[kirby-textarea]',
   styleUrls: ['./textarea.component.scss'],
   templateUrl: './textarea.component.html',
 })
-export class TextareaComponent implements ControlValueAccessor {
-  textareaId = `kirby-textarea-${textareaIds++}`;
-  labelId = this.textareaId + '-label';
-
-  @Input() disabled: boolean = false;
-  @Input() label: string;
-  @Input() name = this.textareaId;
-  @Input() placeholder: string;
-  @Input() rows: number;
-  @Input() set value(v: any) {
-    if (v !== this._innerValue) {
-      this._innerValue = v;
-    }
-  }
-
-  get value(): any {
-    return this._innerValue || '';
-  }
-
-  @Output() blur = new EventEmitter<void>();
-  @Output() focus = new EventEmitter<void>();
-
-  private _propagateChange = (_: any) => {};
-  private _innerValue: any;
-
-  onInput(ev: Event): void {
-    const input = ev.target as HTMLInputElement | null;
-    if (input) {
-      //set changed value
-      this._innerValue = input.value || '';
-    }
-    // propagate value into form control using control value accessor interface
-    this._propagateChange(this._innerValue);
-  }
-
-  onBlur(): void {
-    this.blur.emit();
-  }
-
-  onFocus(): void {
-    this.focus.emit();
-  }
-
-  writeValue(obj: any): void {
-    this._innerValue = obj;
-  }
-  registerOnChange(fn: any): void {
-    this._propagateChange = fn;
-  }
-
-  registerOnTouched(fn: any): void {}
-
-  setDisabledState?(isDisabled: boolean): void {
-    this.disabled = isDisabled;
+export class TextareaComponent {
+  @Input() value: string;
+  @HostBinding('class.state-success')
+  _isSuccess: boolean;
+  @HostBinding('class.state-warning')
+  _isWarning: boolean;
+  @HostBinding('class.state-danger')
+  _isDanger: boolean;
+  @Input() set state(state: 'success' | 'warning' | 'danger') {
+    this._isSuccess = state === 'success';
+    this._isWarning = state === 'warning';
+    this._isDanger = state === 'danger';
   }
 }
-
-let textareaIds = 0;
