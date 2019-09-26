11791f73869540cf69313dde080ade3dd251c9cd
Jakob Engelbrecht Olesen <jakob@basher.dk>
2019-09-26 22:48:40 +0200

    :recycle: Refactor input component

--- src/kirby/components/form-field/input/input.component.ts
+++ src/kirby/components/form-field/input/input.component.ts
@@ -1,106 +1,32 @@
-import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
-import {
-  ChangeDetectionStrategy,
-  Component,
-  EventEmitter,
-  forwardRef,
-  Input,
-  Output,
-} from '@angular/core';
-
-export type InputType =
-  | 'date'
-  | 'email'
-  | 'number'
-  | 'password'
-  | 'search'
-  | 'tel'
-  | 'text'
-  | 'time'
-  | 'url';
-
-export type Inputmode =
-  | 'none'
-  | 'text'
-  | 'decimal'
-  | 'numeric'
-  | 'tel'
-  | 'search'
-  | 'email'
-  | 'url'
-  | undefined;
-
-export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
-  provide: NG_VALUE_ACCESSOR,
-  useExisting: forwardRef(() => InputComponent),
-  multi: true,
-};
+import { ChangeDetectionStrategy, Component, Input, HostBinding } from '@angular/core';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR],
-  selector: 'kirby-input',
+  // tslint:disable-next-line:component-selector
+  selector: 'input[kirby-input]',
   styleUrls: ['./input.component.scss'],
-  templateUrl: './input.component.html',
+  template: '',
 })
-export class InputComponent implements ControlValueAccessor {
-  inputId = `kirby-input-${inputIds++}`;
-  labelId = this.inputId + '-label';
-
-  @Input() autocomplete: 'on' | 'off' = 'off';
-  @Input() autocorrect: 'on' | 'off' = 'off';
-  @Input() disabled: boolean = false;
-  @Input() inputmode: Inputmode;
-  @Input() label: string;
-  @Input() name = this.inputId;
-  @Input() placeholder: string;
-  @Input() type: InputType = 'text';
-  @Input() set value(v: any) {
-    if (v !== this._innerValue) {
-      this._innerValue = v;
-    }
+export class InputComponent {
+  private static typeToInputmodeMap = {
+    number: 'decimal',
+    search: 'search',
+  };
+  @Input() set type(value: string) {
+    this._inputmode = InputComponent.typeToInputmodeMap[value];
   }
+  @HostBinding('attr.inputmode')
+  _inputmode: string;
 
-  get value(): any {
-    return this._innerValue || '';
-  }
-
-  @Output() blur = new EventEmitter<void>();
-  @Output() focus = new EventEmitter<void>();
-
-  private _propagateChange = (_: any) => {};
-  private _innerValue: any;
-
-  onInput(ev: Event): void {
-    const input = ev.target as HTMLInputElement | null;
-    if (input) {
-      //set changed value
-      this._innerValue = input.value || '';
-    }
-    // propagate value into form control using control value accessor interface
-    this._propagateChange(this._innerValue);
-  }
-
-  onBlur(): void {
-    this.blur.emit();
-  }
-
-  onFocus(): void {
-    this.focus.emit();
-  }
-
-  writeValue(obj: any): void {
-    this._innerValue = obj;
-  }
-  registerOnChange(fn: any): void {
-    this._propagateChange = fn;
-  }
-
-  registerOnTouched(fn: any): void {}
-
-  setDisabledState?(isDisabled: boolean): void {
-    this.disabled = isDisabled;
+  @HostBinding('class.state-success')
+  _isSuccess: boolean;
+  @HostBinding('class.state-warning')
+  _isWarning: boolean;
+  @HostBinding('class.state-danger')
+  _isDanger: boolean;
+  @Input() set state(state: 'success' | 'warning' | 'danger') {
+    this._isSuccess = state === 'success';
+    this._isWarning = state === 'warning';
+    this._isDanger = state === 'danger';
   }
 }
-
-let inputIds = 0;
