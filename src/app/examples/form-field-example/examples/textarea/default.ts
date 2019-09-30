8734e42f88c10056f50499e9c9803de6dabcc167
Jakob Engelbrecht Olesen <jakob@basher.dk>
2019-09-30 19:12:35 +0200

    :fire: Remove obsolete form-field-message examples

--- src/app/examples/form-field-example/examples/textarea/default.ts
+++ src/app/examples/form-field-example/examples/textarea/default.ts
@@ -4,7 +4,6 @@ const config = {
   selector: 'kirby-form-field-textarea-example',
   template: `<kirby-form-field>
   <textarea kirby-textarea placeholder="Default textarea with placeholder text"></textarea>
-  <kirby-form-field-message text="132/100" position="right"></kirby-form-field-message>
 </kirby-form-field>`,
 };
 
--- src/app/examples/form-field-example/examples/textarea/label.ts
+++ src/app/examples/form-field-example/examples/textarea/label.ts
@@ -4,7 +4,6 @@ const config = {
   selector: 'kirby-form-field-textarea-label-example',
   template: `<kirby-form-field label="Textarea with label">
   <textarea kirby-textarea></textarea>
-  <kirby-form-field-message text="132/100" position="right"></kirby-form-field-message>
 </kirby-form-field>`,
 };
 
