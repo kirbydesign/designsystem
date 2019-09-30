ec15684f71ba7791cf095f628940a51f915f1914
Jakob Engelbrecht Olesen <jakob@basher.dk>
2019-09-30 19:13:02 +0200

    💬Fix text

--- src/app/examples/form-field-example/examples/input/label-message.ts
+++ src/app/examples/form-field-example/examples/input/label-message.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 const config = {
   selector: 'kirby-form-field-input-label-message-example',
-  template: `<kirby-form-field label="Input with label and messages" message="This is additional info">
+  template: `<kirby-form-field label="Input with label and message" message="This is additional info">
   <input kirby-input />
 </kirby-form-field>`,
 };
