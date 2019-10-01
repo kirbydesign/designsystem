e8cfc79a129c36d26b077e3028ea7440b7984f3c
Peter Lindholm <peter@peterlindholm.com>
2019-10-01 15:01:46 +0200

    Added unit tests

--- src/kirby/components/alert/alert.component.spec.ts
+++ src/kirby/components/alert/alert.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { NavParams } from '@ionic/angular';
 
@@ -32,10 +33,57 @@ describe('AlertComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AlertComponent);
     component = fixture.componentInstance;
+    component.okBtnText = 'Test OK Button Text';
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('ok button', () => {
+    it('should render', () => {
+      const expected = 'Test OK Button Text';
+      const okButton = fixture.debugElement.query(By.css('.ok-button'));
+
+      expect(component.okBtnText).toEqual(expected);
+      expect(okButton.nativeElement.textContent).toEqual(expected);
+    });
+
+    it('should support isDestructive', () => {
+      component.okBtnIsDestructive = true;
+      fixture.detectChanges();
+      const okButton = fixture.debugElement.query(By.css('.ok-button'));
+
+      expect(okButton).toBeDefined();
+      expect(okButton.nativeElement.getAttribute('class')).toContain('destructive');
+    });
+
+    it('should default to not being destructive', () => {
+      const okButton = fixture.debugElement.query(By.css('.ok-button'));
+
+      expect(okButton).toBeDefined();
+      expect(okButton.nativeElement.getAttribute('class')).not.toContain('destructive');
+    });
+  });
+
+  describe('cancel button', () => {
+    it('should render', () => {
+      const expected = 'Test Cancel Button Text';
+      component.cancelBtnText = 'Test Cancel Button Text';
+      fixture.detectChanges();
+      const cancelButton = fixture.debugElement.query(By.css('.cancel-btn'));
+      expect(cancelButton.nativeElement.textContent).toEqual(expected);
+    });
+  });
+
+  describe('icon', () => {
+    it('should render', () => {
+      component.iconName = 'warning';
+      fixture.detectChanges();
+
+      const icon = fixture.debugElement.query(By.css('.icon-outline'));
+      expect(icon).toBeDefined();
+    });
   });
 });
