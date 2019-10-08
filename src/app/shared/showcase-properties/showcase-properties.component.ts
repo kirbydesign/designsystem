6b4a88d9de90b5a88c036adbeea1712805de4d02
Jakob Engelbrecht Olesen <jakob@basher.dk>
2019-10-09 00:42:14 +0200

    :recycle: Refactor page template showcase

--- src/app/shared/showcase-properties/showcase-properties.component.ts
+++ src/app/shared/showcase-properties/showcase-properties.component.ts
@@ -5,7 +5,19 @@ import { ShowcaseProperty } from './showcase-property';
 @Component({
   selector: 'kirby-showcase-properties',
   templateUrl: './showcase-properties.component.html',
+  styles: [':host { display: block; }'],
 })
 export class ShowcasePropertiesComponent {
   @Input() properties: ShowcaseProperty[];
+  @Input() columns: {
+    Name?: string;
+    Description?: string;
+    Type?: string;
+    Default?: string;
+  } = {
+    Name: 'Attribute',
+    Description: 'Description',
+    Type: 'Type',
+    Default: 'Default',
+  };
 }
