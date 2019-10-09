ab116517a86ea578b1af5d038a7e46c6dd2b2577
Jakob Engelbrecht Olesen <jakob@basher.dk>
2019-10-09 10:17:52 +0200

    :pencil: Update example with toast

--- src/app/examples/alert-example/alert-example.component.ts
+++ src/app/examples/alert-example/alert-example.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 
 import { ModalController } from '@kirbydesign/designsystem/modal';
 import { AlertConfig } from '@kirbydesign/designsystem/modal';
+import { ToastConfig, ToastController } from '@kirbydesign/designsystem';
 
 @Component({
   selector: 'kirby-alert-example',
   templateUrl: './alert-example.component.html',
 })
 export class AlertExampleComponent {
-  constructor(private modalController: ModalController) {}
+  constructor(private modalController: ModalController, private toastController: ToastController) {}
 
   showAlert() {
     const config: AlertConfig = {
@@ -17,7 +18,7 @@ export class AlertExampleComponent {
       okBtn: 'I agree',
       cancelBtn: 'Take me back',
     };
-    this.modalController.showAlert(config, this.onAlertClosed);
+    this.modalController.showAlert(config, this.onAlertClosed.bind(this));
   }
 
   showAlertWithIcon() {
@@ -28,7 +29,7 @@ export class AlertExampleComponent {
       cancelBtn: 'Take me back',
       icon: { name: 'warning', themeColor: 'warning' },
     };
-    this.modalController.showAlert(config, this.onAlertClosed);
+    this.modalController.showAlert(config, this.onAlertClosed.bind(this));
   }
 
   showAlertWithoutCancel() {
@@ -37,7 +38,7 @@ export class AlertExampleComponent {
       message: 'This is a slightly longer alert message text than what was previously here',
       okBtn: 'I agree',
     };
-    this.modalController.showAlert(config, this.onAlertClosed);
+    this.modalController.showAlert(config, this.onAlertClosed.bind(this));
   }
 
   showDestructiveAlert() {
@@ -47,10 +48,15 @@ export class AlertExampleComponent {
       cancelBtn: 'Get me out of here',
       okBtn: { text: 'Confirm', isDestructive: true },
     };
-    this.modalController.showAlert(config, this.onAlertClosed);
+    this.modalController.showAlert(config, this.onAlertClosed.bind(this));
   }
 
-  private onAlertClosed(selection?: boolean) {
-    console.log(`Alert selection: ${selection}`);
+  private onAlertClosed(result?: boolean) {
+    const config: ToastConfig = {
+      message: `Alert selection: ${result}`,
+      messageType: result ? 'success' : 'warning',
+      durationInMs: 1500,
+    };
+    this.toastController.showToast(config);
   }
 }
