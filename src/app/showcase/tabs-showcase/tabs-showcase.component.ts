187daf1e7778c9cb2c0ad6443dc8caa12866f335
Jakob Engelbrecht Olesen <jakob@basher.dk>
2019-10-11 19:05:53 +0200

    :pencil: Add selected-tab icon example

--- src/app/showcase/tabs-showcase/tabs-showcase.component.ts
+++ src/app/showcase/tabs-showcase/tabs-showcase.component.ts
@@ -47,7 +47,7 @@ const routes: Routes = [
   properties: ShowcaseProperty[] = [
     {
       name: 'routerLink',
-      description: `Should be a reference to the route path. Path needs to be a child of your "tabs" path otherwise use manually navigation using click event.`,
+      description: `Should be a reference to the route path hosting the contents of the tab. Path needs to be a child of your "tabs" path otherwise use manually navigation by handling the \`click\` event.`,
       defaultValue: '',
       inputValues: ['string'],
     },
