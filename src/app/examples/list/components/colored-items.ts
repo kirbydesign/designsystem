594bef8d46a284f13120fc9298ea7e37da2ce8a2
Alexander Hafstad <alexander@hafstad.dk>
2019-11-15 10:46:40 +0100

    doc(list): adjust examples with iphone (#763)
    * doc(list): adjust examples with iphone
    
    * fix(list): spec of list showcase
    
    * fix(list): safe-area on examples
    
    * fix(list): build
    
    * Update src/app/examples/examples.routes.ts
    
    Co-Authored-By: Jakob Engelbrecht <jakob@basher.dk>
    
    * Update src/app/examples/examples.routes.ts
    
    Co-Authored-By: Jakob Engelbrecht <jakob@basher.dk>
    
    * fix(list): adjust list showcase default list url
    
    * refactor(list-examples): adjust folder structure of examples

---
+++ src/app/examples/list/components/colored-items.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+import { BaseListComponent } from './base-list.component';
+
+export const ListColoredItemsExampleTemplate = `<kirby-list
+  [items]="items"
+  [getItemColor]="getItemColor"
+  (itemSelect)="onItemSelect($event)"
+  showDivider="true"
+>
+  <kirby-list-item
+    *kirbyListItem="let item"
+    [item]="item"
+    [title]="item.title"
+    [amount]="item.amount"
+  ></kirby-list-item>
+</kirby-list>`;
+
+@Component({
+  // tslint:disable-next-line
+  selector: 'list-colored-items-example',
+  template: `
+    <kirby-page title="List colored items">
+      <kirby-page-content>
+        ${ListColoredItemsExampleTemplate}
+      </kirby-page-content>
+    </kirby-page>
+  `,
+})
+export class ListColoredItemsExampleComponent extends BaseListComponent {
+  getItemColor = (item) => item.color;
+}
