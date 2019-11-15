c09999fd368833f3238abe1d6b85058aaeb5fc9d
Alexander Hafstad <alexander@hafstad.dk>
2019-11-15 09:38:42 +0100

    Update src/app/examples/examples.routes.ts
    Co-Authored-By: Jakob Engelbrecht <jakob@basher.dk>

--- src/app/examples/examples.routes.ts
+++ src/app/examples/examples.routes.ts
@@ -140,7 +140,7 @@ export const routes: Routes = [
     component: ListExampleComponent,
     children: [
       {
-        path: 'default',
+        path: '',
         component: ListDefaultExampleComponent,
       },
       {
