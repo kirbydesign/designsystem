b1b0aded2534c6ac833c31349b9c4db28212917c
Jakob Engelbrecht Olesen <jakob@basher.dk>
2019-12-16 14:19:42 +0100

    :pencil: Improve page showcase

--- src/app/examples/page-example/simple/page-simple-example.component.ts
+++ src/app/examples/page-example/simple/page-simple-example.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { BasePageExampleComponent } from '../base-page-example.component';
 
 const config = {
-  template: `<kirby-page title="Simple Page with very very very very looooooooong title!!!" defaultBackHref="/" headerMaxLines="1">
+  template: `<kirby-page title="Simple Page" defaultBackHref="/">
   <kirby-page-content>
     <div [innerHTML]="content"></div>
   </kirby-page-content>
@@ -16,8 +16,4 @@ export class PageSimpleExampleComponent extends BasePageExampleComponent {
   static readonly template = config.template
     .replace(' defaultBackHref="/"', '')
     .replace('<div [innerHTML]="content"></div>', '...');
-
-  constructor() {
-    super();
-  }
 }
