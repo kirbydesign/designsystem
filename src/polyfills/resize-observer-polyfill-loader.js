0f98b8679e4cdd6fdc3365a4e44cf4ba7f6ab910
Jakob Engelbrecht Olesen <jakob@basher.dk>
2019-12-17 17:26:59 +0100

    :bug: Remove check for screen size

--- src/polyfills/resize-observer-polyfill-loader.js
+++ src/polyfills/resize-observer-polyfill-loader.js
@@ -1,16 +1,14 @@
-(function () {
-    // A technique for loading polyfills only when needed. Details here:
-    // https://philipwalton.com/articles/loading-polyfills-only-when-needed/
-    if (polyfillNeeded()) {
-      // Browsers at larger breakpoints that don't support all
-      // required features must load the polyfills first:
-      var script = document.createElement('script');
-      script.src = 'kirby/polyfills/resize-observer-polyfill.min.js';
-      document.head.appendChild(script);
-    }
+(function() {
+  // A technique for loading polyfills only when needed. Details here:
+  // https://philipwalton.com/articles/loading-polyfills-only-when-needed/
+  if (polyfillNeeded()) {
+    // Browsers that doesn't support ResizeObserver must load the polyfill first:
+    var script = document.createElement('script');
+    script.src = 'kirby/polyfills/resize-observer-polyfill.min.js';
+    document.head.appendChild(script);
+  }
 
-    function polyfillNeeded() {
-        return (typeof ResizeObserver === 'undefined') &&
-               (window.screen && window.screen.width >= 576);
-    }
-  })();
+  function polyfillNeeded() {
+    return typeof ResizeObserver === 'undefined';
+  }
+})();
