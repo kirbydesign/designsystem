56ae7c9c4c66676fd2a7b318366eee2677294604
Jakob Engelbrecht Olesen <jakob@basher.dk>
2020-01-10 13:15:18 +0100

    :art: Return tokens as strings

--- src/kirby/helpers/design-token-helper.ts
+++ src/kirby/helpers/design-token-helper.ts
@@ -19,24 +19,20 @@ export class DesignTokenHelper {
     };
   }
 
-  public static size(key: string, includeUnit = true): number | string {
-    const value = DesignTokenHelper.designTokens.global['$sizes'][key] as number;
-    return includeUnit ? value + 'px' : value;
+  public static size(key: string): string {
+    return DesignTokenHelper.designTokens.global['$sizes'][key];
   }
 
-  public static fontSize(key: string, includeUnit = true): number | string {
-    const value = DesignTokenHelper.designTokens.global['$font-sizes'][key] as number;
-    return includeUnit ? value + 'px' : value;
+  public static fontSize(key: string): string {
+    return DesignTokenHelper.designTokens.global['$font-sizes'][key];
   }
 
-  public static borderRadius(includeUnit = true): number | string {
-    const value = DesignTokenHelper.designTokens.global['$border-radius'] as number;
-    return includeUnit ? value + 'px' : value;
+  public static borderRadius(): string {
+    return DesignTokenHelper.designTokens.global['$border-radius'];
   }
 
-  public static borderRadiusRound(includeUnit = true): number | string {
-    const value = DesignTokenHelper.designTokens.global['$border-radius-round'] as number;
-    return includeUnit ? value + 'px' : value;
+  public static borderRadiusRound(): string {
+    return DesignTokenHelper.designTokens.global['$border-radius-round'];
   }
 }
 
