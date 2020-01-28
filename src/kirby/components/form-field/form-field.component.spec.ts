2b80770b253baced7880ed84c89d43c4187fdfaa
Jonas Jensen <jonas.jensen@broelstaerk.dk>
2020-01-28 10:13:02 +0100

    :white_check_mark: Added tests and example with line break.

--- src/kirby/components/form-field/form-field.component.spec.ts
+++ src/kirby/components/form-field/form-field.component.spec.ts
@@ -1,25 +1,60 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { SpectatorHost, createHostFactory } from '@ngneat/spectator';
 
+import { InputCounterComponent } from './input-counter/input-counter.component';
 import { FormFieldComponent } from './form-field.component';
 import { FormFieldMessageComponent } from './form-field-message/form-field-message.component';
+import { InputComponent } from './input/input.component';
+import { TextareaComponent } from './textarea/textarea.component';
 
 describe('FormFieldComponent', () => {
-  let component: FormFieldComponent;
-  let fixture: ComponentFixture<FormFieldComponent>;
+  let spectator: SpectatorHost<FormFieldComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [FormFieldComponent, FormFieldMessageComponent],
-    }).compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FormFieldComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  const createHost = createHostFactory({
+    component: FormFieldComponent,
+    declarations: [
+      FormFieldComponent,
+      FormFieldMessageComponent,
+      InputComponent,
+      TextareaComponent,
+      InputCounterComponent,
+    ],
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    spectator = createHost(
+      '<kirby-form-field><input kirby-input placeholder="Default input with placeholder text"/></kirby-form-field>'
+    );
+    expect(spectator.component).toBeTruthy();
+  });
+
+  describe('and having a message', () => {
+    describe('when having no counter', () => {
+      it('should render the message with the correct flex', () => {
+        spectator = createHost(
+          `<kirby-form-field label="Input with label and message" message="This is additional info that will be shown below the input">
+            <input kirby-input />
+          </kirby-form-field>`
+        );
+        const messageElement = spectator.element.getElementsByTagName(
+          'kirby-form-field-message'
+        )[0];
+        expect(messageElement).toHaveComputedStyle({ 'flex-basis': '100%' });
+      });
+    });
+
+    describe('and having a counter', () => {
+      it('should render the message with the correct flex', () => {
+        spectator = createHost(
+          `<kirby-form-field label="Input with label and message" message="This is additional info that will be shown below the input">
+            <input kirby-input #message/>
+            <kirby-input-counter [listenTo]="message"></kirby-input-counter>
+          </kirby-form-field>`
+        );
+        const messageElement = spectator.element.getElementsByTagName(
+          'kirby-form-field-message'
+        )[0];
+        expect(messageElement).toHaveComputedStyle({ 'flex-basis': '75%' });
+      });
+    });
   });
 });
