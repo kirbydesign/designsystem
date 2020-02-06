12bfdc5a38160e72d997f1782678a148dd8f3d64
Søren Fruergaard Sørensen <soren.fruergaard@gmail.com>
2020-02-06 14:00:14 +0100

    Fix for issue 767

--- src/app/examples/form-field-example/examples/input/counter.ts
+++ src/app/examples/form-field-example/examples/input/counter.ts
@@ -7,6 +7,11 @@ const config = {
   <kirby-input-counter [listenTo]="tweet"></kirby-input-counter>
 </kirby-form-field>
 
+<kirby-form-field style="width:120px">
+  <input kirby-input placeholder="Small width" #small_width maxlength="20" />
+  <kirby-input-counter [listenTo]="small_width"></kirby-input-counter>
+</kirby-form-field>
+
 <kirby-form-field>
   <input kirby-input value="Character counter with prefilled value" #prefilled maxlength="50" />
   <kirby-input-counter [listenTo]="prefilled"></kirby-input-counter>
@@ -15,6 +20,11 @@ const config = {
 <kirby-form-field message="Character counter with message and no maxlength">
   <input kirby-input #message />
   <kirby-input-counter [listenTo]="message"></kirby-input-counter>
+</kirby-form-field>
+
+<kirby-form-field message="Character counter with maxlength and long message **** Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation" >
+  <input kirby-input #longmessage maxlength="150"/>
+  <kirby-input-counter [listenTo]="longmessage"></kirby-input-counter>
 </kirby-form-field>`,
 };
 
