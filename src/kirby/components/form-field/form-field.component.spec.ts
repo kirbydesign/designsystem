b76ec4e94b9ca01e869494b74bb18b0daab94436
Jakob Engelbrecht Olesen <jakob@basher.dk>
2020-02-11 08:39:11 +0100

    :truck: Move input style tests to input spec

--- src/kirby/components/form-field/form-field.component.spec.ts
+++ src/kirby/components/form-field/form-field.component.spec.ts
@@ -5,13 +5,9 @@ import { FormFieldComponent } from './form-field.component';
 import { FormFieldMessageComponent } from './form-field-message/form-field-message.component';
 import { InputComponent } from './input/input.component';
 import { TextareaComponent } from './textarea/textarea.component';
-import { DesignTokenHelper } from '../../helpers/design-token-helper';
-
-const size = DesignTokenHelper.size;
 
 describe('FormFieldComponent', () => {
   let spectator: SpectatorHost<FormFieldComponent>;
-  let element: HTMLElement;
 
   const createHost = createHostFactory({
     component: FormFieldComponent,
@@ -24,34 +20,8 @@ describe('FormFieldComponent', () => {
     ],
   });
 
-  beforeEach(() => {
-    spectator = createHost(
-      '<kirby-form-field><input kirby-input placeholder="Default input with placeholder text"/></kirby-form-field>'
-    );
-    element = spectator.element as HTMLElement;
-  });
-
   it('should create', () => {
+    spectator = createHost('<kirby-form-field><input kirby-input/></kirby-form-field>');
     expect(spectator.component).toBeTruthy();
-  });
-
-  describe('when containing an input element', () => {
-    let inputElement: HTMLInputElement;
-
-    beforeEach(() => {
-      inputElement = element.getElementsByTagName('input')[0] as HTMLInputElement;
-    });
-
-    it('should render with correct height', () => {
-      expect(inputElement).toHaveComputedStyle({ height: size('xxxl') });
-    });
-
-    it('should render with correct box-sizing', () => {
-      expect(inputElement).toHaveComputedStyle({ 'box-sizing': 'border-box' });
-    });
-
-    it('should render with correct border-width', () => {
-      expect(inputElement).toHaveComputedStyle({ 'border-width': '1px' });
-    });
   });
 });
