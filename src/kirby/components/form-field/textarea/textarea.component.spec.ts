c87e3f2f14dc8a758262b033302bd92a54f375e0
Jakob Engelbrecht Olesen <jakob@basher.dk>
2020-02-11 15:42:32 +0100

    :white_check_mark: Add textarea tests

--- src/kirby/components/form-field/textarea/textarea.component.spec.ts
+++ src/kirby/components/form-field/textarea/textarea.component.spec.ts
@@ -1,23 +1,80 @@
-import { Spectator, createComponentFactory } from '@ngneat/spectator';
+import { SpectatorHost, createHostFactory } from '@ngneat/spectator';
 
+import { DesignTokenHelper } from '../../../helpers/design-token-helper';
 import { TextareaComponent } from './textarea.component';
 
-describe('TextareaComponent', () => {
-  let spectator: Spectator<TextareaComponent>;
-  let component: TextareaComponent;
+const getColor = DesignTokenHelper.getColor;
+const size = DesignTokenHelper.size;
 
-  const createComponent = createComponentFactory({
+fdescribe('TextareaComponent', () => {
+  let spectator: SpectatorHost<TextareaComponent>;
+  let element: HTMLTextAreaElement;
+
+  const createHost = createHostFactory({
     component: TextareaComponent,
     declarations: [TextareaComponent],
   });
 
   beforeEach(() => {
-    spectator = createComponent({});
-    spectator.detectChanges();
-    component = spectator.component;
+    spectator = createHost('<textarea kirby-textarea></textarea>');
+    element = spectator.element as HTMLTextAreaElement;
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(spectator.component).toBeTruthy();
+  });
+
+  it('should render with correct color', () => {
+    expect(element).toHaveComputedStyle({ color: getColor('black') });
+  });
+
+  it('should render with correct box-sizing', () => {
+    expect(element).toHaveComputedStyle({ 'box-sizing': 'border-box' });
+  });
+
+  it('should render without border', () => {
+    expect(element).toHaveComputedStyle({
+      'border-width': '0px',
+      'border-style': 'none',
+    });
+  });
+
+  it('should render without outline', () => {
+    expect(element).toHaveComputedStyle({
+      'outline-width': '0px',
+      'outline-style': 'none',
+    });
+  });
+
+  it('should render with correct border-radius', () => {
+    const expected = DesignTokenHelper.borderRadius();
+    expect(element).toHaveComputedStyle({ 'border-radius': expected });
+  });
+
+  describe('when hasError', () => {
+    beforeEach(() => {
+      spectator.component.hasError = true;
+      spectator.detectChanges();
+    });
+
+    it('should render with correct border', () => {
+      expect(element).toHaveComputedStyle({
+        'border-width': '1px',
+        'border-style': 'solid',
+        'border-color': getColor('danger'),
+      });
+    });
+  });
+
+  describe('when disabled', () => {
+    beforeEach(() => {
+      element.disabled = true;
+    });
+
+    it('should render with correct background-color', () => {
+      expect(element).toHaveComputedStyle({
+        'background-color': getColor('light', 'tint'),
+      });
+    });
   });
 });
