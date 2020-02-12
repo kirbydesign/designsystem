29a39ec1de4fa79e4eb3d3cdb2ad7b1448a065f2
Jakob Engelbrecht Olesen <jakob@basher.dk>
2020-02-12 17:03:41 +0100

    :white_check_mark: Add tests

--- src/kirby/components/form-field/input-counter/input-counter.component.spec.ts
+++ src/kirby/components/form-field/input-counter/input-counter.component.spec.ts
@@ -22,4 +22,64 @@ describe('InputCounterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('when configured with no length', () => {
+    it('should not have any text', () => {
+      expect(component.text).toBeUndefined();
+    });
+
+    it('should render counter correctly', () => {
+      expect((fixture.nativeElement as Element).textContent).toEqual('');
+    });
+  });
+
+  describe('when configured with length and no maxlength', () => {
+    const expectedText = '10';
+    beforeEach(() => {
+      component.length = 10;
+      fixture.detectChanges();
+    });
+
+    it('should have correct text', () => {
+      expect(component.text).toEqual(expectedText);
+    });
+
+    it('should render text correctly', () => {
+      expect((fixture.nativeElement as Element).textContent).toEqual(expectedText);
+    });
+  });
+
+  describe('when configured with length and maxlength = 0', () => {
+    const expectedText = '10';
+    beforeEach(() => {
+      component.length = 10;
+      component.maxlength = 0;
+      fixture.detectChanges();
+    });
+
+    it('should have correct text', () => {
+      expect(component.text).toEqual(expectedText);
+    });
+
+    it('should render text correctly', () => {
+      expect((fixture.nativeElement as Element).textContent).toEqual(expectedText);
+    });
+  });
+
+  describe('when configured with length and maxlength', () => {
+    const expectedText = '10/20';
+    beforeEach(() => {
+      component.length = 10;
+      component.maxlength = 20;
+      fixture.detectChanges();
+    });
+
+    it('should have correct text', () => {
+      expect(component.text).toEqual(expectedText);
+    });
+
+    it('should render text correctly', () => {
+      expect((fixture.nativeElement as Element).textContent).toEqual(expectedText);
+    });
+  });
 });
