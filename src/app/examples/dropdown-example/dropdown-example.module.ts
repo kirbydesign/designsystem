029d0efb7815e2d2e2ae69f10231d8b3193bbb7c
Jakob Engelbrecht Olesen <jakob@basher.dk>
2020-02-27 13:18:35 +0100

    :memo: Add NgForms example

--- src/app/examples/dropdown-example/dropdown-example.module.ts
+++ src/app/examples/dropdown-example/dropdown-example.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { KirbyModule } from '@kirbydesign/designsystem';
 import { DropdownExampleDefaultComponent } from './examples/default';
@@ -9,6 +10,7 @@ import { DropdownExampleExpandComponent } from './examples/expand';
 import { DropdownExampleAttentionLevelComponent } from './examples/attention-level';
 import { DropdownExampleItemSelectComponent } from './examples/item-select';
 import { DropdownExampleCustomItemTemplateComponent } from './examples/custom-item-template';
+import { DropdownExampleNgFormsComponent } from './examples/ng-forms';
 
 const COMPONENT_DECLARATIONS = [
   DropdownExampleDefaultComponent,
@@ -18,10 +20,11 @@ const COMPONENT_DECLARATIONS = [
   DropdownExampleAttentionLevelComponent,
   DropdownExampleItemSelectComponent,
   DropdownExampleCustomItemTemplateComponent,
+  DropdownExampleNgFormsComponent,
 ];
 
 @NgModule({
-  imports: [CommonModule, KirbyModule],
+  imports: [CommonModule, KirbyModule, ReactiveFormsModule],
   declarations: COMPONENT_DECLARATIONS,
   exports: COMPONENT_DECLARATIONS,
 })
