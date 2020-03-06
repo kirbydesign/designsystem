cfd64d7ac4c91bfda53726e189c058ed5f66b6df
Søren Fruergaard Sørensen <soren.fruergaard@gmail.com>
2020-03-06 15:10:44 +0100

    added test for ReorderShowcaseComponent

--- src/app/showcase/reorder-showcase/reorder-showcase.component.spec.ts
+++ src/app/showcase/reorder-showcase/reorder-showcase.component.spec.ts
@@ -1,28 +1,28 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-// import { RouterTestingModule } from '@angular/router/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
-// import { ExamplesModule } from '../../examples/examples.module';
-// import { CodeViewerComponent } from '../../shared/code-viewer/code-viewer.component';
-// import { ListNoShapeShowcaseComponent } from './list-no-shape-showcase.component';
+import { ExamplesModule } from '../../examples/examples.module';
+import { CodeViewerComponent } from '../../shared/code-viewer/code-viewer.component';
+import { ReorderShowcaseComponent } from './reorder-showcase.component';
 
-// describe('ListNoShapeShowcaseComponent', () => {
-//   let component: ListNoShapeShowcaseComponent;
-//   let fixture: ComponentFixture<ListNoShapeShowcaseComponent>;
+describe('ReorderShowcaseComponent', () => {
+  let component: ReorderShowcaseComponent;
+  let fixture: ComponentFixture<ReorderShowcaseComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       imports: [ExamplesModule, RouterTestingModule],
-//       declarations: [ListNoShapeShowcaseComponent, CodeViewerComponent],
-//     }).compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [ExamplesModule, RouterTestingModule],
+      declarations: [ReorderShowcaseComponent, CodeViewerComponent],
+    }).compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(ListNoShapeShowcaseComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ReorderShowcaseComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
