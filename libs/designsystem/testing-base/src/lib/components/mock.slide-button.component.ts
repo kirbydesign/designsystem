a5f2cbeaa3de64ef1308421f36333be2627acc93
Jakob Engelbrecht Olesen <jakob@basher.dk>
2020-03-12 09:25:09 +0100

    :fire: Remove obsolete base class

--- libs/designsystem/testing-base/src/lib/components/mock.slide-button.component.ts
+++ libs/designsystem/testing-base/src/lib/components/mock.slide-button.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 // #region AUTO-GENERATED - PLEASE DON'T EDIT CONTENT WITHIN!
 @Component({
   selector: 'kirby-slide-button',
   template: '<ng-content></ng-content>',
 })
-export class MockSlideButtonComponent {}
+export class MockSlideButtonComponent {
+  @Input() text: string;
+  @Input() expand: 'block';
+  @Output() slideDone = new EventEmitter();
+  @Output() slidingPercentageChanged = new EventEmitter<number>();
+}
 
 // #endregion
