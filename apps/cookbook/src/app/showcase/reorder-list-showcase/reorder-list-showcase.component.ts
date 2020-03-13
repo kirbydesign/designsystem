1ab274e4706d1e2a1b11609246a6a4c89b087951
Jakob Engelbrecht Olesen <jakob@basher.dk>
2020-03-13 14:14:47 +0100

    :lipstick: :memo: Improve reorder-list example

--- apps/cookbook/src/app/showcase/reorder-list-showcase/reorder-list-showcase.component.ts
+++ apps/cookbook/src/app/showcase/reorder-list-showcase/reorder-list-showcase.component.ts
@@ -13,8 +13,6 @@ export class ReorderListShowcaseComponent {
   exampleHtml: string = require('!raw-loader!../../examples/reorder-list/reorder-list-example.component.html')
     .default;
 
-  remarks: string = 'The component only works correctly, when it is used inside ion-content.';
-
   properties: ShowcaseProperty[] = [
     {
       name: 'items',
