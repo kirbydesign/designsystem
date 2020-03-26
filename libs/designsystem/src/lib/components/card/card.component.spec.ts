b8d7f6bd5df1fa75d3f0dc90ad31c579b421a955
Jakob Engelbrecht Olesen <jakob@basher.dk>
2020-03-26 10:02:01 +0100

    ✅ Nested Ionic component when mode attribute set

--- libs/designsystem/src/lib/components/card/card.component.spec.ts
+++ libs/designsystem/src/lib/components/card/card.component.spec.ts
@@ -1,24 +1,58 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { SpectatorHost, createHostFactory } from '@ngneat/spectator';
+import { IonicModule } from '@ionic/angular';
 
+import { TestHelper } from '../../testing/test-helper';
 import { CardComponent } from './card.component';
 
 describe('CardComponent', () => {
-  let component: CardComponent;
-  let fixture: ComponentFixture<CardComponent>;
+  let spectator: SpectatorHost<CardComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [CardComponent],
-    }).compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  const createHost = createHostFactory({
+    component: CardComponent,
+    declarations: [CardComponent],
+    imports: [IonicModule.forRoot({ _testing: true })],
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('by default', () => {
+    beforeEach(() => {
+      spectator = createHost('<kirby-card></kirby-card>');
+    });
+
+    it('should create', () => {
+      expect(spectator.component).toBeTruthy();
+    });
+  });
+
+  describe('with mode attribute', () => {
+    beforeEach(() => {
+      spectator = createHost('<kirby-card mode="flat"></kirby-card>');
+    });
+
+    it('should create', () => {
+      expect(spectator.component).toBeTruthy();
+    });
+  });
+
+  describe('with mode attribute and nested Ionic component', () => {
+    beforeEach(() => {
+      spectator = createHost(
+        `<kirby-card mode="flat">
+           <ion-badge>Test</ion-badge>
+         </kirby-card>`
+      );
+    });
+
+    it('should create', () => {
+      expect(spectator.component).toBeTruthy();
+    });
+
+    it('should create nested Ionic component with default Ionic mode', async () => {
+      const document = spectator.query<HTMLElement>('html', { root: true });
+      const ionicGlobalMode = document.getAttribute('mode');
+      const ionBadgeElement = spectator.query<HTMLElement>('ion-badge');
+      expect(ionBadgeElement).toBeTruthy();
+      await TestHelper.whenHydrated(ionBadgeElement);
+      expect(ionBadgeElement).toHaveClass('hydrated', ionicGlobalMode);
+    });
   });
 });
