435091634325fc75a26dbd5ed42a91840fc81428
Jakob Engelbrecht Olesen <jakob@basher.dk>
2020-04-24 15:38:53 +0200

    ✅ Add tests for modal on small screens

--- libs/designsystem/src/lib/components/modal/services/modal.helper.spec.ts
+++ libs/designsystem/src/lib/components/modal/services/modal.helper.spec.ts
@@ -6,6 +6,10 @@ import { Overlay } from './modal.interfaces';
 
 describe('ModalHelper', () => {
   let modalHelper: ModalHelper;
+  const backdropOpacity = '0.4';
+  const ionicTransparentBackdropOpacity = '0.01';
+  const smallScreenWidth = '375px';
+  const smallScreenHeight = '667px';
 
   const spectator = createService({
     service: ModalHelper,
@@ -36,11 +40,11 @@ describe('ModalHelper', () => {
       await overlay.dismiss();
     });
 
-    it('should have correct backdrop style', () => {
+    it('modal should have correct backdrop style', () => {
       expect(backdrop).toHaveComputedStyle({ opacity: backdropOpacity });
     });
 
-    it('should have correct backdrop style when opened on presenting element', async () => {
+    it('modal should have correct backdrop style when opened on presenting element', async () => {
       await overlay.dismiss();
       modalHelper.registerPresentingElement(window.document.body);
       overlay = await modalHelper.showModalWindow({
@@ -54,7 +58,7 @@ describe('ModalHelper', () => {
       expect(backdrop).toHaveComputedStyle({ opacity: backdropOpacity });
     });
 
-    it('should have correct backdrop style when opened on another modal', async () => {
+    it('modal should have no visible backdrop when opened on another modal', async () => {
       const secondOverlay = await modalHelper.showModalWindow({
         title: 'Modal 2',
         component: undefined,
@@ -65,6 +69,107 @@ describe('ModalHelper', () => {
       expect(secondBackdrop).toBeTruthy();
       expect(secondBackdrop).toHaveComputedStyle({ opacity: '0' });
       await secondOverlay.dismiss();
+    });
+
+    it('drawer should have no visible backdrop when opened on another modal', async () => {
+      const secondOverlay = await modalHelper.showModalWindow({
+        title: 'Modal 2',
+        component: undefined,
+        flavor: 'drawer',
+      });
+      const secondIonModal = await ionModalController.getTop();
+      expect(secondIonModal).toBeTruthy();
+      const secondBackdrop = secondIonModal.querySelector('ion-backdrop');
+      expect(secondBackdrop).toBeTruthy();
+      expect(secondBackdrop).toHaveComputedStyle({ opacity: '0' });
+      await secondOverlay.dismiss();
+    });
+  });
+
+  describe('on small screens', () => {
+    describe('showModal', () => {
+      let overlay: Overlay;
+      let ionModal: HTMLIonModalElement;
+      let backdrop: HTMLIonBackdropElement;
+      let ionModalController: IonicModalController;
+
+      beforeAll((done) => {
+        if (window.frameElement) {
+          console.log(
+            `Set test window width and height to: ${smallScreenWidth} x ${smallScreenHeight}`
+          );
+          (window.frameElement as HTMLIFrameElement).style.width = smallScreenWidth;
+          (window.frameElement as HTMLIFrameElement).style.height = smallScreenHeight;
+          // Ensure window has resized before executing:
+          setTimeout(done, 1);
+        }
+      });
+
+      afterAll(() => {
+        if (window.frameElement) {
+          console.log('Resetting test window width');
+          (window.frameElement as HTMLIFrameElement).style.width = null;
+          (window.frameElement as HTMLIFrameElement).style.height = null;
+        }
+      });
+
+      beforeEach(async () => {
+        ionModalController = spectator.inject(IonicModalController);
+        overlay = await modalHelper.showModalWindow({ title: 'Modal 1', component: undefined });
+        ionModal = await ionModalController.getTop();
+        expect(ionModal).toBeTruthy();
+        backdrop = ionModal.querySelector('ion-backdrop');
+        expect(backdrop).toBeTruthy();
+      });
+
+      afterEach(async () => {
+        await overlay.dismiss();
+      });
+
+      it('modal should have correct backdrop style', () => {
+        expect(backdrop).toHaveComputedStyle({ opacity: ionicTransparentBackdropOpacity });
+      });
+
+      it('modal should have correct backdrop style when opened on presenting element', async () => {
+        await overlay.dismiss();
+        modalHelper.registerPresentingElement(window.document.body);
+        overlay = await modalHelper.showModalWindow({
+          title: 'Modal On Presenting Element',
+          component: undefined,
+        });
+        ionModal = await ionModalController.getTop();
+        expect(ionModal).toBeTruthy();
+        backdrop = ionModal.querySelector('ion-backdrop');
+        expect(backdrop).toBeTruthy();
+        expect(backdrop).toHaveComputedStyle({ opacity: ionicTransparentBackdropOpacity });
+      });
+
+      it('modal should have no visible backdrop when opened on another modal', async () => {
+        const secondOverlay = await modalHelper.showModalWindow({
+          title: 'Modal 2',
+          component: undefined,
+        });
+        const secondIonModal = await ionModalController.getTop();
+        expect(secondIonModal).toBeTruthy();
+        const secondBackdrop = secondIonModal.querySelector('ion-backdrop');
+        expect(secondBackdrop).toBeTruthy();
+        expect(secondBackdrop).toHaveComputedStyle({ opacity: ionicTransparentBackdropOpacity });
+        await secondOverlay.dismiss();
+      });
+
+      it('drawer should have correct backdrop style when opened on another modal', async () => {
+        const secondOverlay = await modalHelper.showModalWindow({
+          title: 'Modal 2',
+          component: undefined,
+          flavor: 'drawer',
+        });
+        const secondIonModal = await ionModalController.getTop();
+        expect(secondIonModal).toBeTruthy();
+        const secondBackdrop = secondIonModal.querySelector('ion-backdrop');
+        expect(secondBackdrop).toBeTruthy();
+        expect(secondBackdrop).toHaveComputedStyle({ opacity: backdropOpacity });
+        await secondOverlay.dismiss();
+      });
     });
   });
 });
