1f8d1e1f361e47cdc9f9314c20fd29f756506b89
Jakob Engelbrecht Olesen <jakob@basher.dk>
2020-04-24 07:52:34 +0200

    Revert ":fire: Remove obsolete imports"
    This reverts commit 8c1ffb473fab8cea37409f6c1863c6aa1bf326a5.

--- apps/cookbook/src/app/changelog/changelog.module.ts
+++ apps/cookbook/src/app/changelog/changelog.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
 
 import { KirbyModule } from '@kirbydesign/designsystem';
 import { ChangelogComponent } from './changelog.component';
@@ -14,6 +15,7 @@ import { ChangelogComponent } from './changelog.component';
         component: ChangelogComponent,
       },
     ]),
+    IonicModule.forRoot(),
     KirbyModule,
   ],
   declarations: [ChangelogComponent],
--- apps/cookbook/src/app/component-status/component-status.module.ts
+++ apps/cookbook/src/app/component-status/component-status.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
 
 import { KirbyModule } from '@kirbydesign/designsystem';
 import { ComponentStatusComponent } from './component-status.component';
@@ -14,6 +15,7 @@ import { ComponentStatusComponent } from './component-status.component';
         component: ComponentStatusComponent,
       },
     ]),
+    IonicModule.forRoot(),
     KirbyModule,
   ],
   declarations: [ComponentStatusComponent],
