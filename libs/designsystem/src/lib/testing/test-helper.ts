51db0c50937e8c73839a8ec2f527532f7cfa7625
Jakob Engelbrecht Olesen <jakob@basher.dk>
2020-04-25 08:11:45 +0200

    ✅ Add TestHelper.resizeTestWindow

--- libs/designsystem/src/lib/testing/test-helper.ts
+++ libs/designsystem/src/lib/testing/test-helper.ts
@@ -29,4 +29,48 @@ export class TestHelper {
   public static getCssProperty(element: Element, propertyName: string) {
     return window.getComputedStyle(element).getPropertyValue(propertyName);
   }
+
+  public static resizeTestWindow(size: { width?: string; height?: string }): Promise<void> {
+    return new Promise((resolve, reject) => {
+      if (!window.frameElement) {
+        console.warn('window.frameElement is undefined. Cannot resize test window.');
+        reject();
+      } else {
+        const query = [];
+        if (size.width) {
+          query.push(`(max-width: ${size.width})`);
+        }
+        if (size.height) {
+          query.push(`(max-height: ${size.height})`);
+        }
+        console.log(`Set test window size to: ${JSON.stringify(size)}`);
+        (window.frameElement as HTMLIFrameElement).style.width = size.width;
+        (window.frameElement as HTMLIFrameElement).style.height = size.height;
+        const mediaQuery = window.matchMedia(query.join(' and '));
+        if (mediaQuery.matches) {
+          resolve();
+        } else {
+          // Ensure window has resized before executing:
+          mediaQuery.onchange = (ev) => {
+            if (ev.matches) {
+              // Stop listening for changes:
+              mediaQuery.onchange = null;
+              resolve();
+            }
+          };
+          console.log(`Awaiting media query to match: to: ${mediaQuery.media}`);
+        }
+      }
+    });
+  }
+
+  public static resetTestWindow(): void {
+    if (!window.frameElement) {
+      console.warn('window.frameElement is undefined. Cannot resize test window.');
+      return;
+    }
+    console.log('Resetting test window width');
+    (window.frameElement as HTMLIFrameElement).style.width = null;
+    (window.frameElement as HTMLIFrameElement).style.height = null;
+  }
 }
