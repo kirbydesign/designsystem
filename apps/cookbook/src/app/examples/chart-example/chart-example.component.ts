0dfe587c9ad536ba1c12cf4cf94042b5bc7df34e
Christian Lüdemann <christian@christianlydemann.com>
2020-04-28 16:05:01 +0200

    style: set correct chart color

--- apps/cookbook/src/app/examples/chart-example/chart-example.component.ts
+++ apps/cookbook/src/app/examples/chart-example/chart-example.component.ts
@@ -1,6 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Options } from 'highcharts';
 
+import { DesignTokenHelper } from '@kirbydesign/designsystem';
+
+const getColor = DesignTokenHelper.getColor;
+
+function colorPoints() {
+  var series = this.series;
+  for (var i = 0, ie = series.length; i < ie; ++i) {
+    var points = series[i].data;
+    for (var j = 0, je = points.length; j < je; ++j) {
+      if (points[j].graphic) {
+        if (j === points.length - 1) {
+          points[j].graphic.element.style.fill = getColor('primary').value;
+          points[j].graphic.element.style.stroke = getColor('primary').value;
+        }
+      }
+    }
+  }
+}
+
 @Component({
   selector: 'cookbook-chart-example',
   templateUrl: './chart-example.component.html',
@@ -12,6 +31,13 @@ export class ChartExampleComponent implements OnInit {
   monthlyOverviewOptions: Options = {
     chart: {
       type: 'column',
+      events: {
+        load: colorPoints,
+        redraw: colorPoints,
+      },
+    },
+    title: {
+      text: 'February 2020',
     },
     xAxis: {
       categories: [
@@ -32,21 +58,20 @@ export class ChartExampleComponent implements OnInit {
     credits: {
       enabled: false,
     },
+    plotOptions: {
+      series: {
+        color: getColor('secondary').value,
+      },
+    },
     series: [
       {
-        name: 'Previous',
-        data: [10, 1400, 300, 500, 100, 1000, 1100, 450, 1350, 1200, 1250, 0],
-      },
-      {
-        name: 'Current',
-        data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 600],
+        data: [10, 1400, 300, 500, 100, 1000, 1100, 450, 1350, 1200, 1250, 600],
       },
     ] as any,
   };
-  // TODO: set color kirby green and light green
-  // TODO: center line
   // TODO: avg line
   // TODO: click alert when click a column
+  // Remove axis texts
 
   ngOnInit() {}
 }
