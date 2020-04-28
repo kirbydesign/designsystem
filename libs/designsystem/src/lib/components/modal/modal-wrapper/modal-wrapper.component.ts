ada014827458bd2e27e1565eb93d7547b3f7a43b
Jakob Engelbrecht Olesen <jakob@basher.dk>
2020-04-28 22:22:04 +0200

    :bug: Hide native keyboard... ...by blurring active element on header.touchstart()

--- libs/designsystem/src/lib/components/modal/modal-wrapper/modal-wrapper.component.ts
+++ libs/designsystem/src/lib/components/modal/modal-wrapper/modal-wrapper.component.ts
@@ -86,6 +86,12 @@ export class ModalWrapperComponent implements Modal, AfterViewInit, OnInit, OnDe
     }
   }
 
+  blurActiveElement() {
+    if (document.activeElement instanceof HTMLElement) {
+      document.activeElement.blur();
+    }
+  }
+
   private checkForEmbeddedFooter() {
     const embeddedComponentElement = this.ionContentElement.nativeElement.firstElementChild;
     if (embeddedComponentElement) {
