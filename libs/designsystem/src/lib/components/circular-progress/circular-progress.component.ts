894eccc4202ef12510ae1ff86d19fb3c9e6ab312
Kasper Ellebye Rasmussen <kera@jyskebank.dk>
2020-05-07 12:44:48 +0200

    initial circular-progress PoC

--- libs/designsystem/src/lib/components/circular-progress/circular-progress.component.ts
+++ libs/designsystem/src/lib/components/circular-progress/circular-progress.component.ts
@@ -1,14 +1,32 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'kirby-circular-progress',
-  templateUrl: './circular-progress.component.html',
+  templateUrl: './circular-progress.component.svg',
   styleUrls: ['./circular-progress.component.scss'],
 })
 export class CircularProgressComponent implements OnInit {
+  @Input() radius: number = 40;
   @Input() value: number;
+
+  strokeWidth: number = 4;
+  innerRadius: number;
+  diameter: number;
+  innerCircumference: number;
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.innerRadius = this.radius - this.strokeWidth;
+    this.diameter = this.radius * 2;
+    this.innerCircumference = this.innerRadius * 2 * Math.PI;
+  }
+
+  public changeValue() {
+    this.value = Math.random() * 100;
+  }
+
+  get offset(): number {
+    return this.innerCircumference - this.innerCircumference * (this.value / 100);
+  }
 }
--- libs/designsystem/testing-base/src/lib/components/mock.circular-progress.component.ts
+++ libs/designsystem/testing-base/src/lib/components/mock.circular-progress.component.ts
@@ -14,6 +14,7 @@ import { CircularProgressComponent } from '@kirbydesign/designsystem';
   ],
 })
 export class MockCircularProgressComponent {
+  @Input() radius: number;
   @Input() value: number;
 }
 
