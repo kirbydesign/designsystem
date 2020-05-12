816d2d311d0bd9932a783566b30ef8655bd6e813
Rasmus Holm Andersen <roa@jyskebank.dk>
2020-05-12 14:34:49 +0200

    Changed to getters

--- libs/designsystem/src/lib/components/circular-progress/circular-progress-ring.component.ts
+++ libs/designsystem/src/lib/components/circular-progress/circular-progress-ring.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 
 import { ThemeColor } from '../../helpers/theme-color.type';
 
@@ -8,29 +8,26 @@ import { ThemeColor } from '../../helpers/theme-color.type';
   styleUrls: ['./circular-progress-ring.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CircularProgressRingComponent implements OnInit {
+export class CircularProgressRingComponent {
   @Input() radius: number;
   @Input() value: number;
   @Input() themeColor: ThemeColor;
 
   protected strokeWidth: number = 4;
-  protected innerRadius: number;
-  protected diameter: number;
-  protected innerCircumference: number;
-
-  constructor() {}
-
-  ngOnInit() {
-    this.calculateParams();
-  }
 
   get offset(): number {
     return this.innerCircumference - this.innerCircumference * (this.value / 100);
   }
 
-  private calculateParams() {
-    this.innerRadius = this.radius - this.strokeWidth;
-    this.diameter = this.radius * 2;
-    this.innerCircumference = this.innerRadius * 2 * Math.PI;
+  get diameter(): number {
+    return this.radius * 2;
+  }
+
+  get innerRadius(): number {
+    return this.radius - this.strokeWidth;
+  }
+
+  get innerCircumference(): number {
+    return this.innerRadius * 2 * Math.PI;
   }
 }
