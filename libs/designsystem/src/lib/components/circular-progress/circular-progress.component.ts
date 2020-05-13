01d3e4cde47dbaf7370632b10b943490363b8bab
Rasmus Holm Andersen <roa@jyskebank.dk>
2020-05-13 07:32:29 +0200

    Added animation on view init

--- libs/designsystem/src/lib/components/circular-progress/circular-progress.component.ts
+++ libs/designsystem/src/lib/components/circular-progress/circular-progress.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit, Input, ChangeDetectionStrategy, HostBinding } from '@angular/core';
+import {
+  Component,
+  Input,
+  ChangeDetectionStrategy,
+  HostBinding,
+  ElementRef,
+  AfterViewInit,
+  ChangeDetectorRef,
+} from '@angular/core';
 
 import { ThemeColor } from './../../helpers/theme-color.type';
 
@@ -8,11 +16,28 @@ import { ThemeColor } from './../../helpers/theme-color.type';
   styleUrls: ['./circular-progress.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CircularProgressComponent implements OnInit {
+export class CircularProgressComponent implements AfterViewInit {
   @Input() radius: number = 40;
   @Input() value: number = 0;
 
   @Input() themeColor: ThemeColor;
+
+  private hasElementBeenVisible = false;
+
+  observer: IntersectionObserver;
+
+  constructor(private elementRef: ElementRef, private changeDetectorRef: ChangeDetectorRef) {}
+
+  ngAfterViewInit() {
+    this.observer = new IntersectionObserver(this.onElementVisible);
+    this.observer.observe(this.elementRef.nativeElement);
+  }
+
+  onElementVisible = () => {
+    this.observer.unobserve(this.elementRef.nativeElement);
+    this.hasElementBeenVisible = true;
+    this.changeDetectorRef.markForCheck();
+  };
 
   @HostBinding('style.width.px')
   @HostBinding('style.height.px')
@@ -20,7 +45,8 @@ export class CircularProgressComponent implements OnInit {
     return this.radius * 2;
   }
 
-  constructor() {}
-
-  ngOnInit() {}
+  get shownValue() {
+    // This is needed to make an animation when element is shown to the user
+    return this.hasElementBeenVisible ? this.value : 0;
+  }
 }
