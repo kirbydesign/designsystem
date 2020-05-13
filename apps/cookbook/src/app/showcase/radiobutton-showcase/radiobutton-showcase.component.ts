d773ed76130c058515629295a5117412e0d13498
Jakob Engelbrecht <jakob@basher.dk>
2020-05-13 08:21:46 +0200

    🔥 Clean up

--- apps/cookbook/src/app/showcase/radiobutton-showcase/radiobutton-showcase.component.ts
+++ apps/cookbook/src/app/showcase/radiobutton-showcase/radiobutton-showcase.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'cookbook-radiobutton-showcase',
   templateUrl: './radiobutton-showcase.component.html',
   styleUrls: ['./radiobutton-showcase.component.scss'],
 })
-export class RadiobuttonShowcaseComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class RadiobuttonShowcaseComponent {}
