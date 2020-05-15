a5178b51b5a99c854e3d53dad4956de2993699b7
Rasmus Holm Andersen <roa@jyskebank.dk>
2020-05-15 09:52:20 +0200

    Added initial tests

--- libs/designsystem/src/lib/components/progress-circle/progress-circle-ring.component.spec.ts
+++ libs/designsystem/src/lib/components/progress-circle/progress-circle-ring.component.spec.ts
@@ -1,20 +1,66 @@
 import { Spectator, createComponentFactory } from '@ngneat/spectator';
 
-import { ProgressCircleComponent } from './progress-circle.component';
+import { ProgressCircleRingComponent } from './progress-circle-ring.component';
 
 describe('ProgressCircleRingComponent', () => {
-  let spectator: Spectator<ProgressCircleComponent>;
+  let spectator: Spectator<ProgressCircleRingComponent>;
 
   const createHost = createComponentFactory({
-    component: ProgressCircleComponent,
+    component: ProgressCircleRingComponent,
     declarations: [],
   });
 
   beforeEach(() => {
-    spectator = createHost({ props: { value: 30 } });
+    spectator = createHost({ props: { radius: 50 } });
   });
 
   it('should create', () => {
     expect(spectator.component).toBeTruthy();
+    expect(spectator.component.value).toBe(0);
+  });
+
+  describe('centerRadius', () => {
+    it('should return distance from center to middle of stroke', () => {
+      expect(spectator.component.centerRadius).toBe(
+        spectator.component.radius - spectator.component.strokeWidth / 2
+      );
+    });
+  });
+
+  describe('centerCircumference', () => {
+    it('should return circumference in middle of stroke', () => {
+      expect(spectator.component.centerCircumference).toBe(
+        spectator.component.centerRadius * 2 * Math.PI
+      );
+    });
+  });
+
+  describe('diameter', () => {
+    it('should calculate diameter as 2*radius', () => {
+      expect(spectator.component.diameter).toBe(spectator.component.radius * 2);
+    });
+  });
+
+  describe('offset (progress)', () => {
+    it('should return the non-progress circumference (1-progress) as offset', () => {
+      const value = 33;
+      spectator.setInput({
+        value,
+      });
+      expect(spectator.component.offset).toBe(
+        spectator.component.centerCircumference -
+          spectator.component.centerCircumference * (value / 100)
+      );
+    });
+  });
+
+  describe('rendering', () => {
+    it('computed size should equal diameter', () => {});
+
+    it('should render progress stroke with themeColor', () => {});
+
+    it('should render stroke with the correct width', () => {});
+
+    it('should render circle with a radius equivalent to centerRadius', () => {});
   });
 });
--- libs/designsystem/src/lib/components/progress-circle/progress-circle.component.spec.ts
+++ libs/designsystem/src/lib/components/progress-circle/progress-circle.component.spec.ts
@@ -17,4 +17,28 @@ describe('ProgressCircleComponent', () => {
   it('should create', () => {
     expect(spectator.component).toBeTruthy();
   });
+
+  describe('radius', () => {
+    it('should translate sm to 20px', () => {});
+    it('should translate md to 28px', () => {});
+    it('should translate lg to 48px', () => {});
+  });
+
+  describe('shownValue', () => {
+    it('should return 0 until element has been visible', () => {});
+    it('should return value after element has been visible', () => {});
+  });
+  describe('diameter', () => {
+    it('should calculate diameter as 2*radius', () => {});
+  });
+  describe('ngAfterViewInit', () => {
+    it('should set up an intersection observer on element and viewport after view init', () => {});
+  });
+
+  describe('onElementVisible', () => {
+    it('should mark element as visible if element is intersecting when observer is called', () => {});
+    it('should not mark element as visible if elements are not intersecting when observer is called (on init)', () => {});
+    it('should not mark element as visible if elements are not present when called', () => {});
+    it('should mark component for change detection when visible to start animation', () => {});
+  });
 });
