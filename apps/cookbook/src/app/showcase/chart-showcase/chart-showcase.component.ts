f57fd9604a198a8be4dbff91c50e5f52a1921400
Jakob Engelbrecht <jakob@basher.dk>
2020-05-16 07:33:31 +0200

    💄 Add spacing between chart examples

--- apps/cookbook/src/app/showcase/chart-showcase/chart-showcase.component.ts
+++ apps/cookbook/src/app/showcase/chart-showcase/chart-showcase.component.ts
@@ -4,6 +4,7 @@ declare var require: any;
 @Component({
   selector: 'cookbook-chart-showcase',
   templateUrl: './chart-showcase.component.html',
+  styleUrls: ['./chart-showcase.component.scss'],
 })
 export class ChartShowcaseComponent {
   exampleHtml: string = require('!raw-loader!../../examples/chart-example/chart-example.component.html')
