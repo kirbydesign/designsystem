92d93e861b0df2a820d0c5b2272236de0cc00ebe
Rasmus Holm Andersen <53940402+jb-roa@users.noreply.github.com>
2020-05-19 08:59:15 +0200

    Update apps/cookbook/src/app/examples/progress-circle-example/examples/card.ts
    Co-authored-by: Jakob Engelbrecht <jakob@basher.dk>

--- apps/cookbook/src/app/examples/progress-circle-example/examples/card.ts
+++ apps/cookbook/src/app/examples/progress-circle-example/examples/card.ts
@@ -3,18 +3,18 @@ import { Component } from '@angular/core';
 const config = {
   selector: 'cookbook-progress-circle-example-card',
   template: `<kirby-card themeColor="dark" hasPadding="true" class="content-card">
-              <div class="content">
-                <div>
-                  Your investment savings
-                  <span class="kirby-text-large">
-                    {{ 2435034 | currency }}
-                  </span>
-                </div>
-                <kirby-progress-circle themeColor="success" value="37">
-                  <kirby-icon name="moneybag" size="md"></kirby-icon>
-                </kirby-progress-circle>
-              </div>
-            </kirby-card>`,
+  <div class="content">
+    <div>
+      Your investment savings
+      <span class="kirby-text-large">
+        {{ 2435034 | currency }}
+      </span>
+    </div>
+    <kirby-progress-circle themeColor="success" value="37">
+      <kirby-icon name="moneybag" size="md"></kirby-icon>
+    </kirby-progress-circle>
+  </div>
+</kirby-card>`,
 };
 
 @Component({
