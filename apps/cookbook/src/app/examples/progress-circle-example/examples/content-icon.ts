eefb2a2f4a660c613575a8183d6f4bafd9996cbb
Rasmus Holm Andersen <roa@jyskebank.dk>
2020-05-19 13:36:15 +0200

    Improved icon example

--- apps/cookbook/src/app/examples/progress-circle-example/examples/content-icon.ts
+++ apps/cookbook/src/app/examples/progress-circle-example/examples/content-icon.ts
@@ -3,19 +3,27 @@ import { Component } from '@angular/core';
 const config = {
   selector: 'cookbook-progress-circle-example-content-icon',
   template: `<kirby-progress-circle themeColor="danger" value="70">
-              <div>
-                <kirby-icon name="pension" size="md"></kirby-icon>
-              </div>
-            </kirby-progress-circle>`,
+  <div>
+    <kirby-icon name="pension" size="md"></kirby-icon>
+  </div>
+</kirby-progress-circle>`,
+  styles: `div { 
+  width: 100%; 
+  height: 100%; 
+  background-color: white; 
+  display: flex; 
+  justify-content: center; 
+  align-items: center; 
+}
+  `,
 };
 
 @Component({
   selector: config.selector,
   template: config.template,
-  styles: [
-    'div { width: 100%; height: 100%; background-color: white; display: flex; justify-content: center; align-items: center; }',
-  ],
+  styles: [config.styles],
 })
 export class ProgressCircleExampleContentIconComponent {
   template: string = config.template;
+  styles: string = config.styles;
 }
