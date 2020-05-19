8762dbb4ee59f3c1825afea5471b932170c6e318
Rasmus Holm Andersen <roa@jyskebank.dk>
2020-05-19 10:50:19 +0200

    UX review comments fixed

--- apps/cookbook/src/app/examples/progress-circle-example/examples/content-icon.ts
+++ apps/cookbook/src/app/examples/progress-circle-example/examples/content-icon.ts
@@ -3,13 +3,18 @@ import { Component } from '@angular/core';
 const config = {
   selector: 'cookbook-progress-circle-example-content-icon',
   template: `<kirby-progress-circle themeColor="danger" value="70">
+              <div>
                 <kirby-icon name="pension" size="md"></kirby-icon>
+              </div>
             </kirby-progress-circle>`,
 };
 
 @Component({
   selector: config.selector,
   template: config.template,
+  styles: [
+    'div { width: 100%; height: 100%; background-color: white; display: flex; justify-content: center; align-items: center; }',
+  ],
 })
 export class ProgressCircleExampleContentIconComponent {
   template: string = config.template;
