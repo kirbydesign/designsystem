99b396223fd0df576904a899965ebf4a8f8abd99
Jakob Engelbrecht <jakob@basher.dk>
2020-05-29 15:21:16 +0200

    💄 Clean up examples and fix styles

--- apps/cookbook/src/app/examples/progress-circle-example/progress-circle-example.module.ts
+++ apps/cookbook/src/app/examples/progress-circle-example/progress-circle-example.module.ts
@@ -2,19 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { KirbyModule } from '@kirbydesign/designsystem';
-import { ProgressCircleExampleDefaultMediumComponent } from './examples/default-medium';
+import { ProgressCircleExampleDefaultComponent } from './examples/default';
+import { ProgressCircleExampleSizesComponent } from './examples/sizes';
 import { ProgressCircleExampleContentStepsComponent } from './examples/content-steps';
 import { ProgressCircleExampleContentIconComponent } from './examples/content-icon';
 import { ProgressCircleExampleContentPercentComponent } from './examples/content-percent';
 import { ProgressCircleExampleAnimatedComponent } from './examples/animated';
 import { ProgressCircleExampleCardComponent } from './examples/card';
-import { ProgressCircleExampleDefaultLargeComponent } from './examples/default-large';
-import { ProgressCircleExampleDefaultSmallComponent } from './examples/default-small';
 
 const COMPONENT_DECLARATIONS = [
-  ProgressCircleExampleDefaultSmallComponent,
-  ProgressCircleExampleDefaultMediumComponent,
-  ProgressCircleExampleDefaultLargeComponent,
+  ProgressCircleExampleDefaultComponent,
+  ProgressCircleExampleSizesComponent,
   ProgressCircleExampleContentStepsComponent,
   ProgressCircleExampleContentIconComponent,
   ProgressCircleExampleContentPercentComponent,
