679c0e98f9403436f431d33b9e44f8f244cc0f1d
Jakob Engelbrecht <jakob@basher.dk>
2020-06-02 15:53:38 +0200

    📝 Update Progress Circle in Card example

--- apps/cookbook/src/app/examples/progress-circle-example/examples/card.ts
+++ apps/cookbook/src/app/examples/progress-circle-example/examples/card.ts
@@ -3,37 +3,24 @@ import { Component } from '@angular/core';
 const config = {
   selector: 'cookbook-progress-circle-example-card',
   template: `<kirby-card hasPadding="true">
-  <div class="content">
-    <div class="information">
-      Your investment savings
-      <span class="kirby-text-large">
-        {{ 2435034 | currency }}
-      </span>
-    </div>
-    <kirby-progress-circle themeColor="success" value="37">
+  <kirby-item>
+    <kirby-label>
+      <h2>Your investment savings</h2>
+      <div>
+        <data class="kirby-text-large">{{ 2435034 | currency }}</data>
+      </div>
+    </kirby-label>
+    <kirby-progress-circle themeColor="success" value="37" slot="end">
       <kirby-icon name="moneybag" size="md"></kirby-icon>
     </kirby-progress-circle>
-  </div>
+  </kirby-item>
 </kirby-card>`,
-  styles: `kirby-card .content {
-  display: flex;
-  flex-direction: row;
-}
-  
-kirby-card .content .information {
-  flex: 1;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-}`,
 };
 
 @Component({
   selector: config.selector,
   template: config.template,
-  styles: [config.styles],
 })
 export class ProgressCircleExampleCardComponent {
   template: string = config.template;
-  styles: string = config.styles;
 }
