a625beceffff5f23c4d6e6bf8418c027467fe4bf
Rasmus Pedersen <traeholt@live.dk>
2020-06-08 11:08:22 +0200

    Feature/form field in item (#1054)
    * RAV-172 - Form-field in item
    
    * RAV-172 - Test adjustment
    
    * RAV-172 - removed pattern attr
    
    * RAV-172 - clean up
    
    * RAV-172 - numeric and default separation
    
    * ✅ Add additional tests
    
    * ✏️ Typo
    
    * ✅ Add InputComponent in Item tests
    
    * RAV-172 - Review changes implemented
    
    * RAV-172 - Implemented review changes
    
    * RAV-172 - Added form-field tests + review changes
    
    Co-authored-by: Rasmus Træholt Pedersen <>
    Co-authored-by: Jakob Engelbrecht <jakob@basher.dk>

---
+++ libs/designsystem/src/lib/components/form-field/input/input.component.integration.spec.ts
@@ -0,0 +1,51 @@
+import { SpectatorHost, createHostFactory } from '@ngneat/spectator';
+import { IonicModule } from '@ionic/angular';
+
+import { ItemComponent } from '../../item';
+import { InputComponent } from './input.component';
+
+describe('InputComponent in Item', () => {
+  let spectator: SpectatorHost<InputComponent>;
+  let element: HTMLInputElement;
+
+  const createHost = createHostFactory({
+    component: InputComponent,
+    declarations: [ItemComponent],
+    imports: [IonicModule.forRoot({ mode: 'ios', _testing: true })],
+  });
+
+  beforeEach(() => {
+    spectator = createHost(`
+    <kirby-item>
+      <input kirby-input/>
+    </kirby-item>`);
+    element = spectator.element as HTMLInputElement;
+  });
+
+  it('should render with default padding', () => {
+    expect(element).toHaveComputedStyle({
+      padding: '1px 2px',
+    });
+  });
+
+  it('should render without border-radius', () => {
+    expect(element).toHaveComputedStyle({
+      'border-radius': '0px',
+    });
+  });
+
+  it('should render without box-shadow', () => {
+    expect(element).toHaveComputedStyle({
+      'border-shadow': '',
+    });
+  });
+
+  it('should render with default width', () => {
+    //window.getComputedStyle() returns width in pixels - so use element.computedStyleMap:
+    const styleWidth = (element as any)
+      .computedStyleMap()
+      .get('width')
+      .toString();
+    expect(styleWidth).toBe('auto');
+  });
+});
