b001cadf0cf6610865cda26e613dc5f2205168e3
Morten Bjerg Gregersen <mgr@bankdata.dk>
2020-06-30 12:00:02 +0200

    Update list-helper.ts

--- libs/designsystem/src/lib/components/list/helpers/list-helper.ts
+++ libs/designsystem/src/lib/components/list/helpers/list-helper.ts
@@ -1,5 +1,5 @@
 import { LoadOnDemandEventData } from '../list.event';
-import { ListComponent } from './../list.component';
+import { ListComponent } from '../list.component';
 
 export class ListHelper {
   onLoadOnDemand(component: ListComponent, _event: LoadOnDemandEventData) {
