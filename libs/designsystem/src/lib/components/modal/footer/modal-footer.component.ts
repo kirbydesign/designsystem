040388f5d563d5c11830fb4dd2da0b4b2632a6ec
mvindahl <mvindahl@gmail.com>
2020-08-04 08:49:44 +0200

    Added optional property "snapToKeyboard" to modal footer
    This controls if the footer follows the keyboard on mobile devices or lets the keyboard slide in on top. Defaults to false.

--- libs/designsystem/src/lib/components/modal/footer/modal-footer.component.ts
+++ libs/designsystem/src/lib/components/modal/footer/modal-footer.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, ElementRef, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'kirby-modal-footer',
   templateUrl: './modal-footer.component.html',
   styleUrls: ['./modal-footer.component.scss'],
 })
-export class ModalFooterComponent {}
+export class ModalFooterComponent implements OnInit, OnChanges {
+  @Input() snapToKeyboard = false;
+
+  private kirbyModalFooterElement: HTMLElement;
+
+  constructor(private elementRef: ElementRef<HTMLElement>) {}
+
+  ngOnInit() {
+    this.kirbyModalFooterElement = this.elementRef.nativeElement.closest('ion-modal');
+
+    this.setSnapToKeyboard(this.snapToKeyboard);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.snapToKeyboard && this.kirbyModalFooterElement) {
+      this.setSnapToKeyboard(changes.snapToKeyboard.currentValue);
+    }
+  }
+
+  private setSnapToKeyboard(value: boolean) {
+    this.kirbyModalFooterElement.style.setProperty('--snap-to-keyboard', value ? '1' : '0');
+  }
+}
