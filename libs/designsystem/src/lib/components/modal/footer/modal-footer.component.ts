fac4d1caa849b294c57abd078b7a19e73a6f27ff
Peter Lindholm <peter@peterlindholm.com>
2020-08-11 00:12:29 +0200

    :art: Remove white as default themeColor value

--- libs/designsystem/src/lib/components/modal/footer/modal-footer.component.ts
+++ libs/designsystem/src/lib/components/modal/footer/modal-footer.component.ts
@@ -7,5 +7,5 @@ import { Component, Input } from '@angular/core';
 })
 export class ModalFooterComponent {
   @Input()
-  themeColor?: 'white' | 'light' = 'white';
+  themeColor?: 'white' | 'light';
 }
