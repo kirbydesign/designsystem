795005017b594ff8244531674a306b54b78b265e
Edwin Silva <edwin-ms-1995@hotmail.com>
2020-08-25 16:25:12 +0200

    removed variables in scss

--- libs/designsystem/src/lib/components/text-link/text-link.component.ts
+++ libs/designsystem/src/lib/components/text-link/text-link.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'kirby-text-link',
   templateUrl: './text-link.component.html',
-  styleUrls: ['./text-link.component.css'],
+  styleUrls: ['./text-link.component.scss'],
 })
 export class TextLinkComponent implements OnInit {
+  baseUrl = window.location.origin;
+  @Input() link: string;
+  @Input() title: string;
+
   constructor() {}
 
   ngOnInit(): void {}
