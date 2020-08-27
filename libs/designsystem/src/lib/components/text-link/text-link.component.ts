ddbc06351cbe29ac3185b340523254352293616a
Edwin Silva <edwin-ms-1995@hotmail.com>
2020-08-27 13:37:39 +0200

    loggiken er på plads. eksempler mangles

--- libs/designsystem/src/lib/components/text-link/text-link.component.ts
+++ libs/designsystem/src/lib/components/text-link/text-link.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, ContentChildren } from '@angular/core';
 
 @Component({
   selector: 'kirby-text-link',
@@ -7,16 +7,35 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class TextLinkComponent implements OnInit {
   baseUrl = window.location.origin;
+
   @Input() link: string;
+  @Input() text: string;
 
   constructor() {}
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log('TextLinkComponent -> constructor ->     this.isExternal()', this.isExternal());
+  }
+  /*
+Will not work if link contains multiple nested paths
+Needs more work
+*/
 
-  inAppLink() {
-    let appLink = true;
-    if (!this.link.includes('baseUrl') || this.link.split('/').length != 1) {
-      appLink = false;
+  checkDomain(url) {
+    if (url.indexOf('//') === 0) {
+      url = location.protocol + url;
     }
-    return appLink;
+    return url
+      .toLowerCase()
+      .replace(/([a-z])?:\/\//, '$1')
+      .split('/')[0];
+  }
+
+  isExternal() {
+    const url = this.link;
+    console.log('TextLinkComponent -> isExternal -> url', url);
+    return (
+      (url.indexOf(':') > -1 || url.indexOf('//') > -1) &&
+      this.checkDomain(location.href) !== this.checkDomain(url)
+    );
   }
 }
