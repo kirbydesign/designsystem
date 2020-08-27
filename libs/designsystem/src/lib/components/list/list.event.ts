6cd2d70829acfb866cc37367d999b2f787ae475a
Morten Bjerg Gregersen <mgr@bankdata.dk>
2020-08-27 08:37:21 +0200

    Clean up NativeScript remains (#1119)
    Co-authored-by: Jakob Engelbrecht <jakob@basher.dk>

--- libs/designsystem/src/lib/components/list/list.event.ts
+++ libs/designsystem/src/lib/components/list/list.event.ts
@@ -2,9 +2,6 @@ export interface LoadOnDemandEvent {
   complete: (disableLoadOnDemand?: boolean) => void;
 }
 
-/**
- * This is a wrapper for {N} RadListView LoadOnDemandListViewEventData (https://docs.nativescript.org/ns-ui-api-reference/classes/loadondemandlistvieweventdata)
- */
 export interface LoadOnDemandEventData {
   object: any;
   returnValue: any;
