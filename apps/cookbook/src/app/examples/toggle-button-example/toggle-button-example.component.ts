ef00859585df64f38bc371d4df3bfd4197fcbbb2
Christian Lüdemann <christian@christianlydemann.com>
2020-09-30 14:09:23 +0200

    style: remove unused

--- apps/cookbook/src/app/examples/toggle-button-example/toggle-button-example.component.ts
+++ apps/cookbook/src/app/examples/toggle-button-example/toggle-button-example.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'cookbook-toggle-button-example',
   templateUrl: './toggle-button-example.component.html',
-  styleUrls: ['./toggle-button-example.component.scss'],
 })
 export class ToggleButtonExampleComponent {
   onCheckedChange(checked: boolean) {
