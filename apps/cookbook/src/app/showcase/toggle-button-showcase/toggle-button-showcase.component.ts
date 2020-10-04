eb4909c647620e4e02dfe2a51acf58057921676d
Jakob Engelbrecht <jakob@basher.dk>
2020-10-04 12:44:04 +0200

    📝 Fix toggle button examples

--- apps/cookbook/src/app/showcase/toggle-button-showcase/toggle-button-showcase.component.ts
+++ apps/cookbook/src/app/showcase/toggle-button-showcase/toggle-button-showcase.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+import { ColorHelper } from '@kirbydesign/designsystem';
+
 import { ShowcaseProperty } from '../../shared/showcase-properties/showcase-property';
 @Component({
+  styleUrls: ['./toggle-button-showcase.component.scss'],
   templateUrl: './toggle-button-showcase.component.html',
 })
 export class ToggleButtonShowcaseComponent {
+  notificationColors = ColorHelper.notificationColors.map((color) => color.name);
   properties: ShowcaseProperty[] = [
     {
       name: 'checked',
@@ -17,6 +21,15 @@ export class ToggleButtonShowcaseComponent {
       description: 'Emits the checked state of the toggle button on change (true | false).',
       defaultValue: '',
       inputValues: ['event'],
+    },
+  ];
+  buttonProperties: ShowcaseProperty[] = [
+    {
+      name: 'themeColor',
+      description: `Sets the background color to the provided notification color.
+        **Please note: Only applies when used inside a Toggle Button`,
+      defaultValue: '',
+      inputValues: this.notificationColors,
     },
   ];
 }
