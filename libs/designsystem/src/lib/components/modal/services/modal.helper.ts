b9350216bc49ca9d99c52f62fa85f0293e436453
Jakob Engelbrecht <jakob@basher.dk>
2020-10-09 14:44:35 +0200

    💄 ✨ Add ModalAnimationBuilderService

--- libs/designsystem/src/lib/components/modal/services/modal.helper.ts
+++ libs/designsystem/src/lib/components/modal/services/modal.helper.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Animation, AnimationBuilder, AnimationController, ModalController } from '@ionic/angular';
+import { ModalController } from '@ionic/angular';
 
 import { ModalConfig } from '../modal-wrapper/config/modal-config';
 import { ModalWrapperComponent } from '../modal-wrapper/modal-wrapper.component';
 import { ModalCompactWrapperComponent } from '../modal-wrapper/compact/modal-compact-wrapper.component';
 import { Overlay } from './modal.interfaces';
 import { KirbyAnimation } from '../../../animation/kirby-animation';
-import { DesignTokenHelper } from '../../../helpers';
+import { ModalAnimationBuilderService } from './modal-animation-builder.service';
 
 @Injectable()
 export class ModalHelper {
@@ -16,7 +16,7 @@ export class ModalHelper {
 
   constructor(
     private ionicModalController: ModalController,
-    private animationCtrl: AnimationController
+    private modalAnimationBuilder: ModalAnimationBuilderService
   ) {}
 
   public async showModalWindow(config: ModalConfig): Promise<Overlay> {
@@ -29,8 +29,8 @@ export class ModalHelper {
       currentBackdrop = topMostModal.querySelector<HTMLIonBackdropElement>('ion-backdrop');
     }
 
-    const enterAnimation = this.enterAnimationBuilder(currentBackdrop);
-    const leaveAnimation = this.leaveAnimationBuilder(currentBackdrop);
+    const enterAnimation = this.modalAnimationBuilder.enterAnimation(currentBackdrop);
+    const leaveAnimation = this.modalAnimationBuilder.leaveAnimation(currentBackdrop);
 
     const ionModal = await this.ionicModalController.create({
       component: config.flavor === 'compact' ? ModalCompactWrapperComponent : ModalWrapperComponent,
@@ -44,89 +44,14 @@ export class ModalHelper {
       componentProps: { config: config },
       swipeToClose: config.flavor != 'compact',
       presentingElement: modalPresentingElement,
+      keyboardClose: false,
       enterAnimation,
       leaveAnimation,
-      keyboardClose: false,
     });
 
     await ionModal.present();
 
     return { dismiss: ionModal.dismiss.bind(ionModal), onDidDismiss: ionModal.onDidDismiss() };
-  }
-
-  // TODO: Move to own class/file:
-  private readonly modalAnimationEasing = 'cubic-bezier(0.32,0.72,0,1)';
-  private readonly modalAnimationDuration = KirbyAnimation.Duration.LONG;
-
-  // TODO: Move to own class/file:
-  private enterAnimationBuilder(currentBackdrop?: HTMLIonBackdropElement): AnimationBuilder {
-    return (baseEl: HTMLElement, presentingEl?: HTMLElement): Animation => {
-      const animations = [];
-      if (currentBackdrop) {
-        console.log('Adding fadeOutAnim to currentBackdrop...');
-        const currentBackdropAnimation = this.animationCtrl
-          .create()
-          .addElement(currentBackdrop)
-          .fromTo('opacity', 'var(--backdrop-opacity)', 0.01);
-        animations.push(currentBackdropAnimation);
-      }
-
-      const backdropAnimation = this.animationCtrl
-        .create()
-        .addElement(baseEl.querySelector('ion-backdrop')!)
-        .fromTo('opacity', 0.01, 'var(--backdrop-opacity)');
-
-      const wrapperAnimation = this.animationCtrl
-        .create()
-        .addElement(baseEl.querySelector('.modal-wrapper')!)
-        .beforeStyles({ opacity: 1 })
-        .fromTo('transform', 'translateY(100%)', 'translateY(0%)');
-
-      const baseAnimation = this.animationCtrl
-        .create()
-        .addElement(baseEl)
-        .easing(this.modalAnimationEasing)
-        .duration(this.modalAnimationDuration)
-        .beforeAddClass('show-modal')
-        .addAnimation([...animations, backdropAnimation, wrapperAnimation]);
-
-      return baseAnimation;
-    };
-  }
-
-  // TODO: Move to own class/file:
-  private leaveAnimationBuilder(currentBackdrop?: HTMLIonBackdropElement): AnimationBuilder {
-    return (baseEl: HTMLElement, presentingEl?: HTMLElement): Animation => {
-      const animations = [];
-      if (currentBackdrop) {
-        const currentBackdropAnimation = this.animationCtrl
-          .create()
-          .addElement(currentBackdrop)
-          .fromTo('opacity', 0.01, 'var(--backdrop-opacity)')
-          .afterStyles({ opacity: 'var(--backdrop-opacity)' });
-        animations.push(currentBackdropAnimation);
-      }
-
-      const backdropAnimation = this.animationCtrl
-        .create()
-        .addElement(baseEl.querySelector('ion-backdrop')!)
-        .fromTo('opacity', 'var(--backdrop-opacity)', 0.0);
-
-      const wrapperAnimation = this.animationCtrl
-        .create()
-        .addElement(baseEl.querySelector('.modal-wrapper')!)
-        .beforeStyles({ opacity: 1 })
-        .fromTo('transform', 'translateY(0%)', 'translateY(100%)');
-
-      const baseAnimation = this.animationCtrl
-        .create()
-        .addElement(baseEl)
-        .easing(this.modalAnimationEasing)
-        .duration(this.modalAnimationDuration)
-        .addAnimation([...animations, backdropAnimation, wrapperAnimation]);
-
-      return baseAnimation;
-    };
   }
 
   public registerPresentingElement(element: HTMLElement) {
@@ -139,7 +64,10 @@ export class ModalHelper {
       const topMostModal = await this.ionicModalController.getTop();
       if (!topMostModal) {
         modalPresentingElement = ModalHelper.presentingElement;
-      } else if (!topMostModal.classList.contains('kirby-drawer')) {
+      } else if (
+        !topMostModal.classList.contains('kirby-drawer') &&
+        !topMostModal.classList.contains('kirby-modal-compact')
+      ) {
         modalPresentingElement = topMostModal;
       }
     }
