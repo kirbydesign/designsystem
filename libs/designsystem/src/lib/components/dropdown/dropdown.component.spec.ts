2cf5613fba9ac0b51687eab3e54d11e2727ca812
Søren Fruergaard Sørensen <soren.fruergaard@gmail.com>
2020-11-12 14:01:51 +0100

    added no-blur to host element

--- libs/designsystem/src/lib/components/dropdown/dropdown.component.spec.ts
+++ libs/designsystem/src/lib/components/dropdown/dropdown.component.spec.ts
@@ -85,6 +85,10 @@ describe('DropdownComponent', () => {
       expect(spectator.element.attributes['disabled']).toBeUndefined();
     });
 
+    it('should render no-blur attribute', () => {
+      expect(spectator.element.attributes['no-blur']).toBeDefined();
+    });
+
     it('should not render button as disabled ', () => {
       expect(buttonElement.disabled).toBeFalsy();
     });
