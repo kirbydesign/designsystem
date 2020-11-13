684bc16643227ea5cb4fdd96340e107c6bede025
Jakob Engelbrecht <jakob@basher.dk>
2020-11-13 16:02:43 +0100

    Fix/modal keyboard offset (#1196)
    * 🐛 Fix modal keyboard offset
    
    * ✅ Fix tests
    
    * ✅ Fix tests
    
    * 📝 Add dummy keyboard
    
    * ✅ Fix tests
    
    * ✅ Fix tests
    
    * ✅ Fix tests
    
    * ✅ Fix tests
    
    * 📝 Improve dummy keyboard
    
    * 🐛 💄 Fix modal footer safe area
    
    * 📝 Fix iphone example wrapper safe area
    
    * ✅ Fix tests
    
    Co-authored-by: Alexander Hafstad <alexander@hafstad.dk>

--- apps/cookbook/src/app/examples/examples.component.ts
+++ apps/cookbook/src/app/examples/examples.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+
+import { WindowRef } from '@kirbydesign/designsystem/types/window-ref';
 
 @Component({
   selector: 'cookbook-examples',
@@ -6,5 +8,79 @@ import { Component } from '@angular/core';
   styleUrls: ['./examples.component.scss'],
 })
 export class ExamplesComponent {
-  constructor() {}
+  showDummyKeyboard = !!this.window.sessionStorage.getItem('kirby-cookbook-show-dummy-keyboard');
+  keyboardIsShowing = false;
+  keyboardHeight: number;
+  keyCount = 40;
+  keys = Array(this.keyCount).fill('');
+
+  constructor(private window: WindowRef) {
+    this.setKeyboardSize();
+  }
+
+  private setKeyboardSize() {
+    const keyboardHeights = {
+      tablet: {
+        portrait: 264,
+        landscape: 434,
+      },
+      phone: {
+        portrait: 290,
+        landscape: 200,
+      },
+    };
+    const device = this.window.matchMedia('(min-width: 721px)').matches ? 'tablet' : 'phone';
+    const orientation = this.window.matchMedia('(orientation: landscape)').matches
+      ? 'landscape'
+      : 'portrait';
+    this.keyboardHeight = keyboardHeights[device][orientation];
+  }
+
+  @HostListener('window:ionKeyboardDidShow', ['$event'])
+  _onKeyboardDidShow(event: { detail: { keyboardHeight: number } }) {
+    this.keyboardHeight = event.detail.keyboardHeight;
+    this.keyboardIsShowing = true;
+  }
+
+  @HostListener('window:ionKeyboardDidHide')
+  _onKeyboardDidHide() {
+    this.keyboardIsShowing = false;
+  }
+
+  @HostListener('window:kirbyToggleDummyKeyboard')
+  _onToggleDummyKeyboard() {
+    setTimeout(() => {
+      this.showDummyKeyboard = !this.showDummyKeyboard;
+      const sessionKey = 'kirby-cookbook-show-dummy-keyboard';
+      this.showDummyKeyboard
+        ? this.window.sessionStorage.setItem(sessionKey, 'true')
+        : this.window.sessionStorage.removeItem(sessionKey);
+    });
+  }
+
+  @HostListener('document:focusin', ['$event.target'])
+  _onInputFocus(input: HTMLElement) {
+    if (!this.showDummyKeyboard) return;
+    if (input.tagName === 'INPUT' || input.tagName === 'TEXTAREA') {
+      const ionKeyboardDidShowEvent = new CustomEvent('ionKeyboardDidShow', {
+        detail: { keyboardHeight: this.keyboardHeight },
+      });
+      this.window.dispatchEvent(ionKeyboardDidShowEvent);
+    }
+  }
+
+  @HostListener('document:focusout', ['$event.target'])
+  _onInputFocusOut(input: HTMLElement) {
+    if (!this.showDummyKeyboard) return;
+    if (input.tagName === 'INPUT' || input.tagName === 'TEXTAREA') {
+      const ionKeyboardDidHideEvent = new CustomEvent('ionKeyboardDidHide');
+      this.window.dispatchEvent(ionKeyboardDidHideEvent);
+    }
+  }
+
+  @HostListener('window:resize')
+  _onWindowResize() {
+    if (!this.showDummyKeyboard) return;
+    this.setKeyboardSize();
+  }
 }
