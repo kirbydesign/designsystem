d54145f792f93bf214a896f1c16ab45ec88edf17
MonsterGameDev <per@brf.dk>
2020-11-13 09:30:56 +0100

    Feature/slides (#1180)
    * sync
    
    * fixed export of slides
    
    * unit test and showcase properties
    
    * changes according to review pr 1180
    
    * wierd ludeman files
    
    * wierd files
    
    * added way to activate specific slide at startup
    
    * recreated mock.toggle-button.component
    
    * corrected css for elevated card in carrusel context
    
    * changes according to review
    
    * corrected unit tests
    
    * corrected unit tests
    
    * corrected unit tests
    
    * corrected unit tests
    
    * very resistant example....
    
    * more irritating unittests
    
    * removed fit
    
    * removed 'privat'
    
    * removed comment
    
    Co-authored-by: Alexander Hafstad <alexander@hafstad.dk>

---
+++ apps/cookbook/src/app/examples/slides-example/slides-example.component.ts
@@ -0,0 +1,67 @@
+import { Component, ViewChild } from '@angular/core';
+
+import { SlidesComponent } from '@kirbydesign/designsystem';
+
+@Component({
+  selector: 'cookbook-slides-example',
+  templateUrl: './slides-example.component.html',
+})
+export class SlidesExampleComponent {
+  @ViewChild(SlidesComponent) slidesComponent: SlidesComponent;
+  constructor() {}
+
+  slidesOptions = {
+    spaceBetween: 5,
+    centeredSlides: true,
+    slidesPerView: 1.15,
+    breakpoints: {
+      721: {
+        slidesPerView: 1.9,
+      },
+    },
+  };
+
+  slides = [
+    {
+      title: 'Slide 1',
+      subtitle: 'Subtitle 1',
+      cardContent:
+        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia facere molestias recusandae necessitatibus ab veniam repellendus doloremque culpa quam libero, est quo  accusamus cumque, in quia itaque cupiditate ratione repellat!',
+    },
+    {
+      title: 'Slide 2',
+      subtitle: 'Subtitle 2',
+      cardContent:
+        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia facere molestias recusandae necessitatibus ab veniam repellendus doloremque culpa quam libero, est quo  accusamus cumque, in quia itaque cupiditate ratione repellat!',
+    },
+    {
+      title: 'Slide 3',
+      subtitle: 'Subtitle 3',
+      cardContent:
+        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia facere molestias recusandae necessitatibus ab veniam repellendus doloremque culpa quam libero, est quo  accusamus cumque, in quia itaque cupiditate ratione repellat!',
+    },
+    {
+      title: 'Slide 4',
+      subtitle: 'Subtitle 4',
+      cardContent:
+        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia facere molestias recusandae necessitatibus ab veniam repellendus doloremque culpa quam libero, est quo  accusamus cumque, in quia itaque cupiditate ratione repellat!',
+    },
+    {
+      title: 'Slide 5',
+      subtitle: 'Subtitle 5',
+      cardContent:
+        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia facere molestias recusandae necessitatibus ab veniam repellendus doloremque culpa quam libero, est quo  accusamus cumque, in quia itaque cupiditate ratione repellat!',
+    },
+  ];
+
+  activeSlide = this.slides[0];
+
+  getDataFromActiveSlide(e: any) {
+    // Output onSlideDidChange
+    console.log('Output: ', e);
+  }
+
+  changeSlide() {
+    this.slidesComponent.slideTo(3);
+  }
+}
