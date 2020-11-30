f7bebd2080c03350b4093d97a1974b9e1f9fc34c
Jakob Engelbrecht <jakob@basher.dk>
2020-11-30 22:45:05 +0100

    💡 Add comments on input shims

--- libs/designsystem/src/lib/components/form-field/form-field.component.ts
+++ libs/designsystem/src/lib/components/form-field/form-field.component.ts
@@ -28,6 +28,7 @@ export class FormFieldComponent implements AfterContentChecked, OnDestroy {
 
   constructor(elementRef: ElementRef<HTMLElement>) {
     this.element = elementRef.nativeElement;
+    // Allow optional wrapper to scroll into view:
     this.focusElement = this.element.closest('[scroll-into-view]') || this.element;
   }
 
@@ -39,6 +40,9 @@ export class FormFieldComponent implements AfterContentChecked, OnDestroy {
     ) {
       // Host is connected to dom and slotted input/textarea is present:
       this.isRegistered = true;
+      // Dispatch an `ionInputDidLoad` event to register
+      // form field + input/textarea with Ionic input shims
+      // See: https://github.com/ionic-team/ionic-framework/blob/master/core/src/utils/input-shims/input-shims.ts
       document.dispatchEvent(
         new CustomEvent('ionInputDidLoad', {
           detail: this.focusElement,
@@ -48,6 +52,9 @@ export class FormFieldComponent implements AfterContentChecked, OnDestroy {
   }
 
   ngOnDestroy(): void {
+    // Dispatch an `ionInputDidUnload` event to unregister
+    // form field + input/textarea from Ionic input shims
+    // See: https://github.com/ionic-team/ionic-framework/blob/master/core/src/utils/input-shims/input-shims.ts
     document.dispatchEvent(
       new CustomEvent('ionInputDidUnload', {
         detail: this.focusElement,
