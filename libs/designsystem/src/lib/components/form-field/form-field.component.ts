7df7b03410f1dce94aee2ab2592827b814bd2945
Jakob Engelbrecht <jakob@basher.dk>
2020-12-01 17:17:06 +0100

    ✨ Add `focus()` method

--- libs/designsystem/src/lib/components/form-field/form-field.component.ts
+++ libs/designsystem/src/lib/components/form-field/form-field.component.ts
@@ -8,6 +8,9 @@ import {
   ElementRef,
 } from '@angular/core';
 
+import { PlatformService } from '../../helpers';
+import { WindowRef } from '../../types';
+
 import { InputCounterComponent } from './input-counter/input-counter.component';
 
 @Component({
@@ -20,30 +23,49 @@ export class FormFieldComponent implements AfterContentChecked, OnDestroy {
   private isRegistered = false;
   private element: HTMLElement;
   private focusElement: HTMLElement;
+  private inputElement: HTMLInputElement | HTMLTextAreaElement;
+  private isTouch: boolean;
 
   @Input() label: string;
   @Input() message: string;
 
   @ContentChild(InputCounterComponent, { static: false }) counter: InputCounterComponent;
 
-  constructor(elementRef: ElementRef<HTMLElement>) {
+  constructor(
+    elementRef: ElementRef<HTMLElement>,
+    platform: PlatformService,
+    private window: WindowRef
+  ) {
     this.element = elementRef.nativeElement;
     // Allow optional wrapper to scroll into view:
     this.focusElement = this.element.closest('[scroll-into-view]') || this.element;
+    this.isTouch = platform.isTouch();
+  }
+
+  focus() {
+    if (!this.inputElement) return;
+
+    if (this.isTouch) {
+      // Trigger Ionic's input shims to ensure input is scrolled into view.
+      // See: https://github.com/ionic-team/ionic-framework/blob/master/core/src/utils/input-shims/hacks/scroll-assist.ts
+      const touchStart = new TouchEvent('touchstart');
+      const touchEnd = new TouchEvent('touchend');
+      this.inputElement.dispatchEvent(touchStart);
+      this.inputElement.dispatchEvent(touchEnd);
+    } else {
+      this.inputElement.focus();
+    }
   }
 
   ngAfterContentChecked(): void {
-    if (
-      !this.isRegistered &&
-      this.element.isConnected &&
-      this.element.querySelectorAll('input, textarea').length > 0
-    ) {
+    this.inputElement = this.element.querySelector('input, textarea');
+    if (!this.isRegistered && this.element.isConnected && !!this.inputElement) {
       // Host is connected to dom and slotted input/textarea is present:
       this.isRegistered = true;
       // Dispatch an `ionInputDidLoad` event to register
       // form field + input/textarea with Ionic input shims
       // See: https://github.com/ionic-team/ionic-framework/blob/master/core/src/utils/input-shims/input-shims.ts
-      document.dispatchEvent(
+      this.window.document.dispatchEvent(
         new CustomEvent('ionInputDidLoad', {
           detail: this.focusElement,
         })
@@ -55,7 +77,7 @@ export class FormFieldComponent implements AfterContentChecked, OnDestroy {
     // Dispatch an `ionInputDidUnload` event to unregister
     // form field + input/textarea from Ionic input shims
     // See: https://github.com/ionic-team/ionic-framework/blob/master/core/src/utils/input-shims/input-shims.ts
-    document.dispatchEvent(
+    this.window.document.dispatchEvent(
       new CustomEvent('ionInputDidUnload', {
         detail: this.focusElement,
       })
