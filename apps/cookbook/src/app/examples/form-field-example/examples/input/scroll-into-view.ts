63fff31d1c7d9b350c4a96fd3aa190b24d0dae22
Jakob Engelbrecht <jakob@basher.dk>
2020-12-02 06:20:02 +0100

    Enhance/form field focus (#1218)
    * 📝 Add example configuration wrapper
    
    * 📱 ♻️ Add platform service
    
    * 💄 📝 Fix padding
    
    * 📝 Add dummy keyboard toggle
    
    * 📝 💄 Fix configuration wrapper placement
    
    * 📝 Remove dummy keyboard toggle
    
    * 📝 📱 Fix wrapper padding + max-width
    
    * ✨ Add `focus()` method
    
    * 📝 Add form-field focus example
    
    * 📝 💄 Fix space on subheadings
    
    * Add `focus`method to API description
    
    * ✅ Fix tests
    
    * ⚡️ Fix component lifecycle
    
    * ✅ Fix and add tests
    
    * 📦 Fix barrel imports

--- apps/cookbook/src/app/examples/form-field-example/examples/input/scroll-into-view.ts
+++ apps/cookbook/src/app/examples/form-field-example/examples/input/scroll-into-view.ts
@@ -1,6 +1,4 @@
-import { Component, HostListener } from '@angular/core';
-
-import { WindowRef } from '@kirbydesign/designsystem/types';
+import { Component } from '@angular/core';
 
 const config = {
   selector: 'cookbook-form-field-scroll-into-view-example',
@@ -10,17 +8,7 @@ const config = {
     <input kirby-input placeholder="Focus me on device to scroll wrapper into view" />
   </kirby-form-field>
   <button kirby-button attentionLevel="2">OK</button>
-</div>
-<fieldset>
-  <legend>Configuration</legend>
-  <label (click)="showDummyKeyboard = !showDummyKeyboard">
-    <kirby-checkbox
-      [checked]="showDummyKeyboard"
-      (checkedChange)="toggleDummyKeyboard()"
-    ></kirby-checkbox
-    >Show dummy keyboard</label
-  >
-</fieldset>`,
+</div>`,
 };
 
 @Component({
@@ -29,28 +17,5 @@ const config = {
   styleUrls: ['./scroll-into-view.scss'],
 })
 export class FormFieldScrollIntoViewExampleComponent {
-  constructor(private window: WindowRef) {}
-
-  template: string = config.template.split('<fieldset>')[0]; // Remove config part of the template
-  showDummyKeyboard = !!this.window.sessionStorage.getItem('kirby-cookbook-show-dummy-keyboard');
-
-  toggleDummyKeyboard() {
-    const sessionKey = 'kirby-cookbook-show-dummy-keyboard';
-    this.showDummyKeyboard
-      ? this.window.sessionStorage.removeItem(sessionKey)
-      : this.window.sessionStorage.setItem(sessionKey, 'true');
-    // Timeout prevents ExpressionChangedAfterItHasBeenCheckedError:
-    setTimeout(() =>
-      this.window.dispatchEvent(
-        new CustomEvent('kirbyToggleDummyKeyboard', { detail: this.showDummyKeyboard })
-      )
-    );
-  }
-
-  @HostListener('window:kirbyToggleDummyKeyboard', ['$event.detail'])
-  _onToggleDummyKeyboard(show: boolean) {
-    if (this.showDummyKeyboard !== undefined) {
-      this.showDummyKeyboard = show;
-    }
-  }
+  template: string = config.template;
 }
