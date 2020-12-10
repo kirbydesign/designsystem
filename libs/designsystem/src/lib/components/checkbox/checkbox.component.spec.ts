9a88cbb9e007499678e6811be6cc641c224cdae9
Henrik Stokbro <hstokbro@gmail.com>
2020-12-10 13:12:54 +0100

    Update and add extra tests

--- libs/designsystem/src/lib/components/checkbox/checkbox.component.spec.ts
+++ libs/designsystem/src/lib/components/checkbox/checkbox.component.spec.ts
@@ -1,26 +1,27 @@
-import { Spectator, createComponentFactory } from '@ngneat/spectator';
-import { IonCheckbox } from '@ionic/angular';
+import { Spectator, createComponentFactory, createHostFactory } from '@ngneat/spectator';
+import { IonCheckbox, IonicModule } from '@ionic/angular';
 
 import { MockComponent } from 'ng-mocks';
 import * as ionic from '@ionic/angular';
-import { SimpleChange } from '@angular/core';
 
+import { DesignTokenHelper } from '../../helpers/design-token-helper';
 import { CheckboxComponent } from './checkbox.component';
 
-describe('CheckboxComponent', () => {
-  const checked = true;
-  const shape = 'square';
-  const color = 'primary';
+const getColor = DesignTokenHelper.getColor;
 
+fdescribe('CheckboxComponent', () => {
   let spectator: Spectator<CheckboxComponent>;
+  let element: HTMLButtonElement;
 
-  const createHost = createComponentFactory({
+  const createHost = createHostFactory({
     component: CheckboxComponent,
-    declarations: [MockComponent(ionic.IonCheckbox)],
+    declarations: [IonCheckbox],
+    imports: [IonicModule.forRoot({ mode: 'ios', _testing: true })],
   });
 
   beforeEach(() => {
-    spectator = createHost({ props: { checked, shape, color } });
+    spectator = createHost(`<kirby-checkbox></kirby-checkbox>`);
+    element = spectator.query('ion-checkbox') as HTMLButtonElement;
   });
 
   it('should create', () => {
@@ -28,62 +29,42 @@ describe('CheckboxComponent', () => {
   });
 
   describe('event: checkedChange', () => {
-    it('should emit true', () => {
-      spyOn(spectator.component.checkedChange, 'emit');
-      const change = true;
+    it('should emit true when checked', () => {
+      const checked = true;
 
-      spectator.component.onChecked(change);
+      spyOn(spectator.component.checkedChange, 'emit');
+      spectator.component.onChecked(checked);
 
       expect(spectator.component.checkedChange.emit).toHaveBeenCalledTimes(1);
       expect(spectator.component.checkedChange.emit).toHaveBeenCalledWith(true);
     });
 
-    it('should emit false', () => {
-      spyOn(spectator.component.checkedChange, 'emit');
-      const change = false;
+    it('should emit false when unchecked', () => {
+      const checked = false;
 
-      spectator.component.onChecked(change);
+      spyOn(spectator.component.checkedChange, 'emit');
+      spectator.component.onChecked(checked);
 
       expect(spectator.component.checkedChange.emit).toHaveBeenCalledTimes(1);
       expect(spectator.component.checkedChange.emit).toHaveBeenCalledWith(false);
     });
   });
 
-  describe('Inputs', () => {
-    it('should bind the class .square to ion-checkbox', () => {
-      const squareShape = 'square';
-      spectator.component.ngOnChanges({
-        shape: new SimpleChange(null, squareShape, false),
-      });
+  describe('Input', () => {
+    it('should not set class .multi on ion-checkbox when no type', () => {
+      const type = 'single';
 
-      spectator.setInput('shape', squareShape);
+      spectator.setInput('type', type);
 
-      expect(spectator.query('ion-checkbox')).toHaveClass('square');
-      expect(spectator.query('ion-checkbox')).not.toHaveClass('circle');
+      expect(element).not.toHaveClass('multi');
     });
 
-    it('should bind the class .circle to ion-checkbox', () => {
-      const circleShape = 'circle';
-      spectator.component.ngOnChanges({
-        shape: new SimpleChange(null, circleShape, false),
-      });
+    it('should set class .multi on ion-checkbox when type is "multi"', () => {
+      const type = 'multi';
 
-      spectator.setInput('shape', circleShape);
+      spectator.setInput('type', type);
 
-      expect(spectator.query('ion-checkbox')).toHaveClass('circle');
-      expect(spectator.query('ion-checkbox')).not.toHaveClass('square');
-    });
-
-    it('should bind the class .primary to ion-checkbox', () => {
-      const color = 'primary';
-      spectator.component.ngOnChanges({
-        color: new SimpleChange(null, color, false),
-      });
-
-      spectator.setInput('color', color);
-
-      expect(spectator.query('ion-checkbox')).toHaveClass('primary');
-      expect(spectator.query('ion-checkbox')).not.toHaveClass('secondary');
+      expect(element).toHaveClass('multi');
     });
 
     it('should set the [checked] input on ion-checkbox to true', () => {
@@ -100,6 +81,38 @@ describe('CheckboxComponent', () => {
       spectator.setInput('checked', checked);
 
       expect((spectator.query(IonCheckbox) as IonCheckbox).checked).toBe(false);
+    });
+
+    it('should set the [disabled] input on ion-checkbox to true', () => {
+      const disabled = true;
+
+      spectator.setInput('disabled', disabled);
+
+      expect((spectator.query(IonCheckbox) as IonCheckbox).disabled).toBe(true);
+    });
+
+    it('should set the [disabled] input on ion-checkbox to false', () => {
+      const disabled = false;
+
+      spectator.setInput('disabled', disabled);
+
+      expect((spectator.query(IonCheckbox) as IonCheckbox).disabled).toBe(false);
+    });
+
+    it('should have error class when [error] input is true', () => {
+      const error = true;
+
+      spectator.setInput('error', error);
+
+      expect(element).toHaveClass('error');
+    });
+
+    it('should not have error class when [error] input is false', () => {
+      const error = false;
+
+      spectator.setInput('error', error);
+
+      expect(element).not.toHaveClass('error');
     });
   });
 });
