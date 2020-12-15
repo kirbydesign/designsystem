55812b39f383dcfe0f2d04d0cc64431e6d602e3c
Christian K. Ibsen <jb7612@jyskebank.dk>
2020-12-15 13:47:57 +0100

    Brackets around selectable property in item

--- apps/cookbook/src/app/examples/item-example/examples/card.ts
+++ apps/cookbook/src/app/examples/item-example/examples/card.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 const config = {
   selector: 'cookbook-item-example-card',
   template: `<kirby-card>
-  <kirby-item selectable="true">
+  <kirby-item [selectable]="true">
     <h3>Title</h3>
     <kirby-toggle slot="end"></kirby-toggle>
   </kirby-item>
