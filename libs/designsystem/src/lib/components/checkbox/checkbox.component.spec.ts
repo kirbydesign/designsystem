9d34d906738d4dd08ec23228483595f27a6aff79
Henrik Stokbro <hstokbro@gmail.com>
2020-12-15 16:19:21 +0100

    Update tests - Inlining values and moving default

--- libs/designsystem/src/lib/components/checkbox/checkbox.component.spec.ts
+++ libs/designsystem/src/lib/components/checkbox/checkbox.component.spec.ts
@@ -21,80 +21,66 @@ describe('CheckboxComponent', () => {
     expect(spectator.component).toBeTruthy();
   });
 
-  describe('when configured', () => {
-    describe('by default', () => {
-      it('should have attention level 2', () => {
-        expect(spectator.component.attentionLevel).toBe('2');
-        expect(spectator.element).toHaveClass('attention-level2');
-      });
-
-      it('should not be checked', () => {
-        expect(spectator.component.checked).toBe(false);
-      });
-
-      it('should not be disabled', () => {
-        expect(spectator.component.disabled).toBe(false);
-      });
-
-      it('should not have error', () => {
-        expect(spectator.component.hasError).toBe(false);
-      });
+  describe('by default', () => {
+    it('should have attention level 2', () => {
+      expect(spectator.component.attentionLevel).toBe('2');
+      expect(spectator.element).toHaveClass('attention-level2');
     });
 
-    it('should have class .attention-level1 when attentionLevel is 1', () => {
-      const attentionLevel = '1';
+    it('should not be checked', () => {
+      expect(spectator.component.checked).toBe(false);
+    });
 
-      spectator.setInput('attentionLevel', attentionLevel);
+    it('should not be disabled', () => {
+      expect(spectator.component.disabled).toBe(false);
+    });
+
+    it('should not have error', () => {
+      expect(spectator.component.hasError).toBe(false);
+    });
+  });
+
+  describe('when configured', () => {
+    it('should have class .attention-level1 when attentionLevel is 1', () => {
+      spectator.setInput('attentionLevel', '1');
       spectator.detectChanges();
 
       expect(spectator.element).toHaveClass('attention-level1');
     });
 
     it('should set the [checked] input on ion-checkbox to true', () => {
-      const checked = true;
-
-      spectator.setInput('checked', checked);
+      spectator.setInput('checked', true);
 
       expect(checkbox.checked).toBe(true);
     });
 
     it('should set the [checked] input on ion-checkbox to false', () => {
-      const checked = false;
-
-      spectator.setInput('checked', checked);
+      spectator.setInput('checked', false);
 
       expect(checkbox.checked).toBe(false);
     });
 
     it('should set the [disabled] input on ion-checkbox to true', () => {
-      const disabled = true;
-
-      spectator.setInput('disabled', disabled);
+      spectator.setInput('disabled', true);
 
       expect(checkbox.disabled).toBe(true);
     });
 
     it('should set the [disabled] input on ion-checkbox to false', () => {
-      const disabled = false;
-
-      spectator.setInput('disabled', disabled);
+      spectator.setInput('disabled', false);
 
       expect(checkbox.disabled).toBe(false);
     });
 
     it('should have error class when [hasError] input is true', () => {
-      const hasError = true;
-
-      spectator.setInput('hasError', hasError);
+      spectator.setInput('hasError', true);
       spectator.detectChanges();
 
       expect(spectator.element).toHaveClass('error');
     });
 
     it('should not have error class when [hasError] input is false', () => {
-      const hasError = false;
-
-      spectator.setInput('hasError', hasError);
+      spectator.setInput('hasError', false);
       spectator.detectChanges();
 
       expect(spectator.element).not.toHaveClass('error');
@@ -103,20 +89,16 @@ describe('CheckboxComponent', () => {
 
   describe('when checking checkbox', () => {
     it('should emit true with event checkedChange when checked', () => {
-      const checked = true;
-
       spyOn(spectator.component.checkedChange, 'emit');
-      spectator.component.onChecked(checked);
+      spectator.component.onChecked(true);
 
       expect(spectator.component.checkedChange.emit).toHaveBeenCalledTimes(1);
       expect(spectator.component.checkedChange.emit).toHaveBeenCalledWith(true);
     });
 
     it('should emit false with event checkedChange when unchecked', () => {
-      const checked = false;
-
       spyOn(spectator.component.checkedChange, 'emit');
-      spectator.component.onChecked(checked);
+      spectator.component.onChecked(false);
 
       expect(spectator.component.checkedChange.emit).toHaveBeenCalledTimes(1);
       expect(spectator.component.checkedChange.emit).toHaveBeenCalledWith(false);
