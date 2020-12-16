81c920038c49035eb51b5479f1514587f1661ee2
Henrik Stokbro <hstokbro@gmail.com>
2020-12-16 14:55:12 +0100

    Update mock.checkbox.component.ts

--- libs/designsystem/testing-base/src/lib/components/mock.checkbox.component.ts
+++ libs/designsystem/testing-base/src/lib/components/mock.checkbox.component.ts
@@ -16,7 +16,7 @@ import { CheckboxComponent } from '@kirbydesign/designsystem';
 export class MockCheckboxComponent {
   @Input() checked: boolean;
   @Input() attentionLevel: '1' | '2';
-  @Input() labelText: string;
+  @Input() text: string;
   @Input() hasError: boolean;
   @Input() disabled: boolean;
   @Output() checkedChange = new EventEmitter<boolean>();
