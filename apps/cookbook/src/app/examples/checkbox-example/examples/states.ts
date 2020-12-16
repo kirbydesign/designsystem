fcae1443ab8821e324d6bfb3c4aa181f24f81666
Henrik Stokbro <hstokbro@gmail.com>
2020-12-16 14:49:55 +0100

    change checked to [checked]

--- apps/cookbook/src/app/examples/checkbox-example/examples/states.ts
+++ apps/cookbook/src/app/examples/checkbox-example/examples/states.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 const config = {
   selector: 'cookbook-checkbox-states-example',
   template: `<kirby-checkbox labelText="Default"></kirby-checkbox>
-<kirby-checkbox checked="true" labelText="Checked"></kirby-checkbox>
+<kirby-checkbox [checked]="true" labelText="Checked"></kirby-checkbox>
 <kirby-checkbox disabled="true" labelText="Disabled"></kirby-checkbox>
-<kirby-checkbox disabled="true" checked="true" labelText="Disabled checked"></kirby-checkbox>
+<kirby-checkbox disabled="true" [checked]="true" labelText="Disabled checked"></kirby-checkbox>
 <kirby-checkbox hasError="true" labelText="Has error"></kirby-checkbox>`,
 };
 
