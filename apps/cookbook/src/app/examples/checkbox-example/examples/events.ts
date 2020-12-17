2be68916c808c9dce2642da30cdbdf705853a9ac
Jakob Engelbrecht <jakob@basher.dk>
2020-12-17 21:01:13 +0100

    📝 Replace alert with toast

--- apps/cookbook/src/app/examples/checkbox-example/examples/events.ts
+++ apps/cookbook/src/app/examples/checkbox-example/examples/events.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { ToastConfig, ToastController } from '@kirbydesign/designsystem';
+
 const config = {
   selector: 'cookbook-checkbox-events-example',
   template: `<kirby-checkbox (checkedChange)="checkedChange($event)" text="Toggle to see 'checkedChange' event in action"></kirby-checkbox>`,
@@ -16,7 +18,14 @@ export class CheckboxEventsExampleComponent {
   template: string = config.template;
   checkboxEventCodeSnippet: string = config.checkboxEventCodeSnippet;
 
+  constructor(private toastController: ToastController) {}
+
   checkedChange(checked: boolean) {
-    alert(`Checkbox checked change: ${checked}`);
+    const config: ToastConfig = {
+      message: `Checkbox changed - checked: ${checked}`,
+      messageType: checked ? 'success' : 'warning',
+      durationInMs: 1500,
+    };
+    this.toastController.showToast(config);
   }
 }
