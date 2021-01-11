2aeb0e3b627ee12202b05cb7f58b04fb27980b5f
Jakob Engelbrecht <jakob@basher.dk>
2021-01-11 12:56:12 +0100

    🤡  Update mock

--- libs/designsystem/testing-base/src/lib/components/mock.radio.component.ts
+++ libs/designsystem/testing-base/src/lib/components/mock.radio.component.ts
@@ -16,6 +16,7 @@ import { RadioComponent } from '@kirbydesign/designsystem';
 export class MockRadioComponent {
   @Input() value: any;
   @Input() text: string;
+  @Input() size: 'xs' | 'sm' | 'md';
   @Input() hasError: boolean;
   @Input() disabled: boolean;
 }
