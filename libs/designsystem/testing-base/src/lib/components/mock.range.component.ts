396aca86b54bfd63c360765808c1a53083f8f38b
karlgodtliebsen <karl@godtliebsen.dk>
2021-01-25 08:14:36 +0100

    added the mock components for range

---
+++ libs/designsystem/testing-base/src/lib/components/mock.range.component.ts
@@ -0,0 +1,50 @@
+import { forwardRef, Component, Input, Output, EventEmitter } from '@angular/core';
+
+import { RangeComponent } from '@kirbydesign/designsystem';
+
+// #region AUTO-GENERATED - PLEASE DON'T EDIT CONTENT WITHIN!
+@Component({
+  selector: 'kirby-range',
+  template: '<ng-content></ng-content>',
+  providers: [
+    {
+      provide: RangeComponent,
+      useExisting: forwardRef(() => MockRangeComponent),
+    },
+  ],
+})
+export class MockRangeComponent {
+  @Input() color: string;
+  @Input() debounce: number;
+  @Input() disabled: boolean;
+  @Input() max: number;
+  @Input() min: number;
+  @Input() mode: 'ios' | 'md';
+  @Input() name: string;
+  @Input() pin: boolean;
+  @Input() snaps: boolean;
+  @Input() step: number;
+  @Input() ticks: number;
+  @Output() valueChange = new EventEmitter<RangeValue>();
+  @Input() startLabel: string;
+  @Input() endLabel: string;
+
+  rangeValueChange() {}
+  writeValue() {}
+  registerOnTouched() {}
+  registerOnChange() {}
+  setLabel() {}
+  setLabelFontSize() {}
+  setLabelBackground() {}
+  setKnobBackground() {}
+  setPin() {}
+  setPinFontSize() {}
+  setPinBackground() {}
+  setTick() {}
+  setActiveTick() {}
+  setBar() {}
+  setActiveBar() {}
+  setBarBackground() {}
+}
+
+// #endregion
