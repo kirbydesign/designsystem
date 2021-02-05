4e647d243835dbba9da26fbe05678b8ac3177ab3
Mads Buchmann Frederiksen <bdymfd@bankdata.dk>
2021-02-05 14:59:59 +0100

    ♻️ Don't emit loadOnDemand if there's no observers

--- libs/designsystem/src/lib/components/list/helpers/list-helper.ts
+++ libs/designsystem/src/lib/components/list/helpers/list-helper.ts
@@ -3,7 +3,11 @@ import { LoadOnDemandEventData } from '../list.event';
 
 export class ListHelper {
   onLoadOnDemand(component: ListComponent, _event: LoadOnDemandEventData) {
-    if (!component.disableLoadOnDemand && !component.isLoading) {
+    if (
+      !component.disableLoadOnDemand &&
+      !component.isLoading &&
+      component.hasLoadOnDemandObservers()
+    ) {
       component.isLoading = true;
       component.loadOnDemand.emit({
         complete: (disableLoadOnDemand: boolean) => {
