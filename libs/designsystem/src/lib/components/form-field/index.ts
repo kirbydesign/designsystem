7495b5898ec6cde9a776ca175d3889542bcc9969
karlgodtliebsen <karl@godtliebsen.dk>
2021-02-08 10:59:07 +0100

    mocks

--- libs/designsystem/src/lib/components/form-field/index.ts
+++ libs/designsystem/src/lib/components/form-field/index.ts
@@ -4,3 +4,5 @@ export { InputComponent } from './input/input.component';
 export { InputCounterComponent } from './input-counter/input-counter.component';
 export { TextareaComponent } from './textarea/textarea.component';
 export { InputIconComponent } from './input-icon/input-icon.component';
+export { DateInputDirective } from './directives/date/date-input.directive';
+export { NumericInputDirective } from './directives/numeric/numeric-input.directive';
