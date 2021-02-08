ca110a33e301c3a6a2f207073ef2c9c237ac2773
karlgodtliebsen <karl@godtliebsen.dk>
2021-02-08 09:36:46 +0100

    after merge mock

--- libs/designsystem/testing-base/src/lib/mock-components.ts
+++ libs/designsystem/testing-base/src/lib/mock-components.ts
@@ -1,4 +1,5 @@
 import { MockAccordionItemComponent } from './components/mock.accordion-item.component';
+import { MockActionSheetComponent } from './components/mock.action-sheet.component';
 import { MockAppComponent } from './components/mock.app.component';
 import { MockAvatarComponent } from './components/mock.avatar.component';
 import { MockBadgeComponent } from './components/mock.badge.component';
@@ -16,29 +17,28 @@ import { MockFabSheetComponent } from './components/mock.fab-sheet.component';
 import { MockFlagComponent } from './components/mock.flag.component';
 import { MockFormFieldMessageComponent } from './components/mock.form-field-message.component';
 import { MockFormFieldComponent } from './components/mock.form-field.component';
-import { MockInputComponent } from './components/mock.input.component';
-import { MockInputCounterComponent } from './components/mock.input-counter.component';
-import { MockTextareaComponent } from './components/mock.textarea.component';
+import { MockFullscreenLoadingOverlayComponent } from './components/mock.fullscreen-loading-overlay.component';
 import { MockGridComponent } from './components/mock.grid.component';
 import { MockIconComponent } from './components/mock.icon.component';
+import { MockInputCounterComponent } from './components/mock.input-counter.component';
+import { MockInputIconComponent } from './components/mock.input-icon.component';
+import { MockInputComponent } from './components/mock.input.component';
 import { MockItemComponent } from './components/mock.item.component';
 import { MockLabelComponent } from './components/mock.label.component';
 import { MockListHeaderComponent } from './components/mock.list-header.component';
 import { MockListSectionHeaderComponent } from './components/mock.list-section-header.component';
 import { MockListComponent } from './components/mock.list.component';
-import { MockFullscreenLoadingOverlayComponent } from './components/mock.fullscreen-loading-overlay.component';
 import { MockLoadingOverlayComponent } from './components/mock.loading-overlay.component';
-import { MockActionSheetComponent } from './components/mock.action-sheet.component';
 import { MockModalFooterComponent } from './components/mock.modal-footer.component';
 import {
-  MockPageTitleDirective,
-  MockPageToolbarTitleDirective,
+  MockPageActionsComponent,
   MockPageActionsDirective,
+  MockPageComponent,
+  MockPageContentComponent,
   MockPageContentDirective,
   MockPageTitleComponent,
-  MockPageContentComponent,
-  MockPageActionsComponent,
-  MockPageComponent,
+  MockPageTitleDirective,
+  MockPageToolbarTitleDirective,
 } from './components/mock.page.component';
 import { MockProgressCircleComponent } from './components/mock.progress-circle.component';
 import { MockRadioGroupComponent } from './components/mock.radio-group.component';
@@ -50,8 +50,9 @@ import { MockSlideButtonComponent } from './components/mock.slide-button.compone
 import { MockSpinnerComponent } from './components/mock.spinner.component';
 import { MockTabButtonComponent } from './components/mock.tab-button.component';
 import { MockTabsComponent } from './components/mock.tabs.component';
-import { MockToggleComponent } from './components/mock.toggle.component';
+import { MockTextareaComponent } from './components/mock.textarea.component';
 import { MockToggleButtonComponent } from './components/mock.toggle-button.component';
+import { MockToggleComponent } from './components/mock.toggle.component';
 import { MockToolbarComponent } from './components/mock.toolbar.component';
 
 export const MOCK_COMPONENTS = [
@@ -75,6 +76,7 @@ export const MOCK_COMPONENTS = [
   MockFormFieldComponent,
   MockInputComponent,
   MockInputCounterComponent,
+  MockInputIconComponent,
   MockTextareaComponent,
   MockGridComponent,
   MockIconComponent,
