4f7a6a1bbd44edc2c5665209165b3f39076da878
karlgodtliebsen <karl@godtliebsen.dk>
2021-02-09 15:40:36 +0100

    cleaned prettier errors

--- libs/designsystem/src/lib/components/form-field/input-postfix/input-postfix.component.spec.ts
+++ libs/designsystem/src/lib/components/form-field/input-postfix/input-postfix.component.spec.ts
@@ -1,6 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-
 import { InputPostfixComponent } from './input-postfix.component';
 
 describe('InputPostfixComponent', () => {
--- libs/designsystem/src/lib/components/form-field/input-prefix/input-prefix.component.spec.ts
+++ libs/designsystem/src/lib/components/form-field/input-prefix/input-prefix.component.spec.ts
@@ -1,6 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-
 import { InputPrefixComponent } from './input-prefix.component';
 
 describe('InputPrefixComponent', () => {
