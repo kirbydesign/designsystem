e5c636b284a0ca049e8a69165fe14b5141e0d8db
Mads Buchmann Frederiksen <bdymfd@bankdata.dk>
2021-02-09 10:15:20 +0100

    ✨ scroll to top on route change

--- apps/cookbook/src/app/app-routing.module.ts
+++ apps/cookbook/src/app/app-routing.module.ts
@@ -4,7 +4,12 @@ import { RouterModule } from '@angular/router';
 import { routes } from './app.routes';
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      scrollOffset: [0, 0],
+      scrollPositionRestoration: 'enabled',
+    }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
