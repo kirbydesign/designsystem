44c1407d5bb90def89892caabe8872932ae04ab9
Mads Buchmann Frederiksen <bdymfd@bankdata.dk>
2021-02-11 11:06:32 +0100

    🚨 Reorder imports

--- apps/cookbook/src/app/showcase/showcase.module.ts
+++ apps/cookbook/src/app/showcase/showcase.module.ts
@@ -1,15 +1,17 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { KirbyModule } from '@kirbydesign/designsystem';
-import { COMPONENT_IMPORTS, COMPONENT_EXPORTS, COMPONENT_DECLARATIONS } from './showcase.common';
+
+import { IphoneModule } from '../iphone/iphone.module';
 import { CodeViewerComponent } from '../shared/code-viewer/code-viewer.component';
 import { ExampleViewerComponent } from '../shared/example-viewer/example-viewer.component';
 import { ShowcasePropertiesComponent } from '../shared/showcase-properties/showcase-properties.component';
-import { IphoneModule } from '../iphone/iphone.module';
+
 import { DividerShowcaseComponent } from './divider-showcase/divider-showcase.component';
+import { COMPONENT_DECLARATIONS, COMPONENT_EXPORTS, COMPONENT_IMPORTS } from './showcase.common';
 
 @NgModule({
   imports: [
