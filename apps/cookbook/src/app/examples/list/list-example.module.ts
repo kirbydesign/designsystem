f144054b33a6382aa97010dd6ec777c827dafd18
Mads Buchmann Frederiksen <bdymfd@bankdata.dk>
2021-02-11 12:49:10 +0100

    🚨  reorder imports

--- apps/cookbook/src/app/examples/list/list-example.module.ts
+++ apps/cookbook/src/app/examples/list/list-example.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
 import { KirbyModule } from '@kirbydesign/designsystem';
+
 import { ListBoldTextOnRowSelectionExampleComponent } from './components/bold-text-on-row-selection';
-import { ListWithDividersExampleComponent } from './components/dividers';
-import { ListSelectableItemsExampleComponent } from './components/selectable-items';
 import { ListColoredItemsExampleComponent } from './components/colored-items';
-import { ListWithSectionsAndColoredItemsExampleComponent } from './components/sections-and-colored-items';
+import { ListWithDividersExampleComponent } from './components/dividers';
 import { ListWithHeaderAndFooterExampleComponent } from './components/header-and-footer';
-import { ListWithSectionsExampleComponent } from './components/sections';
 import { ListItemsExampleComponent } from './components/items';
+import { ListWithSectionsExampleComponent } from './components/sections';
+import { ListWithSectionsAndColoredItemsExampleComponent } from './components/sections-and-colored-items';
+import { ListSelectableItemsExampleComponent } from './components/selectable-items';
 
 const listExamples = [
   ListBoldTextOnRowSelectionExampleComponent,
