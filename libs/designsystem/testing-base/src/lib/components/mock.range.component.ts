72de021bd99c8dedec26c3d01a83514a47b449de
karlgodtliebsen <karl@godtliebsen.dk>
2021-02-17 14:58:17 +0100

    changed to embed a ion-range component

--- libs/designsystem/testing-base/src/lib/components/mock.range.component.ts
+++ libs/designsystem/testing-base/src/lib/components/mock.range.component.ts
@@ -17,6 +17,26 @@ import { RangeValue } from '@kirbydesign/designsystem';
 export class MockRangeComponent {
   @Input() minLabel: string;
   @Input() maxLabel: string;
+  @Input() color: string;
+  @Input() debounce: number;
+  @Input() max: number;
+  @Input() min: number;
+  @Input() mode: 'ios' | 'md';
+  @Input() name: string;
+  @Input() pin: boolean;
+  @Input() snaps: boolean;
+  @Input() step: number;
+  @Input() ticks: number;
+  @Output() valueChange = new EventEmitter<RangeValue>();
+  @Input() startLabel: string;
+  @Input() endLabel: string;
+  @Input() disabled: boolean;
+
+  setDisabledState() {}
+  rangeValueChange() {}
+  writeValue() {}
+  registerOnTouched() {}
+  registerOnChange() {}
 }
 
 // #endregion
