0527921d4f674a47259fba6ba90f071b2c876509
Mads Buchmann Frederiksen <mbf@prochimp.dk>
2021-02-18 16:11:50 +0100

    ✅ test button attention level rules

--- libs/designsystem/src/lib/components/empty-state/empty-state.component.spec.ts
+++ libs/designsystem/src/lib/components/empty-state/empty-state.component.spec.ts
@@ -1,8 +1,10 @@
+import { createHostFactory, SpectatorHost } from '@ngneat/spectator';
 import { MockComponent } from 'ng-mocks';
-import { SpectatorHost, createHostFactory } from '@ngneat/spectator';
+
+import { ButtonComponent } from '../button/button.component';
+import { IconComponent } from '../icon/icon.component';
 
 import { DesignTokenHelper } from './../../helpers/design-token-helper';
-import { IconComponent } from '../icon/icon.component';
 import { EmptyStateComponent } from './empty-state.component';
 
 describe('EmptyStateComponent', () => {
@@ -11,7 +13,7 @@ describe('EmptyStateComponent', () => {
 
   const createHost = createHostFactory({
     component: EmptyStateComponent,
-    declarations: [MockComponent(IconComponent)],
+    declarations: [MockComponent(IconComponent), ButtonComponent],
   });
 
   beforeEach(() => {
@@ -22,6 +24,9 @@ describe('EmptyStateComponent', () => {
       subtitle="You don't have any items. Call support to add some items to your account."
     >
       <button kirby-button>Call support</button>
+      <button kirby-button attentionLevel='2'> Mail support</button>
+      <button kirby-button attentionLevel='3'> Get directions </button>
+      <button kirby-button attentionLevel='1'> Mail support</button>
     </kirby-empty-state>
     `);
     element = spectator.element;
@@ -34,5 +39,33 @@ describe('EmptyStateComponent', () => {
   it('should render with the correct border width', () => {
     const outlineElement = element.getElementsByClassName('icon-outline')[0];
     expect(outlineElement).toHaveComputedStyle({ 'border-width': DesignTokenHelper.size('xxxs') });
+  });
+
+  describe('Attention level rules for buttons', () => {
+    let buttons: ButtonComponent[];
+
+    beforeEach(() => {
+      buttons = spectator.queryAll(ButtonComponent);
+    });
+
+    it('should change all buttons to attention level 3 except 1', () => {
+      const numberLvl3Buttons = buttons.filter((button) => button.isAttentionLevel3).length;
+      const numberLvl1Buttons = buttons.filter((button) => button.isAttentionLevel1).length;
+
+      expect(numberLvl1Buttons).toEqual(1);
+      expect(numberLvl3Buttons).toEqual(3);
+    });
+
+    it('should keep the attention level on the first button with attention level 1', () => {
+      expect(buttons.findIndex((button) => button.isAttentionLevel1)).toEqual(0);
+    });
+
+    it('should enforce attention level rules when a button is added', () => {
+      // TODO: implement test...
+    });
+
+    it('should enforce attention level rules when a button is removed', () => {
+      // TODO: implement test...
+    });
   });
 });
