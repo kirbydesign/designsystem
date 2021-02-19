f6351379c46fe06131ffee6544db8c6862b7be51
Mads Buchmann Frederiksen <mbf@prochimp.dk>
2021-02-19 11:10:12 +0100

    🤡 Add mock for Size directive

--- libs/designsystem/testing-base/src/lib/mock-directives.ts
+++ libs/designsystem/testing-base/src/lib/mock-directives.ts
@@ -1,3 +1,4 @@
+import { MockSizeDirective } from './directives/mock.size.directive';
 import { MockThemeColorDirective } from './directives/mock.theme-color.directive';
 
-export const MOCK_DIRECTIVES = [MockThemeColorDirective];
+export const MOCK_DIRECTIVES = [MockSizeDirective, MockThemeColorDirective];
