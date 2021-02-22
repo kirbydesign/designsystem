2918a4fd6505ffc593fdca362ad23958a8e9655b
Jesper Kaltoft <git@jesperkaltoft.dk>
2021-02-22 11:12:25 +0100

    📝 Showcase CSS custom properties in table

--- apps/cookbook/src/app/showcase/item-showcase/item-showcase.component.ts
+++ apps/cookbook/src/app/showcase/item-showcase/item-showcase.component.ts
@@ -8,7 +8,29 @@ import { ShowcaseProperty } from '../../shared/showcase-properties/showcase-prop
   styleUrls: ['./item-showcase.component.scss'],
 })
 export class ItemShowcaseComponent {
-  properties: ShowcaseProperty[] = [];
+  columns = {
+    Name: 'Name',
+    Description: 'Description',
+  };
+
+  properties: ShowcaseProperty[] = [
+    {
+      name: '--kirby-item-background',
+      description: 'Background of the item',
+    },
+    {
+      name: '--kirby-item-background-activated',
+      description: 'Background of the item when pressed',
+    },
+    {
+      name: '--kirby-item-background-focused',
+      description: 'Background of the item when focused with the tab key',
+    },
+    {
+      name: '--kirby-item-background-hover',
+      description: 'Background of the item on hover',
+    },
+  ];
 
   scrollTo(target: Element) {
     target.scrollIntoView({ behavior: 'smooth' });
