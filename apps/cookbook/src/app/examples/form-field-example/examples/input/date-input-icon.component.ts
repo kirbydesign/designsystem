b2cf7552b5efe916b76ae4cdce34fa824ccb2652
karlgodtliebsen <karl@godtliebsen.dk>
2021-02-23 10:24:39 +0100

    simplified the example for date input with icon: calendar demo

--- apps/cookbook/src/app/examples/form-field-example/examples/input/date-input-icon.component.ts
+++ apps/cookbook/src/app/examples/form-field-example/examples/input/date-input-icon.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit, Output } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import moment from 'moment';
 
 const config = {
   selector: 'cookbook-form-field-date-input-icon-example',
@@ -14,7 +15,7 @@ const config = {
   <kirby-card *ngIf="showCalendar">
    <kirby-calendar
     [timezone]="'local'"
-    [selectedDate]="dateDemo"
+    [selectedDate]="calendarDateDemo"
     (dateChange)="onDateChange($event)"
   >
   </kirby-calendar>
@@ -28,27 +29,31 @@ const config = {
 export class FormFieldDateInputIconExampleComponent implements OnInit {
   template: string = config.template;
 
-  dateFormGroup: FormGroup;
+  public dateFormGroup: FormGroup;
   @Input() showCalendar: boolean;
 
   public onClick($event: any): void {
     this.showCalendar = !this.showCalendar;
   }
 
-  public onDateChange($event: Date): void {
-    this.dateFormGroup.patchValue({ dateDemo: $event.toLocaleDateString() });
+  public onDateChange($event: any): void {
+    this.dateFormGroup.patchValue({ dateDemo: '01/01/2021' });
     this.showCalendar = false;
   }
 
-  public get dateDemo(): AbstractControl {
-    return this.dateFormGroup.get('dateDemo');
+  public get dateDemo(): any {
+    return this.dateFormGroup.get('dateDemo').value;
+  }
+
+  public get calendarDateDemo(): moment.Moment {
+    return moment();
   }
 
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
     this.dateFormGroup = this.formBuilder.group({
-      dateDemo: [{ value: '01/01/2021', disabled: false }],
+      dateDemo: [{ value: new Date().toLocaleDateString(), disabled: false }],
     });
   }
 }
