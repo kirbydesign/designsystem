8ab28cdcb8c318e813ce0bc31e416e48a3f5841d
Mads Buchmann Frederiksen <mbf@prochimp.dk>
2021-02-24 17:12:41 +0100

    📝 Update showcase for list loadOnDemand

--- apps/cookbook/src/app/showcase/list-load-on-demand-showcase/list-load-on-demand-showcase.component.ts
+++ apps/cookbook/src/app/showcase/list-load-on-demand-showcase/list-load-on-demand-showcase.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+
+import { ShowcaseProperty } from './../../shared/showcase-properties/showcase-property';
+
 declare var require: any;
 
 @Component({
@@ -9,4 +12,34 @@ declare var require: any;
 export class ListLoadOnDemandShowcaseComponent {
   exampleHtml: string = require('!raw-loader!../../examples/list/components/load-on-demand/list-load-on-demand-example.component.html')
     .default;
+
+  properties: ShowcaseProperty[] = [
+    {
+      name: 'isLoadOnDemandEnabled',
+      description: 'Determines if the loadOnDemand event should be emitted.',
+      inputValues: ['boolean'],
+      defaultValue: 'If there is a subscriber to the loadOnDemand event: true, otherwise false.',
+    },
+    {
+      name: 'noMoreItemsText',
+      description:
+        'Text to display when no more items can be loaded (used for "on demand"-loading).',
+      inputValues: ['string'],
+      defaultValue: 'undefined',
+    },
+  ];
+
+  events: ShowcaseProperty[] = [
+    {
+      name: 'loadOnDemand',
+      description: `Emitted when the user has scrolled 80% of the list.`,
+      inputValues: [`KirbyListLoadMoreEvent { complete: (disableLoadMore?: boolean) => void; }`],
+    },
+  ];
+
+  eventsColumns = {
+    Name: 'Name',
+    Description: 'Description',
+    Type: 'Signature',
+  };
 }
