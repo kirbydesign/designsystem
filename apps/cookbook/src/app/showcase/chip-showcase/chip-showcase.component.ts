a95efc4d1dcbd1a5d9d144dfc9af4f67ff3ac077
Jesper Kaltoft <git@jesperkaltoft.dk>
2021-03-08 16:45:00 +0100

    📝 Use cookbook-showcase-properties in Chip showcase

--- apps/cookbook/src/app/showcase/chip-showcase/chip-showcase.component.ts
+++ apps/cookbook/src/app/showcase/chip-showcase/chip-showcase.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {
+  ShowcaseProperty,
+  ShowcasePropertyColumns,
+} from '~/app/shared/showcase-properties/showcase-properties.component';
 
 @Component({
   selector: 'cookbook-chip-showcase',
@@ -6,6 +10,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./chip-showcase.component.scss'],
 })
 export class ChipShowcaseComponent {
+  columns: ShowcasePropertyColumns = {
+    Name: 'Name',
+    Description: 'Description',
+    Type: 'Input values',
+  };
+  properties: ShowcaseProperty[] = [
+    {
+      name: 'text',
+      description: 'The text displayed inside the chip',
+      inputValues: ['string'],
+    },
+    {
+      name: 'isSelected',
+      description: 'Show chip as selected',
+      inputValues: ['boolean'],
+    },
+  ];
+
   themeColors = [
     { text: 'None', value: '' },
     { text: 'Light', value: 'light' },
