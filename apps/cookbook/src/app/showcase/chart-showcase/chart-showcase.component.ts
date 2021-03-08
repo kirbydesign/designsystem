d7bd7223632ee714be089d952fb97b56c9540091
Jesper Kaltoft <git@jesperkaltoft.dk>
2021-03-08 16:32:46 +0100

    📝 Use cookbook-showcase-properties in Chart showcase

--- apps/cookbook/src/app/showcase/chart-showcase/chart-showcase.component.ts
+++ apps/cookbook/src/app/showcase/chart-showcase/chart-showcase.component.ts
@@ -1,8 +1,53 @@
 import { Component } from '@angular/core';
+import {
+  ShowcaseProperty,
+  ShowcasePropertyColumns,
+} from '~/app/shared/showcase-properties/showcase-properties.component';
 
 @Component({
   selector: 'cookbook-chart-showcase',
   templateUrl: './chart-showcase.component.html',
   styleUrls: ['./chart-showcase.component.scss'],
 })
-export class ChartShowcaseComponent {}
+export class ChartShowcaseComponent {
+  columns: ShowcasePropertyColumns = {
+    Name: 'Name',
+    Description: 'Description',
+    Type: 'Input values',
+    Default: 'Default',
+  };
+  properties: ShowcaseProperty[] = [
+    {
+      name: 'height',
+      description: 'The height of the chart in px for web and dp for native',
+      inputValues: ['number'],
+    },
+    {
+      name: 'type',
+      description: 'The type of chart',
+      inputValues: ['donut', 'pie', 'areaspline'],
+    },
+    {
+      name: 'showDataLabels',
+      description: 'Enable or disable labels around pie/donut charts',
+      inputValues: ['true', 'false'],
+      defaultValue: 'true',
+    },
+    {
+      name: 'description',
+      description: 'Description of chart for accessibility',
+      inputValues: ['string - e.g. "Distribution of assets in custody account"'],
+    },
+    {
+      name: 'data',
+      description: 'Data input for chart',
+      inputValues: ['See examples above for exact format'],
+    },
+    {
+      name: 'options',
+      description:
+        'The Highcharts options to configure the chart with (will override existing options using a deep merge)',
+      inputValues: ['See [Highcharts options docs](https://api.highcharts.com/highcharts/)'],
+    },
+  ];
+}
