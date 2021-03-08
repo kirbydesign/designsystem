f8657291a57e46ade2a8fc0b9231f4e093d10248
Jesper Kaltoft <git@jesperkaltoft.dk>
2021-03-08 16:54:00 +0100

    📝 Use cookbook-showcase-properties in Stock chart showcase

--- apps/cookbook/src/app/showcase/stock-chart-showcase/stock-chart-showcase.component.ts
+++ apps/cookbook/src/app/showcase/stock-chart-showcase/stock-chart-showcase.component.ts
@@ -1,8 +1,40 @@
 import { Component } from '@angular/core';
+import {
+  ShowcaseProperty,
+  ShowcasePropertyColumns,
+} from '~/app/shared/showcase-properties/showcase-properties.component';
 
 @Component({
   selector: 'cookbook-highstock-chart-showcase',
   templateUrl: './stock-chart-showcase.component.html',
   styleUrls: ['./stock-chart-showcase.component.scss'],
 })
-export class StockChartShowcaseComponent {}
+export class StockChartShowcaseComponent {
+  columns: ShowcasePropertyColumns = {
+    Name: 'Name',
+    Description: 'Description',
+    Type: 'Input values',
+  };
+  properties: ShowcaseProperty[] = [
+    {
+      name: 'description',
+      description: 'Description of Highstock Chart for accessibility',
+      inputValues: ['string - e.g. "Microsoft Stock (NASDAQ: MSFT) today"'],
+    },
+    {
+      name: 'options',
+      description: 'Options input for Highstock Chart',
+      inputValues: ['Options'],
+    },
+    {
+      name: 'data',
+      description: 'Data input for Highstock Chart',
+      inputValues: ['StockChartDataPoint[]'],
+    },
+    {
+      name: 'height',
+      description: 'The height of the Highstock Chart in px for web and dp for native',
+      inputValues: ['number'],
+    },
+  ];
+}
