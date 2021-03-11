08a9f114b5c03daec69627d96b6ca73ddc434649
Flemming Steinson <fsteinson@gmail.com>
2021-03-11 13:45:21 +0100

    Fixed e-2-e tests

--- libs/core/src/components/badge/badge.e2e.ts
+++ libs/core/src/components/badge/badge.e2e.ts
@@ -3,7 +3,7 @@ import { newE2EPage } from '@stencil/core/testing';
 describe('kirby-badge', () => {
   it('renders', async () => {
     const page = await newE2EPage();
-    await page.setContent('<kirby-core-badge></kirby-core-badge>');
+    await page.setContent('<kirby-badge></kirby-badge>');
 
     const element = await page.find('kirby-badge');
     expect(element).toHaveClass('hydrated');
