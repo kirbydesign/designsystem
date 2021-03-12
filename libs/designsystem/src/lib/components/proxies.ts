44ebfa024bd9723a3919143260a4e9d0bffb2b46
Flemming Steinson <fsteinson@gmail.com>
2021-03-12 10:43:26 +0100

    try to fix ci build

--- libs/designsystem/src/lib/components/proxies.ts
+++ libs/designsystem/src/lib/components/proxies.ts
@@ -1,25 +1,20 @@
 /* tslint:disable */
 /* auto-generated angular directive proxies */
-import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-  ElementRef,
-  NgZone,
-} from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, NgZone } from '@angular/core';
 import { ProxyCmp, proxyOutputs } from './angular-component-lib/utils';
 
 import { Components } from '@kirbydesign/core';
 
+
 export declare interface KirbyBadge extends Components.KirbyBadge {}
 @ProxyCmp({
-  inputs: ['text'],
+  inputs: ['text']
 })
 @Component({
   selector: 'kirby-badge',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: '<ng-content></ng-content>',
-  inputs: ['text'],
+  inputs: ['text']
 })
 export class KirbyBadge {
   protected el: HTMLElement;
