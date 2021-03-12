ecbcbc7e1aee245723b2c2d0a9260cb44fd85869
Flemming Steinson <fsteinson@gmail.com>
2021-03-12 11:04:31 +0100

    updated prettier ignore

--- libs/designsystem/src/lib/components/angular-component-lib/utils.ts
+++ libs/designsystem/src/lib/components/angular-component-lib/utils.ts
@@ -17,7 +17,7 @@ export const proxyInputs = (Cmp: any, inputs: string[]) => {
 export const proxyMethods = (Cmp: any, methods: string[]) => {
   const Prototype = Cmp.prototype;
   methods.forEach((methodName) => {
-    Prototype[methodName] = function() {
+    Prototype[methodName] = function () {
       const args = arguments;
       return this.z.runOutsideAngular(() => this.el[methodName].apply(this.el, args));
     };
@@ -30,7 +30,7 @@ export const proxyOutputs = (instance: any, el: any, events: string[]) => {
 
 // tslint:disable-next-line: only-arrow-functions
 export function ProxyCmp(opts: { inputs?: any; methods?: any }) {
-  const decorator = function(cls: any) {
+  const decorator = function (cls: any) {
     if (opts.inputs) {
       proxyInputs(cls, opts.inputs);
     }
