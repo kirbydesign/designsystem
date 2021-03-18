f3a847a334954b7ed14bb6e52f31784243484127
Flemming Steinson <fsteinson@gmail.com>
2021-03-18 13:01:49 +0100

    yet another attempt

--- libs/core/stencil.config.ts
+++ libs/core/stencil.config.ts
@@ -4,7 +4,7 @@ import { sass } from '@stencil/sass';
 
 export const config: Config = {
   namespace: 'kirby',
-  plugins: [sass({ injectGlobalPaths: ['../designsystem/src/lib/scss/utils'] })],
+  plugins: [sass({ injectGlobalPaths: ['./src/scss/utils'] })],
   outputTargets: [
     angularOutputTarget({
       componentCorePackage: '@kirbydesign/core',
@@ -13,6 +13,7 @@ export const config: Config = {
     {
       type: 'dist',
       esmLoaderPath: '../loader',
+      // copy: [{ src: './scss', dest: '../scss' }],
     },
     { type: 'docs-readme' },
     { type: 'docs-json', file: './component-docs.json' },
--- libs/designsystem/src/lib/index.ts
+++ libs/designsystem/src/lib/index.ts
@@ -2,7 +2,7 @@ export * from './animation/kirby-animation';
 export * from './components';
 export * from './directives';
 export * from './helpers';
-export * from '@kirbydesign/core/';
+export * from './scss/scss-helper';
 // export * from './testing';
 
 export { KirbyModule } from './kirby.module';
