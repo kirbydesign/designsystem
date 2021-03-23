12202e6a8377d7518d7a14e88512ee4d008d1ae6
karlgodtliebsen <karl@godtliebsen.dk>
2021-03-23 09:43:57 +0100

    starting to extend documentation

--- apps/cookbook/src/app/showcase/range-showcase/range-showcase.component.ts
+++ apps/cookbook/src/app/showcase/range-showcase/range-showcase.component.ts
@@ -1,8 +1,29 @@
 import { Component } from '@angular/core';
+import { ApiDescriptionProperty } from '~/app/shared/api-description/api-description-properties/api-description-properties.component';
 
 @Component({
   selector: 'cookbook-range-showcase',
   templateUrl: './range-showcase.component.html',
   styleUrls: ['./range-showcase.component.scss'],
 })
-export class RangeShowcaseComponent {}
+export class RangeShowcaseComponent {
+  rangeProperties: ApiDescriptionProperty[] = [
+    {
+      name: 'value',
+      description: 'The value represented by the radio button.',
+      defaultValue: 'undefined',
+      type: ['any'],
+    },
+    {
+      name: 'disabled',
+      description: 'Disables the radio button so that the user cannot interact with it.',
+      defaultValue: 'false',
+      type: ['true', 'false'],
+    },
+    {
+      name: 'text',
+      description: 'Adds a label text. This is mandatory when used in a plain radio button list.',
+      type: ['string'],
+    },
+  ];
+}
