763b570d8573bc326dcb2b88cb4ba581d76dceec
Rasmus Kjeldgaard <rkk@bankdata.dk>
2021-03-29 15:53:29 +0200

    ✅  Better readability and structure of tests

--- libs/designsystem/src/lib/components/text-link/text-link.component.spec.ts
+++ libs/designsystem/src/lib/components/text-link/text-link.component.spec.ts
@@ -1,11 +1,12 @@
-import { SpectatorHost, createHostFactory } from '@ngneat/spectator';
 import { RouterTestingModule } from '@angular/router/testing';
+import { createHostFactory, SpectatorHost } from '@ngneat/spectator';
 import { MockComponent } from 'ng-mocks';
 
-import { TextLinkComponent } from './text-link.component';
-import { DesignTokenHelper } from '../../helpers';
 import { SizeDirective } from '../../directives';
+import { DesignTokenHelper } from '../../helpers';
 import { IconComponent } from '../icon';
+
+import { TextLinkComponent } from './text-link.component';
 
 const fontSize = DesignTokenHelper.fontSize;
 const lineHeight = DesignTokenHelper.lineHeight;
@@ -18,67 +19,84 @@ describe('TextLinkComponent', () => {
 
   const createHost = createHostFactory({
     component: TextLinkComponent,
-    declarations: [MockComponent(IconComponent), SizeDirective],
+    declarations: [MockComponent(IconComponent)],
     imports: [RouterTestingModule],
   });
-  describe(`text link should be `, () => {
+
+  describe(`text link should be`, () => {
     beforeEach(() => {
       spectator = createHost(`<kirby-text-link text='Some Link' link='/'></kirby-text-link>`);
     });
+
     it('created', () => {
       expect(spectator.component).toBeTruthy();
     });
 
     it('styled with correct color', () => {
       const anchor = spectator.queryHost<HTMLAnchorElement>('a');
+
       expect(anchor).toHaveComputedStyle({ color: getColor('black') });
     });
 
     it('styled with an underline', () => {
       const anchor = spectator.queryHost<HTMLAnchorElement>('a');
+
       expect(anchor).toHaveComputedStyle({ 'text-decoration-line': 'underline' });
     });
 
     it('styled with a pointer as the cursor', () => {
       const textlink = spectator.queryHost<HTMLElement>('kirby-text-link');
+
       expect(textlink).toHaveComputedStyle({ cursor: 'pointer' });
     });
   });
+
   describe('with external links', () => {
     beforeEach(() => {
       spectator = createHost(
         `<kirby-text-link  text='Some Link' link='https://angular.io/api/router/RouterLink'></kirby-text-link>`
       );
     });
+
     it('should have an icon', () => {
       const icon = spectator.queryHost<HTMLElement>('kirby-icon');
+
       expect(icon).toBeTruthy();
     });
+
     it('should have icon named [link]', () => {
       const icon = spectator.queryHost<HTMLElement>('kirby-icon');
-
       const iconName = icon.getAttribute('name');
+
       expect(iconName).toBe('link');
     });
 
-    it('should render the icon to the right from the text', () => {
+    it('should render text as the first (left) element in anchor-tag', () => {
+      const anchorChildNodes = spectator.queryHost<HTMLAnchorElement>('a').childNodes;
+
+      expect(anchorChildNodes[0].nodeName).toBe('#text');
+    });
+
+    it('should render icon as the second (right) element in anchor-tag', () => {
+      const anchorChildNodes = spectator.queryHost<HTMLAnchorElement>('a').childNodes;
+
+      expect(anchorChildNodes[1].nodeName).toBe('KIRBY-ICON');
+    });
+
+    it('should always center icon vertically', () => {
       const icon = spectator.queryHost<HTMLElement>('kirby-icon');
-      const text = spectator.queryHost<HTMLElement>('a');
-      const textPosition = text.getBoundingClientRect();
-      const iconPosition = icon.getBoundingClientRect();
-      expect(textPosition.right).toBeLessThan(iconPosition.right);
+      expect(icon).toHaveComputedStyle({ 'vertical-align': 'middle' });
     });
   });
+
   describe('size and padding', () => {
     interface TestCase {
       size?: string;
       expected: {
         fontSize: string;
-        fontLineHeight: string;
-        iconPaddingLeft: string;
-        iconPaddingRight: string;
+        lineHeight: string;
+        iconMarginLeft: string;
         iconFontSize: string;
-        iconVerticalAlign: string;
       };
     }
 
@@ -87,47 +105,40 @@ describe('TextLinkComponent', () => {
         size: 'md',
         expected: {
           fontSize: 'n',
-          fontLineHeight: 'n',
-          iconPaddingLeft: 'xxxs',
-          iconPaddingRight: 's',
+          lineHeight: 'n',
+          iconMarginLeft: 'xxxs',
           iconFontSize: 'm',
-          iconVerticalAlign: 'middle',
         },
       },
       {
         size: 'sm',
         expected: {
           fontSize: 's',
-          fontLineHeight: 's',
-          iconPaddingLeft: 'xxxs',
-          iconPaddingRight: 's',
+          lineHeight: 's',
+          iconMarginLeft: 'xxxs',
           iconFontSize: 'm',
-          iconVerticalAlign: 'middle',
         },
       },
       {
         size: 'xs',
         expected: {
           fontSize: 'xs',
-          fontLineHeight: 'xs',
-          iconPaddingLeft: 'xxxxs',
-          iconPaddingRight: 's',
+          lineHeight: 'xs',
+          iconMarginLeft: 'xxxxs',
           iconFontSize: 's',
-          iconVerticalAlign: 'middle',
         },
       },
       {
         size: '',
         expected: {
           fontSize: 'n',
-          fontLineHeight: 'n',
-          iconPaddingLeft: 'xxxs',
-          iconPaddingRight: 's',
+          lineHeight: 'n',
+          iconMarginLeft: 'xxxs',
           iconFontSize: 'm',
-          iconVerticalAlign: 'middle',
         },
       },
     ];
+
     testCases.forEach((scenario) => {
       describe(`when size is ${scenario.size || 'empty'}`, () => {
         describe(`external link`, () => {
@@ -135,22 +146,24 @@ describe('TextLinkComponent', () => {
             spectator = createHost(
               `<kirby-text-link size='${scenario.size}' text='External link' link='https://angular.io/api/router/RouterLink'></kirby-text-link>`
             );
+            console.log(spectator);
           });
+
           it('should render icon with correct styling', () => {
             const icon = spectator.queryHost<HTMLElement>('kirby-icon');
 
             expect(icon).toHaveComputedStyle({
               'font-size': size(scenario.expected.iconFontSize),
-              'padding-left': size(scenario.expected.iconPaddingLeft),
-              'padding-right': size(scenario.expected.iconPaddingRight),
-              'vertical-align': scenario.expected.iconVerticalAlign,
+              'margin-left': size(scenario.expected.iconMarginLeft),
             });
           });
+
           it('should render anchor link with corrrect styling', () => {
             const link = spectator.queryHost<HTMLElement>('a');
+
             expect(link).toHaveComputedStyle({
               'font-size': fontSize(scenario.expected.fontSize),
-              'line-height': lineHeight(scenario.expected.fontLineHeight),
+              'line-height': lineHeight(scenario.expected.lineHeight),
             });
           });
         });
@@ -161,11 +174,13 @@ describe('TextLinkComponent', () => {
               `<kirby-text-link size='${scenario.size}' text='Internal link' link='/'></kirby-text-link>`
             );
           });
+
           it('should render anchor link with corrrect styling', () => {
             const link = spectator.queryHost<HTMLElement>('a');
+
             expect(link).toHaveComputedStyle({
               'font-size': fontSize(scenario.expected.fontSize),
-              'line-height': lineHeight(scenario.expected.fontLineHeight),
+              'line-height': lineHeight(scenario.expected.lineHeight),
             });
           });
         });
