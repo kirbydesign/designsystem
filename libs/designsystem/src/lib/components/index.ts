c86bf98746c2302d342742815c74be6e06c77063
Flemming Steinson <fsteinson@gmail.com>
2021-04-13 14:51:52 +0200

    Cleaned up ordering

--- libs/designsystem/src/lib/components/index.ts
+++ libs/designsystem/src/lib/components/index.ts
@@ -1,5 +1,7 @@
 export * from './app';
 
+export * from './accordion';
+export * from './action-sheet';
 export { AvatarComponent } from './avatar/avatar.component';
 
 export { BadgeComponent } from './badge/badge.component';
@@ -14,18 +16,17 @@ export { CheckboxComponent } from './checkbox/checkbox.component';
 export { ChipComponent } from './chip/chip.component';
 
 export { DividerComponent } from './divider/divider.component';
-
 export { DropdownComponent } from './dropdown/dropdown.component';
 
 export { EmptyStateComponent } from './empty-state/empty-state.component';
 
-export * from './form-field';
 export { FabSheetComponent } from './fab-sheet/fab-sheet.component';
+export { FlagComponent } from './flag/flag.component';
+export * from './form-field';
 
 export { GridComponent } from './grid/grid.component';
 export { GridCardConfiguration } from './grid/grid-card-configuration';
 
-export * from './action-sheet';
 export * from './icon';
 export * from './item';
 
@@ -35,27 +36,21 @@ export * from './reorder-list';
 
 export * from './modal';
 
-export * from './page';
-
+export * from './radio';
 export * from './router-outlet';
 
 export * from './shared';
 export { SegmentedControlComponent } from './segmented-control/segmented-control.component';
 export { SegmentItem } from './segmented-control/segment-item';
 export { SlideButtonComponent } from './slide-button/slide-button.component';
+export * from './slides/slides.component';
 export { SpinnerComponent } from './spinner/spinner.component';
 
 export * from './tabs';
 export * from './toast';
+export { ToggleButtonComponent } from './toggle-button/toggle-button.component';
 export { ToggleComponent } from './toggle/toggle.component';
 export { ToolbarComponent } from './toolbar/toolbar.component';
 
+export * from './page';
 export { ProgressCircleComponent } from './progress-circle/progress-circle.component';
-export { FlagComponent } from './flag/flag.component';
-export * from './slides/slides.component';
-
-export { ToggleButtonComponent } from './toggle-button/toggle-button.component';
-
-export * from './accordion';
-
-export * from './radio';
