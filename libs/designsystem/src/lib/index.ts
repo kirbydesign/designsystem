f59d09bc2081f15d805df990347bb5ff28e66ed2
Jakob Engelbrecht <jakob@basher.dk>
2021-04-15 14:48:09 +0200

    ⚰️ Clean up

--- libs/designsystem/src/lib/index.ts
+++ libs/designsystem/src/lib/index.ts
@@ -4,6 +4,5 @@ export * from './directives';
 export * from './helpers';
 export * from './models';
 export * from './scss/scss-helper';
-// export * from './testing';
 
 export { KirbyModule } from './kirby.module';
