08bb0c5f3d5377455b58e5a4018f3e16756edfac
Karl Godtliebsen <karl@godtliebsen.dk>
2021-04-20 13:27:17 +0200

    Feature/component range 1071 (#1289)
    * work with the pre and postfix, and samples
    
    * modified tests to support a div container to enable pre-postfix to position correctly
    
    * merged master into this branch
    
    * continuing work on kirby version
    
    * continuing work on kirby version
    
    * added box shadiw to range
    
    * added the mock components for range
    
    * handled export of rangevalue
    
    * removed pre-postfix merge
    
    * cleanup in spec
    
    * modified styles
    
    * added disabled support for the labels
    
    * added setDisable method
    
    * work on tests and scss calculations
    
    * work on test
    
    * work on tests for range
    
    * tests - shadowroot
    
    * test on range
    
    * changed polling timeout for test
    
    * fixed lint error
    
    * fixed lint error in spec
    
    * converted range code to use ngContent
    
    * added mock
    
    * cleaned all code and inserted range code
    
    * added mock
    
    * added mock
    
    * added mock
    
    * minor irrelevant changes
    
    * added mock
    
    * added range to index file
    
    * added mock
    
    * changed to embed a ion-range component
    
    * changed to embed a ion-range component
    
    * added mock
    
    * Revert package.json and package-lock.json
    
    * remove debug.log files
    
    * reverted to master
    
    * re added range to clean master copy to make som epsecific tests run
    
    * re added range to clean master copy to make som epsecific tests run
    
    * re added range to clean master copy to make som epsecific tests run
    
    * fixed range mock error
    
    * fixed incomplete failing test
    
    * starting to extend documentation
    
    * added mock
    
    * fixes following review: reange tests refactored to reflect modified implementation
    
    * fixes following review: reange tests refactored to reflect modified implementation
    
    * documentation and codereview issues implemented
    
    * documentation and codereview issues implemented
    
    * 🔥 clean-up
    
    * ♻️ clean-up
    
    * 💄 Clean-up styles & markup
    
    * 💄 Clean-up styles
    
    * 📝 Update showcase & examples
    
    * 💄 Add correct pin styles
    
    * 📝 Adjust documentation
    
    * 💄 add size variable
    
    * 🚨 fix mock-components
    
    * 📝 adjust api docs
    
    * updated mocks
    
    * 🔥 Remove console.log
    
    * 💄 Change box-shadow of knob
    
    * ⏪ Revert file, not releated to this pull-req
    
    * Update apps/cookbook/src/app/showcase/range-showcase/range-showcase.component.html
    
    Co-authored-by: Flemming Steinson <fsteinson@gmail.com>
    
    * Update libs/designsystem/src/lib/components/range/range.component.ts
    
    Co-authored-by: Flemming Steinson <fsteinson@gmail.com>
    
    * ♻️ Review changes
    
    * ♻️ Update mock of range component
    
    * ✨ Ensure provided values match a tick point
    
    * 📝 Updated documentation of step property
    
    Co-authored-by: Alexander Hafstad <alexander@hafstad.dk>
    Co-authored-by: Flemming Steinson <fsteinson@gmail.com>

--- libs/designsystem/src/lib/kirby.module.ts
+++ libs/designsystem/src/lib/kirby.module.ts
@@ -64,6 +64,7 @@ import { ProgressCircleRingComponent } from './components/progress-circle/progre
 import { ProgressCircleComponent } from './components/progress-circle/progress-circle.component';
 import { RadioGroupComponent } from './components/radio/radio-group/radio-group.component';
 import { RadioComponent } from './components/radio/radio.component';
+import { RangeComponent } from './components/range/range.component';
 import { ReorderListComponent } from './components/reorder-list/reorder-list.component';
 import { RouterOutletModule } from './components/router-outlet/router-outlet.module';
 import { SegmentedControlComponent } from './components/segmented-control/segmented-control.component';
@@ -138,6 +139,7 @@ const exportedDeclarations = [
   AccordionItemComponent,
   RadioComponent,
   RadioGroupComponent,
+  RangeComponent,
 ];
 
 const declarations = [
