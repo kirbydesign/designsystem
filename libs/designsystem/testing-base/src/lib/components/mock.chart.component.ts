888a5e0317816c375927f25e8e49bfc55a09444d
Jakob Engelbrecht <jakob@basher.dk>
2021-05-04 14:47:14 +0200

    🤡 Add auto-generated mocks

---
+++ libs/designsystem/testing-base/src/lib/components/mock.chart.component.ts
@@ -0,0 +1,28 @@
+import { Component, forwardRef, Input } from '@angular/core';
+import { Options, XAxisBreaksOptions } from 'highcharts';
+
+import { ChartComponent, ChartType } from '@kirbydesign/designsystem';
+
+// #region AUTO-GENERATED - PLEASE DON'T EDIT CONTENT WITHIN!
+@Component({
+  selector: 'kirby-chart',
+  template: '<ng-content></ng-content>',
+  providers: [
+    {
+      provide: ChartComponent,
+      useExisting: forwardRef(() => MockChartComponent),
+    },
+  ],
+})
+export class MockChartComponent {
+  @Input() data: [];
+  @Input() categories: string[];
+  @Input() breaks: Array<XAxisBreaksOptions>;
+  @Input() height: number;
+  @Input() type: ChartType;
+  @Input() description: string;
+  @Input() showDataLabels: boolean;
+  @Input() options: Options;
+}
+
+// #endregion
