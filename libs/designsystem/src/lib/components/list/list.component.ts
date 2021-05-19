7b1cb083d18949c989d8d36aea176c92d5235857
Flemming Steinson <fsteinson@gmail.com>
2021-05-19 13:15:45 +0200

    fixed null check

--- libs/designsystem/src/lib/components/list/list.component.ts
+++ libs/designsystem/src/lib/components/list/list.component.ts
@@ -200,7 +200,7 @@ export class ListComponent implements OnInit, AfterViewInit, OnChanges {
 
   ngOnChanges(): void {
     this._isSectionsEnabled = !!this.getSectionName;
-    if (this.items.length === 0) return;
+    if (!this.items || this.items.length === 0) return;
 
     this._groupedItems = this._isSectionsEnabled
       ? this.groupBy.transform(this.items, this.getSectionName)
