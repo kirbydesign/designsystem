0b050e021fcf83b30a30b95f08d2720375d162d3
Mads Buchmann Frederiksen <mbf@prochimp.dk>
2021-06-09 09:42:01 +0200

    :sparkles: Add canvas that can be used by chartJS
    ChartJS requires a reference to a canvas in order to draw the charts.

--- libs/designsystem/src/lib/components/chart-wip/chart-wip.component.ts
+++ libs/designsystem/src/lib/components/chart-wip/chart-wip.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
+
+export enum ChartType {
+  column = 'column',
+}
 
 @Component({
   selector: 'kirby-chart-wip',
   templateUrl: './chart-wip.component.html',
   styleUrls: ['./chart-wip.component.scss'],
 })
-export class ChartWipComponent {}
+export class ChartWipComponent implements AfterViewInit {
+  @Input() type: ChartType = ChartType.column;
+
+  @ViewChild(HTMLCanvasElement) canvas: HTMLCanvasElement;
+
+  ngAfterViewInit() {}
+}
