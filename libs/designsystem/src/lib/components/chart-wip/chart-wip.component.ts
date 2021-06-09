f1d93a5008643d4cf52ee7ef530a9800d955950f
Mads Buchmann Frederiksen <mbf@prochimp.dk>
2021-06-09 12:43:40 +0200

    :art: Add a ChartJS ChartService

--- libs/designsystem/src/lib/components/chart-wip/chart-wip.component.ts
+++ libs/designsystem/src/lib/components/chart-wip/chart-wip.component.ts
@@ -1,18 +1,43 @@
-import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Injectable, Input, ViewChild } from '@angular/core';
 
 export enum ChartType {
   column = 'column',
+}
+
+export abstract class ChartService {
+  renderChart: (targetElement: ElementRef, type: ChartType) => void;
+}
+
+@Injectable()
+class ChartJSService implements ChartService {
+  public renderChart(targetElement: ElementRef, type: ChartType): void {
+    console.log('element:', targetElement, 'type:', type);
+  }
 }
 
 @Component({
   selector: 'kirby-chart-wip',
   templateUrl: './chart-wip.component.html',
   styleUrls: ['./chart-wip.component.scss'],
+  providers: [
+    {
+      provide: ChartService,
+      useClass: ChartJSService,
+    },
+  ],
 })
 export class ChartWipComponent implements AfterViewInit {
   @Input() type: ChartType = ChartType.column;
 
-  @ViewChild(HTMLCanvasElement) canvas: HTMLCanvasElement;
+  @ViewChild('chartCanvas') canvas: ElementRef<HTMLCanvasElement>;
 
-  ngAfterViewInit() {}
+  constructor(private chartService: ChartService) {}
+
+  ngAfterViewInit() {
+    this.renderChart();
+  }
+
+  private renderChart() {
+    this.chartService.renderChart(this.canvas, this.type);
+  }
 }
