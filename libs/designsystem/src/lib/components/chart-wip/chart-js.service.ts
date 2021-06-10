dcc17b46ec4afc6f2b33d23139c6ac8c2a461c31
Mads Buchmann Frederiksen <mbf@prochimp.dk>
2021-06-10 15:48:57 +0200

    :recycle: Simplify chartJSService significantly

--- libs/designsystem/src/lib/components/chart-wip/chart-js.service.ts
+++ libs/designsystem/src/lib/components/chart-wip/chart-js.service.ts
@@ -1,76 +1,66 @@
 import { ElementRef, Injectable } from '@angular/core';
-import { ChartConfiguration, ChartDataset } from 'chart.js';
+import { ChartConfiguration, ChartDataset, ChartType as ChartJSType } from 'chart.js';
 
 import { ChartType } from './chart-wip.types';
 import { Chart } from './configured-chart-js';
+
+//---- TODO: move all of this somewhere smart ----//
+const KIRBY_TO_CHARTJS_TYPES_MAP = {
+  [ChartType.bar]: 'bar' as ChartJSType,
+  [ChartType.column]: 'bar' as ChartJSType,
+};
+
+const CHART_TYPE_CONFIGS = {
+  [ChartType.bar]: {
+    options: {
+      indexAxis: 'y',
+    },
+  },
+};
 
 function isNumberArray(value: any): value is number[] {
   return Array.isArray(value) && value.every((item) => typeof item === 'number');
 }
 
+// --------- //
+
 @Injectable()
 export class ChartJSService {
   constructor() {}
 
-  // TODO: This funciton can probably be refactored to use
-  // mappings for type & configurations instead
   public renderChart(
     targetElement: ElementRef<HTMLCanvasElement>,
     type: ChartType,
     data: ChartDataset<'bar'>[] | number[],
     dataLabels: string[]
   ): void {
-    const datasets = isNumberArray(data) ? this.convertNumberArrayToDataset(data) : data;
-
-    switch (type) {
-      case ChartType.column:
-        this.renderColumnChart(targetElement, datasets, dataLabels);
-        break;
-      case ChartType.bar:
-        this.renderBarChart(targetElement, datasets, dataLabels);
-    }
+    const datasets = this.prepareDatasets(data);
+    const config = this.prepareConfig(datasets, dataLabels, type);
+    new Chart(targetElement.nativeElement, config as ChartConfiguration);
   }
 
-  private convertNumberArrayToDataset(numberArray: number[]): ChartDataset<'bar'>[] {
+  private prepareConfig(
+    datasets: ChartDataset<'bar'>[],
+    dataLabels: string[],
+    type: ChartType
+  ): ChartConfiguration {
+    return {
+      type: KIRBY_TO_CHARTJS_TYPES_MAP[type],
+      data: {
+        labels: dataLabels,
+        datasets,
+      },
+      ...CHART_TYPE_CONFIGS[type],
+    };
+  }
+
+  private prepareDatasets(data: ChartDataset<'bar'>[] | number[]): ChartDataset<'bar'>[] {
+    if (!isNumberArray(data)) return data;
+
     return [
       {
-        data: numberArray,
+        data: data,
       },
     ];
-  }
-
-  private renderColumnChart(
-    targetElement: ElementRef<HTMLCanvasElement>,
-    datasets: ChartDataset<'bar'>[],
-    dataLabels: string[]
-  ) {
-    const config: ChartConfiguration = {
-      type: 'bar',
-      data: {
-        labels: dataLabels,
-        datasets,
-      },
-    };
-
-    new Chart(targetElement.nativeElement, config);
-  }
-
-  private renderBarChart(
-    targetElement: ElementRef<HTMLCanvasElement>,
-    datasets: ChartDataset<'bar'>[],
-    dataLabels: string[]
-  ) {
-    const config: ChartConfiguration = {
-      type: 'bar',
-      data: {
-        labels: dataLabels,
-        datasets,
-      },
-      options: {
-        indexAxis: 'y',
-      },
-    };
-
-    new Chart(targetElement.nativeElement, config);
   }
 }
