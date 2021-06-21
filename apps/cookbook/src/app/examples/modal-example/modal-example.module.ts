9433d6373c30caa8aa98995316d62962f958d38e
Jakob Engelbrecht <jakob@basher.dk>
2021-06-21 13:19:40 +0200

    🚚 Move modal examples to own module

---
+++ apps/cookbook/src/app/examples/modal-example/modal-example.module.ts
@@ -0,0 +1,43 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { KirbyModule } from '@kirbydesign/designsystem';
+
+import { ExamplesSharedModule } from '../examples.shared.module';
+
+import { ModalCompactExampleComponent } from './compact-example/modal-compact-example.component';
+import { FirstEmbeddedModalExampleComponent } from './first-embedded-modal-example/first-embedded-modal-example.component';
+import { ModalExampleConfigurationComponent } from './modal-example-configuration/modal-example-configuration.component';
+import { ModalExampleDefaultComponent } from './modal-example-default.component';
+import { ModalExampleOutletComponent } from './modal-example-outlet.component';
+import { ModalExampleComponent } from './modal-example.component';
+import { ModalRoutePage1ExampleComponent } from './modal-route-example/modal-route-page1-example.component';
+import { ModalRoutePage2ExampleComponent } from './modal-route-example/modal-route-page2-example.component';
+import { SecondEmbeddedModalExampleComponent } from './second-embedded-modal-example/second-embedded-modal-example.component';
+
+const COMPONENT_DECLARATIONS = [
+  ModalExampleComponent,
+  ModalExampleDefaultComponent,
+  ModalExampleConfigurationComponent,
+  ModalExampleOutletComponent,
+  FirstEmbeddedModalExampleComponent,
+  SecondEmbeddedModalExampleComponent,
+  ModalCompactExampleComponent,
+  ModalRoutePage1ExampleComponent,
+  ModalRoutePage2ExampleComponent,
+];
+
+@NgModule({
+  imports: [CommonModule, RouterModule, KirbyModule, ExamplesSharedModule],
+  declarations: COMPONENT_DECLARATIONS,
+  exports: COMPONENT_DECLARATIONS,
+  entryComponents: [
+    FirstEmbeddedModalExampleComponent,
+    SecondEmbeddedModalExampleComponent,
+    ModalCompactExampleComponent,
+    ModalRoutePage1ExampleComponent,
+    ModalRoutePage2ExampleComponent,
+  ],
+})
+export class ModalExampleModule {}
