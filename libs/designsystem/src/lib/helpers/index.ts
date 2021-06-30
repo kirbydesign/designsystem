524a4cd04031bcd455d74f745603854cae0fb589
Jakob Engelbrecht <jakob@basher.dk>
2021-06-30 13:24:07 +0200

    ✨ Add KeyboardHandlerService

--- libs/designsystem/src/lib/helpers/index.ts
+++ libs/designsystem/src/lib/helpers/index.ts
@@ -3,3 +3,4 @@ export * from './design-token-helper';
 export * from './theme-color.type';
 export * from './platform.service';
 export * from './string-helper';
+export * from './keyboard-handler.service';
---
+++ libs/designsystem/src/lib/helpers/keyboard-handler.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class KeyboardHandlerService {
+  handle(event: KeyboardEvent, items: any[] | string[], selectedIndex: number) {
+    let newIndex = -1;
+    switch (event.key) {
+      case 'ArrowUp':
+      case 'ArrowLeft':
+        // Select previous item:
+        newIndex = selectedIndex - 1;
+        break;
+      case 'ArrowDown':
+      case 'ArrowRight':
+        if (selectedIndex === undefined) {
+          // None selected, select first item:
+          newIndex = 0;
+        } else if (selectedIndex < items.length - 1) {
+          // Select next item:
+          newIndex = selectedIndex + 1;
+        }
+      case 'Home':
+        // Select first item:
+        newIndex = 0;
+      case 'End':
+        // Select last item:
+        newIndex = items.length - 1;
+      default:
+        break;
+    }
+    return newIndex;
+  }
+}
