e1f5eb570b40222efa520ff6d1be8cf1baadb720
Jesper Kaltoft <git@jesperkaltoft.dk>
2021-07-08 16:57:34 +0200

    🚧 Add markup and styles from PoC in Codepen

--- apps/cookbook/src/app/examples/grid-layout-example/grid-layout-example.component.ts
+++ apps/cookbook/src/app/examples/grid-layout-example/grid-layout-example.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'cookbook-grid-layout-example',
   templateUrl: './grid-layout-example.component.html',
+  styleUrls: ['./grid-layout-example.component.scss'],
 })
 export class GridLayoutExampleComponent {}
