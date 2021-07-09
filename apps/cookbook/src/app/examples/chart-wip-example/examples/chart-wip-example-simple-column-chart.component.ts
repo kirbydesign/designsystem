e06fa8c2c7e9b2edddddb7f4758fddba748b398d
Mads Buchmann Frederiksen <mbf@prochimp.dk>
2021-07-09 14:22:30 +0200

    :fire: Remove annotations from the code

--- apps/cookbook/src/app/examples/chart-wip-example/examples/chart-wip-example-simple-column-chart.component.ts
+++ apps/cookbook/src/app/examples/chart-wip-example/examples/chart-wip-example-simple-column-chart.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { AnnotationOptions } from 'chartjs-plugin-annotation';
 
 const config = {
   selector: 'cookbook-chart-wip-example-simple-column',
-  template: `<kirby-chart-wip [data]="[50, 200, 83, 102]" [annotations]="annotations"></kirby-chart-wip>`,
+  template: `<kirby-chart-wip [data]="[50, 200, 83, 102]"></kirby-chart-wip>`,
 };
 
 @Component({
@@ -12,11 +11,4 @@ const config = {
 })
 export class ChartWipExampleSimpleColumnComponent {
   template: string = config.template;
-  annotations: AnnotationOptions[] = [
-    {
-      type: 'line',
-      yMin: 95,
-      yMax: 95,
-    },
-  ];
 }
