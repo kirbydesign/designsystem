be2c99e2f8ae6f9e419c30ae84e32585c419d6bb
Mads Buchmann Frederiksen <mbf@prochimp.dk>
2021-07-12 15:57:28 +0200

    :memo: Change bar example to use years as labels
    Done for variety

--- apps/cookbook/src/app/examples/chart-wip-example/examples/chart-wip-example-bar.component.ts
+++ apps/cookbook/src/app/examples/chart-wip-example/examples/chart-wip-example-bar.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 const config = {
   selector: 'cookbook-chart-wip-example-bar',
-  template: `<kirby-chart-wip type="bar" [data]="[7, 12, 5, 9, 3]" [dataLabels]="['Monday', 'Tuesday', ['Wedens-','day'], 'Thursday', 'Friday']"></kirby-chart-wip>`,
+  template: `<kirby-chart-wip type="bar" [data]="[7, 12, 5, 9, 3]" [dataLabels]="['2021', '2020', '2019', '2018', '2017']"></kirby-chart-wip>`,
 };
 
 @Component({
