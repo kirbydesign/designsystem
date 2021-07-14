d3910648dff4fd9d89fd08d1ddbf8f3145bb30eb
Mads Buchmann Frederiksen <mbf@prochimp.dk>
2021-07-14 14:00:34 +0200

    :memo: Make the annotation example 'more realistic'
    Henrik asked for a more realistic example instead of just random annotations thrown on a chart. So tried to make it a bit more realistic :)

--- apps/cookbook/src/app/examples/chart-wip-example/examples/chart-wip-example-annotations.component.ts
+++ apps/cookbook/src/app/examples/chart-wip-example/examples/chart-wip-example-annotations.component.ts
@@ -7,34 +7,35 @@ const config = {
     [data]="[7, 12, 5, 9, 3]" 
     [annotations]="annotations"
   ></kirby-chart-wip>`,
-  codeSnippet: `annotations = [
-    { 
-      type: 'line', 
-      yMin: 5, 
-      yMax: 0 
-      borderDash: [0, 0] 
-      drawTime: 'beforeDatasetsDraw', 
-    },
-    { 
-      type: 'line', 
-      yMin: 0, 
-      yMax: 5, 
-    },
-    { 
-      type: 'box', 
-      xMin: -0.3, 
-      xMax: 0.3, 
-      yMin: 6, 
-      yMax: 9 
-    },
-    { 
-      type: 'ellipse', 
-      xMin: 1.5, 
-      xMax: 2, 
-      yMin: 3, 
-      yMax: 10 
-    }
-  ];`,
+  codeSnippet: `annotations: AnnotationOptions[] = [
+  {
+    type: 'line',
+    yMin: 6.5,
+    yMax: 6.5,
+    drawTime: 'beforeDatasetsDraw',
+  },
+  {
+    type: 'box',
+    xMin: -0.3,
+    xMax: 0.3,
+    yMin: 6,
+    yMax: 9,
+  },
+  {
+    type: 'ellipse',
+    xMin: 1.7,
+    xMax: 1.8,
+    yMin: 5.5,
+    yMax: 4.5,
+  },
+  {
+    type: 'line',
+    yMin: 14.5,
+    yMax: 14.5,
+    borderDash: [0, 0],
+  },
+];
+`,
 };
 
 @Component({
@@ -48,8 +49,9 @@ export class ChartWipExampleAnnotationsComponent {
   annotations: AnnotationOptions[] = [
     {
       type: 'line',
-      yMin: 0,
-      yMax: 5,
+      yMin: 6.5,
+      yMax: 6.5,
+      drawTime: 'beforeDatasetsDraw',
     },
     {
       type: 'box',
@@ -60,16 +62,15 @@ export class ChartWipExampleAnnotationsComponent {
     },
     {
       type: 'ellipse',
-      xMin: 1.5,
-      xMax: 2,
-      yMin: 3,
-      yMax: 10,
+      xMin: 1.7,
+      xMax: 1.8,
+      yMin: 5.5,
+      yMax: 4.5,
     },
     {
       type: 'line',
-      yMin: 5,
-      yMax: 0,
-      drawTime: 'beforeDatasetsDraw',
+      yMin: 14.5,
+      yMax: 14.5,
       borderDash: [0, 0],
     },
   ];
