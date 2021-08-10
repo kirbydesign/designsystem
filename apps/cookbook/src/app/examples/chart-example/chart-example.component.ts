320684e05ddeff1e7a8d8b5cfc10d37f3dbe2277
Mads Buchmann Frederiksen <mbf@prochimp.dk>
2021-08-10 15:06:13 +0200

    :alembic: play around with layouting

--- apps/cookbook/src/app/examples/chart-example/chart-example.component.ts
+++ apps/cookbook/src/app/examples/chart-example/chart-example.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
 
+import { ChartOptions } from '@kirbydesign/designsystem';
+
 @Component({
   selector: 'cookbook-chart-example',
   templateUrl: './chart-example.component.html',
   styleUrls: ['./chart-example.component.scss'],
 })
-export class ChartExampleComponent {}
+export class ChartExampleComponent {
+  _customOptions: ChartOptions = {
+    layout: {
+      padding: {
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+      },
+    },
+  };
+}
