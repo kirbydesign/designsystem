d2887ea97cf7fff8cd0e1b6e1d7268e4ff2bab8a
Jesper Kaltoft <git@jesperkaltoft.dk>
2021-08-20 13:05:24 +0200

    🔥 Remove ExampleViewerComponent

--- apps/cookbook/src/app/layout-recipes/layout-recipes.module.ts
+++ apps/cookbook/src/app/layout-recipes/layout-recipes.module.ts
@@ -7,7 +7,6 @@ import { KirbyModule } from '@kirbydesign/designsystem';
 import { ExamplesModule } from '../examples/examples.module';
 import { IphoneModule } from '../iphone/iphone.module';
 import { CodeViewerModule } from '../shared/code-viewer/code-viewer.module';
-import { ExampleViewerComponent } from '../shared/example-viewer/example-viewer.component';
 
 import { GridLayoutComponent } from './grid-layout/grid-layout.component';
 import { LayoutRecipesComponent } from './layout-recipes.component';
@@ -30,7 +29,7 @@ import { LayoutRecipesComponent } from './layout-recipes.component';
     CodeViewerModule,
     ExamplesModule,
   ],
-  declarations: [LayoutRecipesComponent, GridLayoutComponent, ExampleViewerComponent],
-  exports: [LayoutRecipesComponent, GridLayoutComponent, ExampleViewerComponent],
+  declarations: [LayoutRecipesComponent, GridLayoutComponent],
+  exports: [LayoutRecipesComponent, GridLayoutComponent],
 })
 export class LayoutRecipesModule {}
