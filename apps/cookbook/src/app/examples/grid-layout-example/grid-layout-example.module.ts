47fc0641ed64afb164771a75153969745b8b8ea0
Jesper Kaltoft <git@jesperkaltoft.dk>
2021-08-23 13:31:01 +0200

    ♻️ Separate core example into two examples

--- apps/cookbook/src/app/examples/grid-layout-example/grid-layout-example.module.ts
+++ apps/cookbook/src/app/examples/grid-layout-example/grid-layout-example.module.ts
@@ -5,8 +5,15 @@ import { KirbyModule } from '@kirbydesign/designsystem';
 
 import { GridLayoutCoreExampleComponent } from './grid-layout-core-example/grid-layout-core-example.component';
 import { GridLayoutExtendedExampleComponent } from './grid-layout-extended-example/grid-layout-extended-example.component';
+import { GridLayoutMultipleContainersExampleComponent } from './grid-layout-multiple-containers-example/grid-layout-multiple-containers-example.component';
+import { GridLayoutSingleContainerExampleComponent } from './grid-layout-single-container-example/grid-layout-single-container-example.component';
 
-const gridLayoutExamples = [GridLayoutCoreExampleComponent, GridLayoutExtendedExampleComponent];
+const gridLayoutExamples = [
+  GridLayoutCoreExampleComponent,
+  GridLayoutMultipleContainersExampleComponent,
+  GridLayoutSingleContainerExampleComponent,
+  GridLayoutExtendedExampleComponent,
+];
 
 @NgModule({
   imports: [CommonModule, KirbyModule],
