c2446511fc98e928acce8c83731e0d2978d1669f
Jesper Kaltoft <git@jesperkaltoft.dk>
2021-08-25 11:51:57 +0200

    ⏪ Revert changes

--- libs/designsystem/src/lib/components/chart-deprecated/chart-deprecated-helper.ts
+++ libs/designsystem/src/lib/components/chart-deprecated/chart-deprecated-helper.ts
@@ -4,6 +4,7 @@ import * as Highcharts from 'highcharts';
 
 // Docs on importing accessibility: https://www.highcharts.com/docs/chart-concepts/accessibility
 
+declare var require: any;
 require('highcharts/highcharts-more')(Highcharts);
 require('highcharts/modules/solid-gauge')(Highcharts);
 require('highcharts/modules/broken-axis')(Highcharts);
