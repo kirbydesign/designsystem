5ef5f88acd5206be2c6034b05655139d9f1036fc
Jesper Kaltoft <git@jesperkaltoft.dk>
2021-08-25 09:36:46 +0200

    🔥 Remove empty stylesheets

--- apps/cookbook/src/app/layout-recipes/grid-layout/grid-layout-extended/grid-layout-extended.component.ts
+++ apps/cookbook/src/app/layout-recipes/grid-layout/grid-layout-extended/grid-layout-extended.component.ts
@@ -4,7 +4,6 @@ declare var require: any;
 @Component({
   selector: 'cookbook-grid-layout-extended',
   templateUrl: './grid-layout-extended.component.html',
-  styleUrls: ['./grid-layout-extended.component.scss'],
 })
 export class GridLayoutExtendedComponent {
   exampleHtml: string = require('!raw-loader!../../../examples/grid-layout-example/grid-layout-extended-example/grid-layout-extended-example.component.html')
--- apps/cookbook/src/app/layout-recipes/grid-layout/grid-layout-multiple-containers/grid-layout-multiple-containers.component.ts
+++ apps/cookbook/src/app/layout-recipes/grid-layout/grid-layout-multiple-containers/grid-layout-multiple-containers.component.ts
@@ -4,7 +4,6 @@ declare var require: any;
 @Component({
   selector: 'cookbook-grid-layout-multiple-containers',
   templateUrl: './grid-layout-multiple-containers.component.html',
-  styleUrls: ['./grid-layout-multiple-containers.component.scss'],
 })
 export class GridLayoutMultipleContainersComponent {
   exampleHtml: string = require('!raw-loader!../../../examples/grid-layout-example/grid-layout-multiple-containers-example/grid-layout-multiple-containers-example.component.html')
