c927a4d6ccc31fab7efe06edcf01ab435785c184
Mads Buchmann Frederiksen <mbf@prochimp.dk>
2021-08-31 16:04:07 +0200

    :label: Extend 'ChartType' with 'line'

--- libs/designsystem/src/lib/components/chart/chart.types.ts
+++ libs/designsystem/src/lib/components/chart/chart.types.ts
@@ -2,8 +2,9 @@ import { ChartDataset as ChartJSDataset } from 'chart.js';
 
 export type ChartHighlightedElements = number[][];
 
-export type ChartType = 'column' | 'bar';
+export type ChartType = 'column' | 'bar' | 'line';
 
+//TODO: update this type to include 'line'
 export interface ChartDataset extends ChartJSDataset<'bar'> {
   kirbyOptions?: {
     highlightedElements?: number[];
