281cb8aa719b1dee8c3e92c785888f767964d917
Mads Buchmann Frederiksen <mbf@prochimp.dk>
2021-09-02 11:27:43 +0200

    :memo: Finish up basic line chart example without text

--- apps/cookbook/src/app/examples/chart-example/examples/chart-example-line.component.ts
+++ apps/cookbook/src/app/examples/chart-example/examples/chart-example-line.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
 
+import { ChartHighlightedElements } from '@kirbydesign/designsystem';
+
 const config = {
   selector: 'cookbook-chart-example-line',
-  template: `<kirby-chart [type]="'line'" [data]="[50, 200, 83, 102]"></kirby-chart>`,
+  template: `<kirby-chart 
+  type="line" 
+  [data]="[6, 6.37, 6.46, 6.64, 6.78, 7.44, 7.92, 8.18, 8.41, 8.85, 9.33]" 
+  [dataLabels]='["1950", "1951", "1952", "1953", "1954", "1955", "1956", "1957", "1958", "1959", "1960"]' 
+  [customOptions]="_customOptions"
+  ></kirby-chart>`,
 };
 
 @Component({
