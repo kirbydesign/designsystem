cbee2e38657a92ecfa2f0f4d29abad95a7635da1
Mads Buchmann Frederiksen <mbf@prochimp.dk>
2021-09-02 16:14:33 +0200

    :memo: Generate dataset for custom line chart

--- apps/cookbook/src/app/examples/chart-example/examples/chart-example-custom-line.component.ts
+++ apps/cookbook/src/app/examples/chart-example/examples/chart-example-custom-line.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
-import { ChartOptions } from 'chart.js';
+import { AfterViewInit, Component } from '@angular/core';
+import { ChartDataset, ChartOptions } from 'chart.js';
 
 const config = {
   selector: 'cookbook-chart-example-custom-line',
   template: `<kirby-chart 
   type="line" 
-  [data]="[6, 6.37, 6.46, 6.64, 6.78, 7.44, 7.92, 8.18, 8.41, 8.85, 9.33]" 
+  [data]="_dataset" 
   [dataLabels]='_dataLabels' 
   [customOptions]="_customOptions"
   ></kirby-chart>`,
@@ -15,8 +15,10 @@ const config = {
   selector: config.selector,
   template: config.template,
 })
-export class ChartExampleCustomLineComponent {
+export class ChartExampleCustomLineComponent implements AfterViewInit {
   template: string = config.template;
+
+  _dataset: ChartDataset[];
 
   _dataLabels = [
     '1950',
@@ -32,7 +34,7 @@ export class ChartExampleCustomLineComponent {
     '1960',
   ];
 
-  _customOptions: ChartOptions = {
+  _customOptions = {
     scales: {
       x: {
         ticks: {
@@ -41,6 +43,36 @@ export class ChartExampleCustomLineComponent {
           },
         },
       },
+      y: {
+        display: false,
+      },
+    },
+    plugins: {
+      chartAreaBorder: {
+        display: true,
+        borderColor: 'red',
+      },
     },
   };
+
+  generateData(x: number, noDataPoints: number) {
+    return new Array(noDataPoints).fill(undefined).map((_, index) => index ** x);
+  }
+
+  ngAfterViewInit() {
+    this._dataset = [
+      {
+        data: this.generateData(3, 11),
+        borderColor: 'black',
+      },
+      {
+        data: this.generateData(3.2, 11),
+        borderColor: 'green',
+      },
+      {
+        data: this.generateData(2.5, 11),
+        borderColor: 'red',
+      },
+    ];
+  }
 }
