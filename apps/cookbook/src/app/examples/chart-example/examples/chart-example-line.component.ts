a73d7bb4c397b9542367644cfb2da5cf2ede5a48
Mads Buchmann Frederiksen <mbf@prochimp.dk>
2021-09-07 16:12:55 +0200

    :memo: Add documentation for type 'line'

--- apps/cookbook/src/app/examples/chart-example/examples/chart-example-line.component.ts
+++ apps/cookbook/src/app/examples/chart-example/examples/chart-example-line.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 const config = {
   selector: 'cookbook-chart-example-line',
   template: `
-  <button (click)="onClick()">Swap type</button>
   <kirby-chart 
-  [type]="_chartType" 
+  type="line" 
   [data]="[6, 6.37, 6.46, 6.64, 6.78, 7.44, 7.92, 8.18, 8.41, 8.85, 9.33]" 
   [dataLabels]='["1950", "1951", "1952", "1953", "1954", "1955", "1956", "1957", "1958", "1959", "1960"]' 
   ></kirby-chart>`,
@@ -17,13 +16,4 @@ const config = {
 })
 export class ChartExampleLineComponent {
   template: string = config.template;
-
-  private types = ['line', 'bar'];
-  get _chartType() {
-    return this.types[0];
-  }
-
-  onClick() {
-    this.types = this.types.reverse();
-  }
 }
