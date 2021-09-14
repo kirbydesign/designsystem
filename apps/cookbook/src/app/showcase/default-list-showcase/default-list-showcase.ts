aadb26319ddd59c7d08b529a621553a7cdf0ffb1
Jeppe Schou-Larsen <jeppevaldemar@gmail.com>
2021-09-14 14:14:04 +0200

    showcase skeleton

---
+++ apps/cookbook/src/app/showcase/default-list-showcase/default-list-showcase.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cookbook-default-list-showcase',
+  templateUrl: './default-list-showcase.html',
+  styleUrls: ['../_showcase.shared.scss'],
+})
+export class DefaultListShowcaseComponent {}
