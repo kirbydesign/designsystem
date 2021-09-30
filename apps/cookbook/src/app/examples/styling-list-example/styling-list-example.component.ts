38b72f823aa10a11a71593d3321edcc563266797
Jeppe Schou-Larsen <jeppevaldemar@gmail.com>
2021-09-30 14:44:56 +0200

    🎨 Cleanup styling-list-example

--- apps/cookbook/src/app/examples/styling-list-example/styling-list-example.component.ts
+++ apps/cookbook/src/app/examples/styling-list-example/styling-list-example.component.ts
@@ -1,29 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-
-const config = {
-  template: `<div class="menu-items-container">
-  <ul class="menu-items">
-    <li>
-      <a
-        class="menu-item">"Menu Item 1"</a
-      >
-    </li>`,
-  styles: [
-    `li::before { 
-  display: none; 
-}`,
-  ],
-};
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'cookbook-styling-list-example',
   templateUrl: './styling-list-example.component.html',
   styleUrls: ['./styling-list-example.component.scss'],
 })
-export class StylingListExampleComponent implements OnInit {
-  constructor() {}
-  ngOnInit() {}
-  //   template: string = config.template;
-  //   styles: string = config.styles.join(`
-  // `);
-}
+export class StylingListExampleComponent {}
