99ea9c6135a37c1405b3ea101d1f7ece612d5d8d
Jeppe Schou-Larsen <jeppevaldemar@gmail.com>
2021-10-05 08:49:16 +0200

    🔥 Remove example stylesheet and wrapping div

--- apps/cookbook/src/app/examples/styling-list-example/styling-list-example.component.ts
+++ apps/cookbook/src/app/examples/styling-list-example/styling-list-example.component.ts
@@ -2,6 +2,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'cookbook-styling-list-example',
   templateUrl: './styling-list-example.component.html',
-  styleUrls: ['./styling-list-example.component.scss'],
 })
 export class StylingListExampleComponent {}
