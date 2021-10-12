25bd8902f599f383194aa7aeb021b60567c672cb
Mads Buchmann Frederiksen <mbf@prochimp.dk>
2021-10-12 08:49:55 +0200

    Add new MockFitHeadingDirective (#1806)

--- libs/designsystem/testing-base/src/lib/mock-directives.ts
+++ libs/designsystem/testing-base/src/lib/mock-directives.ts
@@ -1,3 +1,4 @@
+import { MockFitHeadingDirective } from './directives/mock.fit-heading.directive';
 import { MockThemeColorDirective } from './directives/mock.theme-color.directive';
 
-export const MOCK_DIRECTIVES = [MockThemeColorDirective];
+export const MOCK_DIRECTIVES = [MockThemeColorDirective, MockFitHeadingDirective];
