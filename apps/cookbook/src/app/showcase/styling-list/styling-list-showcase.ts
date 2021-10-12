5f0781497db9694169cde50c33f00846720dbb3c
Jesper Kaltoft <git@jesperkaltoft.dk>
2021-10-12 10:04:27 +0200

    Revert "💥 Add default styles for HTML lists"
    This reverts commit 1f2baf58ccfefd8482a9903348b0cbe9144fec96.

--- apps/cookbook/src/app/showcase/styling-list/styling-list-showcase.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'cookbook-styling-list-showcase',
-  templateUrl: './styling-list-showcase.html',
-  styleUrls: ['../_showcase.shared.scss'],
-})
-export class StylingListShowcaseComponent {}
