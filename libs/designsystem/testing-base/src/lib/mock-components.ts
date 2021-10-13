a0e743266ab4e6d4dcbcf8bae342a24276d24921
Mads Buchmann Frederiksen <mbf@prochimp.dk>
2021-10-13 13:20:03 +0200

    :recycle: Update scss utils path to new core structure

--- libs/designsystem/testing-base/src/lib/mock-components.ts
+++ libs/designsystem/testing-base/src/lib/mock-components.ts
@@ -59,7 +59,6 @@ import { MockTabsComponent } from './components/mock.tabs.component';
 import { MockTextareaComponent } from './components/mock.textarea.component';
 import { MockToggleButtonComponent } from './components/mock.toggle-button.component';
 import { MockToggleComponent } from './components/mock.toggle.component';
-import { MockBadgeComponent } from './components/mock.web-component-proxies.component';
 
 export const MOCK_COMPONENTS = [
   MockAccordionItemComponent,
@@ -119,5 +118,4 @@ export const MOCK_COMPONENTS = [
   MockTabsComponent,
   MockToggleComponent,
   MockToggleButtonComponent,
-  MockBadgeComponent,
 ];
