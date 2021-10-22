b3e10a225534bafe816cd482d108ee44cc774789
RasmusKjeldgaard <rkk@bankdata.dk>
2021-10-22 14:16:00 +0200

    🔧 Inintialize badge to test global styling
    Global styles seem to work - we get correct font, and global variables.
    Still no luck applying badge styles, probably due to the proxy comp
    not having any style-urls for compodoc and/or storybook to tap into.
    The styles all belong to the 'KirbyBadge' web comp, which sb does
    not know.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,9 @@
 module.exports = {
   "stories": [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    "../libs/designsystem/src/lib/**/*.stories.mdx",
+    "../libs/designsystem/src/lib/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,9 @@
+import { defineCustomElements } from '../libs/core/loader';
+
 import { setCompodocJson } from "@storybook/addon-docs/angular";
 import docJson from "../documentation.json";
 setCompodocJson(docJson);
+defineCustomElements();
 
 import '!style-loader!css-loader!sass-loader!../libs/core/src/scss/_global-styles.scss';
 import '!style-loader!css-loader!sass-loader!../libs/core/src/scss/_utils.scss';
@@ -8,6 +11,7 @@ import '!style-loader!css-loader!sass-loader!../libs/core/src/scss/_utils.scss';
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
+    expanded: true,
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
