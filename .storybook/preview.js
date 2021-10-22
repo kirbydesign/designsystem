d7d4de7c70348b500c76aeca7ec78335abdcbdb6
RasmusKjeldgaard <rkk@bankdata.dk>
2021-10-22 12:03:44 +0200

    ✨ Setup @storybook/angular

---
+++ .storybook/preview.js
@@ -0,0 +1,17 @@
+import { setCompodocJson } from "@storybook/addon-docs/angular";
+import docJson from "../documentation.json";
+setCompodocJson(docJson);
+
+import '!style-loader!css-loader!sass-loader!../libs/core/src/scss/_global-styles.scss';
+import '!style-loader!css-loader!sass-loader!../libs/core/src/scss/_utils.scss';
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  docs: { inlineStories: true },
+}
