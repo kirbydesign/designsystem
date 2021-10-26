26f63e5781cd8a8757325eeb490149c89beb85f6
Jeppe Schou-Larsen <jeppevaldemar@gmail.com>
2021-10-26 13:54:09 +0200

    ♻️ Add default value false to flag

--- libs/designsystem/src/lib/components/card/card.component.ts
+++ libs/designsystem/src/lib/components/card/card.component.ts
@@ -39,7 +39,7 @@ export class CardComponent implements OnInit, OnDestroy {
     this.sizesSortedByBreakpoint = this.sortSizesByBreakpoint(value);
   }
 
-  @HostBinding('class.card-is-clickable') cardIsClickable: boolean;
+  @HostBinding('class.card-is-clickable') cardIsClickable: boolean = false;
   @HostBinding('class.flat')
   flat: boolean = false;
   @HostBinding('class.highlighted')
