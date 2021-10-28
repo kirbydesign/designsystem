5bcfafb4abc5608cc41704c80d19035fd2f4bc88
Jeppe Schou-Larsen <jeppevaldemar@gmail.com>
2021-10-28 15:37:25 +0200

    ♻️ Refactor clickable showcase

--- apps/cookbook/src/app/examples/card/card-clickable-example/card-clickable-example.component.ts
+++ apps/cookbook/src/app/examples/card/card-clickable-example/card-clickable-example.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-clickable-example.component.scss'],
 })
 export class CardClickableExampleComponent {
-  genericMethod() {}
+  function() {}
 }
