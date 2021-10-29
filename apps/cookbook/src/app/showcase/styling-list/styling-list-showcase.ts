cfaa1dbd484b39e8236ea58a60b4a570ca636327
Jesper Kaltoft <git@jesperkaltoft.dk>
2021-10-29 16:19:17 +0200

    📝 Recreate showcase and example in cookbook
    Co-authored-by: Jeppe Valdemar <jeppevaldemar@gmail.com>

---
+++ apps/cookbook/src/app/showcase/styling-list/styling-list-showcase.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cookbook-styling-list-showcase',
+  templateUrl: './styling-list-showcase.html',
+  styleUrls: ['../_showcase.shared.scss'],
+})
+export class StylingListShowcaseComponent {}
