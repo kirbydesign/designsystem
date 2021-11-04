d18f18428a8dcc025985788fc4ff21c7c834968e
Jesper Kaltoft <git@jesperkaltoft.dk>
2021-11-04 19:40:53 +0100

    :lipstick: Use two-column layout for examples

--- apps/cookbook/src/app/examples/styling-list-example/styling-list-example.component.ts
+++ apps/cookbook/src/app/examples/styling-list-example/styling-list-example.component.ts
@@ -2,5 +2,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'cookbook-styling-list-example',
   templateUrl: './styling-list-example.component.html',
+  styleUrls: ['./styling-list-example.component.scss'],
 })
 export class StylingListExampleComponent {}
