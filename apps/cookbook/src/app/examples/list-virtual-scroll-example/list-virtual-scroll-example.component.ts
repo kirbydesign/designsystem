04b44326884a73cdf6760782ff1cb26265c5c385
RasmusKjeldgaard <rkk@bankdata.dk>
2021-11-15 15:29:11 +0100

    :memo: Remove experimental virtual scroll documentation
    A general virtual scroll list has proven to be very error-prone and difficult to make suitable for all the possible use-cases. Virtual scroll should be implemented and finetuned on a per-project basis.

--- apps/cookbook/src/app/examples/list-virtual-scroll-example/list-virtual-scroll-example.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'cookbook-list-virtual-scroll-example',
-  templateUrl: './list-virtual-scroll-example.component.html',
-  styleUrls: ['./list-virtual-scroll-example.component.scss'],
-})
-export class ListVirtualScrollExampleComponent {}
