7b0a7ea022b5e7c258dc44e085b3ed9a939fce2b
Mads Buchmann Frederiksen <mbf@prochimp.dk>
2021-11-17 22:23:00 +0100

    Add accessibility guidelines (#1851)
    * :memo: Add markdown accessibility guidelines
    
    * :memo: Write out accessibility guidelines
    
    * ✏️ change is to are
    
    Co-authored-by: Jesper Kaltoft Vind <git@jesperkaltoft.dk>
    
    * ✏️ change accesible to accessible
    
    Co-authored-by: Jesper Kaltoft Vind <git@jesperkaltoft.dk>
    
    * ✏️ Capitalize Lighthouse & fix definetly typo
    
    Co-authored-by: Jesper Kaltoft Vind <git@jesperkaltoft.dk>
    
    * ✏️ Fix acessibility typo
    
    Co-authored-by: Jesper Kaltoft Vind <git@jesperkaltoft.dk>
    
    * ✏️ Fix metahpor typo
    
    Co-authored-by: Jesper Kaltoft Vind <git@jesperkaltoft.dk>
    
    * :memo: Restructure & compare to html in 'do not be in the way'
    
    * :memo: Detail: a11y is hard, use as html elements, it is a responsibility
    
    * :memo: Add additional topics to discover
    
    * :memo: Rescturture resources section
    
    * :pencil2: Fix spelling mistakes with a spell checker
    
    * :memo: Further highlight importance of WCAG
    
    * :truck: Move accessibility guidelines to cookbook
    
    * :memo: Convert accessibilty guidelines to html
    
    * :sparkles: Add 'accessibility' as menu point
    
    * :fire: Remove table of contents
    
    Linking to heading by setting id="#title" does not work with the angular router for some reason.
    
    * :memo: Convert links from md to html
    
    * :pencil2: Fix a couple of minor typos
    
    * :fire: Remove AccessibilityGuidelinesModule
    
    * :memo: Add link to github discussion
    
    * :recycle: Change AccessibilityGuidelinesComponent selector
    
    Co-authored-by: Jesper Kaltoft Vind <git@jesperkaltoft.dk>

---
+++ apps/cookbook/src/app/accessibility/accessibility-guidelines.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cookbook-accessibility-guidelines',
+  templateUrl: './accessibility-guidelines.html',
+})
+export class AccessibilityGuidelinesComponent {}
