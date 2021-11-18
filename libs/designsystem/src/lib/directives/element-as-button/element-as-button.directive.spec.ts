55a74d026191e0fe96d6eee93b6c4031a847b613
Jeppe Valdemar <jeppevaldemar@gmail.com>
2021-11-18 11:58:32 +0100

    Clickable cards should change cursor type (#1838)
    * ✨ Add button flag to card
    
    * ✨ Add directive for card button
    
    * 💄 Add css for card that is clickable
    
    * 💄 Connect css class and component flag
    
    * ✅ Declare directive in failing tests
    
    * 🔥 Remove ngClass directive
    
    * 💄 Use HostBinding instead of ngClass for css flag
    
    * 🚚 Correct wrong import paths
    
    * ♻️ Add default value false to flag
    
    * 🚚 Rename HasButtonDirective to CardIsClickableDirective
    
    * ✅ Add specs for directive
    
    * 🔥 Remove CSS for opacity
    
    * 🔥 Remove focused spec
    
    * ✅ Remove redundant code in spec
    
    * 💄 Add elevation z2 except for highlighted cards
    
    * ♻️ Refactor z elevation statement
    
    * 💄 Add initial clickable card showcase
    
    * ♻️ Refactor clickable showcase
    
    * 🚚 Remove defunct import path
    
    * 🚚 Reapply corrected import path
    
    * ✨ Give directive applied element role button instead of flag
    
    * ✨ Make card injection in directive optional
    
    * 🚚 Remove unused import
    
    * ✨ Make element affected by directive accessible
    
    * ✨ Make directive useable in designsystem instead of card only
    
    * 🚧 Test directive on avatar
    
    * 🚚 Fix directive import path in specs
    
    * 🔥 Remove localeDa in spec
    
    * ✅ Rename test description to reflect directive name
    
    * 💄 Add outline-offset
    
    * ✅ Add test for outline-offset
    
    * 💄 Edit showcase describing directive
    
    * 🔥 Remove directive from tests
    
    * 🔥 Remove test implementation of directive in avatar
    
    * 💄 Update example text
    
    Co-authored-by: RasmusKjeldgaard <rkk@bankdata.dk>

---
+++ libs/designsystem/src/lib/directives/element-as-button/element-as-button.directive.spec.ts
@@ -0,0 +1,31 @@
+import { createDirectiveFactory, SpectatorDirective } from '@ngneat/spectator';
+
+import { CardComponent } from '../../components/card/card.component';
+
+import { ElementAsButtonDirective } from './element-as-button.directive';
+
+fdescribe('ElementAsButtonDirective', () => {
+  let spectator: SpectatorDirective<ElementAsButtonDirective>;
+
+  const createDirective = createDirectiveFactory({
+    directive: ElementAsButtonDirective,
+    imports: [],
+    declarations: [CardComponent],
+  });
+  beforeEach(() => {
+    spectator = createDirective(`<kirby-card (click)="someMethod()"> </kirby-card>`);
+  });
+
+  it('should get the instance', () => {
+    const instance = spectator.directive;
+    expect(instance).toBeDefined();
+  });
+
+  it('should have cursor: pointer', () => {
+    expect(spectator.element).toHaveComputedStyle({ cursor: 'pointer' });
+  });
+
+  it('should have an outline-offset of 2px', () => {
+    expect(spectator.element).toHaveComputedStyle({ 'outline-offset': '2px' });
+  });
+});
