f7f2bf983244d767d70e740c479362345c70d5b7
RasmusKjeldgaard <rkk@bankdata.dk>
2021-11-23 15:12:26 +0100

    ♻️ Move implementation to list-experimental

---
+++ libs/designsystem/src/lib/components/list/list-experimental/list-experimental.component.spec.ts
@@ -0,0 +1,41 @@
+import { IonItem, IonList } from '@ionic/angular';
+import { createHostFactory, SpectatorHost } from '@ngneat/spectator';
+import { MockComponent } from 'ng-mocks';
+
+import { ItemComponent } from '../../item';
+
+import { ListExperimentalComponent } from './list-experimental.component';
+
+describe('ListExperimental', () => {
+  let spectator: SpectatorHost<ListExperimentalComponent>;
+
+  const createHost = createHostFactory({
+    component: ListExperimentalComponent,
+    declarations: [
+      ListExperimentalComponent,
+      MockComponent(IonList),
+      MockComponent(IonItem),
+      ItemComponent,
+    ],
+  });
+
+  beforeEach(() => {
+    spectator = createHost<ListExperimentalComponent>(`
+      <kirby-list-experimental>
+        <kirby-item>Test Item</kirby-item>
+        <kirby-item>Test Item</kirby-item>
+        <kirby-item>Test Item</kirby-item>
+        <kirby-item>Test Item</kirby-item>
+      </kirby-list-experimental>`);
+  });
+
+  it('should create', () => {
+    expect(spectator.component).toBeTruthy();
+  });
+
+  it('should render items in list', () => {
+    const items = spectator.queryHostAll('kirby-item');
+
+    expect(items).toHaveLength(4);
+  });
+});
---
+++ libs/designsystem/src/lib/components/list/list-experimental/list-experimental.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kirby-list-experimental',
+  templateUrl: './list-experimental.component.html',
+})
+export class ListExperimentalComponent {}
