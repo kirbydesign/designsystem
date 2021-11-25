0fb7e0c074f2428a9996f14387034b8e9acf1d1d
RasmusKjeldgaard <rkk@bankdata.dk>
2021-11-25 13:47:59 +0100

    📝 Add showcase for Item Group

---
+++ apps/cookbook/src/app/showcase/item-group-showcase/item-group-showcase.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cookbook-item-group-showcase',
+  templateUrl: './item-group-showcase.component.html',
+  styleUrls: ['./item-group-showcase.component.scss'],
+})
+export class ItemGroupShowcaseComponent {}
