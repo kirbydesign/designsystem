03567f5cd981a7e8641c5a55b66fe9cbf1d88f01
RasmusKjeldgaard <rkk@bankdata.dk>
2021-11-25 14:31:45 +0100

    📝 Reduce complexity of example by omitting page

--- apps/cookbook/src/app/examples/item-group-example/item-group-example.component.ts
+++ apps/cookbook/src/app/examples/item-group-example/item-group-example.component.ts
@@ -1,58 +1,7 @@
 import { Component } from '@angular/core';
 
-export const ItemGroupsInListTemplate = `
-<kirby-list-experimental>
-  <kirby-item-group>
-      <kirby-item>
-          <kirby-label>
-              <h3 class="kirby-text-bold">Title</h3>
-              <p detail>Detail</p>
-          </kirby-label>
-          <kirby-label slot="end">
-              <data>Value</data>
-          </kirby-label>
-      </kirby-item>
-      <kirby-item>
-          <kirby-label>
-              <h3 class="kirby-text-bold">Title</h3>
-              <p detail>Detail</p>
-          </kirby-label>
-          <kirby-label slot="end">
-              <data>Value</data>
-          </kirby-label>
-      </kirby-item>
-  </kirby-item-group>
-  <kirby-item-group>
-      <kirby-item>
-          <kirby-label>
-              <h3 class="kirby-text-bold">Title</h3>
-              <p detail>Detail</p>
-          </kirby-label>
-          <kirby-label slot="end">
-              <data>Value</data>
-          </kirby-label>
-      </kirby-item>
-      <kirby-item>
-          <kirby-label>
-              <h3 class="kirby-text-bold">Title</h3>
-              <p detail>Detail</p>
-          </kirby-label>
-          <kirby-label slot="end">
-              <data>Value</data>
-          </kirby-label>
-      </kirby-item>
-  </kirby-item-group>
-</kirby-list-experimental>`;
-
 @Component({
   selector: 'cookbook-item-group-example',
-  template: `
-    <kirby-page title="Item Group">
-      <kirby-page-content>
-        ${ItemGroupsInListTemplate}
-      </kirby-page-content>
-    </kirby-page>
-  `,
-  styleUrls: ['./item-group-example.component.scss'],
+  templateUrl: './item-group-example.component.html',
 })
 export class ItemGroupExampleComponent {}
