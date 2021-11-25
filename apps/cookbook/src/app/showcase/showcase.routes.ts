09c87bc7e9581aed2fa58650a5dd158024b02252
RasmusKjeldgaard <rkk@bankdata.dk>
2021-11-25 16:05:04 +0100

    📝 Hide item-group showcase from SideNav

--- apps/cookbook/src/app/showcase/showcase.routes.ts
+++ apps/cookbook/src/app/showcase/showcase.routes.ts
@@ -68,6 +68,9 @@ export const routes: Routes = [
       {
         path: 'item-group',
         component: ItemGroupShowcaseComponent,
+        data: {
+          hide: true,
+        },
       },
       {
         path: 'button',
