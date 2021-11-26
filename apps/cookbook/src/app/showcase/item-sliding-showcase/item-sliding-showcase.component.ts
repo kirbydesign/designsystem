55299e3e5a79ef02ff4e0b7d35d9acf42c003640
Mads Buchmann Frederiksen <mbf@prochimp.dk>
2021-11-26 11:55:57 +0100

    :memo: Add item sliding showcase (without any content)

---
+++ apps/cookbook/src/app/showcase/item-sliding-showcase/item-sliding-showcase.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cookbook-item-sliding-showcase',
+  templateUrl: './item-sliding-showcase.component.html',
+  styleUrls: ['./item-sliding-showcase.component.scss'],
+})
+export class ItemSlidingShowcaseComponent {}
