825a9ff39fd3935e7551f42ecd3766c4d331cf90
Mads Buchmann Frederiksen <mbf@prochimp.dk>
2021-11-29 14:40:41 +0100

    :memo: Add swipeActions to examples

--- apps/cookbook/src/app/examples/item-sliding-example/item-sliding-example.component.ts
+++ apps/cookbook/src/app/examples/item-sliding-example/item-sliding-example.component.ts
@@ -1,8 +1,40 @@
 import { Component } from '@angular/core';
 
+import { ListSwipeAction } from '@kirbydesign/designsystem';
+
 @Component({
   selector: 'cookbook-item-sliding-example',
   templateUrl: './item-sliding-example.component.html',
   styleUrls: [],
 })
-export class ItemSlidingExampleComponent {}
+export class ItemSlidingExampleComponent {
+  swipeActions: ListSwipeAction[][] = [
+    [
+      {
+        title: 'delete',
+        position: 'left',
+        onSelected: (item) => console.log(item),
+        type: 'danger',
+      },
+      {
+        title: 'edit',
+        position: 'left',
+        icon: 'link',
+        type: 'success',
+        onSelected: (item) => console.log(item),
+      },
+      {
+        title: 'archive',
+        position: 'left',
+        icon: () => 'link',
+        onSelected: (item) => console.log(item),
+        type: 'warning',
+      },
+    ],
+    [],
+  ];
+
+  toggleSwipeActions() {
+    this.swipeActions.reverse();
+  }
+}
