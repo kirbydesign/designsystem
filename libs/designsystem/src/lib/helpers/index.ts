29b0960fc1355e126867c7efffdf0504aa4a28db
Mads Buchmann Frederiksen <mbf@prochimp.dk>
2021-12-06 14:07:05 +0100

    :sparkles: Have list close sliding items when action is selected

--- libs/designsystem/src/lib/helpers/index.ts
+++ libs/designsystem/src/lib/helpers/index.ts
@@ -1,5 +1,6 @@
 // re-export helpers from core, to preserve backwards compatability
 // of 'designsystem' library
+export * from './element-has-ancestor';
 export * from './color-helper';
 export * from './design-token-helper';
 export * from './string-helper';
