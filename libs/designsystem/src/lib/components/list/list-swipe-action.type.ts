d0b212fc1150f9a698cc368c9b369b7eef25081e
Mads Buchmann Frederiksen <mbf@prochimp.dk>
2021-12-06 10:26:56 +0100

    :label: Generate ListSwipeAction from ItemSwipeAction
    ListSwipeAction is now generated based on the new ItemListSwipeAction type. This will hopefully reduce the amount of maintenance required to keep the two types in sync until we deprecate ListSwipeAction.

--- libs/designsystem/src/lib/components/list/list-swipe-action.type.ts
+++ libs/designsystem/src/lib/components/list/list-swipe-action.type.ts
@@ -4,22 +4,22 @@ export type ListSwipeDirection = 'right' | 'left';
 
 export type ListSwipeEnd = 'start' | 'end';
 
-export interface ListSwipeAction {
-  position: ListSwipeDirection;
-  title: ((item: any) => string) | string;
-  icon?: ((item: any) => string) | string;
-  type?: ((item: any) => ListSwipeActionType) | ListSwipeActionType;
-  onSelected: (item: any) => void;
-  isDisabled?: ((item: any) => boolean) | boolean;
-}
-
-/* TODO: Is it possible to create this type programatically? 
-https://stackoverflow.com/questions/52931116/decompose-a-typescript-union-type-into-specific-types
- Could be neat to make a generic Evaluted<ListSwipeAction> type that would return the below type */
-export type EvaluatedListSwipeAction = {
+export type ItemListSwipeAction = {
   title: string;
   onSelected: (item: any) => void;
   icon?: string;
   type?: string;
   isDisabled?: boolean;
+};
+
+/* 
+    Copy each property in ItemListSwipeAction but make it possible to provide them via a function.
+    If the property is a function do not modify it.
+   */
+export type ListSwipeAction = {
+  [Property in keyof ItemListSwipeAction]: ItemListSwipeAction[Property] extends Function
+    ? ItemListSwipeAction[Property]
+    : ((item: any) => ItemListSwipeAction[Property]) | ItemListSwipeAction[Property];
+} & {
+  position: ListSwipeDirection;
 };
