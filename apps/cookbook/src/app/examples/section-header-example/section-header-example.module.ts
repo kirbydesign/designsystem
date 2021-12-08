7ef2abc153bfa6dd9e3bb86c41d0c5dc71cc6f1f
RasmusKjeldgaard <rkk@bankdata.dk>
2021-12-08 10:32:34 +0100

    📝  Restructure showcase with additional examples

--- apps/cookbook/src/app/examples/section-header-example/section-header-example.module.ts
+++ apps/cookbook/src/app/examples/section-header-example/section-header-example.module.ts
@@ -5,10 +5,19 @@ import { KirbyModule } from '@kirbydesign/designsystem';
 
 import { SectionHeaderWithCardComponent } from './examples/header-with-card';
 import { SectionHeaderWithItemGroupComponent } from './examples/header-with-item-group';
+import { SectionHeaderHeadingWithLabelExampleComponent } from './examples/heading-with-label';
+import { SectionHeaderLabelAndDetailExampleComponent } from './examples/label-and-detail';
+
+const DECLARATIONS = [
+  SectionHeaderWithCardComponent,
+  SectionHeaderWithItemGroupComponent,
+  SectionHeaderHeadingWithLabelExampleComponent,
+  SectionHeaderLabelAndDetailExampleComponent,
+];
 
 @NgModule({
   imports: [CommonModule, KirbyModule],
-  declarations: [SectionHeaderWithCardComponent, SectionHeaderWithItemGroupComponent],
-  exports: [SectionHeaderWithCardComponent, SectionHeaderWithItemGroupComponent],
+  declarations: DECLARATIONS,
+  exports: DECLARATIONS,
 })
 export class SectionHeaderExampleModule {}
