d3c05cf0250227de7710198d39ffcbcf149b2e87
Mads Buchmann Frederiksen <mbf@prochimp.dk>
2021-12-09 10:17:54 +0100

    :lipstick: Remove 'item sliding' from the navigation sidebar

--- apps/cookbook/src/app/showcase/showcase.routes.ts
+++ apps/cookbook/src/app/showcase/showcase.routes.ts
@@ -69,6 +69,9 @@ export const routes: Routes = [
       {
         path: 'item-sliding',
         component: ItemSlidingShowcaseComponent,
+        data: {
+          hide: true,
+        },
       },
       {
         path: 'item-group',
