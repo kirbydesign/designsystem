115471ed52ec97c4e74403d5b5b6f87f33394ab8
RasmusKjeldgaard <rkk@bankdata.dk>
2021-12-10 10:36:11 +0100

    :white_check_mark: Finalize tests for typography

--- libs/designsystem/src/lib/components/section-header/section-header.component.spec.ts
+++ libs/designsystem/src/lib/components/section-header/section-header.component.spec.ts
@@ -2,11 +2,14 @@ import { IonItemDivider, IonLabel } from '@ionic/angular';
 import { createHostFactory, SpectatorHost } from '@ngneat/spectator';
 import { MockComponent } from 'ng-mocks';
 
+import { DesignTokenHelper } from '../../helpers';
 import { LabelComponent } from '../item/label/label.component';
 
 import { SectionHeaderComponent } from './section-header.component';
 
-fdescribe('SectionHeaderComponent', () => {
+const { fontSize, fontWeight, lineHeight } = DesignTokenHelper;
+
+describe('SectionHeaderComponent', () => {
   let spectator: SpectatorHost<SectionHeaderComponent>;
 
   const createHost = createHostFactory({
@@ -23,21 +26,40 @@ fdescribe('SectionHeaderComponent', () => {
     expect(spectator.component).toBeTruthy();
   });
 
-  describe('with heading, detail and label', () => {
+  describe('with heading, detail and label attributes', () => {
     beforeEach(() => {
       spectator = createHost<SectionHeaderComponent>(`<kirby-section-header>
-        <p heading>Hello</p>
-        <p label>Hello</p>
-        <p detail>Hello</p>
+        <p heading>Section Header</p>
+        <p label>Section Header</p>
+        <p detail>Section Header</p>
       </kirby-section-header>`);
     });
 
-    it('should have correct typography styles', () => {
-      expect(true).toBeFalse();
+    it('should have heading with correct typography styles', () => {
+      const heading = spectator.queryHost<HTMLElement>('[heading]');
+      expect(heading).toHaveComputedStyle({
+        'font-size': fontSize('m'),
+        'font-weight': fontWeight('bold'),
+        'line-height': lineHeight('m'),
+      });
     });
 
-    it('should have correct margin', () => {
-      expect(true).toBeFalse();
+    it('should have detail with correct typography styles', () => {
+      const detail = spectator.queryHost<HTMLElement>('[detail]');
+      expect(detail).toHaveComputedStyle({
+        'font-size': fontSize('s'),
+        'font-weight': fontWeight('light'),
+        'line-height': lineHeight('s'),
+      });
+    });
+
+    it('should have label with correct typography styles', () => {
+      const label = spectator.queryHost<HTMLElement>('[label]');
+      expect(label).toHaveComputedStyle({
+        'font-size': fontSize('s'),
+        'font-weight': fontWeight('light'),
+        'line-height': lineHeight('s'),
+      });
     });
   });
 
@@ -45,19 +67,38 @@ fdescribe('SectionHeaderComponent', () => {
     beforeEach(() => {
       spectator = createHost<SectionHeaderComponent>(`<kirby-section-header>
         <kirby-label>
-          <p heading>Hello</p>
-          <p label>Hello</p>
-          <p detail>Hello</p>
+          <p heading>Section Header</p>
+          <p label>Section Header</p>
+          <p detail>Section Header</p>
         </kirby-label>
       </kirby-section-header>`);
     });
 
-    it('should have correct typography styles', () => {
-      expect(true).toBeFalse();
+    it('should have heading with correct typography styles', () => {
+      const heading = spectator.queryHost<HTMLElement>('[heading]');
+      expect(heading).toHaveComputedStyle({
+        'font-size': fontSize('m'),
+        'font-weight': fontWeight('bold'),
+        'line-height': lineHeight('m'),
+      });
     });
 
-    it('should have correct margin', () => {
-      expect(true).toBeFalse();
+    it('should have detail with correct typography styles', () => {
+      const detail = spectator.queryHost<HTMLElement>('[detail]');
+      expect(detail).toHaveComputedStyle({
+        'font-size': fontSize('s'),
+        'font-weight': fontWeight('light'),
+        'line-height': lineHeight('s'),
+      });
+    });
+
+    it('should have label with correct typography styles', () => {
+      const label = spectator.queryHost<HTMLElement>('[label]');
+      expect(label).toHaveComputedStyle({
+        'font-size': fontSize('s'),
+        'font-weight': fontWeight('light'),
+        'line-height': lineHeight('s'),
+      });
     });
   });
 });
