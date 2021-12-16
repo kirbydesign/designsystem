fb715c48f4431972071222522cdb3807e4562161
RasmusKjeldgaard <rkk@bankdata.dk>
2021-12-16 12:49:40 +0100

    💄 Create 'rounded-corners' global style

--- libs/designsystem/src/lib/components/list/list-experimental/list-experimental.component.ts
+++ libs/designsystem/src/lib/components/list/list-experimental/list-experimental.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'kirby-list-experimental',
   templateUrl: './list-experimental.component.html',
+  styleUrls: ['./list-experimental.component.scss'],
 })
 export class ListExperimentalComponent {}
