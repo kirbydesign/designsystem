28a9f2032c8660a05e3ee59372be78d4cc6809a5
RasmusKjeldgaard <rkk@bankdata.dk>
2022-01-07 15:04:12 +0100

    🚨 Enable Ivy compilation
    Aside from being deprecated in Angular 13, the legacy view engine
    gave a very hard to debug error 'Maximum call stack size exceeded'
    when building the Angular package with 'ng build'. This seems to be
    mitigated with Ivy compilation enabled.
    
    The Ivy engine now catches erros where a module or directive is exposed
    through KirbyModule, without at the same time being exported by the top
    level index.ts barrel file. These missing exports are now added.

--- libs/designsystem/src/lib/components/list/index.ts
+++ libs/designsystem/src/lib/components/list/index.ts
@@ -3,6 +3,7 @@ export { ListShape } from './list.component';
 export { ListHeaderComponent } from './list-header/list-header.component';
 export { ListSectionHeaderComponent } from './list-section-header/list-section-header.component';
 export { ListItemComponent } from './list-item/list-item.component';
+export { ListModule } from './list.module';
 export { BoundaryClass } from './list-item/list-item.component';
 
 export { LoadOnDemandEvent } from './list.event';
