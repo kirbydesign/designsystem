31d079f728003d5b2c2bce696e2769a8828a4a71
RasmusKjeldgaard <rkk@bankdata.dk>
2022-01-28 12:44:08 +0100

    📝 Add examples to showcase

--- apps/cookbook/src/app/showcase/list-experimental-showcase/list-experimental-showcase.component.ts
+++ apps/cookbook/src/app/showcase/list-experimental-showcase/list-experimental-showcase.component.ts
@@ -1,29 +1,8 @@
 import { Component } from '@angular/core';
 
-import { ItemSwipeAction } from '@kirbydesign/designsystem';
-
 @Component({
   selector: 'cookbook-list-experimental-showcase',
   templateUrl: './list-experimental-showcase.component.html',
   styleUrls: ['./list-experimental-showcase.component.scss'],
 })
-export class ListShowcaseComponent {
-  swipeActions: ItemSwipeAction[] = [
-    {
-      title: 'edit',
-      type: 'success',
-      onSelected: () => {},
-    },
-    {
-      title: 'archive',
-      type: 'warning',
-      onSelected: () => {},
-    },
-    {
-      title: 'delete',
-      icon: 'trash',
-      onSelected: () => {},
-      type: 'danger',
-    },
-  ];
-}
+export class ListExperimentalShowcaseComponent {}
