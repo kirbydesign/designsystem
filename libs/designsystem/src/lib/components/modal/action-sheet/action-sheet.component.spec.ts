02cf13ea333a513cc151e2d686727a62ddc0a458
Michael Troelsen <mxt@bankdata.dk>
2022-01-28 16:46:46 +0100

    Change button color on action-sheet

--- libs/designsystem/src/lib/components/modal/action-sheet/action-sheet.component.spec.ts
+++ libs/designsystem/src/lib/components/modal/action-sheet/action-sheet.component.spec.ts
@@ -5,6 +5,7 @@ import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/t
 import * as ionic from '@ionic/angular';
 import { MockComponent } from 'ng-mocks';
 
+import { DesignTokenHelper } from '../../..';
 import { ButtonComponent } from '../../button/button.component';
 import { CardHeaderComponent } from '../../card/card-header/card-header.component';
 import { CardComponent } from '../../card/card.component';
@@ -15,6 +16,8 @@ import { GroupByPipe } from '../../list/pipes/group-by.pipe';
 import { SpinnerComponent } from '../../spinner/spinner.component';
 
 import { ActionSheetComponent } from './action-sheet.component';
+
+const getColor = DesignTokenHelper.getColor;
 
 describe('ActionSheetComponent', () => {
   let component: ActionSheetComponent;
@@ -150,6 +153,22 @@ describe('ActionSheetComponent', () => {
       const cancelButton = fixture.debugElement.query(By.css('.cancel-btn'));
       expect(component.cancelButtonText).toEqual(expected);
       expect(cancelButton.nativeElement.innerText).toEqual(expected);
+    });
+  });
+
+  describe('cancel button color', () => {
+    it('should render with correct color', () => {
+      const cancelButton = fixture.debugElement.query(By.css('.cancel-btn'));
+      const element = cancelButton.nativeElement as HTMLElement;
+      expect(element).toHaveComputedStyle({
+        'border-width': '1px',
+        'border-style': 'solid',
+        'border-color': 'transparent',
+        'background-color': getColor('white', 'contrast'),
+        /* prettier-ignore */
+        // tslint:disable-next-line: prettier
+        'color': getColor('white')
+      });
     });
   });
 });
