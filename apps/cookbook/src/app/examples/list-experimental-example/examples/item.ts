8760edd8cf929069885fae5c43dbc6a73cf3e037
RasmusKjeldgaard <rkk@bankdata.dk>
2022-01-31 17:53:30 +0100

    📝 Improve documentation

--- apps/cookbook/src/app/examples/list-experimental-example/examples/item.ts
+++ apps/cookbook/src/app/examples/list-experimental-example/examples/item.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 import { BaseListComponent } from '../../list-shared/base-list.component';
 
 const template = `<kirby-list-experimental>
-<kirby-section-header list-header><h2 heading>List with items</h2></kirby-section-header>
-<kirby-item *ngFor="let item of items"><p>{{ item.title }}</p></kirby-item>
+  <kirby-section-header outside>
+    <h2 heading>Stocks</h2>
+  </kirby-section-header>
+  <kirby-item *ngFor="let item of items">
+    <p>{{ item.title }}</p>
+    <data slot="end" class="kirby-text-bold">{{item.amount}}</data>
+  </kirby-item>
 </kirby-list-experimental>`;
 
 @Component({
