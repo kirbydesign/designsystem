8a513ce2b335c30a273a376684e29d7182d18bfd
RasmusKjeldgaard <rkk@bankdata.dk>
2022-02-02 12:02:14 +0100

    📝  Add multiple sections description to showcase

--- apps/cookbook/src/app/showcase/list-experimental-showcase/list-experimental-showcase.component.ts
+++ apps/cookbook/src/app/showcase/list-experimental-showcase/list-experimental-showcase.component.ts
@@ -5,4 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './list-experimental-showcase.component.html',
   styleUrls: ['./list-experimental-showcase.component.scss'],
 })
-export class ListExperimentalShowcaseComponent {}
+export class ListExperimentalShowcaseComponent {
+  multipleListsExample: string = `<kirby-list-experimental>
+  <kirby-section-header outside></kirby-section-header>
+  ...
+</kirby-list-experimental>
+
+<kirby-list-experimental>
+  <kirby-section-header outside></kirby-section-header>
+  ...
+</kirby-list-experimental>`;
+}
