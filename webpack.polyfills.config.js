6aa68cea5ff0457c99e1872d564c542d61c9e633
RasmusKjeldgaard <rkk@bankdata.dk>
2022-03-29 15:43:45 +0200

    🎨  Lint and prettify misc project files

--- webpack.polyfills.config.js
+++ webpack.polyfills.config.js
@@ -5,10 +5,14 @@ module.exports = () => {
   const config = {
     mode: 'production',
     entry: {
-      'resize-observer-polyfill': './libs/designsystem/src/lib/polyfills/resize-observer-polyfill.js',
-      'resize-observer-polyfill-loader': './libs/designsystem/src/lib/polyfills/resize-observer-polyfill-loader',
-      'intersection-observer-polyfill': './libs/designsystem/src/lib/polyfills/intersection-observer-polyfill.js',
-      'intersection-observer-polyfill-loader': './libs/designsystem/src/lib/polyfills/intersection-observer-polyfill-loader',
+      'resize-observer-polyfill':
+        './libs/designsystem/src/lib/polyfills/resize-observer-polyfill.js',
+      'resize-observer-polyfill-loader':
+        './libs/designsystem/src/lib/polyfills/resize-observer-polyfill-loader',
+      'intersection-observer-polyfill':
+        './libs/designsystem/src/lib/polyfills/intersection-observer-polyfill.js',
+      'intersection-observer-polyfill-loader':
+        './libs/designsystem/src/lib/polyfills/intersection-observer-polyfill-loader',
     },
     output: {
       filename: '[name].min.js',
@@ -17,7 +21,7 @@ module.exports = () => {
     plugins: [
       new CopyWebpackPlugin([
         { from: './libs/designsystem/src/lib/polyfills/resize-observer-polyfill-loader.js' },
-        { from: './libs/designsystem/src/lib/polyfills/intersection-observer-polyfill-loader.js' }
+        { from: './libs/designsystem/src/lib/polyfills/intersection-observer-polyfill-loader.js' },
       ]),
     ],
   };
