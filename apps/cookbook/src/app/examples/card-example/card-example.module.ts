57e2f0cd281a0540e6a448ab0deb15b5b8fe8af9
Jesper Kaltoft <git@jesperkaltoft.dk>
2022-04-05 20:41:29 +0200

    💚 Fix cookbook build
    - Move template to file and use noop() from rxjs
    - Move example component and use noop from rxjs

--- apps/cookbook/src/app/examples/card-example/card-example.module.ts
+++ apps/cookbook/src/app/examples/card-example/card-example.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { KirbyModule } from '@kirbydesign/designsystem';
 
 import { CardExampleComponent } from './card-example.component';
-import { CardBackgroundImageExampleComponent } from './examples/card-background-image-example.component';
+import { CardBackgroundImageExampleComponent } from './examples/card-background-image-example/card-background-image-example.component';
 import { CardClickableExampleComponent } from './examples/card-clickable-example/card-clickable-example.component';
 import { CardCssBackgroundImageExampleComponent } from './examples/card-css-background-image-example/card-css-background-image-example.component';
 import { CardElevationsExampleComponent } from './examples/card-elevations-example/card-elevations-example.component';
