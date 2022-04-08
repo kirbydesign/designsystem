a1334e167968f3be8e1810259f65991a99402eac
RasmusKjeldgaard <rkk@bankdata.dk>
2022-04-08 11:41:39 +0200

    ✅  Migrate tests to use spectator
    Directly setting the input on the component did not work with OnPush
    change detection, so spectator is used in alert test now, while we
    were at it. This way we can interact with the component via setInput().

--- libs/designsystem/src/lib/components/modal/alert/alert.component.spec.ts
+++ libs/designsystem/src/lib/components/modal/alert/alert.component.spec.ts
@@ -1,124 +1,22 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
 import { createHostFactory, SpectatorHost } from '@ngneat/spectator';
 
 import { DesignTokenHelper } from '@kirbydesign/core';
 
 import { WindowRef } from '../../../types/window-ref';
 import { ButtonComponent } from '../../button/button.component';
+import { EmptyStateComponent } from '../../empty-state/empty-state.component';
 
 import { AlertComponent } from './alert.component';
 
 const getColor = DesignTokenHelper.getColor;
 
 describe('AlertComponent', () => {
-  let component: AlertComponent;
-  let fixture: ComponentFixture<AlertComponent>;
-
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [AlertComponent, ButtonComponent],
-      providers: [
-        {
-          provide: WindowRef,
-          useValue: <WindowRef>{ nativeWindow: window },
-        },
-      ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    }).compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AlertComponent);
-    component = fixture.componentInstance;
-    component.okBtn = 'Test OK Button Text';
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
-  describe('ok button', () => {
-    it('should render', () => {
-      const expected = 'Test OK Button Text';
-      const okButton = fixture.debugElement.query(By.css('.ok-btn'));
-
-      expect(component.okBtn).toEqual(expected);
-      expect(okButton.nativeElement.innerText).toEqual(expected);
-    });
-
-    it('should support isDestructive', () => {
-      component.okBtnIsDestructive = true;
-      fixture.detectChanges();
-      const okButton = fixture.debugElement.query(By.css('.ok-btn'));
-
-      expect(okButton).toBeDefined();
-      expect(okButton.nativeElement.getAttribute('class')).toContain('destructive');
-    });
-
-    it('should default to not being destructive', () => {
-      const okButton = fixture.debugElement.query(By.css('.ok-btn'));
-
-      expect(okButton).toBeDefined();
-      expect(okButton.nativeElement.getAttribute('class')).not.toContain('destructive');
-    });
-
-    it('should have default size', () => {
-      const okButton = fixture.debugElement.query(By.css('.ok-btn'));
-      component.cancelBtn = 'Test Cancel Button Text';
-      fixture.detectChanges();
-
-      expect(okButton.attributes['ng-reflect-size']).toBeUndefined();
-    });
-
-    it('should have large ok button when no cancel button', () => {
-      const okButton = fixture.debugElement.query(By.css('.ok-btn'));
-      component.cancelBtn = null;
-      fixture.detectChanges();
-
-      expect(okButton.attributes['ng-reflect-size']).toBe('lg');
-    });
-  });
-
-  describe('cancel button', () => {
-    it('should render', () => {
-      const expected = 'Test Cancel Button Text';
-      component.cancelBtn = 'Test Cancel Button Text';
-      fixture.detectChanges();
-      const cancelButton = fixture.debugElement.query(By.css('.cancel-btn'));
-
-      expect(cancelButton.nativeElement.innerText).toEqual(expected);
-    });
-
-    it('should not render when cancelBtn not set', () => {
-      component.cancelBtn = null;
-      fixture.detectChanges();
-      const cancelButton = fixture.debugElement.query(By.css('.cancel-btn'));
-
-      expect(cancelButton).toBeNull();
-    });
-  });
-
-  describe('icon', () => {
-    it('should render', () => {
-      component.iconName = 'warning';
-      fixture.detectChanges();
-      const icon = fixture.debugElement.query(By.css('.icon-outline'));
-
-      expect(icon).toBeDefined();
-    });
-  });
-});
-
-describe('AlertComponent with okBtn', () => {
   let spectator: SpectatorHost<AlertComponent>;
   let element: HTMLElement;
 
   const createHost = createHostFactory({
     component: AlertComponent,
-    declarations: [ButtonComponent],
+    declarations: [ButtonComponent, EmptyStateComponent],
     providers: [
       {
         provide: WindowRef,
@@ -129,7 +27,8 @@ describe('AlertComponent with okBtn', () => {
 
   beforeEach(() => {
     spectator = createHost(`
-    <kirby-alert okBtn="OK Button Text">
+    <kirby-alert okBtn="Test OK Button Text"
+    cancelBtnText="Test Cancel Button Text">
     </kirby-alert>
     `);
     element = spectator.element;
@@ -139,12 +38,74 @@ describe('AlertComponent with okBtn', () => {
     expect(spectator.component).toBeTruthy();
   });
 
-  it('should have success colors on button', () => {
-    const okButton = element.querySelector('[kirby-button]');
+  describe('ok button', () => {
+    let okButton: HTMLElement;
 
-    expect(okButton).toHaveComputedStyle({
-      'background-color': getColor('success'),
-      color: getColor('success', 'contrast'),
+    beforeEach(() => {
+      okButton = spectator.query('.ok-btn');
+    });
+    it('should render', () => {
+      const expected = 'Test OK Button Text';
+
+      expect(spectator.component.okBtn).toEqual(expected);
+      expect(okButton).toHaveText(expected);
+    });
+
+    it('should support isDestructive', () => {
+      spectator.setInput({ okBtnIsDestructive: true });
+
+      expect(okButton).toBeDefined();
+      expect(okButton).toHaveClass('destructive');
+    });
+
+    it('should default to not being destructive', () => {
+      expect(okButton).toBeDefined();
+      expect(okButton).not.toHaveClass('destructive');
+    });
+
+    it('should have large ok button when no cancel button', () => {
+      spectator.setInput({ cancelBtn: null });
+
+      expect(okButton).toHaveClass('lg');
+    });
+
+    it('should have success colors on button', () => {
+      expect(okButton).toHaveComputedStyle({
+        'background-color': getColor('success'),
+        color: getColor('success', 'contrast'),
+      });
+    });
+
+    it('should have default size when cancel button', () => {
+      expect(okButton.attributes['ng-reflect-size']).toBeUndefined();
+    });
+  });
+
+  describe('cancel button', () => {
+    let cancelButton: HTMLElement;
+
+    beforeEach(() => {
+      cancelButton = spectator.query('.cancel-btn');
+    });
+    it('should render', () => {
+      const expected = 'Test Cancel Button Text';
+
+      expect(spectator.component.cancelBtn).toEqual(expected);
+      expect(cancelButton).toHaveText(expected);
+    });
+
+    it('should not render when cancelBtn not set', () => {
+      spectator.setInput({ cancelBtn: null });
+
+      expect(spectator.query('.cancel-btn')).toBeNull();
+    });
+  });
+  describe('icon', () => {
+    it('should render', () => {
+      spectator.setInput({ iconName: 'warning' });
+      const icon: HTMLElement = spectator.query('.icon-outline');
+
+      expect(icon).not.toBeNull();
     });
   });
 });
