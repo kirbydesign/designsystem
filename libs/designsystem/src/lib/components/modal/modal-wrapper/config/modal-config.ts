5010cda686333fea7f54980159bf45fe225eb649
RasmusKjeldgaard <rkk@bankdata.dk>
2022-04-21 10:52:53 +0200

    🔥 Remove deprecated dim property on ModalConfig

--- libs/designsystem/src/lib/components/modal/modal-wrapper/config/modal-config.ts
+++ libs/designsystem/src/lib/components/modal/modal-wrapper/config/modal-config.ts
@@ -16,10 +16,6 @@ export interface ModalConfig {
   modalRoute?: ActivatedRoute;
   siblingModalRouteActivated$?: Observable<ActivatedRoute>;
   flavor?: ModalFlavor;
-  /**
-   * @deprecated Will be removed in next major version.
-   */
-  dim?: number;
   componentProps?: { [key: string]: any };
   // the supplementary action is only available in the drawer
   drawerSupplementaryAction?: DrawerSupplementaryAction;
