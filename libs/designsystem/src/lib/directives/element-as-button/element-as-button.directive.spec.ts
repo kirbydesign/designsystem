90a57892e0150e6992331b88f530c22319ae9498
Mads Buchmann Frederiksen <mbf@prochimp.dk>
2022-05-24 11:51:44 +0200

    :white_check_mark: Add tests for aria-pressed hostbinding

--- libs/designsystem/src/lib/directives/element-as-button/element-as-button.directive.spec.ts
+++ libs/designsystem/src/lib/directives/element-as-button/element-as-button.directive.spec.ts
@@ -1,4 +1,4 @@
-import { createDirectiveFactory, SpectatorDirective } from '@ngneat/spectator';
+import { createDirectiveFactory, createKeyboardEvent, SpectatorDirective } from '@ngneat/spectator';
 
 import { CardComponent } from '../../components/card/card.component';
 
@@ -6,6 +6,7 @@ import { ElementAsButtonDirective } from './element-as-button.directive';
 
 describe('ElementAsButtonDirective', () => {
   let spectator: SpectatorDirective<ElementAsButtonDirective>;
+  let cardElement: HTMLElement;
 
   const createDirective = createDirectiveFactory({
     directive: ElementAsButtonDirective,
@@ -14,10 +15,48 @@ describe('ElementAsButtonDirective', () => {
   });
   beforeEach(() => {
     spectator = createDirective(`<kirby-card (click)="someMethod()"> </kirby-card>`);
+    cardElement = spectator.query('kirby-card');
   });
 
   it('should get the instance', () => {
     const instance = spectator.directive;
     expect(instance).toBeDefined();
+  });
+
+  const keyScenarios = ['space', 'enter'];
+
+  keyScenarios.forEach((key) => {
+    describe(`when keydown is activated for ${key}`, () => {
+      beforeEach(() => {
+        expect(cardElement.getAttribute('aria-pressed')).toEqual('false');
+        spectator.dispatchKeyboardEvent('kirby-card', 'keydown', key);
+      });
+
+      it('should set host "aria-pressed" attribute to "true"', () => {
+        expect(cardElement.getAttribute('aria-pressed')).toEqual('true');
+      });
+
+      describe(`and keyup is then activated for ${key}`, () => {
+        beforeEach(() => {
+          expect(cardElement.getAttribute('aria-pressed')).toEqual('true');
+          spectator.dispatchKeyboardEvent('kirby-card', 'keyup', key);
+        });
+
+        it('should set host "aria-pressed" attribute to "false"', () => {
+          expect(cardElement.getAttribute('aria-pressed')).toEqual('false');
+        });
+      });
+
+      describe('and the element is then unfocused', () => {
+        beforeEach(() => {
+          expect(cardElement.getAttribute('aria-pressed')).toEqual('true');
+          spectator.blur(cardElement);
+        });
+
+        it('should set host "aria-pressed" attribute to "false"', () => {
+          expect(cardElement.getAttribute('aria-pressed')).toEqual('false');
+        });
+      });
+    });
   });
 });
