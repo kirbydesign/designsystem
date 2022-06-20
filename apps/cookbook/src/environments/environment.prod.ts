6ab9826ba22f7a5c027aea5dee541afe786a0fff
Mads Buchmann Frederiksen <mbf@prochimp.dk>
2022-06-20 11:01:46 +0200

    :fire: Remove github service related env variables

--- apps/cookbook/src/environments/environment.prod.ts
+++ apps/cookbook/src/environments/environment.prod.ts
@@ -1,9 +1,3 @@
 export const environment = {
   production: true,
-  githubBaseUrl: 'https://github.com/kirbydesign/designsystem',
-  githubApi: 'https://api.github.com',
-  oauth: {
-    githubToken1: 'ghp_jd0nvTUS5YSDCRCbL',
-    githubToken2: '8sHauSnf3pUZS2g0iXV',
-  },
 };
--- apps/cookbook/src/environments/environment.ts
+++ apps/cookbook/src/environments/environment.ts
@@ -4,12 +4,6 @@
 
 export const environment = {
   production: false,
-  githubBaseUrl: 'https://github.com/kirbydesign/designsystem',
-  githubApi: 'https://api.github.com',
-  oauth: {
-    githubToken1: 'ghp_jd0nvTUS5YSDCRCbL',
-    githubToken2: '8sHauSnf3pUZS2g0iXV',
-  },
 };
 
 /*
