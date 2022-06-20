f34deeab9bee3eb6c65592efffeaba685ba83a84
Mads Buchmann Frederiksen <mbf@prochimp.dk>
2022-06-20 10:55:17 +0200

    :fire: Remove firebase dependency

--- apps/cookbook/src/environments/environment.prod.ts
+++ apps/cookbook/src/environments/environment.prod.ts
@@ -6,13 +6,4 @@ export const environment = {
     githubToken1: 'ghp_jd0nvTUS5YSDCRCbL',
     githubToken2: '8sHauSnf3pUZS2g0iXV',
   },
-  firebase: {
-    apiKey: 'AIzaSyAywQylHrFlUNN2v6PD96Gc_82Erwbw7E4',
-    authDomain: 'kirby-1817d.firebaseapp.com',
-    databaseURL: 'https://kirby-1817d.firebaseio.com',
-    projectId: 'kirby-1817d',
-    storageBucket: 'kirby-1817d.appspot.com',
-    messagingSenderId: '168198500869',
-    appId: '1:168198500869:web:d6c5f3fe74a653d0',
-  },
 };
