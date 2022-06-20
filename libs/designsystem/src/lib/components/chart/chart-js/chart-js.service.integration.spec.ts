9b7a02c95ad4774fadd56f2867fdb951963de8dd
Mads Buchmann Frederiksen <mbf@prochimp.dk>
2022-06-20 13:19:37 +0200

    :recycle: Move stock specific integration tests to own suite

--- libs/designsystem/src/lib/components/chart/chart-js/chart-js.service.integration.spec.ts
+++ libs/designsystem/src/lib/components/chart/chart-js/chart-js.service.integration.spec.ts
@@ -266,55 +266,5 @@ fdescribe('ChartJSService with ChartConfigService', () => {
         });
       });
     });
-
-    xdescribe('when type is "stock"', () => {
-      let chart: Chart;
-
-      beforeEach(() => {
-        chartJSService.renderChart({
-          targetElement: canvasElement,
-          type: 'stock',
-          data: [
-            {
-              data: [
-                { x: 10, y: 5 },
-                { x: 11, y: 6 },
-              ],
-            },
-          ],
-          /*dataLabelOptions: {
-            locale: 'da-DK',
-            valueSuffix: '%',
-            showMin: true,
-          },*/
-        });
-        chart = chartJSService['chart'];
-      });
-
-      it('should use correct ChartJS type', () => {
-        expect(chart.config.type).toBe('line');
-      });
-
-      it('should have correct tension', () => {
-        expect(chart.options.elements.line.tension).toBe(0);
-      });
-
-      it('should have datalabels configuration', () => {
-        expect(chart.options.plugins.datalabels).toBeTruthy();
-      });
-
-      describe('when ChartDataLabelOptions is passed', () => {
-        it('chart should have locale if ChartDataLabelOptions.locale', () => {
-          expect(chart.options.locale).toBe('da-DK');
-        });
-
-        it('chart data should have suffix if ChartDataLabelOptions.valueSuffix', () => {
-          const dataItemWithLabel: any = chart.data.datasets[0].data.find(
-            (dataItem) => (dataItem as any).datalabel
-          );
-          expect(dataItemWithLabel.datalabel.value).toContain('%');
-        });
-      });
-    });
   });
 });
