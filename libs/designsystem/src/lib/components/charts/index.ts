4311127b2f9a7a80fd4cbfdc4cd51fb47aa2aa1a
Mads Buchmann Frederiksen <mbf@prochimp.dk>
2022-06-27 13:56:41 +0200

    :sparkles: export stock chart at top-level library endpoint

--- libs/designsystem/src/lib/components/charts/index.ts
+++ libs/designsystem/src/lib/components/charts/index.ts
@@ -1,4 +1,5 @@
 export * from './chart';
+export * from './stock-chart';
 export * from './shared/charts.types';
 export { ChartOptions, ChartEvent, ActiveElement, Chart } from 'chart.js';
 export { AnnotationOptions } from 'chartjs-plugin-annotation';
