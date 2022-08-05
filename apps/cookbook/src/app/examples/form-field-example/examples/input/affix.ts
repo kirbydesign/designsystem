c3690c5abc28132f597db36b6d1e09579abc6f90
Thomas Springborg <thsp@jyskebank.dk>
2022-08-05 14:23:22 +0200

    Initial styling and cookbook example setup for affix

---
+++ apps/cookbook/src/app/examples/form-field-example/examples/input/affix.ts
@@ -0,0 +1,33 @@
+import { Component, Input } from '@angular/core';
+
+import { InputSize } from '@kirbydesign/designsystem';
+
+const config = {
+  selector: 'cookbook-form-field-input-affix-example',
+  template: `
+    <kirby-form-field label="With prefix">
+      <kirby-icon name="payment-card" prefix></kirby-icon>
+      <input kirby-input placeholder="Enter your card number" [size]="size" />
+    </kirby-form-field>
+
+    <kirby-form-field label="With suffix">
+      <input kirby-input kirby-decimal-mask type="number" placeholder="Monthly payments" [size]="size" />
+      <span suffix>$/m</span>
+    </kirby-form-field>
+    
+    <kirby-form-field label="With prefix and suffix">
+      <kirby-icon name="search" prefix></kirby-icon>
+      <input kirby-input decimal-mask placeholder="Seach..." [size]="size" />
+      <kirby-spinner suffix></kirby-spinner>
+    </kirby-form-field>
+  `,
+};
+
+@Component({
+  selector: config.selector,
+  template: config.template,
+})
+export class FormFieldInputAffixExampleComponent {
+  template: string = config.template;
+  @Input() size: InputSize;
+}
