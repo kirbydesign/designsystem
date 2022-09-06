ae08dc1878d39403b9d6a275288a9dba7fb2b77e
RasmusKjeldgaard <rkk@bankdata.dk>
2022-09-06 15:38:44 +0200

    ♻️ Clean up and make list 600 items long

--- apps/user-flows/src/app/app.component.ts
+++ apps/user-flows/src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'kirbydesign-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'user-flows';
