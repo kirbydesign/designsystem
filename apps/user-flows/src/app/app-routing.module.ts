50aa7c7b7568d0f59d112277d4245ec377b4c06c
RasmusKjeldgaard <rkk@bankdata.dk>
2022-09-07 14:42:35 +0200

    ✨ Set up transaction flow component with route

--- apps/user-flows/src/app/app-routing.module.ts
+++ apps/user-flows/src/app/app-routing.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+import { TransactionFlowComponent } from './transaction-flow/transaction-flow.component';
+
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then((m) => m.HomePageModule),
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'transaction-flow',
+    component: TransactionFlowComponent,
   },
 ];
 
--- apps/user-flows/src/app/app.module.ts
+++ apps/user-flows/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { KirbyModule } from '@kirbydesign/designsystem';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TransactionFlowComponent } from './transaction-flow/transaction-flow.component';
+import { TransactionListModule } from './transaction-flow/transaction-list/transaction-list.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, KirbyModule],
+  declarations: [AppComponent, TransactionFlowComponent],
+  imports: [BrowserModule, AppRoutingModule, KirbyModule, TransactionListModule],
   providers: [],
   bootstrap: [AppComponent],
 })
