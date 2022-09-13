87783aacb98eec76b7fcd12fc11592b7f3a1e6a4
Agre Huang <agre88@hotmail.com>
2022-09-13 15:25:15 +0200

    add overview for transactions

--- apps/user-flows/src/app/transaction-details-flow/transaction-details-transactions/transaction-details-transactions.component.ts
+++ apps/user-flows/src/app/transaction-details-flow/transaction-details-transactions/transaction-details-transactions.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Transaction } from '~/app/transaction-flow/transaction';
 
+import { ListSwipeAction } from '@kirbydesign/designsystem';
+
 import transactionsData from '../../transaction-flow/transactions-data.json';
+import { TransactionDetailsTransactionDetailsComponent } from '../transaction-details-transaction-details/transaction-details-transaction-details.component';
 
 @Component({
   selector: 'kirbydesign-transaction-details-transactions',
@@ -14,9 +17,33 @@ export class TransactionDetailsTransactionsComponent implements OnInit {
   transactions: Transaction[] = [];
   ngOnInit(): void {
     this.transactions = transactionsData.transactions;
+    console.log(this.transactions);
   }
 
   getSectionName(transaction: Transaction): string {
     return transaction.date.utc;
   }
+
+  swipeActions: ListSwipeAction[] = [
+    {
+      position: 'left',
+      title: 'Archive',
+      type: 'warning',
+      onSelected: (item) => console.log(item),
+    },
+    {
+      position: 'left',
+      title: 'Flag',
+      icon: 'flag',
+      type: 'success',
+      onSelected: (item) => console.log(item),
+    },
+    {
+      position: 'right',
+      title: 'Delete',
+      icon: 'trash',
+      type: 'danger',
+      onSelected: (item) => console.log(item),
+    },
+  ];
 }
