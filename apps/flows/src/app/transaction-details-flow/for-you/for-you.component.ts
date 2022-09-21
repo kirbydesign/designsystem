90894a04254397c9d8742c01350d2ee4426ee6aa
Agre Huang <agre88@hotmail.com>
2022-09-21 10:29:01 +0200

    Add chart for styling on for you page

--- apps/flows/src/app/transaction-details-flow/for-you/for-you.component.ts
+++ apps/flows/src/app/transaction-details-flow/for-you/for-you.component.ts
@@ -5,4 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './for-you.component.html',
   styleUrls: ['./for-you.component.scss'],
 })
-export class ForYouComponent {}
+export class ForYouComponent {
+  data = [7, 12, 5, 9, 3, 11, 6, 2, 1, 10, 4, 12];
+
+  labels = ['Jan', 'Feb', 'Mar', 'Apr', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+}
