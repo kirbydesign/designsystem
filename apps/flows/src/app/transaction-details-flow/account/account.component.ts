4f0d0e9113386233a87d78b724e877b837371e9a
Agre Huang <agre88@hotmail.com>
2022-09-21 10:28:20 +0200

    Add toast message on card button

--- apps/flows/src/app/transaction-details-flow/account/account.component.ts
+++ apps/flows/src/app/transaction-details-flow/account/account.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ToastConfig, ToastController } from '@kirbydesign/designsystem';
+
 @Component({
   selector: 'kirbydesign-account',
   templateUrl: './account.component.html',
 })
 export class AccountComponent implements OnInit {
-  constructor() {}
+  constructor(private toastController: ToastController) {}
 
   ngOnInit(): void {}
+
+  onCardSelect() {
+    const config: ToastConfig = {
+      message: 'Your toast message',
+      messageType: 'success',
+      durationInMs: 10000,
+    };
+    this.toastController.showToast(config);
+  }
 }
