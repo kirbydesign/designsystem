a91ddb234a6bfbf3e9f293c3220e8213cad20390
RasmusTraeholt <rth@bankdata.dk>
2022-10-12 14:44:53 +0200

    fix module scope

--- libs/designsystem/src/lib/kirby-experimental.module.ts
+++ libs/designsystem/src/lib/kirby-experimental.module.ts
@@ -1,18 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { IconModule, PageLocalNavigationComponent } from './components';
+import { PageLocalNavigationComponent } from './components';
+import { KirbyModule } from './kirby.module';
 
 const COMPONENT_DECLARATIONS = [PageLocalNavigationComponent];
 
 /**
  * This module contains experimental components, that should not be used in a production environment.
- * *Note some experimental components may use standard components which will require KirbyModule to be imported locally as well
  * @see KirbyModule
  */
 @NgModule({
-  imports: [CommonModule, IconModule],
+  imports: [CommonModule, KirbyModule],
   exports: COMPONENT_DECLARATIONS,
   declarations: COMPONENT_DECLARATIONS,
-  providers: [],
 })
 export class KirbyExperimentalModule {}
