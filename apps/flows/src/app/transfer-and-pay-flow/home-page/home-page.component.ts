4350e63e445c5f8b3ffad0f1302292b0cf011904
Agre Huang <agre88@hotmail.com>
2022-10-20 12:15:41 +0200

    Homepage button centered correctly

--- apps/flows/src/app/transfer-and-pay-flow/home-page/home-page.component.ts
+++ apps/flows/src/app/transfer-and-pay-flow/home-page/home-page.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { ModalConfig, ModalController } from '@kirbydesign/designsystem';
 import { TransferAndPayModalComponent } from '../transfer-and-pay-modal/transfer-and-pay-modal.component';
 
