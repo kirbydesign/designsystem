1d958a019c5cd6b9ae8eb89d0173884e750e9210
Agre Huang <agre88@hotmail.com>
2022-10-20 12:29:43 +0200

    Add choose own account component as modal

---
+++ apps/flows/src/app/transfer-and-pay-flow/choose-own-account/choose-own-account.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'flows-choose-own-account',
+  templateUrl: './choose-own-account.component.html',
+  styleUrls: ['./choose-own-account.component.scss'],
+})
+export class ChooseOwnAccountComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
