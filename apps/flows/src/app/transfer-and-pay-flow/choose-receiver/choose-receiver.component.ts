2c98738d03bce5be09a9edd4ac3eb30273364721
Agre Huang <agre88@hotmail.com>
2022-10-25 12:26:45 +0200

    Choose receiver component

---
+++ apps/flows/src/app/transfer-and-pay-flow/choose-receiver/choose-receiver.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { OwnAccountPageComponent } from '../own-account-page/own-account-page.component';
+import { OtherPageComponent } from '../other-page/other-page.component';
+import { SegmentItem } from '@kirbydesign/designsystem';
+import { OtherService } from '../other.service';
+@Component({
+  selector: 'flows-choose-receiver',
+  templateUrl: './choose-reciever.component.html',
+  styleUrls: ['./choose-receiver.component.scss'],
+})
+export class ChooseReceiverComponent implements OnInit {
+  readonly items: SegmentItem[] = [
+    {
+      text: 'Own Account',
+      id: 'OwnAccountPage',
+    },
+    {
+      text: 'Other',
+      id: 'OtherPage',
+    },
+  ];
+
+  selectedSegment = this.items[0];
+
+  constructor(private OtherService: OtherService) {}
+
+  onSegmentSelect(segment: SegmentItem) {
+    this.selectedSegment = segment;
+  }
+
+  ngOnInit(): void {
+    this.selectedSegment = this.items[0];
+  }
+
+  saveReceiver() {
+    this.OtherService.saveReceiver();
+  }
+}
