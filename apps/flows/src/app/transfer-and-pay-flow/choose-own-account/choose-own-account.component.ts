73ab6589f207f9bd02d65fcf14cc86a9863483c6
Agre Huang <agre88@hotmail.com>
2022-10-31 15:07:38 +0100

    Fix spelling and remove styleUrls

--- apps/flows/src/app/transfer-and-pay-flow/choose-own-account/choose-own-account.component.ts
+++ apps/flows/src/app/transfer-and-pay-flow/choose-own-account/choose-own-account.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'flows-choose-own-account',
   templateUrl: './choose-own-account.component.html',
-  styleUrls: ['./choose-own-account.component.scss'],
 })
 export class ChooseOwnAccountComponent implements OnInit {
   constructor() {}
