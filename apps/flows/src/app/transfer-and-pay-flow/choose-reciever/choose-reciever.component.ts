20c40b4b6b76ebf7c29c3447afbbc8357147a55e
Agre Huang <agre88@hotmail.com>
2022-10-31 15:09:39 +0100

    Remove styleUrl

--- apps/flows/src/app/transfer-and-pay-flow/choose-reciever/choose-reciever.component.ts
+++ apps/flows/src/app/transfer-and-pay-flow/choose-reciever/choose-reciever.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'flows-choose-reciever',
   templateUrl: './choose-reciever.component.html',
-  styleUrls: ['./choose-reciever.component.scss'],
 })
 export class ChooseRecieverComponent implements OnInit {
   constructor() {}
