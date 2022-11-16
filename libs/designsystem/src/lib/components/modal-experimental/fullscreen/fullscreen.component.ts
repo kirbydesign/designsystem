7276acf52e3e27753944ef899f192f9b28a39fbd
Mark Drastrup <mark.drastrup@gmail.com>
2022-11-16 19:01:54 +0900

    Create base component for inline fullscreen modal

--- libs/designsystem/src/lib/components/modal-experimental/fullscreen/fullscreen.component.ts
+++ libs/designsystem/src/lib/components/modal-experimental/fullscreen/fullscreen.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'kirby-fullscreen-modal-experimental',
   templateUrl: './fullscreen.component.html',
+  styleUrls: ['./fullscreen.component.scss'],
 })
-export class FullscreenModalExperimentalComponent {}
+export class FullscreenModalExperimentalComponent {
+  @Input() open = false;
+  @Input() hasCollapsibleTitle = false;
+
+  @Output() close = new EventEmitter();
+
+  closeModal() {
+    this.close.emit();
+  }
+}
