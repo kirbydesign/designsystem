c94865ef581352c1cac08d00edc43fee5dae1575
Mark Drastrup <mark.drastrup@gmail.com>
2022-11-17 21:30:22 +0900

    Replace the any type with OverlayEventDetail

--- libs/designsystem/src/lib/components/modal-experimental/fullscreen/fullscreen.component.ts
+++ libs/designsystem/src/lib/components/modal-experimental/fullscreen/fullscreen.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { IonContent, IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
 import { KirbyAnimation } from '../../../animation/kirby-animation';
 
 @Component({
@@ -15,29 +16,28 @@ export class FullscreenModalExperimentalComponent {
   @Input() canDismiss = true;
   @Input() hasCollapsibleTitle = true;
 
-  //TODO - replace any
-  @Output() willPresent = new EventEmitter<any>();
-  @Output() didPresent = new EventEmitter<any>();
-  @Output() didDismiss = new EventEmitter<any>();
-  @Output() willDismiss = new EventEmitter<any>();
+  @Output() willPresent = new EventEmitter<OverlayEventDetail>();
+  @Output() didPresent = new EventEmitter<OverlayEventDetail>();
+  @Output() didDismiss = new EventEmitter<OverlayEventDetail>();
+  @Output() willDismiss = new EventEmitter<OverlayEventDetail>();
 
   _closeModal() {
     this.modal.dismiss(null, 'cancel');
   }
 
-  _onWillPresent(event) {
+  _onWillPresent(event: OverlayEventDetail) {
     this.willPresent.emit(event);
   }
 
-  _onDidPresent(event) {
+  _onDidPresent(event: OverlayEventDetail) {
     this.didPresent.emit(event);
   }
 
-  _onWillDismiss(event) {
+  _onWillDismiss(event: OverlayEventDetail) {
     this.willDismiss.emit(event);
   }
 
-  _onDidDismiss(event) {
+  _onDidDismiss(event: OverlayEventDetail) {
     this.didDismiss.emit(event);
   }
 
