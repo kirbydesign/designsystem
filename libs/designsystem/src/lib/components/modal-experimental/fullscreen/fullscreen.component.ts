0f91358f60bf1d019b203a84720e166d66b6c383
Mark Drastrup <mark.drastrup@gmail.com>
2022-11-17 16:25:37 +0900

    Implement and emit lifecycle events

--- libs/designsystem/src/lib/components/modal-experimental/fullscreen/fullscreen.component.ts
+++ libs/designsystem/src/lib/components/modal-experimental/fullscreen/fullscreen.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
 
 @Component({
   selector: 'kirby-fullscreen-modal-experimental',
@@ -6,12 +7,34 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./fullscreen.component.scss'],
 })
 export class FullscreenModalExperimentalComponent {
-  @Input() open = false;
-  @Input() hasCollapsibleTitle = false;
+  @ViewChild(IonModal) modal: IonModal;
 
-  @Output() close = new EventEmitter();
+  @Input() open = false;
+  @Input() hasCollapsibleTitle = true;
+
+  //TODO - replace any
+  @Output() willPresent = new EventEmitter<any>();
+  @Output() didPresent = new EventEmitter<any>();
+  @Output() didDismiss = new EventEmitter<any>();
+  @Output() willDismiss = new EventEmitter<any>();
 
   closeModal() {
-    this.close.emit();
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  onWillPresent(event) {
+    this.willPresent.emit(event);
+  }
+
+  onDidPresent(event) {
+    this.didPresent.emit(event);
+  }
+
+  onWillDismiss(event) {
+    this.willDismiss.emit(event);
+  }
+
+  onDidDismiss(event) {
+    this.didDismiss.emit(event);
   }
 }
