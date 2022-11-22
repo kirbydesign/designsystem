6203bac4684a3c632fa0ba7f8202157d255fc8c9
Mark Drastrup <mark.drastrup@gmail.com>
2022-11-22 16:30:01 +0900

    Add functionality to close the modal and return data from it

--- libs/designsystem/src/lib/components/modal-experimental/services/modal.controller.ts
+++ libs/designsystem/src/lib/components/modal-experimental/services/modal.controller.ts
@@ -4,18 +4,29 @@ import { ModalController } from '@ionic/angular';
 export type ModalFlavor = 'modal' | 'compact';
 
 export type ModalConfig = {
-  flavor: ModalFlavor;
+  flavor?: ModalFlavor;
   component: any;
+  componentProps?: { [key: string]: any };
 };
 
 @Injectable()
 export class ModalExperimentalController {
   constructor(private ionicModalController: ModalController) {}
+  ionModal: HTMLIonModalElement;
 
-  public async showModal(config: ModalConfig) {
-    const modal = await this.ionicModalController.create({
+  public async showModal(config: ModalConfig): Promise<HTMLIonModalElement> {
+    this.ionModal = await this.ionicModalController.create({
       component: config.component,
+      componentProps: config.componentProps,
     });
-    modal.present();
+
+    await this.ionModal.present();
+
+    return this.ionModal;
+  }
+
+  public async closeModal(data?: any) {
+    // Should we let the developers provide the role as an argument?
+    return this.ionModal.dismiss(data, 'cancel');
   }
 }
--- libs/designsystem/src/lib/components/modal-experimental/wrapper/wrapper.component.ts
+++ libs/designsystem/src/lib/components/modal-experimental/wrapper/wrapper.component.ts
@@ -1,12 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
+import { IonModal } from '@ionic/angular';
 
 @Component({
   selector: 'kirby-modal-wrapper-experimental',
   templateUrl: './wrapper.component.html',
   styleUrls: ['./wrapper.component.scss'],
 })
-export class ModalWrapperExperimentalComponent {
+export class ModalWrapperExperimentalComponent implements OnInit {
+  constructor(private elementRef: ElementRef<HTMLElement>) {}
+
+  private ionModalElement?: HTMLIonModalElement;
+
   @Input() title = '';
   @Input() hasCollapsibleTitle = true;
   @Input() scrollDisabled = false;
+
+  ngOnInit() {
+    this.ionModalElement = this.elementRef.nativeElement.closest('ion-modal');
+  }
+
+  close() {
+    this.ionModalElement.dismiss();
+  }
 }
