15777cbd5b1b46c405e67e2840def5b238737bbd
Thupi Ceylons <thupi@ceylons.net>
2022-11-25 11:08:10 +0100

    Remove grid component showcase leftovers

--- apps/cookbook/src/app/examples/grid-example/grid-example.component.ts
+++ apps/cookbook/src/app/examples/grid-example/grid-example.component.ts
@@ -1,32 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-
-import { GridCardConfiguration } from '@kirbydesign/designsystem';
-
-import { CardExampleComponent } from '../card-example/card-example.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'cookbook-grid-example',
   templateUrl: './grid-example.component.html',
 })
-export class GridExampleComponent implements OnInit {
-  cardConfigurations: GridCardConfiguration[] = [];
-
-  constructor() {}
-
-  ngOnInit() {
-    setTimeout(() => {
-      this.cardConfigurations = [
-        new GridCardConfiguration(CardExampleComponent, 'Nr. 1', 1),
-        new GridCardConfiguration(CardExampleComponent, 'Nr. 2', 1),
-        new GridCardConfiguration(CardExampleComponent, 'Nr. 3', 2),
-        new GridCardConfiguration(CardExampleComponent, 'Nr. 4', 1),
-        new GridCardConfiguration(CardExampleComponent, 'Nr. 5', 1),
-        new GridCardConfiguration(CardExampleComponent, 'Nr. 6', 2),
-        new GridCardConfiguration(CardExampleComponent, 'Nr. 7', 1),
-        new GridCardConfiguration(CardExampleComponent, 'Nr. 8', 1),
-        new GridCardConfiguration(CardExampleComponent, 'Nr. 9', 1),
-        new GridCardConfiguration(CardExampleComponent, 'Nr. 10', 2),
-      ];
-    }, 300);
-  }
-}
+export class GridExampleComponent {}
