b86deec1944ed81a1ded75834cb5bd7ed7b2c46e
RasmusKjeldgaard <rkk@bankdata.dk>
2022-12-06 11:23:15 +0100

    Add segmented control POC

--- apps/cookbook/src/app/examples/segmented-control-example/segmented-control-example.component.ts
+++ apps/cookbook/src/app/examples/segmented-control-example/segmented-control-example.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { SegmentItem } from '@kirbydesign/designsystem';
 
 @Component({
   selector: 'cookbook-segmented-control-example',
   templateUrl: './segmented-control-example.component.html',
   styleUrls: ['./segmented-control-example.component.scss'],
 })
-export class SegmentedControlExampleComponent {}
+export class SegmentedControlExampleComponent {
+  defaultItems: SegmentItem[] = [
+    {
+      text: 'First item',
+      id: 'first',
+    },
+    {
+      text: 'Second item',
+      id: 'second',
+    },
+  ];
+}
