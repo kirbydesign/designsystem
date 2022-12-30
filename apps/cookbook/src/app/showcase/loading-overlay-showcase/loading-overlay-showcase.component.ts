7cbb5fa4c52f6170da25dc109d9d4848febab072
Mark Drastrup <mark.drastrup@gmail.com>
2022-12-30 11:14:49 +0100

    Update documentation

--- apps/cookbook/src/app/showcase/loading-overlay-showcase/loading-overlay-showcase.component.ts
+++ apps/cookbook/src/app/showcase/loading-overlay-showcase/loading-overlay-showcase.component.ts
@@ -1,27 +1,44 @@
 import { Component } from '@angular/core';
-import { ApiDescriptionProperty } from '~/app/shared/api-description/api-description-properties/api-description-properties.component';
-
+import { LoadingOverlayService } from '@kirbydesign/designsystem';
 import exampleHtml from '../../examples/loading-overlay-example/loading-overlay-example.component.html?raw';
-
+import { ApiDescriptionProperty } from '../../shared/api-description/api-description-properties/api-description-properties.component';
+import { loadingOverlayServiceExample } from '../../examples/loading-overlay-example/loading-overlay-service-example';
 @Component({
   selector: 'cookbook-loading-overlay-showcase',
   templateUrl: './loading-overlay-showcase.component.html',
   styleUrls: ['./loading-overlay-showcase.component.scss'],
 })
 export class LoadingOverlayShowcaseComponent {
+  constructor(private loadingOverlayService: LoadingOverlayService) {}
+
   exampleHtml = exampleHtml;
+  loadingOverlayServiceExample = loadingOverlayServiceExample;
   properties: ApiDescriptionProperty[] = [
     {
       name: 'isLoading',
       description: 'Shows the loading spinner.',
       defaultValue: 'false',
-      type: ['true', 'false'],
+      type: ['boolean'],
     },
     {
       name: 'showBackdrop',
       description: 'Adds a dimmer on the background.',
       defaultValue: 'true',
-      type: ['true', 'false'],
+      type: ['boolean'],
+    },
+    {
+      name: 'hideContent',
+      description: 'Hides the content behind a backdrop with 100% opacity.',
+      defaultValue: 'false',
+      type: ['boolean'],
     },
   ];
+
+  public showFullscreenLoadingOverlay(showBackdrop: boolean, hideContent?: boolean) {
+    this.loadingOverlayService.showLoadingOverlay(showBackdrop, hideContent);
+
+    setTimeout(() => {
+      this.loadingOverlayService.hideLoadingOverlay();
+    }, 5000);
+  }
 }
