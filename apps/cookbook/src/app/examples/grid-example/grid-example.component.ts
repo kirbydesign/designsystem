7f9312b8c02f80b93d01280f6beb54b800b3d91b
RasmusKjeldgaard <rkk@bankdata.dk>
2023-01-04 19:49:19 +0100

    reproduce grid problem

--- apps/cookbook/src/app/examples/grid-example/grid-example.component.ts
+++ apps/cookbook/src/app/examples/grid-example/grid-example.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GridCardConfiguration } from '@kirbydesign/designsystem';
+import { CardExampleComponent } from '../card-example/card-example.component';
 
 @Component({
   selector: 'cookbook-grid-example',
   templateUrl: './grid-example.component.html',
   styleUrls: ['./grid-example.component.scss'],
 })
-export class GridExampleComponent {
+export class GridExampleComponent implements OnInit {
   spacing = '';
   spacingSizes = [
     {
@@ -36,5 +38,24 @@ export class GridExampleComponent {
 
   onSpacingSizeChange(value) {
     this.spacing = value;
+  }
+
+  cardConfigurations: GridCardConfiguration[] = [];
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.cardConfigurations = [
+        new GridCardConfiguration(CardExampleComponent, 'Nr. 1', 1),
+        new GridCardConfiguration(CardExampleComponent, 'Nr. 2', 1),
+        new GridCardConfiguration(CardExampleComponent, 'Nr. 3', 2),
+        new GridCardConfiguration(CardExampleComponent, 'Nr. 4', 1),
+        new GridCardConfiguration(CardExampleComponent, 'Nr. 5', 1),
+        new GridCardConfiguration(CardExampleComponent, 'Nr. 6', 2),
+        new GridCardConfiguration(CardExampleComponent, 'Nr. 7', 1),
+        new GridCardConfiguration(CardExampleComponent, 'Nr. 8', 1),
+        new GridCardConfiguration(CardExampleComponent, 'Nr. 9', 1),
+        new GridCardConfiguration(CardExampleComponent, 'Nr. 10', 2),
+      ];
+    }, 300);
   }
 }
