fd8a5600d696054df3ec24dff5297e9c6d38f614
Michael T <micbittro@gmail.com>
2023-01-05 12:46:49 +0100

    Solution for tab item shift on hover and badge margin (#2719)

--- libs/designsystem/src/lib/components/tab-navigation/tab-navigation-item/tab-navigation-item.component.ts
+++ libs/designsystem/src/lib/components/tab-navigation/tab-navigation-item/tab-navigation-item.component.ts
@@ -1,15 +1,40 @@
-import { Component, HostBinding } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import {
+  AfterViewInit,
+  Component,
+  ContentChild,
+  ContentChildren,
+  ElementRef,
+  HostBinding,
+  Inject,
+  Query,
+} from '@angular/core';
 
 @Component({
   selector: 'kirby-tab-navigation-item',
   templateUrl: './tab-navigation-item.component.html',
   styleUrls: ['./tab-navigation-item.component.scss'],
 })
-export class TabNavigationItemComponent {
+export class TabNavigationItemComponent implements AfterViewInit {
   @HostBinding('attr.tabindex')
   tabIndex = 0;
 
-  constructor() {
+  private readonly labelTextElementSelector = 'span[text]';
+  private readonly labelTextElementContentAttribute = 'data-text';
+
+  constructor(private elementRef: ElementRef<HTMLElement>) {
     /* */
+  }
+
+  ngAfterViewInit(): void {
+    const labelTextElement = this.elementRef.nativeElement.querySelector(
+      this.labelTextElementSelector
+    );
+    if (labelTextElement) {
+      labelTextElement.setAttribute(
+        this.labelTextElementContentAttribute,
+        labelTextElement.textContent
+      );
+    }
   }
 }
