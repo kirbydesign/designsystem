486ae8c6c5872689ccc72e91959ab24ddcbdf2af
Mark Drastrup <mark.drastrup@gmail.com>
2023-01-06 10:41:32 +0100

    Create folder structure for the icon secondary entry

--- libs/designsystem/icon/index.ts
+++ libs/designsystem/icon/index.ts
@@ -1,5 +1,1 @@
-export { IconComponent, IconSize } from './icon.component';
-export { IconModule } from './icon.module';
-export { IconSettings, Icon } from './icon-settings';
-export { defaultIcons } from './kirby-icon-settings';
-export { IconRegistryService } from './icon-registry.service';
+export * from './src/index';
---
+++ libs/designsystem/icon/src/public_api.ts
@@ -0,0 +1,5 @@
+export * from './icon-registry.service';
+export * from './icon-settings';
+export * from './icon.component';
+export * from './icon.module';
+export * from './kirby-icon-settings';
