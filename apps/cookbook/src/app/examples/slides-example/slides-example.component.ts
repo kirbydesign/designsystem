f3a75b457f17f56f2937025a30ce7a206eb3fd8a
RasmusKjeldgaard <rkk@bankdata.dk>
2023-01-16 09:59:39 +0100

    Align cookbook example approach

--- apps/cookbook/src/app/examples/slides-example/slides-example.component.ts
+++ apps/cookbook/src/app/examples/slides-example/slides-example.component.ts
@@ -2,12 +2,87 @@ import { Component, ViewChild } from '@angular/core';
 
 import { SlidesComponent } from '@kirbydesign/designsystem/slide';
 
-@Component({
+const config = {
   selector: 'cookbook-slides-example',
-  templateUrl: './slides-example.component.html',
+  template: `<kirby-slides
+  [slidesOptions]="slidesOptions"
+  [slides]="slides"
+  (selectedSlide)="getDataFromActiveSlide($event)"
+>
+      <kirby-card  *kirbySlide="let slide; let i = index" [hasPadding]="true">
+        <kirby-card-header
+          [title]="slide.title"
+          [subtitle]="slide.subtitle"
+        ></kirby-card-header>
+
+        <!-- Card content example: -->
+        <div class="card-content">
+          {{ slide.cardContent }}
+        </div>
+
+      <kirby-card-footer *ngIf="hasFooter">Footer</kirby-card-footer>
+    </kirby-card>
+</kirby-slides>
+<br />
+<div style="text-align: center">
+  <button kirby-button (click)="changeSlide()">Activate slide no. 4</button>
+</div>
+`,
+  codeSnippet: `slidesOptions = {
+  spaceBetween: 5,
+  centeredSlides: true,
+  slidesPerView: 1.15,
+  breakpoints: {
+    721: {
+      slidesPerView: 1.9,
+    },
+  },
+};
+
+slides = [
+  {
+    title: 'Slide 1',
+    subtitle: 'Subtitle 1',
+    cardContent:
+      ' Lorem, ipsum dolor ...',
+  },
+  {
+    title: 'Slide 2',
+    subtitle: 'Subtitle 2',
+    cardContent:
+    ' Lorem, ipsum dolor ...',
+  },
+  {
+    title: 'Slide 3',
+    subtitle: 'Subtitle 3',
+    cardContent:
+    ' Lorem, ipsum dolor ...',
+  },
+  {
+    title: 'Slide 4',
+    subtitle: 'Subtitle 4',
+    cardContent:
+    ' Lorem, ipsum dolor ...',
+  },
+  {
+    title: 'Slide 5',
+    subtitle: 'Subtitle 5',
+    cardContent:
+    ' Lorem, ipsum dolor ...',
+  },
+];
+`,
+};
+
+@Component({
+  selector: config.selector,
+  template: config.template,
 })
 export class SlidesExampleComponent {
   @ViewChild(SlidesComponent) slidesComponent: SlidesComponent;
+  template: string = config.template;
+  codeSnippet: string = config.codeSnippet;
+
   constructor() {}
 
   slidesOptions = {
