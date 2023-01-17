2784e345ea8b5098674f776750a7a4e08681e559
RasmusKjeldgaard <rkk@bankdata.dk>
2023-01-17 13:09:04 +0100

    Import ThemeColorDirective in all components

--- libs/designsystem/card/src/card.module.ts
+++ libs/designsystem/card/src/card.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { ThemeColorDirective } from '@kirbydesign/designsystem/shared';
 import { CardComponent } from './card.component';
 import { CardHeaderComponent } from './card-header/card-header.component';
 import { CardFooterComponent } from './card-footer/card-footer.component';
@@ -13,7 +14,7 @@ const declarations = [
   CardAsButtonDirective,
 ];
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ThemeColorDirective],
   declarations: [...declarations],
   exports: [...declarations],
 })
--- libs/designsystem/src/lib/components/empty-state/empty-state.module.ts
+++ libs/designsystem/src/lib/components/empty-state/empty-state.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { IconModule } from '@kirbydesign/designsystem/icon';
+import { ThemeColorDirective } from '@kirbydesign/designsystem/shared';
 import { EmptyStateComponent } from './empty-state.component';
 
 const declarations = [EmptyStateComponent];
 
 @NgModule({
-  imports: [IconModule, CommonModule],
+  imports: [IconModule, CommonModule, ThemeColorDirective],
   declarations: [...declarations],
   exports: [...declarations],
 })
