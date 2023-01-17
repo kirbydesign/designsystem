f32a4fcbf7a7aeedc0053587ea41399abf7c05f1
RasmusKjeldgaard <rkk@bankdata.dk>
2023-01-17 22:59:49 +0100

    Revert "Update spec directive reference to secondary entry"
    This reverts commit f5d767462e9e4ae2e8b5d74c0af48306dcfd5cc6.

--- libs/designsystem/shared/src/theme-color/theme-color.directive.integration.spec.ts
+++ libs/designsystem/shared/src/theme-color/theme-color.directive.integration.spec.ts
@@ -1,6 +1,6 @@
 import { CardComponent } from '@kirbydesign/designsystem/card';
 import { createDirectiveFactory, SpectatorDirective } from '@ngneat/spectator';
-import { ThemeColorDirective } from '@kirbydesign/designsystem/shared';
+import { ThemeColorDirective } from './theme-color.directive';
 
 describe('ThemeColorDirective', () => {
   let spectator: SpectatorDirective<ThemeColorDirective>;
