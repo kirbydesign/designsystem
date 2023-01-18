c5c5cdc4c1cf2e81c518dd09f4ec7a90fb960576
RasmusKjeldgaard <rkk@bankdata.dk>
2023-01-18 14:35:17 +0100

    Add flag page with flag variant selector

---
+++ apps/cookbook/src/app/examples/card-example/examples/flag.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+
+const config = {
+  selector: 'cookbook-card-example-flag',
+  codeSnippet: `<kirby-card hasPadding="true">
+  <kirby-card-header
+      [title]="'Flagged card header'"
+      [flagged]="flagLevel"
+  ></kirby-card-header>
+
+  <strong>A flagged card</strong> <br />
+  <p>This is a card that uses a <em>kirby-card-header</em> with the <code>flagged</code> input set.</p>
+  <p>Possible values for the flagged header are: <br> <code>'success' | 'warning' | 'danger' | 'info'</code></p>
+  <p>Use the buttons below to see the different options 👇</p>
+
+</kirby-card>`,
+  template: `<kirby-card hasPadding="true">
+    <kirby-card-header
+        [title]="'Flagged card header'"
+        [flagged]="flagLevel"
+    ></kirby-card-header>
+
+    <strong>A flagged card</strong> <br />
+    <p>This is a card that uses a <em>kirby-card-header</em> with the <code>flagged</code> input set.</p>
+    <p>Possible values for the flagged header are: <br> <code>'success' | 'warning' | 'danger' | 'info'</code></p>
+    <p>Use the buttons below to see the different options 👇</p>
+
+</kirby-card>
+
+<div class="card-option-button-group">
+    <button (click)="setFlagLevel('success')" class="success"><kirby-icon name="flag"></kirby-icon></button>
+    <button (click)="setFlagLevel('warning')" class="warning"><kirby-icon name="flag"></kirby-icon></button>
+    <button (click)="setFlagLevel('danger')" class="danger"><kirby-icon name="flag"></kirby-icon></button>
+    <button (click)="setFlagLevel('info')" class="info"><kirby-icon name="flag"></kirby-icon></button>
+</div>`,
+};
+
+@Component({
+  selector: config.selector,
+  template: config.template,
+  styleUrls: ['./card-example.shared.scss'],
+})
+export class CardExampleFlagComponent {
+  template: string = config.template;
+  codeSnippet: string = config.codeSnippet;
+
+  flagLevel: string = 'success';
+
+  setFlagLevel(flagLevel: string) {
+    this.flagLevel = flagLevel;
+  }
+}
