ff8ba2e2d03ac13363191bfe5069764f145647c3
RasmusKjeldgaard <rkk@bankdata.dk>
2023-01-18 16:17:25 +0100

    Add clickable example

---
+++ apps/cookbook/src/app/examples/card-example/examples/clickable.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { noop } from 'rxjs';
+
+const config = {
+  selector: 'cookbook-card-example-clickable',
+  template: `<kirby-card hasPadding="true" (click)="noop()">
+  <strong>A card with state</strong> <br>
+  <p> 
+    Note how this card can also be focussed with tab keyboard navigation.
+  </p>
+  <p>
+    Pressing enter or space will automatically trigger the registered click function too!
+  </p>
+</kirby-card>`,
+};
+
+@Component({
+  selector: config.selector,
+  template: config.template,
+  styleUrls: ['./card-example.shared.scss'],
+})
+export class CardExampleClickableComponent {
+  template: string = config.template;
+  noop: () => void = noop;
+}
