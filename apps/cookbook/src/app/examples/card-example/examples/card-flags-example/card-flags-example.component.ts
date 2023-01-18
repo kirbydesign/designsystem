4e2b25ec5566c841f8b0c243c2efd925b1295fa9
RasmusKjeldgaard <rkk@bankdata.dk>
2023-01-18 09:54:22 +0100

    Add flags example component

---
+++ apps/cookbook/src/app/examples/card-example/examples/card-flags-example/card-flags-example.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cookbook-card-flags-example',
+  templateUrl: './card-flags-example.component.html',
+  styleUrls: ['./card-flags-example.component.scss'],
+})
+export class CardFlagsExampleComponent {}
