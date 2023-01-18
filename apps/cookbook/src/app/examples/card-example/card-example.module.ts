a11125c97e8908264ef07927e767283ce877565d
RasmusKjeldgaard <rkk@bankdata.dk>
2023-01-18 18:03:29 +0100

    Remove unused example components

--- apps/cookbook/src/app/examples/card-example/card-example.module.ts
+++ apps/cookbook/src/app/examples/card-example/card-example.module.ts
@@ -4,12 +4,8 @@ import { NgModule } from '@angular/core';
 import { KirbyModule } from '@kirbydesign/designsystem';
 
 import { CardExampleComponent } from './card-example.component';
-import { CardBackgroundImageExampleComponent } from './examples/card-background-image-example/card-background-image-example.component';
-import { CardClickableExampleComponent } from './examples/card-clickable-example/card-clickable-example.component';
-import { CardCssBackgroundImageExampleComponent } from './examples/card-css-background-image-example/card-css-background-image-example.component';
-import { CardElevationsExampleComponent } from './examples/card-elevations-example/card-elevations-example.component';
-import { CardFlagsExampleComponent } from './examples/card-flags-example/card-flags-example.component';
-import { CardThemecolorExampleComponent } from './examples/card-themecolor-example/card-themecolor-example.component';
+import { CardExampleBackgroundImageComponent } from './examples/background-image';
+
 import { CardWithItemHeaderExampleComponent } from './examples/card-with-item-header-example/card-with-item-header-example.component';
 import { CardExampleClickableComponent } from './examples/clickable';
 import { CardExampleColorComponent } from './examples/color';
@@ -18,17 +14,12 @@ import { CardExampleFlagComponent } from './examples/flag';
 
 const COMPONENT_DECLARATIONS = [
   CardExampleComponent,
-  CardClickableExampleComponent,
-  CardElevationsExampleComponent,
-  CardThemecolorExampleComponent,
-  CardBackgroundImageExampleComponent,
-  CardCssBackgroundImageExampleComponent,
   CardWithItemHeaderExampleComponent,
-  CardFlagsExampleComponent,
   CardExampleDefaultComponent,
   CardExampleFlagComponent,
   CardExampleColorComponent,
   CardExampleClickableComponent,
+  CardExampleBackgroundImageComponent,
 ];
 
 @NgModule({
--- apps/cookbook/src/app/examples/card-example/examples/card-flags-example/card-flags-example.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'cookbook-card-flags-example',
-  templateUrl: './card-flags-example.component.html',
-  styleUrls: ['./card-flags-example.component.scss'],
-})
-export class CardFlagsExampleComponent {}
