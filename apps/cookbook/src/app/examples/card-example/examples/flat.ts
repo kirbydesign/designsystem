5352cd12e82c23893af7c76d3b7c6ddf6abfdfdc
RasmusKjeldgaard <rkk@bankdata.dk>
2023-01-20 10:33:43 +0100

    Update text inside flat card

--- apps/cookbook/src/app/examples/card-example/examples/flat.ts
+++ apps/cookbook/src/app/examples/card-example/examples/flat.ts
@@ -4,8 +4,9 @@ const config = {
   selector: 'cookbook-card-example-flat',
   template: `<kirby-card hasPadding="true" flat="true">
     <strong>A flat card</strong>
-    <p>This is a default card as it looks right out of the box.</p>
-    <p>No header, footer, coloring or background-image is used here, but it does have <code>hasPadding=true</code>.</p>
+    <p>Use with caution.</p>
+    <p>In general the look and feel of the default card is preferred in most use cases as it elevates the card a bit from the background.
+    But if you need a flat look, here it is!</p>
 </kirby-card>`,
 };
 
