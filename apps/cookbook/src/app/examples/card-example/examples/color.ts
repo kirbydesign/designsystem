058f6c3a11befa021cac7e3cc0251732ce85eb32
RasmusKjeldgaard <rkk@bankdata.dk>
2023-01-23 11:22:35 +0100

    Improve card documentation (#2787)

---
+++ apps/cookbook/src/app/examples/card-example/examples/color.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+const config = {
+  selector: 'cookbook-card-example-color',
+  codeSnippet: `<kirby-card hasPadding="true" [themeColor]="color">
+  <strong>A themed card</strong>
+  <p>This cards color is defined by the <code>themeColor</code> input property.</p>
+  <p>Recommended values for themeColor are: <br> <code>'secondary' | 'tertiary' | 'medium' | 'dark'</code></p>
+  <p>Use the buttons below to see the different options 👇</p>
+</kirby-card>`,
+  template: `<kirby-card hasPadding="true" [themeColor]="color">
+    <strong>A themed card</strong>
+    <p>This cards color is defined by the <code>themeColor</code> input property.</p>
+    <p>Recommended values for themeColor are: <br> <code>'secondary' | 'tertiary' | 'medium' | 'dark'</code></p>
+    <p>Use the buttons below to see the different options 👇</p>
+</kirby-card>
+
+<div class="card-option-button-group">
+    <button (click)="setThemeColor('secondary')" class="secondary"></button>
+    <button (click)="setThemeColor('tertiary')" class="tertiary"></button>
+    <button (click)="setThemeColor('medium')" class="medium"></button>
+    <button (click)="setThemeColor('dark')" class="dark"></button>
+</div>`,
+};
+
+@Component({
+  selector: config.selector,
+  template: config.template,
+  styleUrls: ['./card-example.shared.scss'],
+})
+export class CardExampleColorComponent {
+  template: string = config.template;
+  codeSnippet: string = config.codeSnippet;
+
+  color: string = 'secondary';
+
+  setThemeColor(color: string) {
+    this.color = color;
+  }
+}
