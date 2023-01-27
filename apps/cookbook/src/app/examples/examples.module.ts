7685b36661477d7af07a0a1168c04daaf6b69aac
mark-drastrup <mark.drastrup@gmail.com>
2023-01-27 10:53:15 +0100

    Experimental Fullscreen Modal (#2679)

--- apps/cookbook/src/app/examples/examples.module.ts
+++ apps/cookbook/src/app/examples/examples.module.ts
@@ -31,6 +31,7 @@ import { LinkExampleModule } from './link-example/link-example.module';
 import { ListExamplesModule } from './list-example/list-example.module';
 import { ListExperimentalExampleModule } from './list-experimental-example/list-experimental-example.module';
 import { ModalExampleModule } from './modal-example/modal-example.module';
+import { ModalExperimentalExampleModule } from './modal-experimental-example/modal-experimental-example.module';
 import { ProgressCircleExampleModule } from './progress-circle-example/progress-circle-example.module';
 import { RadioExampleModule } from './radio-example/radio-example.module';
 import { RangeExampleModule } from './range-example/range-example.module';
@@ -61,6 +62,7 @@ const IMPORTS = [
   RangeExampleModule,
   LinkExampleModule,
   ModalExampleModule,
+  ModalExperimentalExampleModule,
   GridLayoutExamplesModule,
   SectionHeaderExampleModule,
   ItemGroupExampleModule,
