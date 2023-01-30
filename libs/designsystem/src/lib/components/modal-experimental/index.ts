fa8b0f6f1d091754903b0ee21ce39f58689431fa
Kodeaben <anders@kodeaben.com>
2023-01-30 15:07:41 +0100

    got experimental back

---
+++ libs/designsystem/src/lib/components/modal-experimental/index.ts
@@ -0,0 +1,4 @@
+export { FullscreenModalExperimentalComponent } from './fullscreen/fullscreen.component';
+export { ModalFooterExperimentalComponent } from './footer/footer.component';
+export { ModalWrapperExperimentalComponent } from './wrapper/wrapper.component';
+export { ModalExperimentalController, ModalExperimentalConfig } from './services/modal.controller';
