8ad010c31bd1de7ee3c7a35a3c032e3dd512ecfe
RasmusKjeldgaard <rkk@bankdata.dk>
2023-02-01 15:39:18 +0100

    Add segmented control color example

---
+++ apps/cookbook/src/app/examples/segmented-control-example/color/color.ts
@@ -0,0 +1,127 @@
+import { Component, OnInit } from '@angular/core';
+
+import { SegmentedControlMode, SegmentItem } from '@kirbydesign/designsystem';
+
+const config = {
+  template: `
+<kirby-card hasPadding="true" [themeColor]="color">  
+  <kirby-segmented-control
+  [items]="items"
+  [value]="selectedSegment"
+  [mode]="mode"
+  (segmentSelect)="onSegmentSelect($event)"
+></kirby-segmented-control>
+</kirby-card>
+
+<div class="card-option-button-group">
+    <button (click)="setThemeColor('light')" class="light"></button>
+    <button (click)="setThemeColor('white')" class="white"></button>
+    <button (click)="setThemeColor('secondary')" class="secondary"></button>
+    <button (click)="setThemeColor('dark')" class="dark"></button>
+</div>
+
+<fieldset>
+  <legend>Configuration</legend>
+  <p>
+    <strong>Mode:</strong><br/>
+    <label>
+      <input
+        type="radio"
+        name="mode"
+        value="default"
+        [checked]="mode === 'default'"
+        (change)="setMode($event.target.value)"
+      />
+      Default
+    </label>
+    <label>
+      <input
+        type="radio"
+        name="mode"
+        value="chip"
+        [checked]="mode === 'chip'"
+        (change)="setMode($event.target.value)"
+      />
+      Chip
+    </label>
+    <label> 
+      <input
+        type="radio"
+        name="mode"
+        value="compactChip"
+        [checked]="mode === 'compactChip'"
+        (change)="setMode($event.target.value)"
+      />
+      Compact Chip
+    </label>
+  </p>
+</fieldset>
+`,
+  codeSnippet: `onSegmentSelect(segment: SegmentItem) {
+  this.selectedSegment = segment;
+}`,
+};
+@Component({
+  selector: 'cookbook-segmented-control-example-color',
+  template: config.template,
+  styleUrls: ['./color.scss'],
+})
+export class SegmentedControlExampleColorComponent implements OnInit {
+  get template(): string {
+    return config.template
+      .split('<fieldset>')[0] // Remove config part of the template
+      .replace('[mode]="mode"', `mode="${this.mode}"`);
+  }
+  codeSnippet = config.codeSnippet;
+
+  mode: SegmentedControlMode = SegmentedControlMode.default;
+
+  color: string = 'white';
+
+  selectedSegment: SegmentItem;
+
+  private defaultItems: SegmentItem[] = [
+    {
+      text: 'Item 1',
+      id: '1',
+    },
+    {
+      text: 'Item 2',
+      id: '2',
+    },
+  ];
+
+  private chipItems = [...'123456'].map((i) => ({ text: `Chip-${i}`, id: i }));
+
+  // Showcase compact chips with less chararcters but more chips
+  private compactChipItems = [...'12345678'].map((i) => ({ text: `c${i}`, id: i }));
+
+  get items(): SegmentItem[] {
+    switch (this.mode) {
+      case SegmentedControlMode.default:
+        return this.defaultItems;
+      case SegmentedControlMode.chip:
+        return this.chipItems;
+      case SegmentedControlMode.compactChip:
+        return this.compactChipItems;
+    }
+  }
+
+  ngOnInit() {
+    this.selectedSegment = this.items[0];
+  }
+
+  onSegmentSelect(segment: SegmentItem) {
+    console.log('selectedSegment', segment);
+    this.selectedSegment = segment;
+  }
+
+  setMode(mode: SegmentedControlMode) {
+    this.mode = mode;
+    this.selectedSegment = this.items[0];
+  }
+
+  setThemeColor(color: string) {
+    this.color = color;
+  }
+}
