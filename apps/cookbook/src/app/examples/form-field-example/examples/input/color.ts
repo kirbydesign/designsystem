05a0a31ac18cb136cfab84f4a79eaba8edcdb0d1
RasmusKjeldgaard <rkk@bankdata.dk>
2023-02-01 15:36:44 +0100

    Add form field color example

---
+++ apps/cookbook/src/app/examples/form-field-example/examples/input/color.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+const config = {
+  selector: 'cookbook-form-field-input-color-example',
+  template: `<kirby-card hasPadding="true" [themeColor]="color">
+    <kirby-form-field>
+    <input kirby-input placeholder="Default input with placeholder text" />
+  </kirby-form-field>
+</kirby-card>
+
+<div class="card-option-button-group">
+    <button (click)="setThemeColor('light')" class="light"></button>
+    <button (click)="setThemeColor('white')" class="white"></button>
+    <button (click)="setThemeColor('secondary')" class="secondary"></button>
+    <button (click)="setThemeColor('dark')" class="dark"></button>
+</div>
+`,
+};
+
+@Component({
+  selector: config.selector,
+  template: config.template,
+  styleUrls: ['../../form-field-example.component.scss'],
+})
+export class FormFieldInputColorExampleComponent {
+  template: string = config.template;
+  color: string = 'white';
+
+  setThemeColor(color: string) {
+    this.color = color;
+  }
+}
