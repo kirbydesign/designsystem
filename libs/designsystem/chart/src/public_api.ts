cbf544e6397abb256ccd094232aaae8ceac96f2f
Kodeaben <anders@kodeaben.com>
2023-02-06 12:42:23 +0100

    self review

--- libs/designsystem/chart/src/public_api.ts
+++ libs/designsystem/chart/src/public_api.ts
@@ -1,8 +1,6 @@
 export * from './chart';
 export * from './stock-chart';
 export * from './shared';
-// export { ChartOptions, ChartEvent, ActiveElement, Chart } from 'chart.js';
-// export { AnnotationOptions } from 'chartjs-plugin-annotation';
 
 export { ChartsModule } from './charts.module';
 
