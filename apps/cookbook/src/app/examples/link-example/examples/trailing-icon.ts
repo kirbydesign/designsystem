ff1f1b133a43b7daefc3ff1285e7fae01c6a95c3
bdunni <bdunni@bankdata.dk>
2023-02-13 13:54:20 +0100

    Custom icon functionality to utills and use http

---
+++ apps/cookbook/src/app/examples/link-example/examples/trailing-icon.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+const config = {
+  selector: 'cookbook-link-example-trailing-icon',
+  template: `<a class="trailing-icon-example" target="_blank" href="https://github.com/kirbydesign/designsystem">Trailing icon</a>`,
+  style: `.trailing-icon-example
+{
+@include utils.trailing-icon("navigation")
+/* 
+@include utils.trailing-icon("https://cdn.jsdelivr.net/npm/open-iconic@1.1.1/svg/heart.svg")
+@include utils.trailing-icon()
+*/
+}`,
+};
+
+@Component({
+  selector: config.selector,
+  template: config.template,
+  styleUrls: ['./link-examples.shared.scss'],
+})
+export class LinkExampleTrailingIconComponent {
+  template: string = config.template;
+  style: string = config.style;
+}
