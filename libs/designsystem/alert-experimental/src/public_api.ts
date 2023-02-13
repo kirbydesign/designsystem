572b4b5d26fd5c3eaabe5b470f0f3044ab8f13fb
Mark Drastrup <mark.drastrup@gmail.com>
2023-02-13 17:28:08 +0900

    Fix bug that prevents the bookcook from running

--- libs/designsystem/alert-experimental/src/public_api.ts
+++ libs/designsystem/alert-experimental/src/public_api.ts
@@ -1,1 +1,2 @@
-export { AlertController } from './services/alert.controller';
+export { AlertExperimentalController } from './services/alert.controller';
+export { AlertExperimentalComponent } from './alert.component';
