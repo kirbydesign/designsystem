72a9296f12342351bc1bedfc9c0ee4ca95053bbe
RasmusKjeldgaard <rkk@bankdata.dk>
2023-02-20 15:04:25 +0100

    Add button group secondary entry

---
+++ libs/designsystem/button-group/src/button-group.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'kirby-button-group',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './button-group.component.html',
+  styleUrls: ['./button-group.component.scss'],
+})
+export class ButtonGroupComponent {}
