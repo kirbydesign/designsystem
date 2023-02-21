7ecd462262e17810885e559f9fbaf65c1849df29
RasmusKjeldgaard <rkk@bankdata.dk>
2023-02-21 21:56:13 +0100

    Add decimal mask to form field module

--- libs/designsystem/form-field/src/public_api.ts
+++ libs/designsystem/form-field/src/public_api.ts
@@ -4,6 +4,5 @@ export { InputComponent, InputSize } from './input/input.component';
 export { InputCounterComponent } from './input-counter/input-counter.component';
 export { TextareaComponent } from './textarea/textarea.component';
 export * from './directives/date/date-input.directive';
-export { DecimalMaskDirective } from './directives/decimal-mask/decimal-mask.directive';
 export { AffixDirective } from './directives/affix/affix.directive';
 export * from './form-field.module';
