15da9f1a266bb966cc31377411dc1f8546f03055
Thupi Ceylons <thupi@ceylons.net>
2023-02-24 10:05:17 +0100

    Update docs according to latest changes

--- apps/cookbook/src/app/examples/header-example/examples/customizability.ts
+++ apps/cookbook/src/app/examples/header-example/examples/customizability.ts
@@ -3,17 +3,11 @@ import { Component } from '@angular/core';
 const config = {
   selector: 'cookbook-header-example-customizability',
   template: `
-  <kirby-header title="Title" subtitle1="Subtitle one" subtitle2="Subtitle two" >
-    <ng-container *kirbyHeaderActions>
-      <button kirby-button>
-        <kirby-icon name="edit"></kirby-icon>
-        Action 1
-      </button>
-      <button kirby-button attentionLevel="3">Action 2</button>
-      <button kirby-button attentionLevel="3">
-        <kirby-icon name="more"></kirby-icon>
-      </button>
-    </ng-container>
+  <kirby-header>
+    <ng-container *kirbyHeaderTitle>Title</ng-container>
+    <ng-container *kirbyHeaderValue>12.345,67</ng-container>
+    <ng-container *kirbyHeaderSubtitle1>Subtitle one</ng-container>
+    <ng-container *kirbyHeaderSubtitle2>Subtitle two</ng-container>
   </kirby-header>
   `,
 };
