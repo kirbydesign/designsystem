d73818097d0b2b15fc981ba4a3ff43bc943f6369
RasmusKjeldgaard <rkk@bankdata.dk>
2023-02-24 22:01:49 +0100

    Add IntersectionObserver to manipulate hidden btns

--- libs/designsystem/button-group/src/button-group.component.ts
+++ libs/designsystem/button-group/src/button-group.component.ts
@@ -17,15 +17,13 @@ import { DropdownComponent, DropdownModule } from '@kirbydesign/designsystem/dro
   standalone: true,
   imports: [CommonModule, DropdownModule],
   template: `
-    <div class="collider" #boundingBox>
-      <div class="button-container" #buttonContainer>
-        <ng-content></ng-content>
-      </div>
+    <div class="bounding-box" #boundingBox>
+      <div class="hidden-buttons" #hiddenButtons><ng-content></ng-content></div>
+      <div class="visible-buttons" #visibleButtons></div>
       <kirby-dropdown
         [items]="['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5']"
         popout="left"
         [usePopover]="true"
-        class="hide"
       ></kirby-dropdown>
     </div>
   `,
@@ -35,45 +33,86 @@ export class ButtonGroupComponent implements AfterViewInit, OnDestroy {
   @ContentChildren(ButtonComponent, { read: ElementRef }) children!: QueryList<
     ElementRef<HTMLElement>
   >;
-  @ViewChild('buttonContainer', { read: ElementRef }) buttonContainer!: ElementRef<HTMLElement>;
+  @ViewChild('visibleButtons', { read: ElementRef }) visibleButtons!: ElementRef<HTMLElement>;
+  @ViewChild('hiddenButtons', { read: ElementRef }) hiddenButtons!: ElementRef<HTMLElement>;
   @ViewChild('boundingBox', { read: ElementRef }) boundingBox!: ElementRef<HTMLElement>;
   @ViewChild(DropdownComponent, { read: ElementRef }) dropdown!: ElementRef;
 
-  private observer;
-  private observerOptions;
+  @ContentChildren(ButtonComponent, { read: ElementRef }) buttons!: ElementRef[];
+
+  private visibleButtonsObserver: IntersectionObserver;
+  private hiddenButtonsObserver: IntersectionObserver;
+  private visibleButtonsObserverOptions;
+  private hiddenButtonsObserverOptions;
 
   constructor(private renderer: Renderer2) {}
 
   ngOnDestroy(): void {
-    this.observer.disconnect();
+    this.visibleButtonsObserver.disconnect();
   }
 
   ngAfterViewInit(): void {
-    this.observerOptions = {
+    this.visibleButtonsObserverOptions = {
       root: this.boundingBox.nativeElement,
       rootMargin: '0px',
       threshold: 1.0,
     };
 
-    const observer = new IntersectionObserver(this.removeLastChild, this.observerOptions);
+    this.visibleButtonsObserver = new IntersectionObserver(
+      this.hideLastVisibleChild,
+      this.visibleButtonsObserverOptions
+    );
 
-    observer.observe(this.buttonContainer.nativeElement);
+    this.hiddenButtonsObserverOptions = {
+      root: this.boundingBox.nativeElement,
+      rootMargin: '0px',
+      threshold: 1.0,
+    };
+
+    this.hiddenButtonsObserver = new IntersectionObserver(
+      this.showFirstHiddenChild,
+      this.hiddenButtonsObserverOptions
+    );
+
+    this.visibleButtonsObserver.observe(this.visibleButtons.nativeElement);
+
+    this.buttons.forEach((button) => {
+      this.hiddenButtonsObserver.observe(button.nativeElement);
+    });
   }
 
-  removeLastChild = (entries) => {
+  hideLastVisibleChild = (entries) => {
     entries.forEach((entry: IntersectionObserverEntry) => {
+      console.log('hideLastVisibleChild called');
       if (entry.intersectionRatio < 1) {
-        console.log('intersecting');
+        console.log('hiding child');
 
-        this.dropdown.nativeElement.style.display = 'block';
+        //this.dropdown.nativeElement.style.display = 'block';
+        const buttons = this.visibleButtons.nativeElement.querySelectorAll('button');
 
-        const buttons = this.buttonContainer.nativeElement.querySelectorAll('button');
+        if (buttons.length > 0) {
+          const buttonElement = buttons[buttons.length - 1];
+          this.renderer.appendChild(this.hiddenButtons.nativeElement, buttonElement);
+          this.hiddenButtonsObserver.observe(buttonElement);
+        }
+      }
+    });
+  };
 
-        if (buttons.length > 0)
-          this.renderer.removeChild(
-            this.buttonContainer.nativeElement,
-            buttons[buttons.length - 1]
-          );
+  showFirstHiddenChild = (entries) => {
+    entries.forEach((entry: IntersectionObserverEntry) => {
+      console.log('showFirstHiddenChild called', entry);
+
+      if (entry.intersectionRatio === 1) {
+        console.log('showing child');
+
+        const buttons = this.hiddenButtons.nativeElement.querySelectorAll('button');
+
+        this.hiddenButtonsObserver.unobserve(entry.target);
+
+        if (buttons.length > 0) {
+          this.renderer.appendChild(this.visibleButtons.nativeElement, entry.target);
+        }
       }
     });
   };
