48ce23244db54d1b746c08608e4d4df6107f2ad7
RasmusKjeldgaard <rkk@bankdata.dk>
2023-02-24 07:52:55 +0100

    Temp state with group collapsing from left

--- libs/designsystem/button-group/src/button-group.component.ts
+++ libs/designsystem/button-group/src/button-group.component.ts
@@ -1,11 +1,72 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentInit,
+  AfterViewInit,
+  Component,
+  ContentChildren,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonComponent } from '@kirbydesign/designsystem/button';
+import { DropdownComponent, DropdownModule } from '@kirbydesign/designsystem/dropdown';
+
+// // eslint-disable-next-line
+// @Directive({ selector: '[buttonGroupElement]' })
+// export class ButtonGroupElementDirective {}
 
 @Component({
   selector: 'kirby-button-group',
   standalone: true,
-  imports: [CommonModule],
-  template: '<ng-content></ng-content>',
+  imports: [CommonModule, DropdownModule],
+  template: `
+    <div class="button-container" #buttonContainer>
+      <ng-content></ng-content>
+      <kirby-dropdown
+        [items]="['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5']"
+        popout="left"
+        [usePopover]="true"
+        class="hide"
+      ></kirby-dropdown>
+    </div>
+  `,
   styleUrls: ['./button-group.component.scss'],
 })
-export class ButtonGroupComponent {}
+export class ButtonGroupComponent implements AfterContentInit, AfterViewInit {
+  @ContentChildren(ButtonComponent, { read: ElementRef }) children!: ElementRef[];
+  @ViewChild('buttonContainer', { read: ElementRef }) buttonContainer!: ElementRef;
+  @ViewChild(DropdownComponent, { read: ElementRef }) dropdown!: ElementRef;
+
+  private observerOptions;
+
+  ngAfterContentInit(): void {
+    console.log(this.children);
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.buttonContainer);
+
+    this.observerOptions = {
+      root: this.buttonContainer.nativeElement,
+      rootMargin: '0px',
+      threshold: 1.0,
+    };
+
+    const observer = new IntersectionObserver(this.callback, this.observerOptions);
+
+    this.children.forEach((button) => {
+      observer.observe(button.nativeElement);
+    });
+  }
+
+  callback = (entries) => {
+    entries.forEach((entry: IntersectionObserverEntry) => {
+      if (entry.intersectionRatio < 1) {
+        (entry.target as HTMLElement).style.visibility = 'hidden';
+        this.dropdown.nativeElement.style.display = 'block';
+      }
+      if (entry.intersectionRatio === 1) {
+        (entry.target as HTMLElement).style.visibility = 'visible';
+      }
+    });
+  };
+}
