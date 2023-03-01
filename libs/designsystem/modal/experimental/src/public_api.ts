fd5473f35897cbbd25b51a4e48251fd406aa2177
Kodeaben <anders@kodeaben.com>
2023-03-01 10:06:18 +0100

    added warning for modalExperimental

--- libs/designsystem/modal/experimental/src/public_api.ts
+++ libs/designsystem/modal/experimental/src/public_api.ts
@@ -2,5 +2,10 @@ export { ModalRoutingExperimentalComponent } from './modal-routing/modal-routing
 export { ModalExperimentalComponent } from './modal/modal.component';
 export { ModalFooterExperimentalComponent } from './footer/footer.component';
 export { ModalWrapperExperimentalComponent } from './wrapper/wrapper.component';
-export { ModalExperimentalController, ModalExperimentalConfig } from './services/modal.controller';
+export {
+  // This export (ModalExperimentalController) will cause an error in the mock providers for testing-jest & testing-jasmine. When this error occurs, fix the import manually.
+  ModalExperimentalController,
+  ModalExperimentalConfig,
+} from './services/modal.controller';
+
 export { KirbyModalModule } from './kirby-modal.module';
