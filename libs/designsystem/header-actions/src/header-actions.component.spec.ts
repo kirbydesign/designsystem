e37e33da29d33d88ac36b3efbecf94961c8c60fb
RasmusKjeldgaard <rkk@bankdata.dk>
2023-03-06 07:49:19 +0100

    Rename folder to header-actions

--- libs/designsystem/header-actions/src/header-actions.component.spec.ts
+++ libs/designsystem/header-actions/src/header-actions.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderActionsComponent } from './header-actions.component';
 
-describe('ButtonGroupComponent', () => {
+describe('HeaderActionsComponent', () => {
   let component: HeaderActionsComponent;
   let fixture: ComponentFixture<HeaderActionsComponent>;
 
