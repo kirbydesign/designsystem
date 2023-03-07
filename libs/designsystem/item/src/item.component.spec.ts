5da793216d87a520ed540cff5292f954a3dccd53
Henrik Sejer Pedersen <bdyhsj@bankdata.dk>
2023-03-07 13:06:49 +0100

    add test for correct padding when disclosure is reset to null

--- libs/designsystem/item/src/item.component.spec.ts
+++ libs/designsystem/item/src/item.component.spec.ts
@@ -93,4 +93,22 @@ describe('ItemComponent', () => {
       });
     });
   });
+
+  describe('when disclosure is reset to null', () => {
+    it('should reset inner-padding-end to s', () => {
+      spectator.component.disclosure = 'arrow-more';
+      spectator.detectChanges();
+
+      expect(spectator.query('ion-item')).toHaveComputedStyle({
+        '--inner-padding-end': size('xxs'),
+      });
+
+      spectator.component.disclosure = null;
+      spectator.detectChanges();
+
+      expect(spectator.query('ion-item')).toHaveComputedStyle({
+        '--inner-padding-end': size('s'),
+      });
+    });
+  });
 });
