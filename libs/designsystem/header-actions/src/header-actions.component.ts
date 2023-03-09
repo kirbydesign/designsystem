0b4886d18cf092f44d00c947f9e95ae54d71ba85
RasmusKjeldgaard <rkk@bankdata.dk>
2023-03-09 15:49:36 +0100

    Dynamic button insertion on init

--- libs/designsystem/header-actions/src/header-actions.component.ts
+++ libs/designsystem/header-actions/src/header-actions.component.ts
@@ -1,12 +1,12 @@
 import {
   AfterViewInit,
   ChangeDetectionStrategy,
-  ChangeDetectorRef,
   Component,
   ContentChildren,
   ElementRef,
   HostBinding,
   Input,
+  OnDestroy,
   Renderer2,
   ViewChild,
 } from '@angular/core';
@@ -22,7 +22,7 @@ import { DropdownComponent, DropdownModule } from '@kirbydesign/designsystem/dro
   templateUrl: './header-actions.component.html',
   styleUrls: ['./header-actions.component.scss'],
 })
-export class HeaderActionsComponent implements AfterViewInit {
+export class HeaderActionsComponent implements AfterViewInit, OnDestroy {
   /*
    * When the consumer sets the visibleActions input, we run all our initialization logic.
    * The guard for this.buttons exist because when the component is instantiated,
@@ -31,6 +31,7 @@ export class HeaderActionsComponent implements AfterViewInit {
    */
   @Input() set visibleActions(value: number) {
     this._visibleActions = value;
+    this.hiddenButtonsIntersectionObserver?.disconnect();
 
     if (!this.buttons) return;
     this.initializeCollapsing();
@@ -45,6 +46,8 @@ export class HeaderActionsComponent implements AfterViewInit {
   @ViewChild('hiddenLayer', { read: ElementRef }) hiddenLayer!: ElementRef<HTMLElement>;
   @ViewChild('visibleLayer', { read: ElementRef }) visibleLayer!: ElementRef<HTMLElement>;
 
+  @HostBinding('class.is-collapsed')
+  _isCollapsed: boolean = true;
   _collapsedActions: string[] = [];
   _visibleActions: number;
 
@@ -55,53 +58,21 @@ export class HeaderActionsComponent implements AfterViewInit {
   @ViewChild(DropdownComponent) dropdownComp!: DropdownComponent;
 
   private hiddenButtons: HTMLButtonElement[];
+  private hiddenButtonsIntersectionObserver: IntersectionObserver;
+  private intersectionObserverOptions;
 
-  constructor(private renderer: Renderer2, private cdr: ChangeDetectorRef) {}
+  constructor(private renderer: Renderer2) {}
 
   ngAfterViewInit(): void {
-    this.initializeCollapsing();
-  }
-
-  initializeCollapsing() {
-    if (this.buttons.length <= 2) return;
-    this.moveButtons();
-    this.populateDropdown();
-    this.toggleDropdown();
-  }
-
-  moveButtons() {
-    this.buttons.forEach((button, index) => {
-      if (index > this._visibleActions - 1) {
-        this.renderer.appendChild(this.hiddenLayer.nativeElement, button.nativeElement);
-      } else {
-        this.renderer.appendChild(this.visibleLayer.nativeElement, button.nativeElement);
-      }
-    });
-  }
-
-  toggleDropdown() {
-    if (this.hiddenLayer.nativeElement.childElementCount === 0) {
-      this.renderer.setStyle(this.dropdown.nativeElement, 'display', 'none');
+    if (this._visibleActions) {
+      this.initializeCollapsing();
     } else {
-      this.renderer.setStyle(this.dropdown.nativeElement, 'display', 'inline-block');
+      this.initializeDynamicResizing();
     }
   }
 
-  populateDropdown() {
-    /*
-     * This function maps extracts the button text of all hidden buttons
-     * and updates the array used to populate the dropdown with items.
-     * It also maps the extracted text to the actual button element, for use when
-     * firing the matching buttons click-event in onDropdownActionSelect
-     */
-
-    this._collapsedActions = [];
-    this.hiddenButtons = Array.from(this.hiddenLayer.nativeElement.children) as HTMLButtonElement[];
-
-    this.hiddenButtons.forEach((button) => {
-      const buttonText = button.textContent.trim();
-      this._collapsedActions.push(buttonText);
-    });
+  ngOnDestroy(): void {
+    this.hiddenButtonsIntersectionObserver?.disconnect();
   }
 
   onDropdownActionSelect() {
@@ -120,5 +91,97 @@ export class HeaderActionsComponent implements AfterViewInit {
 
       selectedAction.dispatchEvent(event);
     }
+  }
+
+  private initializeCollapsing() {
+    if (this.buttons.length <= 2) return;
+    this.moveButtons();
+    this.populateDropdown();
+    this.toggleDropdown();
+  }
+
+  private moveButtons() {
+    this.buttons.forEach((button, index) => {
+      if (index > this._visibleActions - 1) {
+        this.renderer.appendChild(this.hiddenLayer.nativeElement, button.nativeElement);
+      } else {
+        this.renderer.appendChild(this.visibleLayer.nativeElement, button.nativeElement);
+      }
+    });
+  }
+
+  private toggleDropdown() {
+    if (this.hiddenLayer.nativeElement.childElementCount === 0) {
+      this._isCollapsed = false;
+    } else {
+      this._isCollapsed = true;
+    }
+  }
+
+  private populateDropdown() {
+    /*
+     * This function maps extracts the button text of all hidden buttons
+     * and updates the array used to populate the dropdown with items.
+     * It also maps the extracted text to the actual button element, for use when
+     * firing the matching buttons click-event in onDropdownActionSelect
+     */
+
+    this._collapsedActions = [];
+    this.hiddenButtons = Array.from(this.hiddenLayer.nativeElement.children) as HTMLButtonElement[];
+
+    this.hiddenButtons.forEach((button) => {
+      const buttonText = button.textContent.trim();
+      this._collapsedActions.push(buttonText);
+    });
+  }
+
+  private initializeDynamicResizing() {
+    this.intersectionObserverOptions = {
+      root: this.visibleLayer.nativeElement,
+      threshold: 1.0,
+    };
+
+    this.hiddenButtonsIntersectionObserver = new IntersectionObserver(
+      this.handleHiddenButtonIntersection,
+      this.intersectionObserverOptions
+    );
+
+    this.buttons.forEach((button) => {
+      this.hiddenButtonsIntersectionObserver.observe(button.nativeElement);
+    });
+  }
+
+  private handleHiddenButtonIntersection = (entries) => {
+    const buttonsToHide: Element[] = [];
+    entries.forEach((entry: IntersectionObserverEntry) => {
+      if (entry.intersectionRatio < 1) {
+        buttonsToHide.push(entry.target);
+      }
+
+      // if (entry.isIntersecting) {
+      //   if (entry.intersectionRatio === 1) {
+      //     if (entry.target.parentElement === this.hiddenLayer.nativeElement) {
+      //       console.log('Intersecting btn: ', entry.target);
+      //     }
+      //   }
+      // }
+    });
+
+    //console.log(buttonsToHide);
+
+    this.hideButtons(buttonsToHide);
+    this.populateDropdown();
+  };
+
+  private hideButtons(buttons: Element[]) {
+    buttons.forEach((button) => {
+      this.renderer.appendChild(this.hiddenLayer.nativeElement, button);
+    });
+
+    this.toggleDropdown();
+  }
+
+  private showButton(button: Element) {
+    this.renderer.appendChild(this.visibleLayer.nativeElement, button);
   }
 }
