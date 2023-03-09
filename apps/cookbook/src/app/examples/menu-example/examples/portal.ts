5ab01917a225f42f0d318d3511578b3908e17d16
Niels Wrang Larsen <nwl@bankdata.dk>
2023-03-09 14:40:53 +0100

    chore: add example of menu with portal

---
+++ apps/cookbook/src/app/examples/menu-example/examples/portal.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+const config = {
+  selector: 'cookbook-menu-portal-example',
+  template: `<kirby-menu [DOMPortalOutlet]="outletElement">
+  <kirby-item [selectable]="true">
+    <h3>Action 1</h3>
+  </kirby-item>
+  <kirby-item [selectable]="true">
+    <h3>Action 2</h3>
+  </kirby-item>
+</kirby-menu>`,
+};
+
+@Component({
+  selector: config.selector,
+  template: config.template,
+})
+export class MenuPortalExampleComponent {
+  template: string = config.template;
+
+  private outletTag: string = 'cookbook-root';
+
+  public outletElement: HTMLElement = this.getOutletElement();
+
+  private getOutletElement(): HTMLElement {
+    const elements: HTMLCollectionOf<Element> = document.getElementsByTagName(this.outletTag);
+
+    if (!elements || elements.length === 0) {
+      throw Error(`Could not locate HTMLElement for ${this.outletTag}. Did you misspell it?`);
+    }
+
+    if (elements.length > 1) {
+      throw Error(
+        `Multiple HTMLElements found for ${this.outletTag}.
+      This can lead to unintended behaviours. Provide an unique outlet`
+      );
+    }
+
+    return elements[0] as HTMLElement;
+  }
+}
