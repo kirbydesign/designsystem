05208e8b8d35c1c99e65761b9bc589db9a470232
Jakob Engelbrecht <jakob@basher.dk>
2023-03-09 17:43:09 +0100

    docs: update page with header example

--- apps/cookbook/src/app/examples/page-example/page-with-header-example.component.ts
+++ apps/cookbook/src/app/examples/page-example/page-with-header-example.component.ts
@@ -3,23 +3,22 @@ import { Component } from '@angular/core';
 import { BasePageExampleComponent } from './base-page-example.component';
 
 const config = {
-  template: `<kirby-page title="Test" defaultBackHref="/">
+  template: `<kirby-page defaultBackHref="/">
+                <kirby-header title="Title" subtitle1="Subtitle one" subtitle2="Subtitle two">
+                  <kirby-header-actions *kirbyHeaderActions>
+                    <button kirby-button>
+                      <kirby-icon name="edit"></kirby-icon>
+                      <span class="text">Action 1</span>
+                    </button>
+                    <button kirby-button attentionLevel="3">Action 2</button>
+                    <button kirby-button attentionLevel="3">Action 3</button>
+                  </kirby-header-actions>
+                </kirby-header>
 
-  <kirby-header title="Title" subtitle1="Subtitle 1" subtitle2="Subtitle 2">
-    <kirby-header-actions *kirbyHeaderActions>
-      <button kirby-button>
-        <kirby-icon name="edit"></kirby-icon>
-        <span class="text">Action 1</span>
-      </button>
-      <button kirby-button attentionLevel="3">Action 2</button>
-      <button kirby-button attentionLevel="3">Action 3</button>
-    </kirby-header-actions>
-  </kirby-header>
-
-  <kirby-page-content>
-    <div [innerHTML]="content"></div>
-  </kirby-page-content>
-</kirby-page>`,
+                <kirby-page-content>
+                  <div [innerHTML]="content"></div>
+                </kirby-page-content>
+              </kirby-page>`,
 };
 @Component({
   template: config.template,
