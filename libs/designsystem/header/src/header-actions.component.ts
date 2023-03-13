03d088d8e8638770fe0c98cf0b10de7ef4ea2e09
Jakob Engelbrecht <jakob@basher.dk>
2023-03-13 15:58:19 +0100

    refactor: clean up and fix initialization

--- libs/designsystem/header/src/header-actions.component.ts
+++ libs/designsystem/header/src/header-actions.component.ts
@@ -21,18 +21,7 @@ import { DropdownComponent } from '@kirbydesign/designsystem/dropdown';
   styleUrls: ['./header-actions.component.scss'],
 })
 export class HeaderActionsComponent implements AfterContentInit, AfterViewInit {
-  /*
-   * When the consumer sets the visibleActions input, we run all our initialization logic.
-   * The guard for this.buttons exist because when the component is instantiated,
-   * the ContentChild query has not yet run, and all the below functions assume that
-   * the buttons array is defined. When undefined, we defer the initialization to ngAfterViewInit().
-   */
-  @Input() set visibleActions(value: number) {
-    this._visibleActions = value;
-
-    if (!this.buttons) return;
-    this.initializeCollapsing();
-  }
+  @Input() visibleActions: number;
 
   @Input() emphasizeActions?: boolean;
 
@@ -40,22 +29,22 @@ export class HeaderActionsComponent implements AfterContentInit, AfterViewInit {
   @Input()
   placement: 'left' | 'right' = 'left';
 
-  @ViewChild(DropdownComponent, { read: ElementRef }) dropdown!: ElementRef<HTMLElement>;
-  @ContentChildren(ButtonComponent, { read: ElementRef }) buttons!: ElementRef<HTMLButtonElement>[];
-  @ContentChildren(ButtonComponent) private buttonComps: QueryList<ButtonComponent>;
-  @ViewChild('hiddenLayer', { read: ElementRef }) hiddenLayer!: ElementRef<HTMLElement>;
-  @ViewChild('visibleLayer', { read: ElementRef }) visibleLayer!: ElementRef<HTMLElement>;
+  @ContentChildren(ButtonComponent, { read: ElementRef }) private buttonElements!: QueryList<
+    ElementRef<HTMLButtonElement>
+  >;
+  @ContentChildren(ButtonComponent) private buttons: QueryList<ButtonComponent>;
+  @ViewChild('hiddenLayer', { read: ElementRef }) private hiddenLayer!: ElementRef<HTMLElement>;
+  /*
+   * TEMPORARY MORE-MENU
+   * dropdown ViewChild is only used for temporary more-menu
+   */
+  @ViewChild(DropdownComponent) private dropdown!: DropdownComponent;
 
   @HostBinding('class.is-collapsed')
   _isCollapsed: boolean;
   _collapsedActions: string[] = [];
-  _visibleActions: number;
 
-  /*
-   * TEMPORARY MORE-MENU
-   * dropdownComp ViewChild is only used for temporary more-menu
-   */
-  @ViewChild(DropdownComponent) dropdownComp!: DropdownComponent;
+  private _nonCollapsibleVisibleActions: number = 2;
 
   private hiddenButtons: HTMLButtonElement[];
 
@@ -63,34 +52,32 @@ export class HeaderActionsComponent implements AfterContentInit, AfterViewInit {
 
   ngAfterContentInit(): void {
     const isInToolbar = this.elementRef.nativeElement.closest('ion-toolbar');
+    const emphasizeActions = !!this.elementRef.nativeElement.closest('.actions.emphasize');
+    if (isInToolbar || emphasizeActions === false) {
+      // Setting default visible actions to 1:
+      this.visibleActions = 1;
+    }
     if (isInToolbar) {
       this.isCondensed = true;
-      this.visibleActions = 2;
-      //  } else {
-      //    const emphasizeActions = !!this.elementRef.nativeElement.closest('.actions.emphasize');
-      //    if (this.visibleActions === undefined && !emphasizeActions) {
-      //      // Setting default visible actions to 2:
-      //      this.visibleActions = 2;
-      //    }
     }
   }
 
   public set isCondensed(value: boolean) {
-    this.buttonComps.forEach((button) => (button.showIconOnly = value));
+    this.buttons.forEach((button) => (button.showIconOnly = value));
   }
 
   ngAfterViewInit(): void {
-    if (this._visibleActions) {
+    if (this.visibleActions) {
       this.initializeCollapsing();
     }
   }
 
   onDropdownActionSelect() {
-    const selectedIndex = this.dropdownComp.selectedIndex;
+    const selectedIndex = this.dropdown.selectedIndex;
     const selectedAction = this.hiddenButtons[selectedIndex];
 
     // Dropdown should not persist selected item, we want it to be re-selectable
-    this.dropdownComp.selectedIndex = -1;
+    this.dropdown.selectedIndex = -1;
 
     if (selectedAction) {
       const event = new PointerEvent('click', {
@@ -104,19 +91,16 @@ export class HeaderActionsComponent implements AfterContentInit, AfterViewInit {
   }
 
   private initializeCollapsing() {
-    if (this.buttons.length <= 2) return;
+    if (this.buttonElements.length <= this._nonCollapsibleVisibleActions) return;
     this.moveButtons();
     this.populateDropdown();
     this.toggleDropdown();
   }
 
   private moveButtons() {
-    this.buttons.forEach((button, index) => {
-      if (index > this._visibleActions - 1) {
-        this.renderer.appendChild(this.hiddenLayer.nativeElement, button.nativeElement);
-      } else {
-        this.renderer.appendChild(this.visibleLayer.nativeElement, button.nativeElement);
-      }
+    const buttonsToHide = [...this.buttonElements].slice(this.visibleActions);
+    buttonsToHide.forEach((button) => {
+      this.renderer.appendChild(this.hiddenLayer.nativeElement, button.nativeElement);
     });
   }
 
@@ -130,18 +114,13 @@ export class HeaderActionsComponent implements AfterContentInit, AfterViewInit {
 
   private populateDropdown() {
     /*
-     * This function maps extracts the button text of all hidden buttons
+     * This function extracts the button text of all hidden buttons
      * and updates the array used to populate the dropdown with items.
      * It also maps the extracted text to the actual button element, for use when
      * firing the matching buttons click-event in onDropdownActionSelect
      */
 
-    this._collapsedActions = [];
     this.hiddenButtons = Array.from(this.hiddenLayer.nativeElement.children) as HTMLButtonElement[];
-
-    this.hiddenButtons.forEach((button) => {
-      const buttonText = button.textContent.trim();
-      this._collapsedActions.unshift(buttonText);
-    });
+    this._collapsedActions = this.hiddenButtons.map((button) => button.textContent.trim());
   }
 }
