e6d3379d510440d5b8178a13dcb8cd2ff0747db6
RasmusKjeldgaard <rkk@bankdata.dk>
2023-03-14 16:27:32 +0100

    Update page header example

--- apps/cookbook/src/app/examples/page-example/page-with-header-example.component.ts
+++ apps/cookbook/src/app/examples/page-example/page-with-header-example.component.ts
@@ -4,21 +4,25 @@ import { BasePageExampleComponent } from './base-page-example.component';
 
 const config = {
   template: `<kirby-page defaultBackHref="/">
-                <kirby-header title="Title" subtitle1="Subtitle one" subtitle2="Subtitle two">
-                  <kirby-header-actions *kirbyHeaderActions>
-                    <button kirby-button>
-                      <kirby-icon name="edit"></kirby-icon>
-                      <span class="text">Action 1</span>
-                    </button>
-                    <button kirby-button attentionLevel="3">Action 2</button>
-                    <button kirby-button attentionLevel="3">Action 3</button>
-                  </kirby-header-actions>
-                </kirby-header>
+  <kirby-header title="Title" subtitle1="Subtitle one" subtitle2="Subtitle two">
+    <kirby-action-group *kirbyHeaderActions>
+      <button kirby-button (click)="actionClicked('Action 1')">
+        <kirby-icon name="edit"></kirby-icon>
+        <span class="text">Action 1</span>
+      </button>
+      <button kirby-button attentionLevel="3" (click)="actionClicked('Action 2')">
+        Action 2
+      </button>
+      <button kirby-button attentionLevel="3" (click)="actionClicked('Action 3')">
+        Action 3
+        </button>
+    </kirby-action-group>
+  </kirby-header>
 
-                <kirby-page-content>
-                  <div [innerHTML]="content"></div>
-                </kirby-page-content>
-              </kirby-page>`,
+  <kirby-page-content>
+    <div [innerHTML]="content"></div>
+  </kirby-page-content>
+</kirby-page>`,
 };
 @Component({
   template: config.template,
@@ -31,5 +35,9 @@ export class PageWithHeaderExampleComponent extends BasePageExampleComponent {
 
   constructor() {
     super();
+  }
+
+  actionClicked(action: string) {
+    console.log(action);
   }
 }
