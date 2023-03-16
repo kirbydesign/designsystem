08cd89ecbac6841595b576ad8b8d00d1b6e6b672
Mark Drastrup <mark.drastrup@gmail.com>
2023-03-16 20:58:57 +0900

    Remove imports that prevents the cookbook from building

--- libs/designsystem/carousel/src/public_api.ts
+++ libs/designsystem/carousel/src/public_api.ts
@@ -1,2 +1,3 @@
 export { CarouselComponent, CarouselConfig } from './carousel.component';
+export { CarouselSlideComponent } from './carousel-slide/carousel-slide.component';
 export { CarouselModule } from './carousel.module';
