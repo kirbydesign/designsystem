78420e9a1e3d23d7bc43da88ef980bb9b5a78d4c
RasmusKjeldgaard <rkk@bankdata.dk>
2023-03-28 16:02:16 +0200

    InjectionToken for page back button (#2963)
    Co-authored-by: mictro <micbittro@gmail.com>

--- libs/designsystem/page/src/public_api.ts
+++ libs/designsystem/page/src/public_api.ts
@@ -1,14 +1,18 @@
-export { PageComponent } from './page.component';
-export { PageProgressComponent } from './page.component';
-export { PageTitleComponent } from './page.component';
-export { PageTitleDirective } from './page.component';
-export { PageSubtitleDirective } from './page.component';
-export { PageToolbarTitleDirective } from './page.component';
-export { PageActionsDirective } from './page.component';
-export { PageContentDirective } from './page.component';
-export { PageContentComponent } from './page.component';
-export { PageActionsComponent } from './page.component';
+export {
+  PageComponent,
+  PageProgressComponent,
+  PageTitleComponent,
+  PageTitleDirective,
+  PageSubtitleDirective,
+  PageToolbarTitleDirective,
+  PageActionsDirective,
+  PageContentDirective,
+  PageContentComponent,
+  PageActionsComponent,
+  PullToRefreshEvent,
+  PageStickyContentDirective,
+  PAGE_BACK_BUTTON_OVERRIDE,
+  PageBackButtonOverride,
+} from './page.component';
 export { PageFooterComponent } from './page-footer/page-footer.component';
 export { PageModule } from './page.module';
-export { PullToRefreshEvent } from './page.component';
-export { PageStickyContentDirective } from './page.component';
