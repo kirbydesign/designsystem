7fda684e28b4b47048633efed2c74b958d1c150f
Kodeaben <anders@kodeaben.com>
2023-04-13 08:08:26 +0200

    fixed lint errors

--- apps/cookbook/src/app/showcase/list-action/list-action-showcase.component.ts
+++ apps/cookbook/src/app/showcase/list-action/list-action-showcase.component.ts
@@ -2,6 +2,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'cookbook-list-action-showcase',
   templateUrl: './list-action-showcase.component.html',
-  styleUrls: ['./list-action-showcase.component.scss'],
 })
 export class ListActionShowcaseComponent {}
