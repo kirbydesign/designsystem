380d959a20c539335a4387b2a5568d115d9a9964
Jakob Engelbrecht <jakob@basher.dk>
2023-04-17 15:07:52 +0200

    refactor: clean up redundant export

--- libs/designsystem/testing-base/src/lib/index.ts
+++ libs/designsystem/testing-base/src/lib/index.ts
@@ -1,4 +1,4 @@
 export * from './components';
 export * from './directives';
-export { MockProgressCircleRingComponent } from './components/mock.progress-circle-ring.component';
+
 export { KirbyTestingBaseModule } from './kirby-testing-base.module';
