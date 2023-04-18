0d836d465074b578837e671ae61e162a51fa081e
Mark Drastrup <mark.drastrup@gmail.com>
2023-04-18 20:58:13 +0900

    Write tests

--- libs/designsystem/carousel/src/carousel.component.spec.ts
+++ libs/designsystem/carousel/src/carousel.component.spec.ts
@@ -1,30 +1,103 @@
 import { TestHelper } from '@kirbydesign/designsystem/testing';
 import { createHostFactory, SpectatorHost } from '@ngneat/spectator';
+import { ButtonComponent } from 'button/src';
+import { IconModule } from 'icon/src';
+import { CardModule } from 'card/src';
+import { DesignTokenHelper } from '@kirbydesign/designsystem/helpers';
+import { CarouselComponent, SlidesOptions } from './carousel.component';
+import { CarouselSlideDirective } from './carousel-slide.directive';
 
-import { CarouselComponent } from './carousel.component';
-import { CarouselSlideComponent } from './carousel-slide/carousel-slide.component';
+const { getColor } = DesignTokenHelper;
 
 describe('CarouselComponent', () => {
   let spectator: SpectatorHost<CarouselComponent>;
 
+  const customOptions: SlidesOptions = {
+    slidesPerView: 1.2,
+    centeredSlides: true,
+    centeredSlidesBounds: false,
+    spaceBetween: 8,
+    slidesPerGroup: 1,
+  };
+
   const createHost = createHostFactory({
     component: CarouselComponent,
-    imports: [TestHelper.ionicModuleForTest],
-    declarations: [CarouselSlideComponent],
+    imports: [TestHelper.ionicModuleForTest, ButtonComponent, IconModule, CardModule],
+    declarations: [CarouselSlideDirective],
   });
 
-  describe('by default', () => {
-    beforeEach(() => {
-      spectator = createHost(`<kirby-carousel>
-        <kirby-carousel-slide> Slide 1 </kirby-carousel-slide>
-        <kirby-carousel-slide> Slide 2 </kirby-carousel-slide>
-        <kirby-carousel-slide> Slide 3 </kirby-carousel-slide>
-      </kirby-carousel>`);
-    });
+  beforeEach(() => {
+    spectator = createHost(
+      `<kirby-carousel  [slides]="slides" [slidesOptions]="slidesOptions">
+      <kirby-card *kirbyCarouselSlide="let slide">
+          {{ slide }}
+      </kirby-card>
+    </kirby-carousel>`,
+      {
+        hostProps: {
+          slides: [0, 1, 2, 3, 4],
+          slidesOptions: customOptions,
+        },
+      }
+    );
+  });
 
-    it('should create', () => {
-      console.log(spectator.fixture.nativeElement);
-      expect(spectator.component).toBeTruthy();
+  it('should create', () => {
+    expect(spectator.component).toBeTruthy();
+  });
+
+  it('should contain 5 slides', () => {
+    expect(spectator.queryAll('swiper-slide').length).toBe(5);
+  });
+
+  it('should update the active slide index, when calling slideTo ', () => {
+    spectator.component.slideTo(2);
+
+    expect(spectator.component.swiperContainer.nativeElement.swiper.activeIndex).toBe(2);
+  });
+
+  it('should emit selectedSlide when the active slide is changed', () => {
+    const selectedSlideSpy = spyOn(spectator.component.selectedSlide, 'emit');
+
+    spectator.component.slideTo(3);
+
+    expect(selectedSlideSpy).toHaveBeenCalledWith({ slide: 3, index: 3 });
+  });
+
+  it('should disable the previous button, when the active slide is the first slide', () => {
+    const previousButton = spectator.query('.swiper-button-prev');
+
+    expect(previousButton.classList).toContain('swiper-button-disabled');
+
+    spectator.component.slideTo(1);
+
+    expect(previousButton.classList).not.toContain('swiper-button-disabled');
+  });
+
+  it('should disable the next button, when the active slide is the last slide', () => {
+    const nextButton = spectator.query('.swiper-button-next');
+
+    expect(nextButton.classList).not.toContain('swiper-button-disabled');
+
+    spectator.component.slideTo(4);
+
+    expect(nextButton.classList).toContain('swiper-button-disabled');
+  });
+
+  it('should have pagination dots with custom styling', () => {
+    const paginationDot = spectator.queryAll('.swiper-pagination-bullet')[0];
+
+    expect(paginationDot).toHaveComputedStyle({
+      'border-radius': '2px',
+      'background-color': getColor('black'),
+      height: '6px',
+      width: '10px',
     });
+  });
+
+  it('should extend the default slides options with the provided slides options', () => {
+    expect(spectator.component.swiperContainer.nativeElement.swiper.passedParams).toEqual(
+      jasmine.objectContaining(customOptions)
+    );
   });
 });
