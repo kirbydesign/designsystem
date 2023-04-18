e170a981b4e00f47ac92f121eb033a486d9c1536
Jakob Engelbrecht <jakob@basher.dk>
2023-04-18 15:04:10 +0200

    mocks: replace hardcoded directives declarations

--- libs/designsystem/testing-base/src/lib/mock-directives.ts
+++ libs/designsystem/testing-base/src/lib/mock-directives.ts
@@ -1,9 +1,45 @@
+// AUTO-GENERATED - PLEASE DON'T EDIT THIS FILE MANUALLY
+
 import { MockAccordionDirective } from './directives/mock.accordion.directive';
+import { MockCardAsButtonDirective } from './directives/mock.card-as-button.directive';
+import { MockAffixDirective } from './directives/mock.affix.directive';
+import { MockDateInputDirective } from './directives/mock.date-input.directive';
+import { MockDecimalMaskDirective } from './directives/mock.decimal-mask.directive';
+import { MockInfiniteScrollDirective } from './directives/mock.infinite-scroll.directive';
+import { MockListItemColorDirective } from './directives/mock.list-item-color.directive';
+import {
+  MockListFooterDirective,
+  MockListHeaderDirective,
+  MockListItemTemplateDirective,
+  MockListSectionHeaderDirective,
+} from './directives/mock.list.directive';
+import { MockFloatingDirective } from './directives/mock.floating.directive';
+import { MockPortalDirective } from './directives/mock.portal.directive';
+import { MockComponentLoaderDirective } from './directives/mock.component-loader.directive';
 import { MockFitHeadingDirective } from './directives/mock.fit-heading.directive';
 import { MockThemeColorDirective } from './directives/mock.theme-color.directive';
+import { MockSlideDirective } from './directives/mock.slide.directive';
+import { MockKeyHandlerDirective } from './directives/mock.key-handler.directive';
+import { MockModalRouterLinkDirective } from './directives/mock.modal-router-link.directive';
 
 export const MOCK_DIRECTIVES = [
-  MockThemeColorDirective,
-  MockFitHeadingDirective,
   MockAccordionDirective,
+  MockCardAsButtonDirective,
+  MockAffixDirective,
+  MockDateInputDirective,
+  MockDecimalMaskDirective,
+  MockInfiniteScrollDirective,
+  MockListItemColorDirective,
+  MockListItemTemplateDirective,
+  MockListHeaderDirective,
+  MockListSectionHeaderDirective,
+  MockListFooterDirective,
+  MockFloatingDirective,
+  MockPortalDirective,
+  MockComponentLoaderDirective,
+  MockFitHeadingDirective,
+  MockThemeColorDirective,
+  MockSlideDirective,
+  MockKeyHandlerDirective,
+  MockModalRouterLinkDirective,
 ];
