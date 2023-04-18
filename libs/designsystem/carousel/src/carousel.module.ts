7cc6a4183d7830bb932efd91dc25a968139a6dc9
Mark Drastrup <mark.drastrup@gmail.com>
2023-04-18 15:12:03 +0900

    Import KirbyIonicModule instead of CardComponent

--- libs/designsystem/carousel/src/carousel.module.ts
+++ libs/designsystem/carousel/src/carousel.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
-import { CardModule } from '@kirbydesign/designsystem/card';
+import { KirbyIonicModule } from '@kirbydesign/designsystem/kirby-ionic-module';
 import { ButtonComponent } from '@kirbydesign/designsystem/button';
 import { IconModule } from '@kirbydesign/designsystem/icon';
 import { CarouselComponent } from './carousel.component';
@@ -9,7 +9,7 @@ import { CarouselSlideDirective } from './carousel-slide.directive';
 const declarations = [CarouselComponent, CarouselSlideDirective];
 
 @NgModule({
-  imports: [CommonModule, CardModule, ButtonComponent, IconModule],
+  imports: [CommonModule, KirbyIonicModule, ButtonComponent, IconModule],
   declarations: [...declarations],
   exports: [...declarations],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
