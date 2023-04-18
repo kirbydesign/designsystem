bca1d1a65351a45103ce32280e2df7fa279a6813
Jakob Engelbrecht <jakob@basher.dk>
2023-04-18 15:03:18 +0200

    mocks: replace hardcoded barrel files

--- libs/designsystem/testing-base/src/lib/directives/index.ts
+++ libs/designsystem/testing-base/src/lib/directives/index.ts
@@ -1,3 +1,23 @@
+// AUTO-GENERATED - PLEASE DON'T EDIT THIS FILE MANUALLY
+
 export { MockAccordionDirective } from './mock.accordion.directive';
+export { MockCardAsButtonDirective } from './mock.card-as-button.directive';
+export { MockAffixDirective } from './mock.affix.directive';
+export { MockDateInputDirective } from './mock.date-input.directive';
+export { MockDecimalMaskDirective } from './mock.decimal-mask.directive';
+export { MockInfiniteScrollDirective } from './mock.infinite-scroll.directive';
+export { MockListItemColorDirective } from './mock.list-item-color.directive';
+export {
+  MockListItemTemplateDirective,
+  MockListHeaderDirective,
+  MockListSectionHeaderDirective,
+  MockListFooterDirective,
+} from './mock.list.directive';
+export { MockFloatingDirective } from './mock.floating.directive';
+export { MockPortalDirective } from './mock.portal.directive';
+export { MockComponentLoaderDirective } from './mock.component-loader.directive';
 export { MockFitHeadingDirective } from './mock.fit-heading.directive';
 export { MockThemeColorDirective } from './mock.theme-color.directive';
+export { MockSlideDirective } from './mock.slide.directive';
+export { MockKeyHandlerDirective } from './mock.key-handler.directive';
+export { MockModalRouterLinkDirective } from './mock.modal-router-link.directive';
