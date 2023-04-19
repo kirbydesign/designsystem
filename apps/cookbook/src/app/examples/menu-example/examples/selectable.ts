3be44ed2e9f4bcd5555396a64b35cfb9f226ebdd
Jakob Engelbrecht <jakob@basher.dk>
2023-04-19 16:54:02 +0200

    docs: improve selectable example with click event

--- apps/cookbook/src/app/examples/menu-example/examples/selectable.ts
+++ apps/cookbook/src/app/examples/menu-example/examples/selectable.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { ToastConfig, ToastController } from '@kirbydesign/designsystem/toast';
 
 const config = {
   selector: 'cookbook-menu-selectable-example',
-  template: `<kirby-menu [closeOnSelect]="true">
-  <kirby-item [selectable]="true">
+  template: `<kirby-menu>
+  <kirby-item [selectable]="true" (click)="actionClicked('Action 1')">
     <h3>Action 1</h3>
   </kirby-item>
-  ...
+  <kirby-item [selectable]="true" (click)="actionClicked('Action 2')">
+    <h3>Action 2</h3>
+  </kirby-item>
 </kirby-menu>`,
 };
 
@@ -16,4 +19,15 @@ const config = {
 })
 export class MenuSelectableExampleComponent {
   template: string = config.template;
+
+  constructor(private toastController: ToastController) {}
+
+  actionClicked(action: string) {
+    const config: ToastConfig = {
+      message: `${action} was selected.`,
+      messageType: 'success',
+      durationInMs: 1500,
+    };
+    this.toastController.showToast(config);
+  }
 }
