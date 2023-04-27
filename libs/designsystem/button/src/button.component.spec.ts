2314e2b8ebab812c756a891404fc75e85c120a34
Kodeaben <114976818+kodeaben@users.noreply.github.com>
2023-04-27 13:24:46 +0200

    Undecorated button has background color (#3014)

--- libs/designsystem/button/src/button.component.spec.ts
+++ libs/designsystem/button/src/button.component.spec.ts
@@ -85,6 +85,15 @@ describe('ButtonComponent', () => {
           color: getColor('semi-dark', 'shade'),
         });
       });
+
+      it('should not have a background-color when disabled', () => {
+        expect(element).toHaveComputedStyle({ 'background-color': getColor('semi-light') });
+
+        spectator.component.noDecoration = true;
+        spectator.detectChanges();
+
+        expect(element).toHaveComputedStyle({ 'background-color': 'transparent' });
+      });
     });
 
     describe('when configured with attentionlevel 1', () => {
