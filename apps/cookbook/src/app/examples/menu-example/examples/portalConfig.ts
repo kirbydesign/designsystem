fe0e6ed3878a2c1fe11d01559269e0fefd47b8f6
Mark Drastrup <mark.drastrup@gmail.com>
2023-05-03 16:37:43 +0200

    Remove test code

--- apps/cookbook/src/app/examples/menu-example/examples/portalConfig.ts
+++ apps/cookbook/src/app/examples/menu-example/examples/portalConfig.ts
@@ -3,7 +3,7 @@ import { OutletSelector, PortalOutletConfig } from '@kirbydesign/designsystem/sh
 
 const config = {
   selector: 'cookbook-menu-portal-config-example',
-  template: `<kirby-menu [portalOutletConfig]="outletConfig" [isDisabled]="true">
+  template: `<kirby-menu [portalOutletConfig]="outletConfig">
   <kirby-item [selectable]="true">
     <h3>Action 1</h3>
   </kirby-item>
