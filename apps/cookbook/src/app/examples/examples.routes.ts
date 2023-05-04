7e8b894e43691e318908888074dd0ac304a26008
Jakob Engelbrecht <jakob@basher.dk>
2023-05-04 09:22:00 +0200

    Enhancement: interactive header title (#3035)
    Co-authored-by: mark-drastrup <mark.drastrup@gmail.com>

--- apps/cookbook/src/app/examples/examples.routes.ts
+++ apps/cookbook/src/app/examples/examples.routes.ts
@@ -81,6 +81,7 @@ import { HeaderExampleComponent } from './header-example/header-example.componen
 import { NestedModalsV2ExampleComponent } from './modal-v2-example/nested-modals/nested-modals-v2-example.component';
 import { HeaderWithActionGroupExampleComponent } from './header-example/examples/action-group';
 import { HeaderWithCustomActionsExampleComponent } from './header-example/examples/custom-actions';
+import { HeaderWithInteractiveTitleExampleComponent } from './header-example/examples/interactive-title';
 import { MenuExampleComponent } from './menu-example/menu-example.component';
 
 export const routes: Routes = [
@@ -183,6 +184,10 @@ export const routes: Routes = [
           {
             path: 'header-and-custom-actions',
             component: HeaderWithCustomActionsExampleComponent,
+          },
+          {
+            path: 'header-and-interactive-title',
+            component: HeaderWithInteractiveTitleExampleComponent,
           },
         ],
       },
