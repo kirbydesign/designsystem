5c31cf08eaa0da89f5e97dc69afc5ac277023dfd
Jakob Engelbrecht <jakob@basher.dk>
2023-05-22 15:27:14 +0200

    docs: re-style config wrapper

--- apps/cookbook/src/app/examples/example-configuration-wrapper/example-configuration-wrapper.component.ts
+++ apps/cookbook/src/app/examples/example-configuration-wrapper/example-configuration-wrapper.component.ts
@@ -6,7 +6,6 @@ import { Component, HostBinding, Input } from '@angular/core';
   styleUrls: ['./example-configuration-wrapper.component.scss'],
 })
 export class ExampleConfigurationWrapperComponent {
-  @HostBinding('class')
   @Input()
   configAppearance: 'block' | 'snap-to-viewport' | 'toggle' = 'block';
 
@@ -14,6 +13,14 @@ export class ExampleConfigurationWrapperComponent {
 
   @HostBinding('class.show-config')
   showConfig: boolean = false;
+
+  @Input()
+  align: 'start' | 'end' = 'end';
+
+  @HostBinding('class')
+  get _cssClass() {
+    return ['align-' + this.align, this.configAppearance].filter(Boolean);
+  }
 
   toggleConfig() {
     this.showConfig = !this.showConfig;
