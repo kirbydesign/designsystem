85530078e58a8247395e58550225246ea74c32ab
Mark Drastrup <mark.drastrup@gmail.com>
2023-05-31 16:33:43 +0200

    Create the sortable component

--- libs/designsystem/data-table/src/sortable/sortable.component.spec.ts
+++ libs/designsystem/data-table/src/sortable/sortable.component.spec.ts
@@ -1,5 +1,6 @@
 import { MockComponent } from 'ng-mocks';
 import { createHostFactory, Spectator } from '@ngneat/spectator';
+import { IconComponent } from '@kirbydesign/designsystem/icon';
 import { TableComponent } from '../table/table.component';
 import { TableSortableComponent } from './sortable.component';
 
@@ -9,25 +10,47 @@ describe('TableSortableComponent', () => {
 
   const createHost = createHostFactory({
     component: TableSortableComponent,
-    declarations: [MockComponent(TableComponent)],
+    declarations: [MockComponent(TableComponent), MockComponent(IconComponent)],
   });
 
   describe('by default', () => {
     beforeEach(() => {
-      spectator = createHost(`
-      <table kirby-table>
-        <thead id="tbody">
-          <tr kirby-tr>
-            <th kirby-th>Data1</th>
-            <th kirby-th>Data2</th>
-          </tr>
-        </thead>
-      </table>`);
+      spectator = createHost(`<th [sortable]="true">Data1</th>`);
       element = spectator.element as HTMLElement;
     });
 
     it('should create', () => {
       expect(spectator.component).toBeTruthy();
+    });
+
+    it('should render a button, when sortable is true', () => {
+      const button = spectator.query('button');
+
+      expect(button).toBeTruthy();
+    });
+
+    it('should render a span, when sortable is false', () => {
+      spectator.setInput('sortable', false);
+
+      const span = spectator.query('span');
+
+      expect(span).toBeTruthy();
+    });
+
+    it('should render an "arrow-up" icon, when sortDirection is "asc"', () => {
+      spectator.setInput('sortDirection', 'asc');
+
+      const icon = spectator.query('kirby-icon');
+
+      expect(icon.attributes['name'].value).toBe('arrow-up');
+    });
+
+    it('should render an "arrow-down" icon, when sortDirection is "desc"', () => {
+      spectator.setInput('sortDirection', 'desc');
+
+      const icon = spectator.query('kirby-icon');
+
+      expect(icon.attributes['name'].value).toBe('arrow-down');
     });
   });
 });
--- libs/designsystem/data-table/src/sortable/sortable.component.ts
+++ libs/designsystem/data-table/src/sortable/sortable.component.ts
@@ -9,27 +9,13 @@ import {
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'th[sortable]',
-  template: `
-    <button>
-      <ng-content></ng-content>
-    </button>
-  `,
+  templateUrl: './sortable.component.html',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TableSortableComponent {
-  /**
-   * Sets the header to sortable
-   */
-  @HostBinding('class.kirby-sortable-head') @Input() sortable: boolean = false;
-
   @Input() sortDirection: 'asc' | 'desc';
-
-  @Input() textAlignment?: 'start' | 'center' | 'end';
+  @Input() sortable: boolean;
 
   @HostBinding('class.kirby-header-active') @Input() active = false;
-
-  @HostBinding('class') get HeadingClass() {
-    return this.textAlignment ? `kirby-align-heading-text ${this.textAlignment}` : '';
-  }
 }
