72f81fafe7a8db06071b6f09b01bc274ae694c54
RasmusKjeldgaard <rkk@bankdata.dk>
2023-05-31 16:15:49 +0200

    Add platform based animation config to IonicModule

--- libs/designsystem/kirby-ionic-module/src/kirby-ionic.module.ts
+++ libs/designsystem/kirby-ionic-module/src/kirby-ionic.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, isPlatform } from '@ionic/angular';
 
+const getIonicConfig = () => {
+  let config: any = {
+    mode: 'ios',
+    inputShims: true,
+    scrollAssist: true,
+    scrollPadding: false,
+  };
+
+  if (!isPlatform('hybrid')) {
+    config = {
+      ...config,
+      navAnimation: () => null,
+    };
+  }
+
+  return config;
+};
 @NgModule({
-  imports: [
-    IonicModule.forRoot({
-      mode: 'ios',
-      inputShims: true,
-      scrollAssist: true,
-      scrollPadding: false,
-    }),
-  ],
+  imports: [IonicModule.forRoot(getIonicConfig())],
   exports: [IonicModule],
 })
 export class KirbyIonicModule {}
