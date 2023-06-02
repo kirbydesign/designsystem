a2771d34d5e99655d2cdfe2f012d58b2836e69da
Mark Drastrup <mark.drastrup@gmail.com>
2023-06-02 13:05:10 +0200

    Write additional tests

--- libs/designsystem/data-table/src/sortable/sortable.component.spec.ts
+++ libs/designsystem/data-table/src/sortable/sortable.component.spec.ts
@@ -1,8 +1,11 @@
 import { MockComponent } from 'ng-mocks';
 import { createHostFactory, Spectator } from '@ngneat/spectator';
 import { IconComponent } from '@kirbydesign/designsystem/icon';
+import { DesignTokenHelper } from '@kirbydesign/designsystem/helpers';
 import { TableComponent } from '../table/table.component';
 import { TableSortableComponent } from './sortable.component';
+
+const getFontWeight = DesignTokenHelper.fontWeight;
 
 describe('TableSortableComponent', () => {
   let spectator: Spectator<TableSortableComponent>;
@@ -12,15 +15,17 @@ describe('TableSortableComponent', () => {
     declarations: [MockComponent(TableComponent), MockComponent(IconComponent)],
   });
 
-  describe('by default', () => {
-    beforeEach(() => {
-      spectator = createHost(`<th [sortable]="true">Data1</th>`);
-    });
+  beforeEach(() => {
+    spectator = createHost(`<th [sortable]="true">Data1</th>`);
+  });
 
+  describe('by default', () => {
     it('should create', () => {
       expect(spectator.component).toBeTruthy();
     });
+  });
 
+  describe('sortable', () => {
     it('should render a button, when sortable is true', () => {
       const button = spectator.query('button');
 
@@ -34,7 +39,9 @@ describe('TableSortableComponent', () => {
 
       expect(span).toBeTruthy();
     });
+  });
 
+  describe('sortDirection', () => {
     it('should render an "arrow-up" icon, when sortDirection is "asc"', () => {
       spectator.setInput('sortDirection', 'asc');
 
@@ -49,6 +56,48 @@ describe('TableSortableComponent', () => {
       const icon = spectator.query('kirby-icon');
 
       expect(icon.attributes['name'].value).toBe('arrow-down');
+    });
+  });
+
+  describe('when active', () => {
+    it('should get the "header-active" class when active is set to true', () => {
+      spectator.setInput('active', true);
+
+      expect(spectator.element).toHaveClass('header-active');
+    });
+
+    it('should make the text bold', () => {
+      spectator.setInput('active', true);
+
+      expect(spectator.element).toHaveComputedStyle({
+        'font-weight': getFontWeight('bold'),
+      });
+    });
+  });
+
+  describe('textAlign', () => {
+    it('should apply the "button-content-start" class if textAlignment is set to "start"', () => {
+      spectator.setInput('textAlignment', 'start');
+
+      const button = spectator.query('button');
+
+      expect(button).toHaveClass('button-content-start');
+    });
+
+    it('should apply the "button-content-center" class if textAlignment is set to "center"', () => {
+      spectator.setInput('textAlignment', 'center');
+
+      const button = spectator.query('button');
+
+      expect(button).toHaveClass('button-content-center');
+    });
+
+    it('should apply the "button-content-end" class if textAlignment is set to "end"', () => {
+      spectator.setInput('textAlignment', 'end');
+
+      const button = spectator.query('button');
+
+      expect(button).toHaveClass('button-content-end');
     });
   });
 });
