a4880046c68cb29b203466f8b5440e2843873f3d
Jakob Engelbrecht <jakob@basher.dk>
2023-06-06 15:54:44 +0200

    docs: fix indentation

--- apps/cookbook/src/app/examples/slides-example/slides-code-snippets.ts
+++ apps/cookbook/src/app/examples/slides-example/slides-code-snippets.ts
@@ -1,49 +1,46 @@
 export const slidesDefaultConfig = `defaultConfig: KirbySwiperOptions = {
-	spaceBetween: 16,
-	pagination: {
-			el: '.pagination',
-			type: 'bullets',
-	},
-	navigation: {
-			nextEl: 'swiper-button-prev',
-			prevEl: 'swiper-button-next',
-	},
-	on: {
-			slideChange: (swiper) => {
-					this.selectedSlide.emit({
-							slide: this.slides[swiper.activeIndex],
-							index: swiper.activeIndex,
-					});
-			},
-	},
-}
-`;
+  spaceBetween: 16,
+  pagination: {
+    el: '.pagination',
+    type: 'bullets',
+  },
+  navigation: {
+    nextEl: 'swiper-button-prev',
+    prevEl: 'swiper-button-next',
+  },
+  on: {
+    slideChange: (swiper) => {
+      this.selectedSlide.emit({
+        slide: this.slides[swiper.activeIndex],
+        index: swiper.activeIndex,
+      });
+    },
+  },
+}`;
 
 export const exampleComponentHTML = `<kirby-slides
-	[slidesOptions]="customConfig"
-	[ignorePagePadding]="ignorePagePadding"
-	[slides]="slides"
-	(selectedSlide)="getDataFromActiveSlide($event)"
+[slidesOptions]="customConfig"
+[ignorePagePadding]="ignorePagePadding"
+[slides]="slides"
+(selectedSlide)="getDataFromActiveSlide($event)"
 >
-	<kirby-card *kirbyCarouselSlide="let slide; let i = index" [hasPadding]="true">
-			<kirby-card-header [title]="slide.title" [subtitle]="slide.subtitle"></kirby-card-header>
-
-			<div class="card-content">
-					{{ slide.cardContent }}
-			</div>
-	</kirby-card>
-</kirby-slides>
-`;
+<kirby-card *kirbyCarouselSlide="let slide; let i = index" [hasPadding]="true">
+  <kirby-card-header [title]="slide.title" [subtitle]="slide.subtitle"></kirby-card-header>
+  <div class="card-content">
+	 {{ slide.cardContent }}
+  </div>
+</kirby-card>
+</kirby-slides>`;
 
 export const customConfigExample = `customConfig: KirbySwiperOptions = {
-	slidesPerView: 1.2,
-	centeredSlides: true,
-	slidesPerGroup: 1,
-	breakpoints: {
-		992: {
-		centeredSlides: false,
-		slidesPerView: 3,
-		slidesPerGroup: 3,
-		},
-	},
-};`;
+  slidesPerView: 1.2,
+  centeredSlides: true,
+  slidesPerGroup: 1,
+  breakpoints: {
+    992: {
+      centeredSlides: false,
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    },
+  },
+}`;
