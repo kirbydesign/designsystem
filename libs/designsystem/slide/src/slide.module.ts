c1e91dbb21657968bfc3d46b151930a815c962fd
Jakob Engelbrecht <jakob@basher.dk>
2023-06-08 07:52:33 +0200

    refactor: reorder imports

--- libs/designsystem/slide/src/slide.module.ts
+++ libs/designsystem/slide/src/slide.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+
 import { KirbyIonicModule } from '@kirbydesign/designsystem/kirby-ionic-module';
 import { ButtonComponent } from '@kirbydesign/designsystem/button';
 import { IconModule } from '@kirbydesign/designsystem/icon';
@@ -9,8 +10,8 @@ import { SlideDirective } from './slide.directive';
 const declarations = [SlidesComponent, SlideDirective];
 
 @NgModule({
-  imports: [CommonModule, KirbyIonicModule, ButtonComponent, IconModule],
   declarations: [...declarations],
+  imports: [CommonModule, KirbyIonicModule, ButtonComponent, IconModule],
   exports: [...declarations],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
