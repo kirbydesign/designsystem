5cee9e31ff9751285692f32f111e6ea7a0c133a3
RasmusKjeldgaard <rkk@bankdata.dk>
2023-06-12 13:09:42 +0200

    Add styleable attribute to modal router link elm

--- libs/designsystem/src/lib/directives/modal-router-link/modal-router-link.directive.ts
+++ libs/designsystem/src/lib/directives/modal-router-link/modal-router-link.directive.ts
@@ -1,15 +1,17 @@
-import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, OnInit } from '@angular/core';
 import { Params } from '@angular/router';
 import { AlertConfig, ModalNavigationService } from '@kirbydesign/designsystem/modal';
 
 @Directive({
   selector: `[kirbyModalRouterLink]`,
 })
-export class ModalRouterLinkDirective {
+export class ModalRouterLinkDirective implements OnInit {
   constructor(
     private modalNavigationService: ModalNavigationService,
     private elementRef: ElementRef
-  ) {
+  ) {}
+
+  ngOnInit(): void {
     this.elementRef.nativeElement.setAttribute('modal-router-link', '');
   }
 
