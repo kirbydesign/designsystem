b49f9daafccf9ecd55f6cf7631fd791bb5eff787
Jakob Engelbrecht <jakob@basher.dk>
2023-06-13 09:05:51 +0200

    docs: sync code snippets

--- apps/cookbook/src/app/examples/slides-example/slides-code-snippets.ts
+++ apps/cookbook/src/app/examples/slides-example/slides-code-snippets.ts
@@ -1,22 +1,40 @@
+import { DesignTokenHelper } from '@kirbydesign/designsystem/helpers';
+
+const desktopBreakpoint = parseInt(DesignTokenHelper.breakpoints.medium) - 1;
+const spaceBetween = parseInt(DesignTokenHelper.size('s'));
+const transitionDuration = parseInt(DesignTokenHelper.transitionDuration('long'));
+
 export const slidesDefaultConfig = `defaultConfig: KirbySwiperOptions = {
-  spaceBetween: 16,
+  centeredSlides: true,
+  centeredSlidesBounds: true,
+  slidesPerView: 1.2,
+  slidesPerGroup: 1,
+  breakpoints: {
+    ${desktopBreakpoint}: {
+      centeredSlides: false,
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    },
+  },
+  spaceBetween: ${spaceBetween},
+  speed: ${transitionDuration},
   pagination: {
     el: '.pagination',
     type: 'bullets',
   },
   navigation: {
-    nextEl: 'swiper-button-prev',
-    prevEl: 'swiper-button-next',
+    nextEl: '.swiper-button-prev',
+    prevEl: '.swiper-button-next',
   },
   on: {
     slideChange: (swiper) => {
-      this.selectedSlide.emit({
+      this.slideChange.emit({
         slide: this.slides[swiper.activeIndex],
         index: swiper.activeIndex,
       });
     },
   },
-}`;
+};`;
 
 export const defaultExampleComponentHTML = `<kirby-slides [slides]="slides" [title]="'Title'" [showNavigation]="true">
   <kirby-card *kirbySlide="let slide; let i = index" [hasPadding]="true">
@@ -30,23 +48,27 @@ export const advancedExampleComponentHTML = `<kirby-slides
   (selectedSlide)="getDataFromActiveSlide($event)"
   [slidesOptions]="customConfig"
   [showNavigation]="true"
-  [title]="'Title'"
->
+  [title]="'Title'">
+
   <kirby-card *kirbySlide="let slide; let i = index" [hasPadding]="true">
     <kirby-card-header [title]="slide.title" [subtitle]="slide.subtitle"></kirby-card-header>
     <div>{{ slide.cardContent }}</div>
   </kirby-card>
+
+  <button kirby-button attentionLevel="3" size="xs" (click)="showAll()">See all</button>
+
 </kirby-slides>`;
 
 export const customConfigExample = `customConfig: KirbySwiperOptions = {
-  slidesPerView: 1.2,
+  centeredSlides: true,
+  slidesPerView: 1,
   centeredSlides: true,
   slidesPerGroup: 1,
   breakpoints: {
-    992: {
+    768: {
       centeredSlides: false,
-      slidesPerView: 3,
-      slidesPerGroup: 3,
+      slidesPerView: 2,
+      slidesPerGroup: 1,
     },
   },
-}`;
+};`;
