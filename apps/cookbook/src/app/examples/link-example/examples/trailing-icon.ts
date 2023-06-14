70b09a91333cf316e5c99807c1eb6775bb1e20cc
AgreSanGit <112690169+AgreSanGit@users.noreply.github.com>
2023-06-14 12:37:36 +0200

    Update apps/cookbook/src/app/examples/link-example/examples/trailing-icon.ts
    Co-authored-by: Jakob Engelbrecht <jakob@basher.dk>

--- apps/cookbook/src/app/examples/link-example/examples/trailing-icon.ts
+++ apps/cookbook/src/app/examples/link-example/examples/trailing-icon.ts
@@ -4,12 +4,13 @@ const config = {
   selector: 'cookbook-link-example-trailing-icon',
   template: `<a class="trailing-icon-example" target="_blank" href="https://github.com/kirbydesign/designsystem">Trailing icon</a>`,
   style: `@use '@kirbydesign/core/src/scss/utils';
-.trailing-icon-example
-{
-@include utils.trailing-icon("navigation")
-/* 
-@include utils.trailing-icon("https://cdn.jsdelivr.net/npm/open-iconic@1.1.1/svg/heart.svg")
-*/
+
+.trailing-icon-example {
+  // Using a Kirby icon:
+  @include utils.trailing-icon("navigation");
+  
+  // Using a URL to an svg:
+  @include utils.trailing-icon("https://cdn.jsdelivr.net/npm/open-iconic@1.1.1/svg/heart.svg");
 }`,
 };
 
