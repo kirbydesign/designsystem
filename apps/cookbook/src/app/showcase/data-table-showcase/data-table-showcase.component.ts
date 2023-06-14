6efee2c88c07353d1ed17fc4ebe0b79c7952a158
Mark Drastrup <mark.drastrup@gmail.com>
2023-06-14 13:15:26 +0200

    Remove temp test code from the docs

--- apps/cookbook/src/app/showcase/data-table-showcase/data-table-showcase.component.ts
+++ apps/cookbook/src/app/showcase/data-table-showcase/data-table-showcase.component.ts
@@ -2,10 +2,6 @@ import { Component } from '@angular/core';
 import { dataTableApi } from './data-table-api';
 import { ApiDescriptionProperty } from '~/app/shared/api-description/api-description-properties/api-description-properties.component';
 
-interface Test {
-  subData: number;
-}
-
 @Component({
   selector: 'cookbook-data-table-showcase',
   templateUrl: './data-table-showcase.component.html',
@@ -13,20 +9,4 @@ interface Test {
 })
 export class DataTableShowcaseComponent {
   _apiDescriptionPropertiesTable: ApiDescriptionProperty[] = [...dataTableApi];
-
-  sortDirection: 'asc' | 'desc' = 'asc';
-
-  data: Test[] = [{ subData: 1 }, { subData: 1 }];
-  sortFunction(object: any) {
-    this.sortDirection = this.sortDirection == 'asc' ? 'desc' : 'asc';
-    this.data.sort((a, b) => {
-      if (a.subData > b.subData) return 1;
-      if (a.subData < b.subData) return -1;
-      if (a.subData == b.subData) return 0;
-    });
-  }
-
-  rowClick(object: any) {
-    alert(`row, also: ${object}`);
-  }
 }
