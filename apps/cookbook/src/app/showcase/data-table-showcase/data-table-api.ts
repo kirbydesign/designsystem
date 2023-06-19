770c7a7889ba735456a1576f6fab38234aaace95
Mark Drastrup <mark.drastrup@gmail.com>
2023-06-19 20:46:49 +0200

    Update the API description

--- apps/cookbook/src/app/showcase/data-table-showcase/data-table-api.ts
+++ apps/cookbook/src/app/showcase/data-table-showcase/data-table-api.ts
@@ -3,7 +3,7 @@ import { ApiDescriptionProperty } from '../../shared/api-description/api-descrip
 export const dataTableApi: ApiDescriptionProperty[] = [
   {
     name: '[sortable]',
-    description: `Enables / disables sorting on the th element. When true it will automatically render the arrow icon`,
+    description: `Renders sorting UI on the th element`,
     type: ['boolean'],
     defaultValue: 'false',
   },
@@ -11,16 +11,16 @@ export const dataTableApi: ApiDescriptionProperty[] = [
     name: '[sortDirection]',
     description: `Controls whether to show the up arrow (ascending) or down arrow (descending)`,
     type: ['asc', 'desc'],
-    defaultValue: 'undefined',
+    defaultValue: 'asc',
   },
   {
     name: '[iconAlignment]',
-    description: `Controls whether to render the icon to the left or right of the text in the header `,
+    description: `Controls whether to render the sorting icon before or after the text in the header`,
     type: ['start', 'end'],
     defaultValue: 'end',
   },
   {
-    name: '[textAlignment]',
+    name: '[alignment]',
     description: `Controls the alignment of the text and icon in the header `,
     type: ['start', 'center', 'end'],
     defaultValue: 'start',
