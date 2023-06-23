78d0095fce7cff50c2f271ee6123aa66d9d41042
Mark Drastrup <mark.drastrup@gmail.com>
2023-06-23 12:12:57 +0200

    Update the mocks array in modal.helper.spec.ts

--- libs/designsystem/modal/src/modal/services/modal.helper.spec.ts
+++ libs/designsystem/modal/src/modal/services/modal.helper.spec.ts
@@ -105,7 +105,7 @@ describe('ModalHelper', () => {
       ContentOverflowsWithFooterEmbeddedComponent,
       ContentWithNoOverflowEmbeddedComponent,
     ],
-    mocks: [ModalNavigationService, CanDismissHelper, AlertHelper],
+    mocks: [ModalNavigationService, AlertHelper, CanDismissHelper],
   });
 
   beforeAll(() => {
