7cb2cfc820645b46cbdc93a3f2f02e4db5c54c56
Mark Drastrup <mark.drastrup@gmail.com>
2023-06-30 11:58:18 +0200

    Update example and export code snippet

--- apps/cookbook/src/app/examples/item-example/examples/disclosure-animation.ts
+++ apps/cookbook/src/app/examples/item-example/examples/disclosure-animation.ts
@@ -3,22 +3,35 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
 
 const config = {
   selector: 'cookbook-item-example-disclosure-animation',
-  template: `
-  <kirby-card [hasPadding]="true" (click)="isExpanded = !isExpanded">
-    <kirby-card-header [hasPadding]="false">
+  template: `<kirby-card [hasPadding]="true" (click)="isExpanded = !isExpanded">
+  <kirby-card-header [hasPadding]="false">
     <kirby-item disclosure="arrow-down" [rotateIcon]="isExpanded">
       <p class="kirby-text-bold">Disclosure animation example</p>
     </kirby-item>
   </kirby-card-header>
 
-    <div [@isExpanded]="isExpanded">
-      <p class="kirby-text-large">A card with animated disclosure icon</p>
-      <p> 
-        lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      </p>
-    </div>
-  </kirby-card>
-  `,
+  <div [@isExpanded]="isExpanded">
+    <p class="kirby-text-large">A card with animated disclosure icon</p>
+    <p> 
+      lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    </p>
+  </div>
+</kirby-card>`,
+  animationCodeSnippetTS: `import { animate, state, style, transition, trigger } from '@angular/animations';
+
+@Component({
+  ...
+  animations: [
+    trigger('isExpanded', [
+      state('true', style({ height: '*', visibility: 'visible' })),
+      state('false', style({ height: '0px', visibility: 'hidden' })),
+      transition('true <=> false', animate('0.2s')),
+    ]),
+  ],
+})
+export class MyComponent {  
+  isExpanded = false;
+}`,
 };
 
 @Component({
@@ -35,6 +48,7 @@ const config = {
 })
 export class ItemExampleDisclosureAnimationComponent {
   template: string = config.template;
+  animationCodeSnippetTS: string = config.animationCodeSnippetTS;
 
   isExpanded = false;
 }
