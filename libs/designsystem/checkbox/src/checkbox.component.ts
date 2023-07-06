40d21ccb35d98e311e43b4a6a30ff6dd4863cc64
AgreSanGit <agre88@hotmail.com>
2023-07-06 13:08:27 +0200

    Implement ControlValueAcessor

--- libs/designsystem/checkbox/src/checkbox.component.ts
+++ libs/designsystem/checkbox/src/checkbox.component.ts
@@ -3,12 +3,14 @@ import {
   ChangeDetectionStrategy,
   Component,
   EventEmitter,
+  forwardRef,
   HostBinding,
   Input,
   Output,
 } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { UniqueIdGenerator } from '@kirbydesign/designsystem/helpers';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   standalone: true,
@@ -17,8 +19,15 @@ import { UniqueIdGenerator } from '@kirbydesign/designsystem/helpers';
   templateUrl: './checkbox.component.html',
   styleUrls: ['./checkbox.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => CheckboxComponent),
+      multi: true,
+    },
+  ],
 })
-export class CheckboxComponent {
+export class CheckboxComponent implements ControlValueAccessor {
   @Input() checked: boolean = false;
   @Input() attentionLevel: '1' | '2' = '2';
 
@@ -51,9 +60,47 @@ export class CheckboxComponent {
 
   onChecked(checked: boolean): void {
     this.checked = checked;
+    this.onChange(this.checked);
     this.checkedChange.emit(this.checked);
+    this.onTouched();
   }
 
   // IDs used for a11y labelling
   _labelId = UniqueIdGenerator.scopedTo('kirby-checkbox-label').next();
+
+  //ControleValueAcessor
+  private onChange: (value: boolean) => void = () => {};
+  private onTouched: () => void = () => {};
+
+  /**
+   * Writes a new value to the form control associated with the custom checkbox control.
+   * Part of the ControlValueAccessor interface.
+   */
+  writeValue(value: boolean): void {
+    this.checked = value;
+  }
+
+  /**
+   * Registers a callback function that should be called when the value of the checkbox changes.
+   * Part of the ControlValueAccessor interface.
+   */
+  registerOnChange(fn: (value: boolean) => void): void {
+    this.onChange = fn;
+  }
+
+  /**
+   * Registers a callback function that should be called when the checkbox is touched.
+   * Part of the ControlValueAccessor interface.
+   */
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  /**
+   * Sets the disabled state of the checkbox.
+   * Part of the ControlValueAccessor interface.
+   */
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
 }
