4264a262a1264d5e7a74fed155a28365c0017fda
Mark Drastrup <mark.drastrup@gmail.com>
2023-07-17 16:32:39 +0200

    Add comment in test file to trigger GH action

--- libs/designsystem/item/src/item.component.spec.ts
+++ libs/designsystem/item/src/item.component.spec.ts
@@ -155,6 +155,7 @@ describe('ItemComponent', () => {
 
         const icon = spectator.query('kirby-icon');
         await TestHelper.waitForTimeout(50); // Wait a tick
+        // Comment to trigger test
 
         expect(icon).toHaveComputedStyle({
           transform: 'none',
