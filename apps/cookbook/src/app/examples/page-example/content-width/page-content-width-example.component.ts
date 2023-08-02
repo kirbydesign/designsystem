82fb3927724f78b8ebef20f3158deb53997b39e4
RasmusKjeldgaard <rkk@bankdata.dk>
2023-08-02 10:40:19 +0200

    Set default page width top correct value

--- apps/cookbook/src/app/examples/page-example/content-width/page-content-width-example.component.ts
+++ apps/cookbook/src/app/examples/page-example/content-width/page-content-width-example.component.ts
@@ -6,7 +6,6 @@ const fieldsetHtml = `
   <fieldset>
     <legend>Max Width</legend>
     <select (change)="onMaxWidthChange($event.target.value)">
-      <option value="">-</option>
       <option
         *ngFor="let option of maxWidthOptions"
         value="{{ option.value }}"
