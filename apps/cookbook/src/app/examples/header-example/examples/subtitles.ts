6ca15888c34149b69bb328ac506f9eac1438f76f
Jakob Engelbrecht <jakob@basher.dk>
2023-09-21 15:19:04 +0200

    docs: add subtitle list example

---
+++ apps/cookbook/src/app/examples/header-example/examples/subtitles.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+const config = {
+  selector: 'cookbook-header-example-subtitle-list',
+  template: `<kirby-header [title]="'Title'" [subtitle1]="['Mrs. Lady Like Tiff Tuff Escargoon Tokkori ', '&', 'Mr. Chef Kawasaki Tiff Tuff Whippy Tokkori']">
+</kirby-header>`,
+};
+
+@Component({
+  selector: config.selector,
+  template: config.template,
+})
+export class HeaderExampleSubtitleListComponent {
+  template: string = config.template;
+}
