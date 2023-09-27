d9a2a0af98e828692f05c4dce6f044a7f50530f8
RasmusKjeldgaard <rkk@bankdata.dk>
2023-09-27 09:20:56 +0200

    Improve setPart() readability

--- libs/designsystem/helpers/src/set-element-part.ts
+++ libs/designsystem/helpers/src/set-element-part.ts
@@ -4,16 +4,20 @@ import { ElementRef, Injectable, Renderer2 } from '@angular/core';
 export class IonicElementPartService {
   constructor(private renderer: Renderer2) {}
 
-  public setPart(customElementRef: ElementRef, selector: string, partName: string) {
-    // Ensure custom element has been defined
-    customElements.whenDefined(customElementRef.nativeElement.localName).then(() => {
-      customElementRef.nativeElement.componentOnReady().then((customElement) => {
-        const partElement: HTMLElement = customElement.shadowRoot.querySelector(selector);
-        if (partElement) {
-          this.renderer.addClass(customElement, 'multiline');
-          this.renderer.setAttribute(partElement, 'part', partName);
-        }
-      });
-    });
+  public async setPart(
+    ionicElementRef: ElementRef,
+    selector: string,
+    partName: string,
+    condition?: (part: HTMLElement) => boolean
+  ) {
+    // Ensure custom element has been defined and rendered
+    await customElements.whenDefined(ionicElementRef.nativeElement.localName);
+    const customElement = await ionicElementRef.nativeElement.componentOnReady();
+
+    const partElement: HTMLElement = customElement.shadowRoot.querySelector(selector);
+    if (partElement) {
+      if (typeof condition === 'function' && !condition(partElement)) return;
+      this.renderer.setAttribute(partElement, 'part', partName);
+    }
   }
 }
