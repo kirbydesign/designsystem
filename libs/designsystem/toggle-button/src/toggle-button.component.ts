b74e0ff3da59b601c5c7afbb3975d3f99590e389
AgreSanGit <agre88@hotmail.com>
2023-09-29 13:56:21 +0200

    implement ControlValueAccessor

--- libs/designsystem/toggle-button/src/toggle-button.component.ts
+++ libs/designsystem/toggle-button/src/toggle-button.component.ts
@@ -2,23 +2,82 @@ import {
   ChangeDetectionStrategy,
   Component,
   EventEmitter,
+  forwardRef,
   HostListener,
   Input,
   Output,
 } from '@angular/core';
-
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 @Component({
   selector: 'kirby-toggle-button',
   templateUrl: './toggle-button.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => ToggleButtonComponent),
+      multi: true,
+    },
+  ],
 })
-export class ToggleButtonComponent {
+export class ToggleButtonComponent implements ControlValueAccessor {
   @Input() checked: boolean;
   @Output() checkChanged = new EventEmitter<boolean>();
 
   @HostListener('click')
   onClick() {
     this.checked = !this.checked;
+    this._onChange(this.checked);
     this.checkChanged.emit(this.checked);
   }
+
+  @HostListener('focusout')
+  onBlur() {
+    this._onTouched();
+    console.log('blur');
+  }
+  // Initialize default ControlValueAccessor callback functions (noop)
+  // eslint-disable-next-line no-empty-function
+  private _onChange: (value: boolean) => void = () => {};
+  // eslint-disable-next-line no-empty-function
+  private _onTouched = () => {};
+
+  /**
+   * Sets the toggle buttons's value. Part of the ControlValueAccessor interface
+   * required to integrate with Angular's core forms API.
+   *
+   * @param value New value to be written to the model.
+   */
+  writeValue(value: boolean): void {
+    this.checked = value;
+  }
+  /**
+   * Saves a callback function to be invoked when the toggle buttons's value
+   * changes from user input. Part of the ControlValueAccessor interface
+   * required to integrate with Angular's core forms API.
+   *
+   * @param fn Callback to be triggered when the value changes.
+   */
+  registerOnChange(fn: any): void {
+    this._onChange = fn;
+  }
+  /**
+   * Saves a callback function to be invoked when the toggle button is blurred
+   * by the user. Part of the ControlValueAccessor interface required
+   * to integrate with Angular's core forms API.
+   *
+   * @param fn Callback to be triggered when the component has been touched.
+   */
+  registerOnTouched(fn: any): void {
+    this._onTouched = fn;
+  }
+  /**
+   * Disables the toggle button. Part of the ControlValueAccessor interface required
+   * to integrate with Angular's core forms API.
+   *
+   * @param isDisabled Sets whether the component is disabled.
+   */
+  // setDisabledState?(isDisabled: boolean): void {
+  //   this.disabled = isDisabled;
+  // }
 }
