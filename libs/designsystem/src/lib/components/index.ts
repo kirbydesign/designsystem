c0063198790f12fa376abe9371154289dcf19fbf
RasmusKjeldgaard <rkk@bankdata.dk>
2023-10-05 09:43:46 +0200

    Remove badge export from top level entry point
    Badge is a secondary entry

--- libs/designsystem/src/lib/components/index.ts
+++ libs/designsystem/src/lib/components/index.ts
@@ -1,5 +1,3 @@
-export { BadgeComponent } from '@kirbydesign/designsystem/badge';
-
 /* prettier-ignore */
 export { SegmentedControlComponent, SegmentedControlMode} from './segmented-control/segmented-control.component'; // See issue #1555 for prettier-ignore explanation
 export { SegmentItem } from './segmented-control/segment-item';
