d92dec8ecdc67064d3df9e2619bde1f8bf5d1662
RasmusKjeldgaard <rkk@bankdata.dk>
2023-12-13 23:00:36 +0100

    Improve readability

--- libs/designsystem/form-field/src/form-field.component.ts
+++ libs/designsystem/form-field/src/form-field.component.ts
@@ -89,13 +89,13 @@ export class FormFieldComponent
   }
 
   public focus() {
+    if (!this.inputElement) return;
+
     /*
      * This timeout ensures that any previous manipulation of inputElement
      * (e.g. setting disabled state) has been synced to the DOM before trying to focus.
      */
     setTimeout(() => {
-      if (!this.inputElement) return;
-
       if (this.isTouch) {
         // Trigger Ionic's input shims to ensure input is scrolled into view.
         // See: https://github.com/ionic-team/ionic-framework/blob/master/core/src/utils/input-shims/hacks/scroll-assist.ts
