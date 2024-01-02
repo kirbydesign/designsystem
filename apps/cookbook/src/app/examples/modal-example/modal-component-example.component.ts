29de12db66c4cf89e6194e8395094b12dbff9712
RasmusKjeldgaard <rkk@bankdata.dk>
2024-01-02 16:10:50 +0100

    Simplify modal component example

--- apps/cookbook/src/app/examples/modal-example/modal-component-example.component.ts
+++ apps/cookbook/src/app/examples/modal-example/modal-component-example.component.ts
@@ -1,99 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-
-import { ModalConfig } from '@kirbydesign/designsystem';
-
-import { ModalSizeOption } from './modal-example-configuration/modal-example-size-selector.component';
+import { Component } from '@angular/core';
 
 const config = {
   selector: 'cookbook-modal-component-example',
-  template: `
-<button kirby-button size="lg" (click)="openModal()">Show modal</button>
+  template: `<button kirby-button size="lg" id="open-modal">Show modal</button>
 
 <kirby-modal
-  (willDismiss)="closeModal()"
-  [open]="open"
-  [config]="config"
+  trigger="open-modal"
+  flavor="compact"
 >
 
   <ng-template>
-    <kirby-page-progress>
-      <kirby-progress-circle themeColor="warning" value="50" size="sm" class="kirby-text-xsmall">
-        2/4
-      </kirby-progress-circle>
-    </kirby-page-progress>
-
-    <kirby-page-title>Hello there</kirby-page-title>
+    <kirby-page-title>Modal Component</kirby-page-title>
     
-    <p>hey</p>
-    <p>hey</p>
-    <p>hey</p>
-    <p>hey</p>
-    <p>hey</p>
-    <p>hey</p>
-    <p>hey</p>
-    <p>hey</p>
-    <p>hey</p>
-    <p>hey</p>
-
-    <kirby-modal-footer>
-      <button kirby-button attentionLevel="3">
-        <kirby-icon name="arrow-back"></kirby-icon>
-      </button>
-
-      <button kirby-button>Finish</button>
-    </kirby-modal-footer>
-    
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+      ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+      voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+    </p>
   </ng-template>
 </kirby-modal>
-
-<kirby-card>
-  <kirby-card-header>
-    <strong>Size of modal/drawer</strong><br />
-    <em>(on screens larger than 768px)</em>
-  </kirby-card-header>
-  <cookbook-modal-example-size-selector (sizeChange)="sizeChange($event)"></cookbook-modal-example-size-selector>
-</kirby-card>
 `,
-  showModalCodeSnippet: `openModal() {
-  this.open = true;
-}
-
-closeModal() {
-  this.open = false;
-}`,
 };
 @Component({
   selector: config.selector,
   template: config.template,
   styleUrls: ['./modal-component-example.component.scss'],
 })
-export class ModalComponentExampleComponent implements OnInit {
-  static readonly template = config.template.split('<kirby-card')[0]; // Remove config part of the template
-  static readonly showModalCodeSnippet = config.showModalCodeSnippet;
-
-  open: boolean = false;
-  config: ModalConfig;
-
-  ngOnInit(): void {
-    this.config = {
-      flavor: 'modal',
-      collapseTitle: true,
-    };
-  }
-
-  sizeChange(size: ModalSizeOption) {
-    this.config.size = size.value;
-  }
-
-  openModal() {
-    this.open = true;
-  }
-
-  closeModal() {
-    this.open = false;
-  }
-
-  drawerSupplementaryAction: {
-    iconName: 'qr';
-  };
+export class ModalComponentExampleComponent {
+  static readonly template = config.template;
 }
