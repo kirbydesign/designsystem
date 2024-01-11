9326e513148f6b7bbe2c98f5161d8f7d60afc5f4
RasmusKjeldgaard <rkk@bankdata.dk>
2024-01-11 09:51:17 +0100

    Improve readability

--- libs/designsystem/modal/src/modal/services/modal.helper.ts
+++ libs/designsystem/modal/src/modal/services/modal.helper.ts
@@ -127,7 +127,8 @@ export class ModalHelper {
      * is set to false we manually focus it, but only if focus is not already inside the modal
      * See here: https://github.com/ionic-team/ionic-framework/issues/28775#issuecomment-1875475739
      */
-    if (!ionModal.contains(document.activeElement)) {
+    const focussedElementWithinModal = ionModal.contains(document.activeElement);
+    if (!focussedElementWithinModal) {
       ionModal.focus();
     }
 
