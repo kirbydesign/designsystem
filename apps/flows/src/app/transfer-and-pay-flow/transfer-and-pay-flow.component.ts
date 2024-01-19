4c87babb5258ff8c8ef3dd3cf0e5506838240e3e
RasmusKjeldgaard <rkk@bankdata.dk>
2024-01-19 10:52:15 +0100

    Fix flows eslint errors

--- apps/flows/src/app/transfer-and-pay-flow/transfer-and-pay-flow.component.ts
+++ apps/flows/src/app/transfer-and-pay-flow/transfer-and-pay-flow.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ToastConfig, ToastController } from '@kirbydesign/designsystem';
 
 @Component({
