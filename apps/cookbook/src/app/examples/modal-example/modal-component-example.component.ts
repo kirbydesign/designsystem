7d812d37eb942b1d961d1afe40a6783f6b3d27e3
RasmusKjeldgaard <rkk@bankdata.dk>
2024-01-29 13:21:15 +0100

    Reduce modal component example

--- apps/cookbook/src/app/examples/modal-example/modal-component-example.component.ts
+++ apps/cookbook/src/app/examples/modal-example/modal-component-example.component.ts
@@ -1,17 +1,14 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 const config = {
   selector: 'cookbook-modal-component-example',
   template: `<button kirby-button size="lg" id="open-modal">Show modal</button>
 
-<kirby-modal
-  trigger="open-modal"
-  flavor="compact"
->
+<kirby-modal trigger="open-modal">
 
   <ng-template>
     <kirby-page-title>Modal Component</kirby-page-title>
-    
+
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
       ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
@@ -19,6 +16,11 @@ const config = {
       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
     </p>
+
+
+    <kirby-modal-footer>
+      <button kirby-button>Button in footer</button>
+    </kirby-modal-footer>
   </ng-template>
 </kirby-modal>
 `,
@@ -27,6 +29,7 @@ const config = {
   selector: config.selector,
   template: config.template,
   styleUrls: ['./modal-component-example.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ModalComponentExampleComponent {
   static readonly template = config.template;
