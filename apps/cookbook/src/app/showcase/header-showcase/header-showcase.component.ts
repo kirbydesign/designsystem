c63832dbc9e4aff142209807a56e1397be5362ab
AgreSanGit <agre88@hotmail.com>
2024-01-30 13:41:09 +0100

    Update Header directives

--- apps/cookbook/src/app/showcase/header-showcase/header-showcase.component.ts
+++ apps/cookbook/src/app/showcase/header-showcase/header-showcase.component.ts
@@ -100,7 +100,7 @@ export class HeaderShowcaseComponent {
     {
       name: '*kirbyHeaderCustomFlagSection',
       description:
-        'For custom content to be shown in the flag section the `*kirbyHeaderCustomFlagSection` directive can be applied to any `div` element.\n\n**Please note: You should not have Custom Content in the flag section and Kirby-flag present at the same time.',
+        'For custom content to be shown in the flag section the `*kirbyHeaderCustomFlagSection` directive can be applied to any host or container element.\n\n**Please note: You should not have Custom Content in the flag section and Kirby-flag present at the same time.',
     },
   ];
 
