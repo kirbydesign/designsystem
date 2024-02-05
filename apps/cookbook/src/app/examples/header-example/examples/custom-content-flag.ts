c99c592a09f4a8dd010064ab623b2a7798e926f0
AgreSanGit <112690169+AgreSanGit@users.noreply.github.com>
2024-02-05 15:27:03 +0100

    Update apps/cookbook/src/app/examples/header-example/examples/custom-content-flag.ts
    Co-authored-by: RasmusKjeldgaard <rkk@bankdata.dk>

--- apps/cookbook/src/app/examples/header-example/examples/custom-content-flag.ts
+++ apps/cookbook/src/app/examples/header-example/examples/custom-content-flag.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 const config = {
   selector: 'cookbook-header-example-custom-content-flag',
   template: `<kirby-header [title]="'Title'" subtitle1="Subtitle one" subtitle2="Subtitle two">
-  <div class="custom-section" *kirbyHeaderCustomFlagSection>
+  <div class="custom-section" *kirbyHeaderCustomFlag>
   <kirby-badge size="sm" themeColor="success"></kirby-badge> Custom content in flag section
 </div>
 </kirby-header>`,
