55c5ca8a7c8c38ffd1b850654bdfdb1201790d8d
AgreSanGit <112690169+AgreSanGit@users.noreply.github.com>
2024-02-06 20:59:27 +0100

    Update apps/cookbook/src/app/examples/header-example/examples/custom-content-flag.ts
    Co-authored-by: RasmusKjeldgaard <rkk@bankdata.dk>

--- apps/cookbook/src/app/examples/header-example/examples/custom-content-flag.ts
+++ apps/cookbook/src/app/examples/header-example/examples/custom-content-flag.ts
@@ -4,8 +4,8 @@ const config = {
   selector: 'cookbook-header-example-custom-flag',
   template: `<kirby-header [title]="'Title'" subtitle1="Subtitle one" subtitle2="Subtitle two">
   <div class="custom-flag" *kirbyHeaderCustomFlag>
-  <kirby-badge size="sm" themeColor="success"></kirby-badge> Custom content in flag section
-</div>
+    <kirby-badge size="sm" themeColor="success"></kirby-badge> Custom content in flag section
+  </div>
 </kirby-header>`,
   styles: [
     `.custom-flag {
