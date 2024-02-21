2a7fb6d26b580a5e18af83ec08e72d0166899ac6
Jakob Engelbrecht <jakob@basher.dk>
2024-02-21 15:48:59 +0100

    test: fix flaky test

--- libs/designsystem/helpers/scss/src/link.spec.ts
+++ libs/designsystem/helpers/scss/src/link.spec.ts
@@ -3,24 +3,27 @@ import { DesignTokenHelper } from '@kirbydesign/designsystem/helpers';
 const { getColor } = DesignTokenHelper;
 
 describe('Anchor tag', () => {
-  let element: HTMLElement;
+  let element: HTMLAnchorElement;
+
+  beforeEach(() => {
+    element = document.createElement('a');
+    element.id = 'fixture';
+    element.href = '/test/';
+    element.text = 'Text';
+    element.style.transitionDuration = '0ms';
+    document.body.appendChild(element);
+  });
 
   afterEach(() => {
     document.body.removeChild(element);
   });
-  describe(`by default`, () => {
-    beforeEach(() => {
-      const fixture = `<a href="/test/" id="fixture">Text</a>`;
-      document.body.insertAdjacentHTML('afterbegin', fixture);
-      console.log('document.body.style.color - BEFORE:', document.body.style.color);
-      document.body.style.color = getColor('black').value;
-      console.log('document.body.style.color - AFTER:', document.body.style.color);
-      element = document.body.querySelector('#fixture');
-    });
 
+  describe(`by default`, () => {
     it(`should inherit its color`, () => {
-      expect(document.body).toHaveComputedStyle({ color: getColor('black') });
+      document.body.style.color = getColor('black').value;
+
       expect(element).toHaveComputedStyle({ color: getColor('black') });
+      document.body.style.removeProperty('color');
     });
 
     it(`should be styled with an underline`, () => {
@@ -33,14 +36,9 @@ describe('Anchor tag', () => {
   });
 
   describe(`with class for link-icon applied`, () => {
-    beforeEach(() => {
-      const fixture = `<a href="/test/" class="kirby-external-icon" id="fixture">Text</a>`;
-      document.body.insertAdjacentHTML('afterbegin', fixture);
-      element = document.body.querySelector('#fixture');
-    });
-
     it(`should have a link icon`, () => {
       const baseURI = window.document.baseURI;
+      element.className = 'kirby-external-icon';
 
       expect(element).toHaveComputedStyle({
         'background-image': `url("${baseURI}assets/kirby/icons/svg/link.svg")`,
