d5af2aee2f0e4c2dc248890025538411378eefc6
Troels Lenda Strand <tsd@bankdata.dk>
2024-02-28 11:33:38 +0100

    update cookbook implementation

--- apps/cookbook/src/app/examples/slides-example/slides-simple-example/slides-simple-example.component.ts
+++ apps/cookbook/src/app/examples/slides-example/slides-simple-example/slides-simple-example.component.ts
@@ -5,7 +5,6 @@ import { ToastConfig, ToastController } from '@kirbydesign/designsystem/toast';
 @Component({
   styleUrls: ['../slides-example.shared.scss'],
   templateUrl: './slides-simple-example.component.html',
-  styles: `kirby-card {height: 100%;justify-content: start}`,
 })
 export class SlidesSimpleExampleComponent {
   constructor(private toastController: ToastController) {}
@@ -14,6 +13,8 @@ export class SlidesSimpleExampleComponent {
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce rhoncus leo quis libero posuere auctor. Quisque ornare lectus finibus tellus sollicitudin, et blandit quam semper. Ut sed lacus eget dui blandit consequat. Nam commodo sit amet augue vel dapibus. Mauris tincidunt nulla eget porttitor euismod. Ut at massa massa. Curabitur suscipit ullamcorper felis, vitae tincidunt eros varius in. Duis et tellus eu turpis varius dictum. Mauris mattis posuere ligula nec pharetra. Vestibulum a augue at nulla elementum fringilla. Duis vehicula finibus turpis, vel dignissim magna ullamcorper vitae. Nam vel elit orci.';
 
   randomIntegerBetween = (max, min) => Math.floor(Math.random() * (max - min + 1) + min);
+
+  stretch = false;
 
   slides = [...Array(9).keys()].map((number) => ({
     title: `Slide ${number + 1}`,
@@ -28,5 +29,8 @@ export class SlidesSimpleExampleComponent {
       durationInMs: 1000,
     };
     this.toastController.showToast(config);
+  }
+  toggleStretch() {
+    this.stretch = !this.stretch;
   }
 }
