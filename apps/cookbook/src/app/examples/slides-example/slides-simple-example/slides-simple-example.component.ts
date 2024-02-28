6c450f4e8fdcb84b05ed5f69034ee0b9c20e064e
Troels Lenda Strand <tsd@bankdata.dk>
2024-02-28 22:24:22 +0100

    Cleanup example with faker-js

--- apps/cookbook/src/app/examples/slides-example/slides-simple-example/slides-simple-example.component.ts
+++ apps/cookbook/src/app/examples/slides-example/slides-simple-example/slides-simple-example.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { SelectedSlide } from '@kirbydesign/designsystem/slide';
 import { ToastConfig, ToastController } from '@kirbydesign/designsystem/toast';
+import { faker } from '@faker-js/faker';
 
 @Component({
   styleUrls: ['../slides-example.shared.scss'],
@@ -9,17 +10,12 @@ import { ToastConfig, ToastController } from '@kirbydesign/designsystem/toast';
 export class SlidesSimpleExampleComponent {
   constructor(private toastController: ToastController) {}
 
-  lorem =
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce rhoncus leo quis libero posuere auctor. Quisque ornare lectus finibus tellus sollicitudin, et blandit quam semper. Ut sed lacus eget dui blandit consequat. Nam commodo sit amet augue vel dapibus. Mauris tincidunt nulla eget porttitor euismod. Ut at massa massa. Curabitur suscipit ullamcorper felis, vitae tincidunt eros varius in. Duis et tellus eu turpis varius dictum. Mauris mattis posuere ligula nec pharetra. Vestibulum a augue at nulla elementum fringilla. Duis vehicula finibus turpis, vel dignissim magna ullamcorper vitae. Nam vel elit orci.';
-
-  randomIntegerBetween = (max, min) => Math.floor(Math.random() * (max - min + 1) + min);
-
   stretch = false;
 
   slides = [...Array(9).keys()].map((number) => ({
     title: `Slide ${number + 1}`,
     subtitle: `Subtitle ${number + 1}`,
-    cardContent: this.lorem.split(' ').slice(0, this.randomIntegerBetween(6, 12)).join(' '),
+    cardContent: faker.lorem.lines({ min: 1, max: 4 }),
   }));
 
   getDataFromActiveSlide(selectedSlide: SelectedSlide) {
