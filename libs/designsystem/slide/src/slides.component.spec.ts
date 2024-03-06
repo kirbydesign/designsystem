f19316a8ced4fb70d4445e31d93faaeef019a1db
Troels Lenda Strand <tsd@bankdata.dk>
2024-03-06 12:31:33 +0100

    Add a few tests

--- libs/designsystem/slide/src/slides.component.spec.ts
+++ libs/designsystem/slide/src/slides.component.spec.ts
@@ -6,6 +6,7 @@ import { CardModule } from '@kirbydesign/designsystem/card';
 import { DesignTokenHelper } from '@kirbydesign/designsystem/helpers';
 import { KirbySwiperOptions, SlidesComponent } from './slides.component';
 import { SlideDirective } from './slide.directive';
+import { slideStretchHeightDirective } from './slide-stretch-height.directive';
 
 const { getColor } = DesignTokenHelper;
 
@@ -28,38 +29,38 @@ describe('SlidesComponent', () => {
   const createHost = createHostFactory({
     component: SlidesComponent,
     imports: [TestHelper.ionicModuleForTest, ButtonComponent, IconModule, CardModule],
-    declarations: [SlideDirective],
+    declarations: [SlideDirective, slideStretchHeightDirective],
   });
 
-  beforeEach(() => {
-    spectator = createHost(
-      `<kirby-slides [slides]="slides" [slidesOptions]="slidesOptions" [title]="'Title'" [showNavigation]="true">
-         <kirby-card *kirbySlide="let slide">
-           {{ slide }}
-         </kirby-card>
-       </kirby-slides>`,
-      {
-        hostProps: {
-          slides: [0, 1, 2, 3, 4],
-          slidesOptions: customOptions,
-        },
-      }
-    );
-  });
+  const hostTemplate = `<kirby-slides [slides]="slides" [slidesOptions]="slidesOptions" [title]="'Title'" [showNavigation]="true">
+                          <kirby-card *kirbySlide="let slide">
+                            {{ slide }}
+                          </kirby-card>
+                        </kirby-slides>`;
+  const props = {
+    hostProps: {
+      slides: [0, 1, 2, 3, 4],
+      slidesOptions: customOptions,
+    },
+  };
 
   it('should create', () => {
+    spectator = createHost(hostTemplate, props);
     expect(spectator.component).toBeTruthy();
   });
 
   it('should have correct title', () => {
+    spectator = createHost(hostTemplate, props);
     expect(spectator.query('.kirby-text-medium')).toHaveExactTrimmedText('Title');
   });
 
   it('should contain 5 slides', () => {
+    spectator = createHost(hostTemplate, props);
     expect(spectator.queryAll('swiper-slide')).toHaveLength(5);
   });
 
   it('should update the active slide index, when calling slideTo ', () => {
+    spectator = createHost(hostTemplate, props);
     expect(spectator.component.swiperContainer.nativeElement.swiper.activeIndex).toBe(0);
 
     spectator.component.slideTo(2);
@@ -68,6 +69,7 @@ describe('SlidesComponent', () => {
   });
 
   it('should emit slideChange when the active slide is changed', () => {
+    spectator = createHost(hostTemplate, props);
     const slideChangeSpy = spyOn(spectator.component.slideChange, 'emit');
 
     expect(slideChangeSpy).not.toHaveBeenCalled();
@@ -78,6 +80,7 @@ describe('SlidesComponent', () => {
   });
 
   it('should disable the previous button, when the active slide is the first slide', () => {
+    spectator = createHost(hostTemplate, props);
     const previousButton = spectator.query('.swiper-button-prev');
 
     expect(previousButton.classList).toContain('swiper-button-disabled');
@@ -90,6 +93,7 @@ describe('SlidesComponent', () => {
   });
 
   it('should disable the next button, when the active slide is the last slide', () => {
+    spectator = createHost(hostTemplate, props);
     const nextButton = spectator.query('.swiper-button-next');
 
     expect(nextButton.classList).not.toContain('swiper-button-disabled');
@@ -102,6 +106,7 @@ describe('SlidesComponent', () => {
   });
 
   it('should hide the navigation buttons, if the Slider doesn´t contain enough slides for sliding', () => {
+    spectator = createHost(hostTemplate, props);
     spectator.setInput('slides', [0]);
 
     // It is necessary to update the Slides container, because it does not updated automatically when the "slides" input changes
@@ -115,6 +120,7 @@ describe('SlidesComponent', () => {
     let paginationDots;
 
     beforeEach(() => {
+      spectator = createHost(hostTemplate, props);
       paginationDots = spectator.queryAll('.swiper-pagination-bullet');
     });
 
@@ -159,6 +165,7 @@ describe('SlidesComponent', () => {
 
   describe('when showNavigation=false', () => {
     beforeEach(() => {
+      spectator = createHost(hostTemplate, props);
       spectator.component.showNavigation = false;
       spectator.detectComponentChanges();
     });
@@ -187,6 +194,7 @@ describe('SlidesComponent', () => {
     });
 
     it('should hide the navigation buttons', () => {
+      spectator = createHost(hostTemplate, props);
       expect(spectator.query('.swiper-button-prev')).toBeHidden();
       expect(spectator.query('.swiper-button-next')).toBeHidden();
     });
@@ -194,6 +202,7 @@ describe('SlidesComponent', () => {
 
   describe('on touch device', () => {
     beforeEach(() => {
+      spectator = createHost(hostTemplate, props);
       spectator.component._isTouch = true;
       spectator.detectComponentChanges();
     });
@@ -206,6 +215,65 @@ describe('SlidesComponent', () => {
     it('should render the pagination buttons', () => {
       expect(spectator.query('.pagination')).toBeTruthy();
       expect(spectator.query('.pagination')).toBeVisible();
+    });
+  });
+
+  const stretchProps = {
+    hostProps: {
+      slides: [
+        { title: 'Title', height: 50, content: 0 },
+        { title: 'Title', height: 150, content: 1 },
+        { title: 'Title', height: 40, content: 2 },
+        { title: 'Title', height: 25, content: 3 },
+        { title: 'Title', height: 50, content: 4 },
+      ],
+      slidesOptions: customOptions,
+    },
+  };
+
+  describe('with slideStretchHeight directive', () => {
+    it('slides should have equal height', () => {
+      spectator = createHost(
+        `<kirby-slides [slides]="slides" [slidesOptions]="slidesOptions" [title]="'Title'" [showNavigation]="true">
+        <kirby-card *kirbySlide="let slide" slideStretchHeight>
+        <kirby-card-header [title]="slide.title" [subtitle]="slide.subtitle"></kirby-card-header>
+        <div class="card-content" [style.height.px]="slide.height">
+          {{slide.content}}
+        </div>
+        </kirby-card>
+      </kirby-slides>`,
+        stretchProps
+      );
+      const slides = spectator.queryAll('swiper-slide');
+      expect(
+        slides.every(
+          (slide) =>
+            window.getComputedStyle(slide).height === window.getComputedStyle(slides[0]).height
+        )
+      ).toBeTruthy();
+    });
+  });
+  describe('without slideStretchHeight directive', () => {
+    it('slides should not have equal height', () => {
+      spectator = createHost(
+        `<kirby-slides [slides]="slides" [slidesOptions]="slidesOptions" [title]="'Title'" [showNavigation]="true">
+          <kirby-card *kirbySlide="let slide">
+          <kirby-card-header [title]="slide.title" [subtitle]="slide.subtitle"></kirby-card-header>
+          <div class="card-content" [style.height.px]="slide.height">
+            {{slide.content}}
+          </div>
+          </kirby-card>
+        </kirby-slides>`,
+        stretchProps
+      );
+
+      const slides = spectator.queryAll('swiper-slide');
+      expect(
+        slides.every(
+          (slide) =>
+            window.getComputedStyle(slide).height === window.getComputedStyle(slides[0]).height
+        )
+      ).toBeFalsy();
     });
   });
 });
