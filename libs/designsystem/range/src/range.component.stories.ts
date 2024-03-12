42c11d8692d93189799d9a84691ef63edf9e1b3b
Rasmus Sundgaard Reimer <rasmus.reimer@gmail.com>
2024-03-12 10:10:18 +0100

    fix range picker story

--- libs/designsystem/range/src/range.component.stories.ts
+++ libs/designsystem/range/src/range.component.stories.ts
@@ -1,10 +1,17 @@
-import type { Meta, StoryObj } from '@storybook/angular';
+import { applicationConfig, type Meta, type StoryObj } from '@storybook/angular';
 
+import { importProvidersFrom } from '@angular/core';
+import { KirbyIonicModule } from '@kirbydesign/designsystem/kirby-ionic-module';
 import { RangeComponent } from './range.component';
 
 const meta: Meta<RangeComponent> = {
   component: RangeComponent,
   title: 'RangeComponent',
+  decorators: [
+    applicationConfig({
+      providers: [importProvidersFrom(KirbyIonicModule)],
+    }),
+  ],
 };
 export default meta;
 type Story = StoryObj<RangeComponent>;
@@ -13,6 +20,8 @@ export const Default: Story = {
   args: {
     value: 0,
     step: 1,
+    min: 0,
+    max: 100,
     minLabel: '',
     maxLabel: '',
     debounce: 0,
