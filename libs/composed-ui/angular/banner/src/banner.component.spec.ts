d61f67574f85033611bede37f2579b1fcda2b45a
RasmusKjeldgaard <rkk@bankdata.dk>
2024-05-15 11:14:52 +0200

    Add banner component

---
+++ libs/composed-ui/angular/banner/src/banner.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BannerComponent } from './banner.component';
+
+describe('BannerComponent', () => {
+  let component: BannerComponent;
+  let fixture: ComponentFixture<BannerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [BannerComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(BannerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ libs/composed-ui/angular/banner/src/banner.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'kirby-banner',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './banner.component.html',
+  styleUrl: './banner.component.scss',
+})
+export class BannerComponent {}
