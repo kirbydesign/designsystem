d52edf5f240f424e78499482e6d5c119811c99d2
RasmusKjeldgaard <rkk@bankdata.dk>
2024-05-21 14:11:50 +0200

    Add tag name prefix

--- libs/composed-ui/angular/banner/src/banner.component.ts
+++ libs/composed-ui/angular/banner/src/banner.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'kirby-banner',
+  selector: 'kirby-x-banner',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './banner.component.html',
