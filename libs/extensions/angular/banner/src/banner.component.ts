36c4afe0debf86562af3f920a56dd39cf811b02f
RasmusKjeldgaard <rkk@bankdata.dk>
2024-05-22 13:47:12 +0200

    WIP: designsystem dist output

--- libs/extensions/angular/banner/src/banner.component.ts
+++ libs/extensions/angular/banner/src/banner.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CardModule } from '@kirbydesign/designsystem';
 
 @Component({
   selector: 'kirby-x-banner',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CardModule],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.scss',
 })
