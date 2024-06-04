d4afa904e948083fbdf318d92604936c83b0b88a
RasmusKjeldgaard <rkk@bankdata.dk>
2024-06-04 13:22:03 +0200

    Create empty primary entry point to force import from secondary entries

--- libs/extensions/angular/index.ts
+++ libs/extensions/angular/index.ts
@@ -1,1 +1,4 @@
-export * from '@kirbydesign/extensions-angular/banner';
+//  All components should be imported through their individual entry-points.
+// This file and empty export is needed to satisfy ng_packagr.
+// This workaround is inspired by Material UI https://github.com/angular/components/blob/main/src/material/index.ts
+export default {};
