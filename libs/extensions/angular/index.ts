da7194ec044c460d31f3042b00f6765b7c15858a
RasmusKjeldgaard <rkk@bankdata.dk>
2024-06-04 15:33:46 +0200

    Fix typos
    Co-authored-by: Jakob Engelbrecht <jakob@basher.dk>

--- libs/extensions/angular/index.ts
+++ libs/extensions/angular/index.ts
@@ -1,4 +1,4 @@
 //  All components should be imported through their individual entry-points.
 // This file and empty export is needed to satisfy ng_packagr.
-// This workaround is inspired by Material UI https://github.com/angular/components/blob/main/src/material/index.ts
+// This workaround is inspired by Material Design https://github.com/angular/components/blob/main/src/material/index.ts
 export default {};
