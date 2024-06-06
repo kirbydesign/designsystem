f0b70b3d34ec75cda9d7a2714b123819d895088d
hjorte-bankdata <ehj@bankdata.dk>
2024-06-06 06:50:25 +0000

    Add real banner component

--- libs/extensions/angular/banner/src/banner.component.stories.ts
+++ libs/extensions/angular/banner/src/banner.component.stories.ts
@@ -12,15 +12,26 @@ export default meta;
 type Story = StoryObj<BannerComponent>;
 
 /**
- * By default, a banner will render as a card with a header, and a placeholder text.
+ * By default, a banner will render as a card with a header, image, body, action button and dismiss button.
  */
-export const Default: Story = {};
+export const Default: Story = {
+  args: {
+    title: 'My amazing title',
+    bodyText: 'This is a really good body text for use with a banner',
+    actionButtonText: 'Go somewhere!',
+    imagePath: 'https://placehold.co/2000x1000/333/EEEEEE',
+  },
+};
 
 /**
- * If the title-property is set a header with the title is added to the card within the banner.
+ * If the image filter is set to dark, the banner darkens the blurred backround image.
  */
-export const Title: Story = {
+export const DarkImageFilter: Story = {
   args: {
-    title: 'Example title',
+    title: 'Another amazing title',
+    bodyText: 'This is a really good body text for use with a banner',
+    imagePath: 'https://placehold.co/2000x1000/333/EEEEEE',
+    actionButtonText: 'Go!',
+    imageFilter: 'dark',
   },
 };
--- libs/extensions/angular/banner/src/banner.component.ts
+++ libs/extensions/angular/banner/src/banner.component.ts
@@ -1,11 +1,13 @@
-import { CommonModule } from '@angular/common';
+import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { CardModule } from '@kirbydesign/designsystem/card';
+import { ButtonComponent } from '@kirbydesign/designsystem/button';
+import { IconModule } from '@kirbydesign/designsystem/icon';
 
 @Component({
   selector: 'kirby-x-banner',
   standalone: true,
-  imports: [CommonModule, CardModule],
+  imports: [CardModule, ButtonComponent, IconModule, NgClass],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.scss',
 })
@@ -14,4 +16,29 @@ export class BannerComponent {
    * The title placed inside the banners header.
    */
   @Input() title: string | undefined;
+
+  /**
+   * The image shown on the banner, also used for background blur effect.
+   */
+  @Input() imagePath: string | undefined;
+
+  /**
+   * The body text placed inside the banners main area.
+   */
+  @Input() bodyText: string | undefined;
+
+  /**
+   * The text used for the CTA button.
+   */
+  @Input() actionButtonText: string | undefined;
+
+  /**
+   * The link used on the CTA button.
+   */
+  @Input() actionButtonLink: string | undefined;
+
+  /**
+   * The filter used for the blurred background image.
+   */
+  @Input() imageFilter: 'dark' | 'white' = 'white';
 }
