55ec357f28c5b2f4b645598e048c9794a221160a
Emil Hjort <ehj@bankdata.dk>
2024-06-18 21:35:46 +0200

    Add current files

--- libs/extensions/angular/banner/src/banner.component.stories.ts
+++ libs/extensions/angular/banner/src/banner.component.stories.ts
@@ -12,15 +12,26 @@ export default meta;
 type Story = StoryObj<BannerComponent>;
 
 /**
- * By default, a banner will render as a card with a header, and a placeholder text.
+ * By default, a banner will render as a card with a header, image, body, action button and dismiss button.
  */
-export const Default: Story = {};
+export const Default: Story = {
+  args: {
+    title: 'My amazing title',
+    bodyText: 'This is a really good body text for use with a banner',
+    actionButtonText: 'Go somewhere!',
+    imagePath: 'https://images.unsplash.com/photo-1515876305430-f06edab8282a',
+  },
+};
 
 /**
- * If the title-property is set a header with the title is added to the card within the banner.
+ * If the image filter is set to dark, the banner darkens the blurred backround image.
  */
-export const Title: Story = {
+export const DarkImageFilter: Story = {
   args: {
-    title: 'Example title',
+    title: 'Another amazing title',
+    bodyText: 'This is a really good body text for use with a banner',
+    imagePath: 'https://images.unsplash.com/photo-1515876305430-f06edab8282a',
+    actionButtonText: 'Go!',
+    imageFilter: 'dark',
   },
 };
