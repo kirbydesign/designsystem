78e095280a638b0f434acc901b8bd7c482f45883
Jakob Engelbrecht <jakob@basher.dk>
2024-07-04 17:07:40 +0200

    feat: add badge angular proxy component

--- libs/angular/badge/src/badge.component.ts
+++ libs/angular/badge/src/badge.component.ts
@@ -1,1 +1,40 @@
-export { BadgeComponent } from '@kirbydesign/designsystem/badge';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, Component, ElementRef, Input, NgZone } from '@angular/core';
+import { BadgeSize, BadgeVariant, KirbyBadge } from '@kirbydesign/core'; //TODO Should be '@kirbydesign/core/badge'
+
+@Component({
+  selector: 'kirby-badge',
+  template: '<ng-content></ng-content>',
+  standalone: true,
+  imports: [CommonModule],
+})
+export class BadgeComponent {
+  constructor(elementRef: ElementRef<KirbyBadge>, cdr: ChangeDetectorRef, private zone: NgZone) {
+    this.el = elementRef.nativeElement;
+    cdr.detach();
+  }
+
+  private el: KirbyBadge;
+
+  //TODO: Should we deprecate `text` prop?
+  get text(): string {
+    return this.el.innerText;
+  }
+  @Input() set text(value: string) {
+    this.zone.runOutsideAngular(() => (this.el.innerText = value));
+  }
+
+  get size(): BadgeSize {
+    return this.el.size;
+  }
+  @Input() set size(value: BadgeSize) {
+    this.zone.runOutsideAngular(() => (this.el.size = value));
+  }
+
+  get themeColor(): BadgeVariant {
+    return this.el.themeColor;
+  }
+  @Input() set themeColor(value: BadgeVariant) {
+    this.zone.runOutsideAngular(() => (this.el.themeColor = value));
+  }
+}
