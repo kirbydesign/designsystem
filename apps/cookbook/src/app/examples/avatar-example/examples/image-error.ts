c1abae970bc3c2dab3f2761fa07d816034c505cb
Søren Oest Balmer <oestbalmer@gmail.com>
2024-08-06 11:01:00 +0200

    show ts code in showcase

--- apps/cookbook/src/app/examples/avatar-example/examples/image-error.ts
+++ apps/cookbook/src/app/examples/avatar-example/examples/image-error.ts
@@ -2,9 +2,21 @@ import { Component } from '@angular/core';
 
 const config = {
   selector: 'cookbook-avatar-example-image-error',
-  template: `<kirby-avatar imageSrc="very-bad-src-path.png" (imageError)="showFallbackImageOnError($event)" size="lg"></kirby-avatar>`,
-};
+  template: `<kirby-avatar
+  [imageSrc]="avatarSrc"
+  (imageError)="showFallbackImageOnError($event)"
+  size="lg"
+></kirby-avatar>`,
+  ts: `avatarSrc: string = 'bad-image-url.png';
 
+showFallbackImageOnError(event: ErrorEvent) {
+  const fallbackImageSrc =
+                'assets/images/avatar-not-found.png';
+  this.avatarSrc = fallbackImageSrc;
+  // Possibly do something with the ErrorEvent...
+}
+  `,
+};
 @Component({
   selector: config.selector,
   template: config.template,
@@ -12,8 +24,12 @@ const config = {
 })
 export class AvatarExampleImageErrorComponent {
   template: string = config.template;
+  ts: string = config.ts;
+  avatarSrc: string = 'bad-image-url.png';
 
   showFallbackImageOnError(event: ErrorEvent) {
-    (event.target as HTMLImageElement).src = '/assets/images/avatar-not-found.png';
+    const fallbackImageSrc = 'assets/images/avatar-not-found.png';
+    this.avatarSrc = fallbackImageSrc;
+    // Possibly do something with the ErrorEvent...
   }
 }
