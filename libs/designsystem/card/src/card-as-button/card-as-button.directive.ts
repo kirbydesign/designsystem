64de325c4dd74d978bfd5de6f595308c39ec79cd
RasmusKjeldgaard <rkk@bankdata.dk>
2024-08-08 15:38:28 +0200

    Make anchor focusable instead of card when external link is provided

--- libs/designsystem/card/src/card-as-button/card-as-button.directive.ts
+++ libs/designsystem/card/src/card-as-button/card-as-button.directive.ts
@@ -1,4 +1,12 @@
-import { Directive, ElementRef, HostBinding, HostListener, Optional } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  Input,
+  OnInit,
+  Optional,
+} from '@angular/core';
 
 import { CardComponent } from '../card.component';
 
@@ -6,24 +14,26 @@ import { CardComponent } from '../card.component';
   // eslint-disable-next-line @angular-eslint/directive-selector
   selector: 'kirby-card[click]',
 })
-export class CardAsButtonDirective {
+export class CardAsButtonDirective implements OnInit {
+  @Input() interactionStatesOnly = false;
+
   @HostBinding('attr.role') role: string = 'button';
-  @HostBinding('attr.tabindex') tabindex: number = 0;
+  @HostBinding('attr.tabindex') tabindex: number;
 
   @HostBinding('class.interaction-state-active') _pressed = false;
-  constructor(@Optional() private card: CardComponent, private clickableElement: ElementRef) {}
 
   @HostListener('keydown.space', ['$event'])
   @HostListener('keydown.enter', ['$event'])
   _onKeydownHandler(event: KeyboardEvent) {
     this._pressed = true;
     this.clickableElement.nativeElement.click(event);
+
+    if (this.interactionStatesOnly) return;
     /*
      * Prevent default event from firing so the UA wont
      * catch this event and e.g. scroll the page on space,
      * which is the default behavior in major browsers
      */
-
     event.preventDefault();
   }
 
@@ -32,5 +42,11 @@ export class CardAsButtonDirective {
   @HostListener('blur')
   _onInactiveHandler() {
     this._pressed = false;
+  }
+
+  constructor(private clickableElement: ElementRef) {}
+
+  ngOnInit(): void {
+    this.tabindex = this.interactionStatesOnly ? -1 : 0;
   }
 }
