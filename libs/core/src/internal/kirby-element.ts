cce33cedea5076879b1b0f8239afcfb88c194713
Jakob Engelbrecht <jakob@basher.dk>
2024-08-19 13:12:21 +0200

    refactor: use type inference

--- libs/core/src/internal/kirby-element.ts
+++ libs/core/src/internal/kirby-element.ts
@@ -1,7 +1,7 @@
 import { LitElement } from 'lit';
 
 export default class KirbyElement extends LitElement {
-  static define(name: string = this.tagName, elementConstructor = this) {
+  static define(name = this.tagName, elementConstructor = this) {
     const currentlyRegisteredConstructor = customElements.get(name) as CustomElementConstructor;
     if (!currentlyRegisteredConstructor) {
       window.customElements.define(name, elementConstructor);
