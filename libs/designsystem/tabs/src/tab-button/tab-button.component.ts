d2efc28272dc592ba8d87684002f6e4acc51ebad
Jakob Engelbrecht <jakob@basher.dk>
2024-09-10 14:18:41 +0200

    Tabs: remove deprecated `routerLink` input (#3575)

--- libs/designsystem/tabs/src/tab-button/tab-button.component.ts
+++ libs/designsystem/tabs/src/tab-button/tab-button.component.ts
@@ -9,12 +9,6 @@ import { selectedTabClickEvent } from './tab-button.events';
   styleUrls: ['./tab-button.component.scss'],
 })
 export class TabButtonComponent {
-  /**
-   * @deprecated Using routerLink for defining routes for kirby-tab-button is deprecated,
-   * as it clashes with Angulars builtin routerLink directive and causes unexpected behavior in Angular 16
-   * and above, causing the tab stack to not be preserved. Use the tab input instead, as a direct replacement.
-   */
-  @Input() routerLink: string;
   @Input() tab: string;
   @Output() click = new EventEmitter<Event>();
   @ContentChildren(IconComponent) icons: QueryList<IconComponent>;
