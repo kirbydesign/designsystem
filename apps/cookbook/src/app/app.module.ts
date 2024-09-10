65e075a86dc04648b208809806ce0264ffe1229f
Jakob Engelbrecht <jakob@basher.dk>
2024-09-10 13:48:19 +0200

    Page Local Navigation: remove (deprecated) (#3574)

--- apps/cookbook/src/app/app.module.ts
+++ apps/cookbook/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { KirbyExperimentalModule, KirbyModule } from '@kirbydesign/designsystem';
+import { KirbyModule } from '@kirbydesign/designsystem';
 
 import { KirbyModalModule } from '@kirbydesign/designsystem/modal/v2';
 import { AppRoutingModule } from './app-routing.module';
@@ -41,7 +41,6 @@ registerLocaleData(localeData);
     HttpClientModule,
     FormsModule,
     KirbyModule,
-    KirbyExperimentalModule,
     KirbyModalModule,
   ],
 
--- apps/cookbook/src/app/examples/experimental-examples/components/index.ts
+++
@@ -1,1 +0,0 @@
-export { PageLocalNavigationExampleComponent } from './page-local-navigation/page-local-navigation-example.component';
