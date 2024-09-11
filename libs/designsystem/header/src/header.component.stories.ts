6b5be61ce3d6eff4a809509fe2db6be0df7e1bb3
RasmusKjeldgaard <rkk@bankdata.dk>
2024-09-11 16:51:49 +0200

    Add chromatic modes to header

--- libs/designsystem/header/src/header.component.stories.ts
+++ libs/designsystem/header/src/header.component.stories.ts
@@ -2,6 +2,7 @@ import { type Meta, moduleMetadata, type StoryObj } from '@storybook/angular';
 
 import { HeaderComponent, HeaderModule } from '@kirbydesign/designsystem/header';
 
+import { sharedChromaticModes } from 'tools/storybook-config/shared-parameters';
 import { HeaderExampleModule } from '~/app/examples/header-example/header-example.module';
 
 const meta: Meta<HeaderComponent> = {
@@ -12,6 +13,13 @@ const meta: Meta<HeaderComponent> = {
       imports: [HeaderModule, HeaderExampleModule],
     }),
   ],
+  parameters: {
+    chromatic: {
+      modes: {
+        ...sharedChromaticModes,
+      },
+    },
+  },
 };
 export default meta;
 type Story = StoryObj<HeaderComponent>;
