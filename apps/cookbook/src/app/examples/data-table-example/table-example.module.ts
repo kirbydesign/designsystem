2e71caf27dfa5b351341cac3f43354e891a7b562
Jakob Engelbrecht <jakob@basher.dk>
2024-09-11 08:07:57 +0200

    Data Table: remove deprecated `Table` component (#3576)

--- apps/cookbook/src/app/examples/data-table-example/table-example.module.ts
+++ apps/cookbook/src/app/examples/data-table-example/table-example.module.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { KirbyModule } from '@kirbydesign/designsystem';
+import { CardModule } from '@kirbydesign/designsystem/card';
+import { TableSortableComponent } from '@kirbydesign/designsystem/data-table';
 
 import { DataTableCardExampleComponent } from './examples/card';
 import { DataTableDefaultExampleComponent } from './examples/default';
@@ -14,7 +15,7 @@ const COMPONENT_DECLARATIONS = [
 ];
 
 @NgModule({
-  imports: [CommonModule, KirbyModule],
+  imports: [CommonModule, CardModule, TableSortableComponent],
   declarations: COMPONENT_DECLARATIONS,
   exports: COMPONENT_DECLARATIONS,
 })
