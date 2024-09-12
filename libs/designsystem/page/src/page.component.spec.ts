acaf0cf28f5244b6746fc0b0a8bb2f7afb4a8313
Jakob Engelbrecht <jakob@basher.dk>
2024-09-12 13:47:54 +0200

    test: add logs

--- libs/designsystem/page/src/page.component.spec.ts
+++ libs/designsystem/page/src/page.component.spec.ts
@@ -232,6 +232,41 @@ describe('PageComponent', () => {
 
               await ionContent.scrollToPoint(0, verticalScrollAmount, 0);
               await TestHelper.whenTrue(() => spectator.component.isContentScrolled);
+              console.warn(' *** TOOLBAR BEFORE WAIT ***');
+              console.warn('ionToolbar.classList:', ionToolbar.classList);
+              console.warn(
+                'ionToolbar => --background:',
+                window.getComputedStyle(ionToolbar).getPropertyValue('--background')
+              );
+              console.warn(
+                'ionToolbar::before => --background:',
+                window.getComputedStyle(ionToolbar, 'before').getPropertyValue('--background')
+              );
+              console.warn(
+                'ionToolbar::before => background-color:',
+                window.getComputedStyle(ionToolbar).getPropertyValue('background-color')
+              );
+
+              await TestHelper.whenTrue(
+                () =>
+                  spectator.component.isContentScrolled &&
+                  ionToolbar.classList.contains('content-scrolled')
+              );
+
+              console.warn(' *** TOOLBAR AFTER WAIT ***');
+              console.warn('ionToolbar.classList:', ionToolbar.classList);
+              console.warn(
+                'ionToolbar => --background:',
+                window.getComputedStyle(ionToolbar).getPropertyValue('--background')
+              );
+              console.warn(
+                'ionToolbar::before => --background:',
+                window.getComputedStyle(ionToolbar, 'before').getPropertyValue('--background')
+              );
+              console.warn(
+                'ionToolbar::before => background-color:',
+                window.getComputedStyle(ionToolbar).getPropertyValue('background-color')
+              );
             });
 
             it('should render toolbar divider', () => {
