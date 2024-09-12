0581ed6cd996a439ee1f9ad4ed65d1c3fb1f5604
Jakob Engelbrecht <jakob@basher.dk>
2024-09-12 10:27:15 +0200

    docs: add DataTable to Storybook

--- apps/cookbook/src/app/examples/data-table-example/table-example.module.ts
+++ apps/cookbook/src/app/examples/data-table-example/table-example.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { CardModule } from '@kirbydesign/designsystem/card';
 import { TableSortableComponent } from '@kirbydesign/designsystem/data-table';
+import { ToastController, ToastHelper } from '@kirbydesign/designsystem/toast';
 
 import { DataTableCardExampleComponent } from './examples/card';
 import { DataTableDefaultExampleComponent } from './examples/default';
 import { DataTableSortableExampleComponent } from './examples/sortable';
+import { DataTableExampleComponent } from './data-table-example.component';
 
 const COMPONENT_DECLARATIONS = [
+  DataTableExampleComponent,
   DataTableCardExampleComponent,
   DataTableDefaultExampleComponent,
   DataTableSortableExampleComponent,
@@ -16,6 +19,7 @@ const COMPONENT_DECLARATIONS = [
 
 @NgModule({
   imports: [CommonModule, CardModule, TableSortableComponent],
+  providers: [ToastController, ToastHelper],
   declarations: COMPONENT_DECLARATIONS,
   exports: COMPONENT_DECLARATIONS,
 })
