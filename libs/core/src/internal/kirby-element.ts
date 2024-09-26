8f61354ec859683132c66dfe36a140d8b073c87a
Jakob Engelbrecht <jakob@basher.dk>
2024-09-26 09:51:47 +0200

    throw error if tagName is missing

--- libs/core/src/internal/kirby-element.ts
+++ libs/core/src/internal/kirby-element.ts
@@ -3,8 +3,8 @@ import { LitElement } from 'lit';
 export default class KirbyElement extends LitElement {
   static define(name = this.tagName, elementConstructor = this) {
     if (!name) {
-      console.error(
-        'Custom element name is missing. Please ensure class inheriting from `KirbyElement` has static the `tagName` property set to a valid custom element name.'
+      throw new Error(
+        'Custom element name is missing. Please ensure class inheriting from `KirbyElement` has static the `tagName` property is set to a valid custom element name.'
       );
     }
     const currentlyRegisteredConstructor = customElements.get(name) as CustomElementConstructor;
