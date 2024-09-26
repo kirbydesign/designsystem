544f97c10a82ca4e8f40408bfc499090176f423d
Jakob Engelbrecht <jakob@basher.dk>
2024-09-26 15:12:55 +0200

    docs: remove obsolete prop from API

--- apps/cookbook/src/app/showcase/button-showcase/button-showcase.component.ts
+++ apps/cookbook/src/app/showcase/button-showcase/button-showcase.component.ts
@@ -10,6 +10,13 @@ import { ApiDescriptionProperty } from '~/app/shared/api-description/api-descrip
 export class ButtonShowcaseComponent {
   properties: ApiDescriptionProperty[] = [
     {
+      name: 'attentionLevel',
+      description:
+        'Sets the attention level for the button. Button color will be updated automatically depending on host color.',
+      defaultValue: '1',
+      type: ['1', '2', '3'],
+    },
+    {
       name: 'expand',
       description:
         'If the button needs to expand to full width of its parent container, then use expand.',
@@ -22,20 +29,6 @@ export class ButtonShowcaseComponent {
         'Sets the size of the button. The touch area will always be a minimum of 44px by 44px. If buttons are smaller than this, the surrounding area will still be clickable, to preserve accessibility.',
       defaultValue: 'md',
       type: ['xs', 'sm', 'md', 'lg'],
-    },
-    {
-      name: 'attentionLevel',
-      description:
-        'Sets the attention level for the button. Button color will be updated automatically depending on host color.',
-      defaultValue: '1',
-      type: ['1', '2', '3'],
-    },
-    {
-      name: 'isFloating',
-      description:
-        '(Optional) Determine if the button is going to be a Floating Action Button (FAB). Please note: FABs have an elevation of z8 by default. FABs have only an icon and no text. FABs always have a size of 64x64.',
-      defaultValue: 'false',
-      type: ['boolean'],
     },
   ];
 
