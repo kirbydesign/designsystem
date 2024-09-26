91cae2fd4b2f3d73ef23d27cc9c9f36365ae847b
Jakob Engelbrecht <jakob@basher.dk>
2024-09-26 16:30:46 +0200

    Badge: Convert Badge to web component (#3521)

--- libs/angular/badge/src/public_api.ts
+++ libs/angular/badge/src/public_api.ts
@@ -1,1 +1,5 @@
+import { KirbyBadgeElement } from '@kirbydesign/core/badge.element';
+
 export * from './badge.component';
+
+KirbyBadgeElement.define();
--- libs/designsystem/badge/src/badge.component.ts
+++ libs/designsystem/badge/src/badge.component.ts
@@ -5,6 +5,9 @@ import { IonBadge } from '@ionic/angular/standalone';
 
 export type BadgeSize = 'sm' | 'md';
 
+const KIRBY_BADGE_TEXT_PROPERTY_DEPRECATION_WARNING =
+  '[Kirby Badge] The `text` property has been deprecated. Please set the text of the Badge between the opening and closing tag.';
+
 @Component({
   selector: 'kirby-badge',
   templateUrl: './badge.component.html',
@@ -14,7 +17,18 @@ export type BadgeSize = 'sm' | 'md';
   hostDirectives: [{ directive: ThemeColorDirective, inputs: ['themeColor'] }],
 })
 export class BadgeComponent {
-  @Input() text: string;
+  private _text: string;
+  get text(): string {
+    return this._text;
+  }
+  /**
+   * @deprecated The 'text' property has been deprecated.
+   * Please set the text of the Badge between the opening and closing tag.
+   */
+  @Input() set text(value: string) {
+    console.warn(KIRBY_BADGE_TEXT_PROPERTY_DEPRECATION_WARNING);
+    this._text = value;
+  }
 
   @HostBinding('class')
   @Input()
