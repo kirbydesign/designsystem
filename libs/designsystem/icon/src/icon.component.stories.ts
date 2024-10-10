4daeb518c60457fa8f5904f9fa2d1b430aed4c79
Jesper Kaltoft Vind <git@jesperkaltoft.dk>
2024-10-11 00:12:45 +0200

    Housekeeping: Create Cookbook example story for Icon (#3659)

--- libs/designsystem/icon/src/icon.component.stories.ts
+++ libs/designsystem/icon/src/icon.component.stories.ts
@@ -1,15 +1,29 @@
-import { type Meta, type StoryObj } from '@storybook/angular';
-import { IconComponent } from '@kirbydesign/designsystem/icon';
+import { type Meta, moduleMetadata, type StoryObj } from '@storybook/angular';
+
+import { IconComponent, IconModule } from '@kirbydesign/designsystem/icon';
+
+import { IconExampleModule } from '~/app/examples/icon-example/icon-example.module';
 
 const meta: Meta<IconComponent> = {
   component: IconComponent,
   title: 'Components / Icon',
+  decorators: [
+    moduleMetadata({
+      imports: [IconModule, IconExampleModule],
+    }),
+  ],
 };
 export default meta;
 type Story = StoryObj<IconComponent>;
 
-export const Icon: Story = {
+export const Default: Story = {
   args: {
     name: '',
   },
+};
+
+export const CookbookExamples: Story = {
+  render: () => ({
+    template: `<cookbook-icon-example></cookbook-icon-example>`,
+  }),
 };
