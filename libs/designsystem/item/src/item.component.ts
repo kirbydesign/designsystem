65346e2ef6e7ff92ca2834356c45f630bc913d22
RasmusKjeldgaard <rkk@bankdata.dk>
2024-10-10 10:28:42 +0200

    Clean up item

--- libs/designsystem/item/src/item.component.ts
+++ libs/designsystem/item/src/item.component.ts
@@ -1,18 +1,14 @@
 import {
-  AfterContentInit,
   ChangeDetectionStrategy,
   Component,
   ContentChild,
   ElementRef,
   HostBinding,
   Input,
-  Renderer2,
 } from '@angular/core';
 
 import { CheckboxComponent } from '@kirbydesign/designsystem/checkbox';
 import { RadioComponent } from '@kirbydesign/designsystem/radio';
-
-import { LabelComponent } from './label/label.component';
 
 export enum ItemSize {
   XS = 'xs',
@@ -26,9 +22,7 @@ export enum ItemSize {
   styleUrls: ['./item.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ItemComponent implements AfterContentInit {
-  constructor(private renderer: Renderer2) {}
-
+export class ItemComponent {
   @HostBinding('class.disabled')
   @Input()
   disabled: boolean;
@@ -48,15 +42,10 @@ export class ItemComponent implements AfterContentInit {
 
   @Input() rotateIcon: boolean = false;
 
-  // Only query for label as direct child of item:
-  @ContentChild(LabelComponent, { static: false, read: ElementRef, descendants: false })
-  private label: ElementRef<HTMLElement>;
   @ContentChild(CheckboxComponent, { static: false })
   private checkboxComponent: CheckboxComponent;
   @ContentChild(RadioComponent, { static: false, read: ElementRef })
   private radio: ElementRef<HTMLElement>;
-  @ContentChild(RadioComponent, { static: false })
-  private radioComponent: RadioComponent;
 
   // Prevent default when inside kirby-dropdown to avoid blurring dropdown:
   onMouseDown(event: MouseEvent) {
@@ -66,20 +55,6 @@ export class ItemComponent implements AfterContentInit {
     ) {
       event.preventDefault();
     }
-  }
-
-  ngAfterContentInit(): void {
-    if (this.label) {
-      if (this.radio && !this.radioComponent._ariaLabel) {
-        this.moveLabel(this.radio.nativeElement.querySelector('ion-radio'));
-      }
-    }
-  }
-
-  private moveLabel(newParent: HTMLElement) {
-    const labelElement = this.label.nativeElement;
-    this.renderer.removeChild(labelElement.parentElement, labelElement);
-    this.renderer.appendChild(newParent, labelElement);
   }
 
   get _isIonicButton() {
