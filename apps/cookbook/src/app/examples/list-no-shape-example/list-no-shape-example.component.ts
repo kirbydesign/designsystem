6cad91615ea2734ed85b46e746076a9e3d2a0a85
Søren Oest Balmer <oestbalmer@gmail.com>
2024-10-15 06:17:16 +0200

    List no shape showcase

--- apps/cookbook/src/app/examples/list-no-shape-example/list-no-shape-example.component.ts
+++ apps/cookbook/src/app/examples/list-no-shape-example/list-no-shape-example.component.ts
@@ -1,37 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'cookbook-list-no-shape-example',
   templateUrl: './list-no-shape-example.component.html',
   styleUrls: ['./list-no-shape-example.component.scss'],
+  changeDetection: ChangeDetectionStrategy.Default,
 })
-export class ListNoShapeExampleComponent {
-  items: any[] = [
-    {
-      title: 'Holiday with friends',
-      amount: -37445.02325,
-    },
-    {
-      title: 'Savings',
-      amount: 923367.2356,
-    },
-    {
-      title: 'Expenses',
-      amount: 65128.45,
-      shadowAccounts: [
-        {
-          title: 'Food',
-          amount: 376.12,
-        },
-        {
-          title: 'Car',
-          amount: 62376.12,
-        },
-        {
-          title: 'Misc',
-          amount: 2376.12,
-        },
-      ],
-    },
-  ];
-}
+export class ListNoShapeExampleComponent {}
---
+++ apps/cookbook/src/app/examples/list-no-shape-example/list-no-shape-example.module.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { KirbyModule } from '@kirbydesign/designsystem';
+import { MultiCardListNoShapeExampleComponent } from './examples/multi-card-list-no-shape';
+import { ListNoShapeExampleComponent } from './list-no-shape-example.component';
+import { SimpleListNoShapeExampleComponent } from './examples/simple-list-no-shape';
+import { DetailedCardWithListNoShapeComponent } from './examples/detailed-card-with-list-no-shape';
+
+const COMPONENT_DECLARATIONS = [
+  ListNoShapeExampleComponent,
+  MultiCardListNoShapeExampleComponent,
+  SimpleListNoShapeExampleComponent,
+  DetailedCardWithListNoShapeComponent,
+];
+
+@NgModule({
+  imports: [CommonModule, KirbyModule],
+  providers: [],
+  declarations: COMPONENT_DECLARATIONS,
+  exports: COMPONENT_DECLARATIONS,
+})
+export class ListNoShapeExampleModule {}
