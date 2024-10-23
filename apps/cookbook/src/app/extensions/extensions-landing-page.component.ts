870a8b5c1d6392f5f83dd435cf121d70cb0714d8
RasmusKjeldgaard <rkk@bankdata.dk>
2024-10-23 13:14:42 +0200

    Add cookbook page and nav item for extensions landing page

---
+++ apps/cookbook/src/app/extensions/extensions-landing-page.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cookbook-extensions-landing-page',
+  templateUrl: './extensions-landing-page.component.html',
+})
+export class ExtensionsLandingPageComponent {}
