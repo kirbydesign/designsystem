d6dc6e1872862215dd428f3ce6cd3a8140c1d69f
RasmusKjeldgaard <rkk@bankdata.dk>
2024-11-05 22:06:45 +0100

    Empty state in modal should set aria-labelledby

--- libs/designsystem/empty-state/src/empty-state.component.ts
+++ libs/designsystem/empty-state/src/empty-state.component.ts
@@ -1,12 +1,15 @@
 import {
   AfterContentInit,
+  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
   ContentChildren,
+  ElementRef,
   Input,
   QueryList,
 } from '@angular/core';
 import { ButtonComponent } from '@kirbydesign/designsystem/button';
+import { UniqueIdGenerator } from '@kirbydesign/designsystem/helpers';
 
 @Component({
   selector: 'kirby-empty-state',
@@ -14,13 +17,17 @@ import { ButtonComponent } from '@kirbydesign/designsystem/button';
   styleUrls: ['./empty-state.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class EmptyStateComponent implements AfterContentInit {
+export class EmptyStateComponent implements AfterContentInit, AfterViewInit {
   @Input() iconName: string;
   @Input() title: string;
   @Input() subtitle: string;
 
   @ContentChildren(ButtonComponent)
   private slottedButtons: QueryList<ButtonComponent>;
+
+  _titleId = UniqueIdGenerator.scopedTo('kirby-modal-title').next();
+
+  constructor(private elementRef: ElementRef<HTMLElement>) {}
 
   ngAfterContentInit() {
     this.enforceAttentionLevelRules();
@@ -30,6 +37,16 @@ export class EmptyStateComponent implements AfterContentInit {
     this.slottedButtons.changes.subscribe(() => {
       setTimeout(() => this.enforceAttentionLevelRules());
     });
+  }
+
+  ngAfterViewInit(): void {
+    /* If we are inside a modal that is not labelled by e.g. a title, we want to set 
+    aria-labelledby attribute on ion-modal to point to the title of empty state */
+    const ionModal = this.elementRef.nativeElement.closest('ion-modal');
+    const ionModalNotLabelled = !ionModal?.getAttribute('aria-labelledby');
+    if (ionModal && ionModalNotLabelled) {
+      ionModal.setAttribute('aria-labelledby', this._titleId);
+    }
   }
 
   /** Enforces that all slotted buttons will have their attention
