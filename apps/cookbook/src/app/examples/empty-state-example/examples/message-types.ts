f269296ea301e66c3da6ae3915c9e1b0fe3b521e
RasmusKjeldgaard <rkk@bankdata.dk>
2024-11-07 16:50:48 +0100

    Review comments

--- apps/cookbook/src/app/examples/empty-state-example/examples/message-types.ts
+++ apps/cookbook/src/app/examples/empty-state-example/examples/message-types.ts
@@ -1,37 +1,31 @@
 import { Component } from '@angular/core';
 
 const config = {
-  template: `<kirby-empty-state
-  iconName="verify"
-  themeColor="success"
-  title="Success"
-  subtitle="Additional messaging via subtitle"
-></kirby-empty-state>
-<kirby-empty-state
-  iconName="help"
-  themeColor="warning"
-  title="Warning"
-  subtitle="Additional messaging via subtitle"
-></kirby-empty-state>
-<kirby-empty-state
-  iconName="overview-outline"
-  title="Empty"
-  subtitle="Additional messaging via subtitle"
-></kirby-empty-state>`,
+  template: `<div class="message-type-container">
+  <kirby-empty-state
+    iconName="verify"
+    themeColor="success"
+    title="Success"
+    subtitle="Additional messaging via subtitle"
+  ></kirby-empty-state>
+  <kirby-empty-state
+    iconName="help"
+    themeColor="warning"
+    title="Warning"
+    subtitle="Additional messaging via subtitle"
+  ></kirby-empty-state>
+  <kirby-empty-state
+    iconName="overview-outline"
+    title="Empty"
+    subtitle="Additional messaging via subtitle"
+  ></kirby-empty-state>
+</div>`,
 };
 
 @Component({
   selector: 'cookbook-empty-state-message-types-example',
   template: config.template,
-  styles: [
-    `
-      :host {
-        display: flex;
-        gap: 2rem;
-        justify-content: space-around;
-      }
-    `,
-  ],
+  styleUrls: ['./empty-state-example.shared.scss'],
 })
 export class EmptyStateMessageTypesExampleComponent {
   template: string = config.template;
