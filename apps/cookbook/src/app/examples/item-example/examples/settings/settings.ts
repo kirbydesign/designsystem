79f3aec2546cfd9e01a3c2ea7b82de8043d11f11
Søren Oest Balmer <oestbalmer@gmail.com>
2024-11-12 15:34:02 +0100

    revert selected on item example

--- apps/cookbook/src/app/examples/item-example/examples/settings/settings.ts
+++ apps/cookbook/src/app/examples/item-example/examples/settings/settings.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 const config = {
   selector: 'cookbook-item-example-settings',
-  template: `<kirby-item [selectable]="true">
+  template: `<kirby-item>
   <h3>Title</h3>
   <kirby-toggle slot="end" checked="true"></kirby-toggle>
 </kirby-item>`,
