7ef042c6b28f151efb854844a80add615a3b128f
RasmusKjeldgaard <rkk@bankdata.dk>
2024-11-14 09:50:50 +0100

    Add ariaLabel property code snippet

--- apps/cookbook/src/app/examples/modal-example/compact-example/modal-compact-example.component.ts
+++ apps/cookbook/src/app/examples/modal-example/compact-example/modal-compact-example.component.ts
@@ -2,10 +2,17 @@ import { Component, Optional, SkipSelf } from '@angular/core';
 
 import { Modal } from '@kirbydesign/designsystem';
 
+const config = {
+  ariaLabelPropertyCodeSnippet: `constructor(@Optional() @SkipSelf() private modal: Modal) {
+  this.modal.ariaLabel = 'My Custom Accessible Title';
+}`,
+};
 @Component({
   templateUrl: './modal-compact-example.component.html',
 })
 export class ModalCompactExampleComponent {
+  static readonly ariaLabelPropertyCodeSnippet = config.ariaLabelPropertyCodeSnippet;
+
   constructor(@Optional() @SkipSelf() private modal: Modal) {}
 
   onHideModal() {
