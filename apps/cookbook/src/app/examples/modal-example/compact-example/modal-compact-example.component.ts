bdb4700e0004cf22fbf4c148436358c8618678b6
RasmusKjeldgaard <rkk@bankdata.dk>
2024-12-03 11:21:08 +0100

    Remove unused code snippet

--- apps/cookbook/src/app/examples/modal-example/compact-example/modal-compact-example.component.ts
+++ apps/cookbook/src/app/examples/modal-example/compact-example/modal-compact-example.component.ts
@@ -2,17 +2,10 @@ import { Component, Optional, SkipSelf } from '@angular/core';
 
 import { Modal } from '@kirbydesign/designsystem';
 
-const config = {
-  ariaLabelPropertyCodeSnippet: `constructor(@Optional() @SkipSelf() private modal: Modal) {
-  this.modal.ariaLabel = 'My Custom Accessible Title';
-}`,
-};
 @Component({
   templateUrl: './modal-compact-example.component.html',
 })
 export class ModalCompactExampleComponent {
-  static readonly ariaLabelPropertyCodeSnippet = config.ariaLabelPropertyCodeSnippet;
-
   constructor(@Optional() @SkipSelf() private modal: Modal) {}
 
   onHideModal() {
