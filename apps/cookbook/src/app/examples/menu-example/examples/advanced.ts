9876ec0422d6153eddea7efbbdd61d2b8c7de0ec
Søren Oest Balmer <oestbalmer@gmail.com>
2024-12-05 16:04:16 +0100

    Fix build

--- apps/cookbook/src/app/examples/menu-example/examples/advanced.ts
+++ apps/cookbook/src/app/examples/menu-example/examples/advanced.ts
@@ -27,6 +27,14 @@ const config = {
       </kirby-label>
     </kirby-toggle>
   </kirby-item>
+  <kirby-item>
+    <kirby-icon name="default" slot="start"></kirby-icon>
+    <kirby-radio slot="end">
+      <kirby-label>      
+        Multiplayer mode
+      </kirby-label>
+    </kirby-radio>
+  </kirby-item>
 </kirby-menu>`,
 };
 
