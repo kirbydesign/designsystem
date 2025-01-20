b4b200e80fada00fa93221dfcf9377964b2223cf
RasmusKjeldgaard <rkk@bankdata.dk>
2025-01-20 14:11:00 +0100

    Remove redundant NoInfer type

--- libs/designsystem/src/lib/components/segmented-control/segmented-control.component.ts
+++ libs/designsystem/src/lib/components/segmented-control/segmented-control.component.ts
@@ -22,11 +22,6 @@ export enum SegmentedControlMode {
   default = 'default',
 }
 
-// Workaround until TS 5.4 official NoInfer
-// https://github.com/millsp/ts-toolbelt/blob/master/sources/Function/NoInfer.ts
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type NoInfer<T> = [T][T extends any ? 0 : never];
-
 @Component({
   imports: [IconModule, BadgeComponent, CommonModule, IonSegment, IonSegmentButton],
   selector: 'kirby-segmented-control',
