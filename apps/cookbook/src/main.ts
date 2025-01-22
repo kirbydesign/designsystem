d1962fb816a544d9cbd41704cfd1630410be3c3f
RasmusKjeldgaard <rkk@bankdata.dk>
2025-01-22 12:54:19 +0100

    Register icons and locale data

--- apps/cookbook/src/main.ts
+++ apps/cookbook/src/main.ts
@@ -1,11 +1,18 @@
-import { enableProdMode, importProvidersFrom, LOCALE_ID } from '@angular/core';
+import {
+  enableProdMode,
+  importProvidersFrom,
+  inject,
+  LOCALE_ID,
+  provideAppInitializer,
+} from '@angular/core';
 
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { HashLocationStrategy, LocationStrategy, registerLocaleData } from '@angular/common';
+import localeData from '@angular/common/locales/en-DK';
 import { provideHttpClient } from '@angular/common/http';
 import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-import { KirbyModule } from '@kirbydesign/designsystem';
+import { IconRegistryService, KirbyModule } from '@kirbydesign/designsystem';
 import { provideRouter, withInMemoryScrolling, withRouterConfig } from '@angular/router';
 import { environment } from './environments/environment';
 
@@ -17,11 +24,11 @@ if (environment.production) {
   enableProdMode();
 }
 
+registerLocaleData(localeData);
+
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(BrowserModule, FormsModule, KirbyModule),
-    { provide: LOCALE_ID, useValue: 'en-DK' },
-    { provide: LocationStrategy, useClass: HashLocationStrategy },
     provideHttpClient(),
     provideAnimations(),
     provideRouter(
@@ -31,5 +38,20 @@ bootstrapApplication(AppComponent, {
         scrollPositionRestoration: 'enabled',
       })
     ),
+    { provide: LOCALE_ID, useValue: 'en-DK' },
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+    provideAppInitializer(() => {
+      const iconRegistry = inject(IconRegistryService);
+      iconRegistry.addIcons([
+        {
+          name: 'football',
+          svg: 'assets/icons/football.svg',
+        },
+        {
+          name: 'umbrella',
+          svg: 'assets/icons/umbrella.svg',
+        },
+      ]);
+    }),
   ],
 }).catch((err) => console.error(err));
