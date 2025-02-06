40b263995c12f6d2790c8fb2c5da45551f9bf75c
RasmusKjeldgaard <rkk@bankdata.dk>
2025-02-06 16:52:14 +0100

    Simplify docs

--- libs/extensions/angular/skeleton-loader/src/skeleton-loader.component.stories.ts
+++ libs/extensions/angular/skeleton-loader/src/skeleton-loader.component.stories.ts
@@ -5,26 +5,10 @@ import { CardModule } from '@kirbydesign/designsystem/card';
 import { ItemModule } from '@kirbydesign/designsystem/item';
 
 /**
- * A Skeleton is a visual indicator that is used to render placeholder content and mimic the full content shown when loaded.
- * ### Themes
- * There are two types of themes:
- * 1. OnLight - A darker version to be used on white and light grey backgrounds.
- * 2. OnDark - A lighter version used on dark colors and brand background.
+ * A skeleton loader is a visual indicator that is used to render placeholder content and mimic the full content shown when loaded.
  *
- * ### Best practices
- * - The Skeleton is used as an indicator to load content on an entire page, content on a card.
- * - Only use skeleton states on container-based components. Fx cards and lists or data-based components like data tables. In most cases, buttons, input fields, checkboxes, toggles should not have a skeleton state.
- * - Aim for simple low contrast skeleton screens that does not attract too much attention. Avoid designing high contrast skeleton views, where dark skeletons are placed on white backgrounds. Go for white skeletons on white background even though the final rendering is fx a dark card on white background.
- * - Use af skeleton or spinner if load time is between 750ms and 10 seconds.
- * - If a load is faster than 750ms don’t use af skeleton or spinner, as this most likely won’t make a positive difference for the user experience. Most likely it will just show an annoying flash of spinner og skeleton that wil be distracting.
- * - If page load is slower than 10 seconds consider using a progress bar instead.
- *
- * ### Loading
- *  - A skeleton is placed inside a loading component to load content from the backend (E.g. loading a Cards content or a sites Content)
- *  - We try to show the data that is available and only load the content that has to be loaded by the backend (E.g. if we load a sites content we show the page-title and the content that we know of).
- *
- * ### Sizing
- * The skeleton-dimensions can be controlled with width and height css-properties on the element itself. Consider using the existing design tokens from Kirby when assigning these values.
+ * ## Sizing
+ * The skeleton loaders dimensions can be controlled with width and height css-properties on the element itself. Consider using the existing design tokens from Kirby when assigning these values.
  * Out of the box, the skeleton loader will fill its parent containers width, while maintaining a height matching the browsers base font size.
  *
  */
@@ -66,6 +50,7 @@ export const Default: Story = {
 
 /**
  * The skeleton loader can be styled as a rectangle, pill or circle.
+ * Any custom border radius can be set by using the `border-radius` css property directly, instead of using the shape property.
  */
 export const Shape: Story = {
   render: (args) => ({
@@ -75,6 +60,7 @@ export const Shape: Story = {
       <kirby-x-skeleton-loader style="width: 100px;" shape="rectangle"></kirby-x-skeleton-loader>
       <kirby-x-skeleton-loader style="width: 100px; height: var(--kirby-spacing-l);" shape="pill"></kirby-x-skeleton-loader>
       <kirby-x-skeleton-loader style="height: var(--kirby-spacing-xxxl); width: var(--kirby-spacing-xxxl);" shape="circle"></kirby-x-skeleton-loader>
+      <kirby-x-skeleton-loader style="border-radius: var(--kirby-border-radius-n); height: var(--kirby-spacing-xxxl); width: var(--kirby-spacing-xxxl);"></kirby-x-skeleton-loader>
     </div>`,
   }),
   argTypes: {
@@ -84,7 +70,7 @@ export const Shape: Story = {
 };
 
 /**
- * A light skeleton loader on a dark background.
+ * A skeleton loader on a dark background. In this case theme selection happens automatically based on `themeColor` on kirby-card.
  */
 export const DarkCard: Story = {
   render: (args) => ({
@@ -97,7 +83,7 @@ export const DarkCard: Story = {
     template: `<kirby-card themeColor="dark" class="skeleton-card">
       <kirby-item style="--kirby-item-background: transparent;">
         <div slot="start">
-          <kirby-x-skeleton-loader style="height: var(--kirby-spacing-xxxl); width: var(--kirby-spacing-xxxl); border-radius: var(--kirby-border-radius-circle);"></kirby-x-skeleton-loader>
+          <kirby-x-skeleton-loader shape="circle" style="height: var(--kirby-spacing-xxxl); width: var(--kirby-spacing-xxxl);"></kirby-x-skeleton-loader>
         </div>
         <div class="text">
           <kirby-x-skeleton-loader style="height: var(--kirby-font-size-m); width: 100px;"></kirby-x-skeleton-loader>
@@ -117,7 +103,7 @@ export const DarkCard: Story = {
 };
 
 /**
- * A dark skeleton loader on a light background.
+ * A skeleton loader on a light background.
  */
 export const LightCard: Story = {
   render: (args) => ({
@@ -130,7 +116,7 @@ export const LightCard: Story = {
     template: `<kirby-card class="skeleton-card">
       <kirby-item style="--kirby-item-background: transparent;">
         <div slot="start">
-          <kirby-x-skeleton-loader style="height: var(--kirby-spacing-xxxl); width: var(--kirby-spacing-xxxl); border-radius: var(--kirby-border-radius-circle);"></kirby-x-skeleton-loader>
+          <kirby-x-skeleton-loader shape="circle" style="height: var(--kirby-spacing-xxxl); width: var(--kirby-spacing-xxxl);"></kirby-x-skeleton-loader>
         </div>
         <div class="text">
           <kirby-x-skeleton-loader style="height: var(--kirby-font-size-m); width: 100px;"></kirby-x-skeleton-loader>
