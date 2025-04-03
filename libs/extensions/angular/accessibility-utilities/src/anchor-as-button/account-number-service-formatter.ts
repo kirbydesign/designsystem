762f332ae2476578ac399ad1d2b150261e79fd06
Kemal Pajevic <kp@digitalcraft.dk>
2025-04-03 22:19:07 +0200

    Deleted some mistakenly copied files.

--- libs/extensions/angular/accessibility-utilities/src/anchor-as-button/account-number-service-formatter.ts
+++
@@ -1,5 +0,0 @@
-import { AccountNumber } from './account-number.model';
-
-export function formatAccountNumber(value: AccountNumber): string {
-  return `${value.regNo.padStart(4, '0')} ${value.accountNo.replace(/^0+/, '')}`;
-}
--- libs/extensions/angular/accessibility-utilities/src/anchor-as-button/account-number.model.ts
+++
@@ -1,4 +0,0 @@
-export interface AccountNumber {
-  regNo: string;
-  accountNo: string;
-}
