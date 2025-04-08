213ff62da40f54836751ad67f9f5da9e49998aa5
Troels Strand <troelslenda@gmail.com>
2025-04-08 17:20:56 +0200

    Checkbox: Add indeterminate state (#3882)
    Co-authored-by: Troels Lenda Strand <tsd@bankdata.dk>
    Co-authored-by: RasmusKjeldgaard <rkk@bankdata.dk>
    Co-authored-by: RasmusKjeldgaard <raskjeldgaard@gmail.com>

--- apps/cookbook/src/app/examples/checkbox-example/examples/states.ts
+++ apps/cookbook/src/app/examples/checkbox-example/examples/states.ts
@@ -4,9 +4,11 @@ import { CheckboxComponent } from '@kirbydesign/designsystem/checkbox';
 const config = {
   selector: 'cookbook-checkbox-states-example',
   template: `<kirby-checkbox text="Default"></kirby-checkbox>
-<kirby-checkbox [checked]="true" text="Checked"></kirby-checkbox>
 <kirby-checkbox disabled="true" text="Disabled"></kirby-checkbox>
+<kirby-checkbox [checked]="true" text="Checked"></kirby-checkbox>
 <kirby-checkbox disabled="true" [checked]="true" text="Disabled checked"></kirby-checkbox>
+<kirby-checkbox [indeterminate]="true" text="Indeterminate"></kirby-checkbox>
+<kirby-checkbox [indeterminate]="true" text="Disabled Indeterminate" [disabled]="true"></kirby-checkbox>
 <kirby-checkbox hasError="true" text="Has error"></kirby-checkbox>`,
 };
 
@@ -16,8 +18,10 @@ const config = {
   styles: [
     `
       :host {
-        display: flex;
-        flex-wrap: wrap;
+        max-width: 480px;
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        gap: 0 var(--kirby-spacing-s);
       }
     `,
   ],
