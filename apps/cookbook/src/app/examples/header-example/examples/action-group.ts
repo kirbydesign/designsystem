6f90a31aeee25219f57031056011732f5c478a15
Søren Oest Balmer <oestbalmer@gmail.com>
2025-05-16 11:30:32 +0200

    revert action group example

--- apps/cookbook/src/app/examples/header-example/examples/action-group.ts
+++ apps/cookbook/src/app/examples/header-example/examples/action-group.ts
@@ -11,12 +11,12 @@ import { BasePageExampleComponent } from '../../page-example/base-page-example.c
 const config = {
   template: `<kirby-page defaultBackHref="/">
   <kirby-header [title]="'Action Group'" subtitle1="Subtitle one" subtitle2="Subtitle two">
-    <kirby-action-group *kirbyHeaderActions [visibleActions]="3">
+    <kirby-action-group *kirbyHeaderActions>
       <button kirby-button attentionLevel="3" (click)="actionClicked('Action 1')">
         <kirby-icon name="edit"></kirby-icon>
         <span class="text">Action 1</span>
       </button>
-      <button kirby-button attentionLevel="3" (click)="actionClicked('Action 2')" [disabled]="disabled">
+      <button kirby-button attentionLevel="3" (click)="actionClicked('Action 2')">
         Action 2
       </button>
       <button kirby-button attentionLevel="3" (click)="actionClicked('Action 3')">
@@ -35,7 +35,6 @@ const config = {
   imports: [PageModule, HeaderModule, ActionGroupComponent, ButtonComponent, IconModule],
 })
 export class HeaderWithActionGroupExampleComponent extends BasePageExampleComponent {
-  disabled = false;
   static readonly template = config.template
     .replace(' defaultBackHref="/"', '')
     .replace(' [innerHTML]="content">', '>...');
@@ -51,6 +50,5 @@ export class HeaderWithActionGroupExampleComponent extends BasePageExampleCompon
       durationInMs: 1500,
     };
     this.toastController.showToast(config);
-    this.disabled = !this.disabled;
   }
 }
