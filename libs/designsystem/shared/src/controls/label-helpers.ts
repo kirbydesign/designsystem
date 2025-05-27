2310ed7e2d6608690f865456d0459581244b641f
Søren Oest Balmer <oestbalmer@gmail.com>
2025-05-27 11:58:28 +0200

    revert label-helpers.ts

--- libs/designsystem/shared/src/controls/label-helpers.ts
+++ libs/designsystem/shared/src/controls/label-helpers.ts
@@ -32,21 +32,27 @@ export function inheritAriaLabelText(element: HTMLElement): string {
  * @param sourceElement - The HTML element from which attributes will be removed from.
  * @param attributes - An array of attribute names to inherit from the source element.
  * @param renderer - An instance of Angular's Renderer2.
- * @param targetElement - (Optional) The HTML element to which the attributes should be applied. If not provided, the attributes will only be removed from the source element.
+ * @param targetElement - (Optional) The HTML element to which the attributes should be applied.
+ *                        If not provided, the attributes will only be removed from the source element.
+ *
+ * @returns A record of the inherited attributes and their values. If no attributes are inherited, an empty object is returned.
  */
 export function forwardAttributes(
   sourceElement: HTMLElement,
   attributes: string[],
   renderer: Renderer2,
   targetElement?: HTMLElement
-) {
+): Record<string, any> {
+  const inheritedAttributes: Record<string, any> = {};
+
   if (!sourceElement || !attributes || attributes.length === 0) {
-    return;
+    return inheritedAttributes;
   }
 
   attributes.forEach((attribute) => {
     if (sourceElement.hasAttribute(attribute)) {
       const value = sourceElement.getAttribute(attribute);
+      inheritedAttributes[attribute] = value;
 
       if (targetElement) {
         renderer.setAttribute(targetElement, attribute, value);
@@ -55,4 +61,6 @@ export function forwardAttributes(
       renderer.removeAttribute(sourceElement, attribute);
     }
   });
+
+  return inheritedAttributes;
 }
