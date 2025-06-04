53154dca1ce2b13af7e7181a6d8731e15ca30875
RasmusKjeldgaard <raskjeldgaard@gmail.com>
2025-06-04 08:22:17 +0200

    Revert original example and add new

--- apps/cookbook/src/app/examples/form-field-example/examples/input/pad-precision-digits.ts
+++ apps/cookbook/src/app/examples/form-field-example/examples/input/pad-precision-digits.ts
@@ -9,9 +9,9 @@ import {
 } from '@kirbydesign/designsystem/form-field';
 
 const config = {
-  selector: 'cookbook-form-field-input-decimal-mask-example',
-  template: `<kirby-form-field label="Decimal mask input">
-  <input type="number" kirby-input kirby-decimal-mask />
+  selector: 'cookbook-form-field-input-pad-precision-digits-example',
+  template: `<kirby-form-field label="Decimal mask with pad precision digits">
+  <input type="number" kirby-input kirby-decimal-mask [padPrecisionDigits]="true"/>
 </kirby-form-field>
 `,
 };
@@ -21,7 +21,7 @@ const config = {
   template: config.template,
   imports: [FormFieldModule, DateInputDirective, InputComponent, DecimalMaskDirective],
 })
-export class FormFieldInputDecimalMaskExampleComponent {
+export class FormFieldInputPadPrecisionDigitsExampleComponent {
   template: string = config.template;
   @Input() size: InputSize;
 }
