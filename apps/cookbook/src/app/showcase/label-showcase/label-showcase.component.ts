d0fe970ee9aec927c6eb5b2582b3040df79989b4
Louise <lou.bankdata.dk>
2025-06-17 09:15:23 +0200

    add description columns

--- apps/cookbook/src/app/showcase/label-showcase/label-showcase.component.ts
+++ apps/cookbook/src/app/showcase/label-showcase/label-showcase.component.ts
@@ -3,7 +3,11 @@ import { ExampleViewerComponent } from '../../shared/example-viewer/example-view
 import { LabelExampleItemComponent } from '../../examples/label-example/examples/item';
 import { LabelFormFieldInputLabelExampleComponent } from '../../examples/label-example/examples/label';
 
-import { ApiDescriptionProperty } from '~/app/shared/api-description/api-description-properties/api-description-properties.component';
+import { ApiDescriptionPropertiesComponent } from '../../shared/api-description/api-description-properties/api-description-properties.component';
+import {
+  ApiDescriptionProperty,
+  ApiDescriptionPropertyColumns,
+} from '~/app/shared/api-description/api-description-properties/api-description-properties.component';
 
 @Component({
   selector: 'cookbook-label-showcase',
@@ -13,6 +17,7 @@ import { ApiDescriptionProperty } from '~/app/shared/api-description/api-descrip
     ExampleViewerComponent,
     LabelExampleItemComponent,
     LabelFormFieldInputLabelExampleComponent,
+    ApiDescriptionPropertiesComponent,
   ],
 })
 export class LabelShowcaseComponent {
@@ -22,6 +27,32 @@ export class LabelShowcaseComponent {
       description: 'Sets the direction of the label to be either "vertical" or "horizontal".',
       defaultValue: "'horizontal'",
       type: ["'vertical' | 'horizontal' "],
+    },
+  ];
+
+  customCssPropertiesColumns: ApiDescriptionPropertyColumns = {
+    name: 'Attribute',
+    description: 'Description',
+    default: 'Default',
+  };
+  customCssProperties: ApiDescriptionProperty[] = [
+    {
+      name: '--kirby-label-font-size',
+      description: 'Sets the font size of the label.',
+      defaultValue: '1rem',
+      type: ['string'],
+    },
+    {
+      name: '--kirby-label-font-weight',
+      description: 'Sets the font weight of the label.',
+      defaultValue: '400',
+      type: ['string'],
+    },
+    {
+      name: '--kirby-label-color',
+      description: 'Sets the color of the label.',
+      defaultValue: 'var(--kirby-color-text)',
+      type: ['string'],
     },
   ];
 }
