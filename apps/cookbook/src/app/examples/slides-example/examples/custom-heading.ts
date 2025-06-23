50eca6b88384cc0f346d960ce547bb7ff1407eb3
Louise <lou.bankdata.dk>
2025-06-23 15:10:14 +0200

    change h4 to h3

--- apps/cookbook/src/app/examples/slides-example/examples/custom-heading.ts
+++ apps/cookbook/src/app/examples/slides-example/examples/custom-heading.ts
@@ -5,7 +5,7 @@ import { CardModule } from '@kirbydesign/designsystem/card';
 const config = {
   selector: 'cookbook-slides-custom-heading-example',
   template: `<kirby-slides [slides]="slides"   [title]="'Title'" [showNavigation]="true">    
-  <h4 class="kirby-text-medium">Custom heading</h4>
+  <h3 class="kirby-text-medium">Custom heading</h3>
   <kirby-card *kirbySlide="let slide; let i = index" slideStretchHeight [hasPadding]="true">
     <kirby-card-header [title]="slide.title" [subtitle]="slide.subtitle"></kirby-card-header>
     <div class="card-content">
