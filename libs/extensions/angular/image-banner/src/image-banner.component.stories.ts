8b0e02197733a4807ed03c6e3defd86d985ef1b8
Louise <lou.bankdata.dk>
2025-07-08 10:57:34 +0200

    add custom heading

--- libs/extensions/angular/image-banner/src/image-banner.component.stories.ts
+++ libs/extensions/angular/image-banner/src/image-banner.component.stories.ts
@@ -42,6 +42,20 @@ export const Default: Story = {
 };
 
 /**
+ * This is a image banner with a custom header.
+ */
+export const CustomHeader: Story = {
+  args: {
+    bodyText: 'This is the body text.',
+    imagePath: 'assets/images/leaves.jpg',
+  },
+  render: (args) => ({
+    props: args,
+    template: `<kirby-x-image-banner ${argsToTemplate(args)}><h3 title>Custom header</h3></kirby-x-image-banner>`,
+  }),
+};
+
+/**
  * By default the background is a darkened and blurred version of the provided image.
  * If needed, the background blur property can be set to `light` to make the background brigther, or `none` to make the background white.
  * You can test out different background images by inserting your own link for the `imagePath` in the API table above.
