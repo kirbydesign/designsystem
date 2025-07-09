be7fd1f91fdc9d474f14487bf4b18dbb0598ca89
Louise <lou.bankdata.dk>
2025-07-09 09:30:26 +0200

    htags no visuel difference

--- apps/cookbook/src/app/examples/slides-example/examples/custom-heading.ts
+++ apps/cookbook/src/app/examples/slides-example/examples/custom-heading.ts
@@ -5,7 +5,7 @@ import { CardModule } from '@kirbydesign/designsystem/card';
 const config = {
   selector: 'cookbook-slides-custom-heading-example',
   template: `<kirby-slides [slides]="slides" [title]="'Title'" [showNavigation]="true">    
-  <h3>Custom heading</h3>
+  <h3 title class="kirby-text-medium">Custom heading</h3>
   <kirby-card *kirbySlide="let slide; let i = index" slideStretchHeight [hasPadding]="true">
     <kirby-card-header [title]="slide.title" [subtitle]="slide.subtitle"></kirby-card-header>
     <div class="card-content">
