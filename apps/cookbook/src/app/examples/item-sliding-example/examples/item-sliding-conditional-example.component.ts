3f31a9b52c408b9108ce1a82ff188d2c4b9b2b55
RasmusKjeldgaard <raskjeldgaard@gmail.com>
2025-07-17 09:04:37 +0200

    Fix cookbook build

--- apps/cookbook/src/app/examples/item-sliding-example/examples/item-sliding-conditional-example.component.ts
+++ apps/cookbook/src/app/examples/item-sliding-example/examples/item-sliding-conditional-example.component.ts
@@ -10,7 +10,7 @@ const config = {
   selector: 'cookbook-item-sliding-conditional-example',
   template: `<p>{{text}}</p>
 <kirby-list-experimental>
-  @for (item of items; track item.id; let i = $index) {
+  @for (item of items; track item.value; let i = $index) {
     <kirby-item-sliding
       [swipeActions]="getSwipeActions(i)"
     >
