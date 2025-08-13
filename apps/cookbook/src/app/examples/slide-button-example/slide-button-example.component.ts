7d45c5bdd6917b535b3a1217fa7b6a87e80564ec
Søren Oest Balmer <oestbalmer@gmail.com>
2025-08-13 11:01:40 +0200

    removed unused config

--- apps/cookbook/src/app/examples/slide-button-example/slide-button-example.component.ts
+++ apps/cookbook/src/app/examples/slide-button-example/slide-button-example.component.ts
@@ -1,30 +1,11 @@
 import { Component } from '@angular/core';
-
-import { AlertConfig, ModalController } from '@kirbydesign/designsystem';
 import { SimpleSlideButtonExampleComponent } from './examples/simple';
 import { ExpandBlockSlideButtonExampleComponent } from './examples/expand-block';
 
-const config = {
-  template: `<kirby-slide-button
-  [text]="'Slide to confirm'"
-  aria-label="Confirm"
-  (slideDone)="showAlert()"
-  aria-label="Unlock"
-></kirby-slide-button>
-<kirby-slide-button
-  [text]="'Slide to confirm'"
-  aria-label="Confirm"
-  expand="block"
-  (slideDone)="showAlert()"
-  aria-label="Transfer"
-></kirby-slide-button>`,
-};
 @Component({
   selector: 'cookbook-slide-button-example',
   templateUrl: './slide-button-example.component.html',
   styleUrl: './slide-button-example.component.scss',
   imports: [SimpleSlideButtonExampleComponent, ExpandBlockSlideButtonExampleComponent],
 })
-export class SlideButtonExampleComponent {
-  template: string = config.template;
-}
+export class SlideButtonExampleComponent {}
