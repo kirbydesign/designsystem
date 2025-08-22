c2002e9a2df002aecd66aec41c0954e688bd0bf0
Søren Oest Balmer <oestbalmer@gmail.com>
2025-08-22 10:59:44 +0200

    character counter screen reader announcer

--- libs/designsystem/form-field/src/input-counter/input-counter.component.ts
+++ libs/designsystem/form-field/src/input-counter/input-counter.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component, Inject, Input, LOCALE_ID, OnDestroy, OnInit } from '@angular/core';
+import { debounceTime, map, skip, Subscription, tap } from 'rxjs';
 
 import { InputComponent } from '../input/input.component';
 
@@ -8,13 +8,16 @@ import { TextareaComponent } from './../textarea/textarea.component';
 @Component({
   selector: 'kirby-input-counter',
   templateUrl: './input-counter.component.html',
+  styleUrls: ['./input-counter.component.scss'],
   standalone: false,
 })
 export class InputCounterComponent implements OnInit, OnDestroy {
   @Input() listenTo: InputComponent | TextareaComponent;
   length: number;
-  maxlength: number;
+  maxlength: number | undefined;
   private _inputChangeSubscription: Subscription;
+  textToAnnounce: string;
+  lastAnnouncedLength: number;
 
   get text(): string {
     if (this.length === undefined) {
@@ -24,19 +27,41 @@ export class InputCounterComponent implements OnInit, OnDestroy {
     return `${this.length}${ofMaxlength}`;
   }
 
+  constructor(@Inject(LOCALE_ID) locale: string) {}
+
   ngOnInit(): void {
     if (this.listenTo) {
       this.length = this.listenTo.value ? this.listenTo.value.length : 0;
-      this.maxlength = this.listenTo.maxlength;
-      this._inputChangeSubscription = this.listenTo.kirbyChange.subscribe((value) => {
-        this.length = value?.length || 0;
-      });
+      this.maxlength = this.maxlength = this.listenTo.maxlength
+        ? +this.listenTo.maxlength
+        : undefined;
+
+      this._inputChangeSubscription = this.listenTo.kirbyChange
+        .pipe(
+          skip(1),
+          tap((value) => (this.length = value?.length || 0)),
+          debounceTime(1000)
+        )
+        .subscribe((_) => {
+          this.announceText();
+        });
     }
   }
 
   ngOnDestroy(): void {
-    if (this._inputChangeSubscription) {
-      this._inputChangeSubscription.unsubscribe();
+    this._inputChangeSubscription?.unsubscribe();
+  }
+
+  announceText(): void {
+    if (this.maxlength === undefined) {
+      this.textToAnnounce = this.length + ' characters entered';
+    } else {
+      if (this.maxlength === this.length) {
+        this.textToAnnounce = 'maximum characters reached';
+      } else {
+        const lengthDiff = this.maxlength - this.length;
+        this.textToAnnounce = `${lengthDiff} character${lengthDiff === 1 ? '' : 's'} remaining`;
+      }
     }
   }
 }
