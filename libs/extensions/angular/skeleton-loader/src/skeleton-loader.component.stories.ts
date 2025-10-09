65734c996e4f564a7fbb5539b228e431df380005
RasmusKjeldgaard <raskjeldgaard@gmail.com>
2025-10-09 22:21:01 +0200

    Fix themecolor for skeleton card example

--- libs/extensions/angular/skeleton-loader/src/skeleton-loader.component.stories.ts
+++ libs/extensions/angular/skeleton-loader/src/skeleton-loader.component.stories.ts
@@ -3,6 +3,7 @@ import { SkeletonLoaderComponent } from '@kirbydesign/extensions-angular/skeleto
 import { ButtonComponent } from '@kirbydesign/designsystem/button';
 import { CardComponent } from '@kirbydesign/designsystem/card';
 import { ItemComponent } from '@kirbydesign/designsystem/item';
+import { ThemeColorDirective } from '@kirbydesign/designsystem/shared';
 
 /**
  * A skeleton loader is a visual indicator that is used to render placeholder content and mimic the full content shown when loaded.
@@ -17,7 +18,7 @@ const meta: Meta<SkeletonLoaderComponent> = {
   title: 'Components/Loaders/Skeleton Loader',
   decorators: [
     moduleMetadata({
-      imports: [ButtonComponent, CardComponent, ItemComponent],
+      imports: [ButtonComponent, CardComponent, ItemComponent, ThemeColorDirective],
     }),
   ],
   args: {
