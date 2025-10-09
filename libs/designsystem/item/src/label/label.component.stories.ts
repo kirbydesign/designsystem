07f338b0490f9abe55a0f5f28b45367de2791b8f
RasmusKjeldgaard <raskjeldgaard@gmail.com>
2025-10-09 17:36:24 +0200

    Revert unintentional change

--- libs/designsystem/item/src/label/label.component.stories.ts
+++ libs/designsystem/item/src/label/label.component.stories.ts
@@ -1,14 +1,10 @@
 import { argsToTemplate, type Meta, type StoryObj } from '@storybook/angular';
 
 import { LabelComponent } from '@kirbydesign/designsystem/item';
-import { responsiveModes } from 'tools/storybook-config/shared-config';
 
 const meta: Meta<LabelComponent> = {
   component: LabelComponent,
   title: 'Components / Label',
-  parameters: {
-    chromatic: { modes: { ...responsiveModes } },
-  },
 };
 export default meta;
 type Story = StoryObj<LabelComponent>;
@@ -20,15 +16,5 @@ export const Label: Story = {
   render: (args) => ({
     props: args,
     template: `<kirby-label ${argsToTemplate(args)}>Label</kirby-label>`,
-  }),
-};
-
-export const HorizontalLabel: Story = {
-  args: {
-    direction: 'horizontal',
-  },
-  render: (args) => ({
-    props: args,
-    template: `<kirby-label ${argsToTemplate(args)}><p>My Label Text that is horizontal</p><data>Value with some length</data></kirby-label>`,
   }),
 };
