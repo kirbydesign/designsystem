5d16b7758b75569477bf1e9af6aa3b7404d6cfde
Jonaythaz <jonasplesner@gmail.com>
2025-10-17 07:52:52 +0200

    added module to allow convenient import of all three sidebar components at once

--- libs/extensions/angular/sidebar-menu/src/index.ts
+++ libs/extensions/angular/sidebar-menu/src/index.ts
@@ -2,3 +2,4 @@ export * from './models';
 export * from './public-components/sidebar';
 export * from './public-components/sidebar-footer';
 export * from './public-components/sidebar-header';
+export * from './sidebar.module';
