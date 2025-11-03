91c584c96662c65e20949d1081bb18d2fd738202
Louise <lou.bankdata.dk>
2025-11-03 13:45:58 +0100

    preburrner added

--- apps/cookbook/src/app/examples/item-example/examples/ellipsis-behavior.ts
+++ apps/cookbook/src/app/examples/item-example/examples/ellipsis-behavior.ts
@@ -1,57 +1,19 @@
 import { Component } from '@angular/core';
 import { ItemModule } from '@kirbydesign/designsystem/item';
-import { BadgeComponent } from '@kirbydesign/designsystem/badge';
-import { AvatarComponent, IconModule } from '@kirbydesign/designsystem';
 
 const config = {
   selector: 'cookbook-item-example-ellipsis-behavior',
-  template: `<kirby-item>
-  <kirby-avatar slot="start" overlay="true" imageSrc="/assets/images/woman.png">
-    <kirby-badge>
-      <kirby-icon name="attach"></kirby-icon>
-    </kirby-badge>
-  </kirby-avatar>
-  <kirby-label>
-    <kirby-label direction="horizontal">  
-        <kirby-label >
-            <p class="kirby-item-title">Fusce id neque suscipit, finibus urna convallis, auctor arcu.</p> 
-        </kirby-label>
-        <kirby-label >
-            <data class="kirby-item-detail" slot="end">22.86%</data> 
-        </kirby-label>    
-    </kirby-label>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non neque vitae felis ultricies imperdiet in ut orci.</p>
-    </kirby-label>
+  template: `
+<kirby-item>
+    <p class="kirby-item-title">Fusce id neque suscipit, finibus urna convallis, auctor arcu.</p>
+    <data class="kirby-item-detail" slot="end">22.86%</data> 
 </kirby-item>
 
 <kirby-item>
-  <kirby-avatar slot="start" overlay="true" imageSrc="/assets/images/woman.png">
-    <kirby-badge>
-      <kirby-icon name="attach"></kirby-icon>
-    </kirby-badge>
-  </kirby-avatar>
-  <kirby-label>
     <kirby-label direction="horizontal">
-      <p class="kirby-item-title">Fusce id neque suscipit, finibus urna convallis, auctor arcu.</p>
-      <data class="kirby-item-detail">22.86%</data> 
+        <p class="kirby-item-title" >Fusce id neque suscipit, finibus urna convallis, auctor arcu.</p>
+        <data class="kirby-item-detail">22.86%</data> 
     </kirby-label>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non neque vitae felis ultricies imperdiet in ut orci.</p>
-  </kirby-label>
-</kirby-item>
-
-<kirby-item>
-  <kirby-avatar slot="start" overlay="true" imageSrc="/assets/images/woman.png">
-    <kirby-badge>
-      <kirby-icon name="attach"></kirby-icon>
-    </kirby-badge>
-  </kirby-avatar>
-    <kirby-label>
-        <kirby-label direction="horizontal">
-            <p class="kirby-item-title" slot="start">Fusce id neque suscipit, finibus urna convallis, auctor arcu.</p>
-            <data class="kirby-item-detail" slot="end">22.86%</data> 
-        </kirby-label>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non neque vitae felis ultricies imperdiet in ut orci.</p>
-        </kirby-label>
 </kirby-item>
 `,
 };
@@ -60,7 +22,7 @@ const config = {
   selector: config.selector,
   template: config.template,
   styleUrls: ['./_shared.scss'],
-  imports: [ItemModule, IconModule, BadgeComponent, AvatarComponent],
+  imports: [ItemModule],
 })
 export class ItemExampleEllipsisBehaviorComponent {
   template: string = config.template;
