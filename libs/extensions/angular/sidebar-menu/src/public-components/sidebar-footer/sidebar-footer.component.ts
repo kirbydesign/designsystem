ad4f2028ab899e063b031e6112b106258f45bfea
Jonaythaz <jonasplesner@gmail.com>
2025-11-06 15:09:38 +0100

    Enhancement: adjusted header and footer styling to allow more customization

--- libs/extensions/angular/sidebar-menu/src/public-components/sidebar-footer/sidebar-footer.component.ts
+++ libs/extensions/angular/sidebar-menu/src/public-components/sidebar-footer/sidebar-footer.component.ts
@@ -3,5 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'kirby-x-sidebar-footer',
   template: '<ng-content></ng-content>',
+  styles: `
+    :host {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      min-height: 80px;
+    }
+  `,
 })
 export class SidebarFooterComponent {}
--- libs/extensions/angular/sidebar-menu/src/public-components/sidebar-header/sidebar-header.component.ts
+++ libs/extensions/angular/sidebar-menu/src/public-components/sidebar-header/sidebar-header.component.ts
@@ -2,16 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'kirby-x-sidebar-header',
-  template: '<span class="sidebar-logo"><ng-content></ng-content></span>',
-  styles: [
-    `
-      .sidebar-logo {
-        display: flex;
-        align-items: center;
-        margin: 32px 0;
-        height: 50px;
-      }
-    `,
-  ],
+  template: '<ng-content></ng-content>',
+  styles: `
+    :host {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      height: var(--kirby-x-sidebar-header-height, 50px);
+      padding: var(--kirby-x-sidebar-header-padding-top, 32px)
+        var(--kirby-x-sidebar-header-padding-right, 24px)
+        var(--kirby-x-sidebar-header-padding-bottom, 32px)
+        var(--kirby-x-sidebar-header-padding-left, 24px);
+    }
+  `,
 })
 export class SidebarHeaderComponent {}
