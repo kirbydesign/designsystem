af1b74678cfb80c56c3521350469a4240582e568
RasmusKjeldgaard <raskjeldgaard@gmail.com>
2025-11-11 10:03:04 +0100

    Use new plugin in extraction script

--- scripts/sass-extract-to-ts.js
+++ scripts/sass-extract-to-ts.js
@@ -1,11 +1,12 @@
 import { SassToTypescriptEngine } from '../tools/sass-to-ts/dist/sass-to-ts.engine.js';
+import projectConfig from '../apps/cookbook/project.json' with { type: 'json' };
+
+const plugins = projectConfig.targets.build.options.plugins;
+
+const filesToTransform = plugins.find((plugin) => plugin.path.includes('sass-to-ts'))?.options
+  .transform;
 
 // Configuration for SCSS to TypeScript transformation
-const filesToTransform = [
-  'libs/core/src/helpers/color-helper.scss',
-  'libs/core/src/helpers/design-token-helper.scss',
-];
-
 console.log('Transforming SCSS files to `*.styles.ts`:');
 console.log(filesToTransform);
 
