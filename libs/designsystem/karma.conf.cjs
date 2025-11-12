b556c9da8f583a3e3927c941957d77be718b27f1
RasmusKjeldgaard <raskjeldgaard@gmail.com>
2025-11-12 11:57:29 +0100

    Use cjs file endings for karma conf to avoid esmodule migration

--- libs/designsystem/karma.conf.cjs
+++ libs/designsystem/karma.conf.cjs
@@ -2,7 +2,7 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 const { join } = require('path');
-const getBaseKarmaConfig = require('../../karma.conf');
+const getBaseKarmaConfig = require('../../karma.conf.cjs');
 
 module.exports = function (config) {
   const baseConfig = getBaseKarmaConfig();
