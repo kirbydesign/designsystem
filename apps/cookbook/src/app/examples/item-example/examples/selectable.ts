9e2d47e69c4afb1afc39b934eed9ea9e1dfc49b1
Søren Oest Balmer <oestbalmer@gmail.com>
2025-11-13 09:47:15 +0100

    using outline instead of box shadow since item is a container

--- apps/cookbook/src/app/examples/item-example/examples/selectable.ts
+++ apps/cookbook/src/app/examples/item-example/examples/selectable.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '@kirbydesign/designsystem';
 import { ItemComponent } from '@kirbydesign/designsystem/item';
 
 const config = {
   selector: 'cookbook-item-example-selectable',
-  template: `<kirby-item [selectable]="true">
+  template: `
+  <kirby-item [selectable]="true">
   <p class="kirby-item-title">Selectable</p>
 </kirby-item>
 
@@ -13,14 +15,24 @@ const config = {
 
 <kirby-item href="https://github.com/kirbydesign/designsystem" disclosure="link" target="_blank">
   <p class="kirby-item-title">Link with disclosure</p>
-</kirby-item>`,
+</kirby-item><kirby-card><kirby-item [selectable]="true">
+  <p class="kirby-item-title">Selectable</p>
+</kirby-item>
+
+<kirby-item [selectable]="true" disclosure="arrow-more">
+  <p class="kirby-item-title">Selectable with disclosure</p>
+</kirby-item>
+
+<kirby-item href="https://github.com/kirbydesign/designsystem" disclosure="link" target="_blank">
+  <p class="kirby-item-title">Link with disclosure</p>
+</kirby-item></kirby-card>`,
 };
 
 @Component({
   selector: config.selector,
   template: config.template,
   styleUrls: ['./_shared.scss'],
-  imports: [ItemComponent],
+  imports: [ItemComponent, CardComponent],
 })
 export class ItemExampleSelectableComponent {
   template: string = config.template;
