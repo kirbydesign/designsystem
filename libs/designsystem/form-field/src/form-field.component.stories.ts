019d9b5130ddde103400b6ce70c01f4852260323
Søren Oest Balmer <oestbalmer@gmail.com>
2025-11-14 11:12:00 +0100

    made correct storybook story

--- libs/designsystem/form-field/src/form-field.component.stories.ts
+++ libs/designsystem/form-field/src/form-field.component.stories.ts
@@ -1,10 +1,4 @@
-import {
-  argsToTemplate,
-  componentWrapperDecorator,
-  type Meta,
-  moduleMetadata,
-  type StoryObj,
-} from '@storybook/angular';
+import { argsToTemplate, type Meta, moduleMetadata, type StoryObj } from '@storybook/angular';
 import { userEvent, within } from 'storybook/test';
 
 import {
@@ -15,33 +9,8 @@ import {
 } from '@kirbydesign/designsystem/form-field';
 
 import { IconComponent } from '@kirbydesign/designsystem';
-import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { FormFieldExampleComponent } from '~/app/examples/form-field-example/form-field-example.component';
-
-@Component({
-  // eslint-disable-next-line @angular-eslint/component-selector
-  selector: 'text-area-form-field-wrapper',
-  imports: [FormFieldModule, ReactiveFormsModule, TextareaComponent],
-  template: `
-    <kirby-form-field label="test">
-      <textarea
-        rows="3"
-        kirby-textarea
-        [formControl]="control"
-        [maxlength]="1000"
-        #textarea
-      ></textarea>
-      <kirby-input-counter [listenTo]="textarea"></kirby-input-counter>
-    </kirby-form-field>
-  `,
-})
-class TextAreaFormFieldWrapperComponent {
-  readonly control = new FormControl<string>('');
-  constructor() {
-    this.control.setValue('test');
-  }
-}
 
 const meta: Meta<FormFieldComponent> = {
   component: FormFieldComponent,
@@ -91,13 +60,21 @@ export const InputCounter: Story = {
   }),
 };
 
-export const TextAreaInputCounter: Story = {
+export const TextareaWithInputCounter: Story = {
   decorators: [
     moduleMetadata({
-      imports: [TextAreaFormFieldWrapperComponent],
+      imports: [ReactiveFormsModule],
     }),
-    componentWrapperDecorator(TextAreaFormFieldWrapperComponent),
   ],
+  render: () => ({
+    props: {
+      textControl: new FormControl('Text area in form control'),
+    },
+    template: `<kirby-form-field label="Textarea with label and message" >
+    <textarea kirby-textarea [formControl]="textControl" #textarea></textarea>
+    <kirby-input-counter [listenTo]="textarea"></kirby-input-counter>
+  </kirby-form-field>`,
+  }),
 };
 
 export const CookbookExample: Story = {
