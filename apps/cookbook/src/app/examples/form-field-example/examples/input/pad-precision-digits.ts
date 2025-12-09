cbd98dccd943c80d807fc1f80e3918c37d28dcaa
RasmusKjeldgaard <raskjeldgaard@gmail.com>
2025-12-09 16:26:39 +0100

    Add temp manual test

--- apps/cookbook/src/app/examples/form-field-example/examples/input/pad-precision-digits.ts
+++ apps/cookbook/src/app/examples/form-field-example/examples/input/pad-precision-digits.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
-import { InputSize } from '@kirbydesign/designsystem';
+import { ButtonComponent, InputSize } from '@kirbydesign/designsystem';
 import {
   DateInputDirective,
   DecimalMaskDirective,
@@ -10,18 +11,39 @@ import {
 
 const config = {
   selector: 'cookbook-form-field-input-pad-precision-digits-example',
-  template: `<kirby-form-field label="Decimal mask with pad precision digits">
-  <input type="number" kirby-input kirby-decimal-mask [padPrecisionDigits]="true"/>
+  template: `<div [formGroup]="form">
+<kirby-form-field label="Decimal mask with pad precision digits">
+  <input type="number" kirby-input kirby-decimal-mask formControlName="amount" [precision]="4" [padPrecisionDigits]="true"/>
 </kirby-form-field>
+</div>
+<button (click)="changeAmount()" kirby-button>Klik på mig</button> 
 `,
 };
 
 @Component({
   selector: config.selector,
   template: config.template,
-  imports: [FormFieldComponent, DateInputDirective, InputComponent, DecimalMaskDirective],
+  imports: [
+    FormFieldComponent,
+    DateInputDirective,
+    InputComponent,
+    ReactiveFormsModule,
+    DecimalMaskDirective,
+    ButtonComponent,
+  ],
 })
 export class FormFieldInputPadPrecisionDigitsExampleComponent {
   template: string = config.template;
   @Input() size: InputSize;
+
+  protected readonly form = new FormGroup({
+    amount: new FormControl<number | null>(null),
+  });
+
+  changeAmount() {
+    this.form.patchValue({
+      amount: 4,
+    });
+    console.log('Forms example: updating value with new value');
+  }
 }
