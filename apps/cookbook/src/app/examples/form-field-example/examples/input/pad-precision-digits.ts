4ba8a46ee92bd0981aaa70d850fb6ae3e306ecfc
RasmusKjeldgaard <raskjeldgaard@gmail.com>
2025-12-18 16:14:18 +0100

    Revert temp docs changes

--- apps/cookbook/src/app/examples/form-field-example/examples/input/pad-precision-digits.ts
+++ apps/cookbook/src/app/examples/form-field-example/examples/input/pad-precision-digits.ts
@@ -1,7 +1,6 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, Input } from '@angular/core';
 
-import { ButtonComponent, InputSize } from '@kirbydesign/designsystem';
+import { InputSize } from '@kirbydesign/designsystem';
 import {
   DateInputDirective,
   DecimalMaskDirective,
@@ -11,39 +10,18 @@ import {
 
 const config = {
   selector: 'cookbook-form-field-input-pad-precision-digits-example',
-  template: `<div [formGroup]="form">
-<kirby-form-field label="Decimal mask with pad precision digits">
-  <input type="number" kirby-input kirby-decimal-mask formControlName="amount" [precision]="4" [padPrecisionDigits]="true"/>
+  template: `<kirby-form-field label="Decimal mask with pad precision digits">
+  <input type="number" kirby-input kirby-decimal-mask [padPrecisionDigits]="true"/>
 </kirby-form-field>
-</div>
-<button (click)="changeAmount()" kirby-button>Klik på mig</button>
 `,
 };
 
 @Component({
   selector: config.selector,
   template: config.template,
-  imports: [
-    FormFieldComponent,
-    DateInputDirective,
-    InputComponent,
-    ReactiveFormsModule,
-    DecimalMaskDirective,
-    ButtonComponent,
-  ],
+  imports: [FormFieldComponent, DateInputDirective, InputComponent, DecimalMaskDirective],
 })
 export class FormFieldInputPadPrecisionDigitsExampleComponent {
   template: string = config.template;
   @Input() size: InputSize;
-
-  protected readonly form = new FormGroup({
-    amount: new FormControl<number | null>(null),
-  });
-
-  changeAmount() {
-    this.form.patchValue({
-      amount: 4,
-    });
-    console.log('Forms example: updating value with new value');
-  }
 }
