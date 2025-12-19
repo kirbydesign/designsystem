5d9c3e8f88a9745f6130115ae0d5850e53b690e2
RasmusKjeldgaard <raskjeldgaard@gmail.com>
2025-12-19 12:50:42 +0100

    Extract base pixel value in helper

--- libs/core/src/helpers/design-token-helper.ts
+++ libs/core/src/helpers/design-token-helper.ts
@@ -2,6 +2,8 @@ import { ColorHelper } from './color-helper';
 import { styles } from './design-token-helper.styles';
 import { ThemeColor } from './theme-color.type';
 
+const BASE_PIXEL_VALUE = 16;
+
 export class DesignTokenHelper {
   public static readonly breakpoints = styles.breakpoints;
   public static readonly softKeyboardTransitionEnter = styles.softKeyboardTransitionEnter;
@@ -55,13 +57,13 @@ export class DesignTokenHelper {
 
   public static fontSize(key: keyof typeof styles.fontSizes): string {
     const remValue = styles.fontSizes[key];
-    const remToPxValue = parseFloat(remValue) * 16;
+    const remToPxValue = parseFloat(remValue) * BASE_PIXEL_VALUE;
     return `${remToPxValue}px`;
   }
 
   public static iconFontSize(key: keyof typeof styles.iconFontSizes): string {
     const remValue = styles.iconFontSizes[key];
-    const remToPxValue = parseFloat(remValue) * 16;
+    const remToPxValue = parseFloat(remValue) * BASE_PIXEL_VALUE;
 
     return `${remToPxValue}px`;
   }
