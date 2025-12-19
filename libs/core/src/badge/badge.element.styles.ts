3bd589125d0aca3321092b72cb088fa6126c0a84
RasmusKjeldgaard <raskjeldgaard@gmail.com>
2025-12-19 14:47:49 +0100

    Rename to relative-size throughout

--- libs/core/src/badge/badge.element.styles.ts
+++ libs/core/src/badge/badge.element.styles.ts
@@ -24,8 +24,8 @@ export default css`
     justify-content: center;
     box-sizing: border-box;
     padding: 0.3em 0.5em;
-    min-height: var(--kirby-relative-spacing-s);
-    min-width: var(--kirby-relative-spacing-s);
+    min-height: var(--kirby-relative-size-s);
+    min-width: var(--kirby-relative-size-s);
   }
 
   :host([themecolor='white']) {
@@ -59,8 +59,8 @@ export default css`
     padding: initial;
     min-height: initial;
     min-width: initial;
-    height: var(--kirby-relative-spacing-xxs);
-    width: var(--kirby-relative-spacing-xxs);
+    height: var(--kirby-relative-size-xxs);
+    width: var(--kirby-relative-size-xxs);
   }
 
   ::slotted(kirby-icon) {
