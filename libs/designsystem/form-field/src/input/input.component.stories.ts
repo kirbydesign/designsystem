adb21cb5f7d0f2fdecf5cc53125504915a0eb689
RasmusKjeldgaard <rkk@bankdata.dk>
2026-01-05 10:36:07 +0100

    Input: format inputmask correctly when padded precision digits are set via form control (#4198)

--- libs/designsystem/form-field/src/input/input.component.stories.ts
+++ libs/designsystem/form-field/src/input/input.component.stories.ts
@@ -1,6 +1,11 @@
-import type { Meta, StoryObj } from '@storybook/angular';
-
-import { InputComponent, InputSize } from '@kirbydesign/designsystem/form-field';
+import { argsToTemplate, type Meta, moduleMetadata, type StoryObj } from '@storybook/angular';
+import {
+  DecimalMaskDirective,
+  FormFieldComponent,
+  InputComponent,
+  InputSize,
+} from '@kirbydesign/designsystem/form-field';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 type InputProps = InputComponent & { placeholder?: string };
 
@@ -32,6 +37,25 @@ export const Input: Story = {
   },
   render: (args) => ({
     props: args,
-    template: `<input kirby-input />`,
+    template: `<input kirby-input ${argsToTemplate(args)} />`,
+  }),
+};
+
+export const InputWithDecimalMaskAndPrecision: Story = {
+  decorators: [
+    moduleMetadata({
+      imports: [ReactiveFormsModule, DecimalMaskDirective, FormFieldComponent],
+    }),
+  ],
+  render: () => ({
+    props: {
+      numberControl: new FormControl(1.2345),
+    },
+    template: `<kirby-form-field label="Decimal Mask with padded default precision">
+  <input kirby-input kirby-decimal-mask [formControl]="numberControl" />
+</kirby-form-field>
+<kirby-form-field label="Decimal Mask with padded precision of 4">
+    <input kirby-input precision="4" kirby-decimal-mask [formControl]="numberControl" />
+</kirby-form-field>`,
   }),
 };
