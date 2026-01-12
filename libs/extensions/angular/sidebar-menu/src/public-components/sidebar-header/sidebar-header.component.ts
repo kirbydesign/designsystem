05489a1f4f24d5cd10d186158a3bfa70c4938aa9
Jonaythaz <jonasplesner@gmail.com>
2026-01-12 07:55:42 +0100

    fixed sizing for content provided in logo slot

--- libs/extensions/angular/sidebar-menu/src/public-components/sidebar-header/sidebar-header.component.ts
+++ libs/extensions/angular/sidebar-menu/src/public-components/sidebar-header/sidebar-header.component.ts
@@ -28,14 +28,14 @@ import { Component } from '@angular/core';
     }
 
     :host::ng-deep [slot='logo'] {
-      height: inherit;
+      display: inline-flex;
     }
 
     :host::ng-deep [slot='logo'] img,
     :host::ng-deep img[slot='logo'] {
       object-fit: contain;
-      width: 100%;
-      height: 100%;
+      max-width: 100%;
+      max-height: 100%;
     }
 
     .sidebar-logo {
