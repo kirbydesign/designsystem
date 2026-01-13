aeda876adc2460876347c81eef985208c8346b0c
RasmusKjeldgaard <raskjeldgaard@gmail.com>
2026-01-13 16:21:12 +0100

    Add more visual tests for popout direction

--- libs/designsystem/dropdown/src/dropdown.component.stories.ts
+++ libs/designsystem/dropdown/src/dropdown.component.stories.ts
@@ -43,7 +43,6 @@ export const Dropdown: Story = {
     size: 'md',
     selectedIndex: -1,
     tabindex: 0,
-    usePopover: false,
   },
   argTypes: {
     attentionLevel: {
@@ -121,6 +120,105 @@ export const DropdownOpened: Story = {
     const dropdownToOpen = canvas.getByRole('combobox');
 
     await userEvent.click(dropdownToOpen);
+  },
+};
+
+export const DropdownOpenedTopRight: Story = {
+  args: {
+    items: items,
+    selectedIndex: 0,
+    popout: 'left',
+  },
+  render: (args) => ({
+    props: args,
+    template: `
+      <div style="display: flex; justify-content: flex-end; width: 100%;">
+        <kirby-dropdown ${argsToTemplate(args)}></kirby-dropdown>
+      </div>
+    `,
+  }),
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const dropdown = canvas.getByRole('combobox');
+    await userEvent.click(dropdown);
+
+    await waitFor(() => {
+      expect(dropdown).toHaveAttribute('aria-expanded', 'true');
+    });
+  },
+};
+
+export const DropdownOpenedAtBottom: Story = {
+  args: {
+    items: items,
+    selectedIndex: 0,
+  },
+  render: (args) => ({
+    props: args,
+    template: `
+      <div style="display: flex; flex-direction: column; justify-content: flex-end; height: 400px;">
+        <kirby-dropdown ${argsToTemplate(args)}></kirby-dropdown>
+      </div>
+    `,
+  }),
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const dropdown = canvas.getByRole('combobox');
+    await userEvent.click(dropdown);
+
+    await waitFor(() => {
+      expect(dropdown).toHaveAttribute('aria-expanded', 'true');
+    });
+  },
+};
+
+export const DropdownOpenedBlockExpand: Story = {
+  args: {
+    items: items,
+    selectedIndex: 0,
+    expand: 'block',
+  },
+  render: (args) => ({
+    props: args,
+    template: `
+      <div style="width: 300px;">
+        <kirby-dropdown ${argsToTemplate(args)}></kirby-dropdown>
+      </div>
+    `,
+  }),
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const dropdown = canvas.getByRole('combobox');
+    await userEvent.click(dropdown);
+
+    await waitFor(() => {
+      expect(dropdown).toHaveAttribute('aria-expanded', 'true');
+    });
+  },
+};
+
+export const MultipleDropdowns: Story = {
+  args: {
+    items: items,
+    selectedIndex: 0,
+  },
+  render: (args) => ({
+    props: args,
+    template: `
+      <div style="display: flex; gap: 16px;">
+        <kirby-dropdown ${argsToTemplate(args)} data-testid="dropdown-1"></kirby-dropdown>
+        <kirby-dropdown ${argsToTemplate(args)} data-testid="dropdown-2"></kirby-dropdown>
+      </div>
+    `,
+  }),
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const dropdown1 = canvas.getByTestId('dropdown-1').querySelector('[role="combobox"]');
+    await userEvent.click(dropdown1);
+
+    await waitFor(() => {
+      expect(dropdown1).toHaveAttribute('aria-expanded', 'true');
+    });
   },
 };
 
