16eb6c312ee71482e136b093bb39ea2497c10201
RasmusKjeldgaard <raskjeldgaard@gmail.com>
2026-01-15 17:31:57 +0100

    Update docs with new example structure

--- apps/cookbook/src/app/showcase/toast-showcase/toast-showcase.component.ts
+++ apps/cookbook/src/app/showcase/toast-showcase/toast-showcase.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { CodeViewerComponent } from '../../shared/code-viewer/code-viewer.component';
 import { ApiDescriptionPropertiesComponent } from '../../shared/api-description/api-description-properties/api-description-properties.component';
+import {
+  ApiDescriptionMethod,
+  ApiDescriptionMethodsComponent,
+} from '../../shared/api-description/api-description-methods/api-description-methods.component';
 import { ExampleViewerComponent } from '~/app/shared/example-viewer/example-viewer.component';
 import { ApiDescriptionProperty } from '~/app/shared/api-description/api-description-properties/api-description-properties.component';
-import { IphoneComponent } from '~/app/iphone/iphone.component';
-import { ToastExampleComponent } from '~/app/examples/toast-example/toast-example.component';
+import { ToastExampleDefaultComponent } from '~/app/examples/toast-example/examples/default';
+import { ToastExampleDurationComponent } from '~/app/examples/toast-example/examples/duration';
+import { ToastExampleDismissComponent } from '~/app/examples/toast-example/examples/dismiss';
 import { ImportViewerComponent } from '~/app/shared/import-code-viewer/import-code-viewer.component';
 
 @Component({
@@ -15,32 +20,62 @@ import { ImportViewerComponent } from '~/app/shared/import-code-viewer/import-co
     CodeViewerComponent,
     ExampleViewerComponent,
     ApiDescriptionPropertiesComponent,
-    IphoneComponent,
+    ApiDescriptionMethodsComponent,
+    ToastExampleDefaultComponent,
+    ToastExampleDurationComponent,
+    ToastExampleDismissComponent,
     ImportViewerComponent,
   ],
 })
 export class ToastShowcaseComponent {
-  toastExample = ToastExampleComponent;
+  methods: ApiDescriptionMethod[] = [
+    {
+      name: 'showToast',
+      description:
+        'Shows a toast with the given configuration. Returns a Promise that resolves to an Overlay object which can be used to dismiss the toast programmatically.',
+      signature: 'showToast(config: ToastConfig, onCloseToast?: () => void): Promise<Overlay>',
+    },
+  ];
 
   properties: ApiDescriptionProperty[] = [
     {
       name: 'message',
-      description: 'Sets the message for the toast',
+      description: 'Sets the message for the toast.',
       defaultValue: 'null',
       type: ['string'],
     },
     {
       name: 'messageType',
       description:
-        "Message type defines which color the toast will have. There are two different toast types: 'success' (green) and 'warning' (yellow)",
+        "Message type defines which color the toast will have. There are two different toast types: 'success' (green) and 'warning' (yellow).",
       defaultValue: 'success',
       type: ['success', 'warning'],
     },
     {
       name: 'durationInMs',
-      description: '(Optional) Duration in milliseconds before the toast dismisses automatically.',
+      description:
+        'Duration in milliseconds before the toast dismisses automatically. Set to 0 to keep the toast visible until dismissed programmatically.',
       defaultValue: '4000',
       type: ['number'],
+    },
+  ];
+
+  overlayProperties: ApiDescriptionProperty[] = [
+    {
+      name: 'dismiss',
+      description:
+        'Dismisses the toast. Returns a Promise that resolves when the toast has been dismissed.',
+      type: ['(data?: any) => Promise<boolean>'],
+    },
+    {
+      name: 'onWillDismiss',
+      description: 'A Promise that resolves when the toast is about to be dismissed.',
+      type: ['Promise<OverlayEventDetail>'],
+    },
+    {
+      name: 'onDidDismiss',
+      description: 'A Promise that resolves when the toast has been dismissed.',
+      type: ['Promise<OverlayEventDetail>'],
     },
   ];
 }
