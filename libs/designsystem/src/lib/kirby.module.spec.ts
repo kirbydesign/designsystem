04d43fe614b0766ea633e769d8caab3585a17d75
Jonaythaz <jonasplesner@gmail.com>
2026-02-18 11:51:25 +0100

    adjusted KirbyModule tests since it no longer initializes modalcontroller itself

--- libs/designsystem/src/lib/kirby.module.spec.ts
+++ libs/designsystem/src/lib/kirby.module.spec.ts
@@ -1,20 +1,13 @@
-import { ModalController } from '@kirbydesign/designsystem/modal';
 import { KirbyModule } from './kirby.module';
 
 describe('KirbyModule', () => {
   let kirbyModule: KirbyModule;
-  let modalControllerSpy: ModalController;
 
   beforeEach(() => {
-    modalControllerSpy = jasmine.createSpyObj<ModalController>('ModalController', ['initialize']);
     kirbyModule = new KirbyModule();
   });
 
   it('should create an instance', () => {
     expect(kirbyModule).toBeTruthy();
-  });
-
-  it('should initialize modalController', () => {
-    expect(modalControllerSpy.initialize).toHaveBeenCalledTimes(1);
   });
 });
