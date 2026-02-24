2e9c9c1c71441a1272417e5102e628271ca12e9b
Louise <lou.bankdata.dk>
2026-02-24 10:00:45 +0100

    avatar adjustments

--- apps/cookbook/src/app/examples/avatar-example/examples/colors.ts
+++ apps/cookbook/src/app/examples/avatar-example/examples/colors.ts
@@ -9,43 +9,7 @@ const config = {
   <kirby-icon name="kirby"></kirby-icon>
 </kirby-avatar>
 
-<kirby-avatar themeColor="semi-light" title="semi-light">
-  <kirby-icon name="kirby"></kirby-icon>
-</kirby-avatar>
-
-<kirby-avatar themeColor="medium" title="medium">
-  <kirby-icon name="kirby"></kirby-icon>
-</kirby-avatar>
-
 <kirby-avatar themeColor="white" title="white">
-  <kirby-icon name="kirby"></kirby-icon>
-</kirby-avatar>
-
-<kirby-avatar themeColor="dark" title="dark">
-  <kirby-icon name="kirby"></kirby-icon>
-</kirby-avatar>
-
-<kirby-avatar themeColor="primary" title="primary">
-  <kirby-icon name="kirby"></kirby-icon>
-</kirby-avatar>
-
-<kirby-avatar themeColor="secondary" title="secondary">
-  <kirby-icon name="kirby"></kirby-icon>
-</kirby-avatar>
-
-<kirby-avatar themeColor="tertiary" title="tertiary">
-  <kirby-icon name="kirby"></kirby-icon>
-</kirby-avatar>
-
-<kirby-avatar themeColor="success" title="success">
-  <kirby-icon name="kirby"></kirby-icon>
-</kirby-avatar>
-
-<kirby-avatar themeColor="warning" title="warning">
-  <kirby-icon name="kirby"></kirby-icon>
-</kirby-avatar>
-
-<kirby-avatar themeColor="danger" title="danger">
   <kirby-icon name="kirby"></kirby-icon>
 </kirby-avatar>`,
 };
--- apps/cookbook/src/app/examples/avatar-example/examples/default.ts
+++ apps/cookbook/src/app/examples/avatar-example/examples/default.ts
@@ -1,19 +1,20 @@
 import { Component } from '@angular/core';
 import { AvatarComponent } from '@kirbydesign/designsystem/avatar';
+import { IconComponent } from '@kirbydesign/designsystem/icon';
 
 const config = {
   selector: 'cookbook-avatar-example-default',
-  template: `<kirby-avatar size="xs" text="A" title="xs"></kirby-avatar>
-<kirby-avatar size="sm" text="A" title="sm"></kirby-avatar>
-<kirby-avatar size="md" text="A" title="md"></kirby-avatar>
-<kirby-avatar size="lg" text="A" title="lg"></kirby-avatar>`,
+  template: `<kirby-avatar size="xs">  <kirby-icon name="kirby"></kirby-icon></kirby-avatar>
+<kirby-avatar size="sm">  <kirby-icon name="kirby"></kirby-icon></kirby-avatar>
+<kirby-avatar size="md">  <kirby-icon name="kirby"></kirby-icon></kirby-avatar>
+<kirby-avatar size="lg">  <kirby-icon name="kirby"></kirby-icon></kirby-avatar>`,
 };
 
 @Component({
   selector: config.selector,
   template: config.template,
   styleUrls: ['./avatar-examples.shared.scss'],
-  imports: [AvatarComponent],
+  imports: [AvatarComponent, IconComponent],
 })
 export class AvatarExampleDefaultComponent {
   template: string = config.template;
