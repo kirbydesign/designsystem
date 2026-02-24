082724cdbe2dddce16e25298042eab11128210d4
Louise <lou.bankdata.dk>
2026-02-24 10:54:57 +0100

    adjust formatting

--- apps/cookbook/src/app/examples/avatar-example/examples/default.ts
+++ apps/cookbook/src/app/examples/avatar-example/examples/default.ts
@@ -4,10 +4,21 @@ import { IconComponent } from '@kirbydesign/designsystem/icon';
 
 const config = {
   selector: 'cookbook-avatar-example-default',
-  template: `<kirby-avatar size="xs">  <kirby-icon name="kirby"></kirby-icon></kirby-avatar>
-<kirby-avatar size="sm">  <kirby-icon name="kirby"></kirby-icon></kirby-avatar>
-<kirby-avatar size="md">  <kirby-icon name="kirby"></kirby-icon></kirby-avatar>
-<kirby-avatar size="lg">  <kirby-icon name="kirby"></kirby-icon></kirby-avatar>`,
+  template: `<kirby-avatar size="xs">
+  <kirby-icon name="kirby"></kirby-icon>
+</kirby-avatar>
+
+<kirby-avatar size="sm">
+  <kirby-icon name="kirby"></kirby-icon>
+</kirby-avatar>
+
+<kirby-avatar size="md">
+  <kirby-icon name="kirby"></kirby-icon>
+</kirby-avatar>
+
+<kirby-avatar size="lg">
+  <kirby-icon name="kirby"></kirby-icon>
+</kirby-avatar>`,
 };
 
 @Component({
