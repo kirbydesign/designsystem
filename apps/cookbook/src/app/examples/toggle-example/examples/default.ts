48a7e6e299f7073244f5797c5c9a8d2b3a6459b6
Louise <lou.bankdata.dk>
2026-03-03 11:19:09 +0100

    remove label in component

--- apps/cookbook/src/app/examples/toggle-example/examples/default.ts
+++ apps/cookbook/src/app/examples/toggle-example/examples/default.ts
@@ -3,7 +3,27 @@ import { ToggleComponent } from '@kirbydesign/designsystem/toggle';
 
 const config = {
   selector: 'cookbook-toggle-default-example',
-  template: `<kirby-toggle>Default</kirby-toggle>
+  template: `<div class="toggle-item-container">
+  <div class="toggle-item-inner-container">
+    <kirby-toggle></kirby-toggle>
+  </div>
+    <span class="toggle-item-title">Default</span>
+</div>
+
+<div class="toggle-item-container">
+  <div class="toggle-item-inner-container">
+    <kirby-toggle checked="true" (checkedChange)="onCheckedChange($event)"></kirby-toggle>
+  </div>
+    <span class="toggle-item-title">Checked</span>
+</div>
+
+<div class="toggle-item-container">
+  <div class="toggle-item-inner-container">
+    <kirby-toggle disabled="true"></kirby-toggle>
+  </div>
+    <span class="toggle-item-title">Disabled</span>
+</div>`,
+  htmlSnippet: `<kirby-toggle>Default</kirby-toggle>
 <kirby-toggle checked="true" (checkedChange)="onCheckedChange($event)">Checked</kirby-toggle>
 <kirby-toggle disabled="true">Disabled</kirby-toggle>`,
 };
@@ -15,7 +35,7 @@ const config = {
   imports: [ToggleComponent],
 })
 export class ToggleDefaultExampleComponent {
-  template = config.template;
+  htmlSnippet: string = config.htmlSnippet;
 
   onCheckedChange(checked: boolean) {
     console.log(`Toggle onCheckedChange: ${checked}`);
