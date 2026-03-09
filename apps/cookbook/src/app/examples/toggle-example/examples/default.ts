873092121efb2fc03b270ee8aff64d20ac287b97
Louise <lou.bankdata.dk>
2026-03-09 15:03:07 +0100

    Revert "remove label in component"
    This reverts commit 48a7e6e299f7073244f5797c5c9a8d2b3a6459b6.

--- apps/cookbook/src/app/examples/toggle-example/examples/default.ts
+++ apps/cookbook/src/app/examples/toggle-example/examples/default.ts
@@ -3,27 +3,7 @@ import { ToggleComponent } from '@kirbydesign/designsystem/toggle';
 
 const config = {
   selector: 'cookbook-toggle-default-example',
-  template: `<div class="toggle-item-container">
-  <div class="toggle-item-inner-container">
-    <kirby-toggle></kirby-toggle>
-  </div>
-    <span class="toggle-item-title">Default</span>
-</div>
-
-<div class="toggle-item-container">
-  <div class="toggle-item-inner-container">
-    <kirby-toggle checked="true" (checkedChange)="onCheckedChange($event)"></kirby-toggle>
-  </div>
-    <span class="toggle-item-title">Checked</span>
-</div>
-
-<div class="toggle-item-container">
-  <div class="toggle-item-inner-container">
-    <kirby-toggle disabled="true"></kirby-toggle>
-  </div>
-    <span class="toggle-item-title">Disabled</span>
-</div>`,
-  htmlSnippet: `<kirby-toggle>Default</kirby-toggle>
+  template: `<kirby-toggle>Default</kirby-toggle>
 <kirby-toggle checked="true" (checkedChange)="onCheckedChange($event)">Checked</kirby-toggle>
 <kirby-toggle disabled="true">Disabled</kirby-toggle>`,
 };
@@ -35,7 +15,7 @@ const config = {
   imports: [ToggleComponent],
 })
 export class ToggleDefaultExampleComponent {
-  htmlSnippet: string = config.htmlSnippet;
+  template = config.template;
 
   onCheckedChange(checked: boolean) {
     console.log(`Toggle onCheckedChange: ${checked}`);
