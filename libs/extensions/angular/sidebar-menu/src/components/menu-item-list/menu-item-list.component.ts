77549b1e1e30fc99bc088692a9f116a1b806c374
bducrv <crv@bankdata.dk>
2026-03-13 10:16:50 +0100

    Add draggable flag to menu item list

--- libs/extensions/angular/sidebar-menu/src/components/menu-item-list/menu-item-list.component.ts
+++ libs/extensions/angular/sidebar-menu/src/components/menu-item-list/menu-item-list.component.ts
@@ -10,6 +10,7 @@ type ViewModel = {
   id: Signal<string | undefined>;
   size: Signal<MenuItemSize>;
   items: Signal<SidebarMenuItem[]>;
+  isDraggable: Signal<boolean>;
 };
 
 @Component({
@@ -32,10 +33,12 @@ export class MenuItemListComponent {
   readonly items = input.required<SidebarMenuItem[]>();
   readonly size = input<MenuItemSize>('lg');
   readonly id = input<string>();
+  readonly isDraggable = input<boolean>(false);
 
   readonly vm: ViewModel = {
     id: this.id,
     size: this.size,
     items: this.items,
+    isDraggable: this.isDraggable,
   };
 }
