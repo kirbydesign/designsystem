f5ae77724856f2a02ca36a7262ffa53df01ad565
Rasmus <erasmus1212@hotmail.com>
2026-03-18 11:19:51 +0100

    refactor: rename dropdownId to comboboxId for improved accessibility

--- libs/extensions/angular/multi-select-autocomplete/src/multi-select-autocomplete.component.ts
+++ libs/extensions/angular/multi-select-autocomplete/src/multi-select-autocomplete.component.ts
@@ -63,7 +63,7 @@ export class MultiSelectAutocompleteComponent
   private _popout: HorizontalDirection | `${HorizontalDirection}` = HorizontalDirection.right;
   private _attributesToForward = ['aria-label', 'aria-labelledby'];
 
-  public dropdownId: string = UniqueIdGenerator.scopedTo('kirby-dropdown').next();
+  public comboboxId: string = UniqueIdGenerator.scopedTo('kirby-dropdown').next();
   public inputId: string = UniqueIdGenerator.scopedTo('kirby-input').next();
 
   private readonly _defaultSearchFunction = (searchTerm: string): unknown[] => {
@@ -105,7 +105,7 @@ export class MultiSelectAutocompleteComponent
 
     // object is complex, but the user uses standard template, so we generate an id based on the text
     if (item && typeof item === 'object' && !this.itemTemplate) {
-      return this.dropdownId + '-item-' + this.getItemText(item);
+      return this.comboboxId + '-item-' + this.getItemText(item);
     }
 
     // object is complex, and the user uses custom template, so we rely on itemIdProperty
