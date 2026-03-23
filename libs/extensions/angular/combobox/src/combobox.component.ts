546480a0305c9f27cf2f443f8019a6d3816b570e
Rasmus <erasmus1212@hotmail.com>
2026-03-23 07:52:19 +0100

    refactor: add has-value class binding and restore text-overflow for input with value

--- libs/extensions/angular/combobox/src/combobox.component.ts
+++ libs/extensions/angular/combobox/src/combobox.component.ts
@@ -302,6 +302,11 @@ export class ComboboxComponent implements OnInit, AfterViewInit, OnDestroy, Cont
     return this.state === OpenState.open;
   }
 
+  @HostBinding('class.has-value')
+  public get hasValue(): boolean {
+    return this.selectedItem != null;
+  }
+
   @ContentChild(ListItemTemplateDirective, { read: TemplateRef })
   public itemTemplate?: TemplateRef<unknown>;
 
