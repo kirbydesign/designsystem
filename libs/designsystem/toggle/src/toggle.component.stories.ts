92866e19beb20a947cdcc830519ccf9a8da8079f
loujuu <lojuujen@gmail.com>
2026-04-20 14:42:18 +0200

    dlt states in storybook

--- libs/designsystem/toggle/src/toggle.component.stories.ts
+++ libs/designsystem/toggle/src/toggle.component.stories.ts
@@ -1,39 +1,24 @@
 import type { Meta, StoryObj } from '@storybook/angular';
 import { moduleMetadata } from '@storybook/angular';
 import { ToggleComponent } from '@kirbydesign/designsystem/toggle';
-import { ToggleExampleComponent } from '~/app/examples/toggle-example/toggle-example.component';
-import { ToggleStateExampleComponent } from '~/app/examples/toggle-example/examples/state';
 
 const meta: Meta<ToggleComponent> = {
   component: ToggleComponent,
   title: 'Components / Toggle',
   decorators: [
     moduleMetadata({
-      imports: [ToggleExampleComponent, ToggleStateExampleComponent],
+      imports: [ToggleComponent],
     }),
   ],
 };
 export default meta;
 type Story = StoryObj<ToggleComponent>;
 
-export const Default: Story = {
+export const Toggle: Story = {
   args: {
     checked: false,
     disabled: false,
   },
-};
-
-export const Disabled: Story = {
-  args: {
-    checked: false,
-    disabled: true,
-  },
-};
-
-export const StateExample: Story = {
-  render: () => ({
-    template: `<cookbook-toggle-state-example></cookbook-toggle-state-example>`,
-  }),
 };
 
 export const CookbookExample: Story = {
