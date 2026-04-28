dcad5e4a2c23a4ec2fcd96ce5766f1b8d8ba41a4
RasmusKjeldgaard <raskjeldgaard@gmail.com>
2026-04-28 16:36:00 +0200

    Add input in item example and do some boyscouting of wording to avoid confusion with nested interactives

---
+++ apps/cookbook/src/app/examples/item-example/examples/with-input.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { InputComponent } from '@kirbydesign/designsystem/form-field';
+import { ItemComponent } from '@kirbydesign/designsystem/item';
+import { CardComponent } from '@kirbydesign/designsystem/card';
+import { DividerComponent } from '@kirbydesign/designsystem/divider';
+
+const config = {
+  selector: 'cookbook-item-example-with-input',
+  template: `<kirby-card>
+  <kirby-item>
+    Item with medium size input
+    <input kirby-input slot="end" size="md">
+  </kirby-item>
+</kirby-card>
+<kirby-card>
+  <kirby-item>
+    Item with large (default) size input
+    <input kirby-input slot="end">
+  </kirby-item>
+</kirby-card>`,
+};
+
+@Component({
+  selector: config.selector,
+  template: config.template,
+  styleUrls: ['./_shared.scss'],
+  imports: [ItemComponent, InputComponent, CardComponent, DividerComponent],
+})
+export class ItemExampleWithInputComponent {
+  template: string = config.template;
+}
