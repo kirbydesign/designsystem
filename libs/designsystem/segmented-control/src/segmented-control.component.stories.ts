776b8f64d104a0131d6e113076779b55a65d4edb
Troels Lenda Strand <troelslenda@gmail.com>
2026-05-20 22:20:44 +0200

    Attempt to allow content projection for component segmented-control

--- libs/designsystem/segmented-control/src/segmented-control.component.stories.ts
+++ libs/designsystem/segmented-control/src/segmented-control.component.stories.ts
@@ -1,4 +1,6 @@
 import { type Meta, moduleMetadata, type StoryObj } from '@storybook/angular';
+import { ButtonComponent } from '@kirbydesign/designsystem/button';
+import { IconComponent } from '@kirbydesign/designsystem/icon';
 /* eslint-disable-next-line no-restricted-imports */
 import { SegmentedControlComponent, SegmentedControlMode } from './segmented-control.component';
 
@@ -8,7 +10,12 @@ const meta: Meta<SegmentedControlComponent> = {
   component: SegmentedControlComponent,
   decorators: [
     moduleMetadata({
-      imports: [SegmentedControlExampleComponent],
+      imports: [
+        SegmentedControlComponent,
+        ButtonComponent,
+        IconComponent,
+        SegmentedControlExampleComponent,
+      ],
     }),
   ],
   title: 'Components / Segmented Control',
@@ -42,5 +49,66 @@ export const Default: Story = {
 export const CookbookExamples: Story = {
   render: () => ({
     template: `<cookbook-segmented-control-example></cookbook-segmented-control-example>`,
+  }),
+};
+
+export const ProjectedContent: Story = {
+  args: {
+    mode: SegmentedControlMode.default,
+    selectedIndex: 0,
+    size: 'md',
+  },
+  render: (args) => ({
+    props: args,
+    template: `
+      <kirby-segmented-control
+        [selectedIndex]="selectedIndex"
+        [mode]="mode"
+        [size]="size"
+      >
+        <li>First Item</li>
+        <li>Second Item</li>
+        <li><span class="kirby-text-bold">Third Item</span></li>
+      </kirby-segmented-control>
+    `,
+  }),
+};
+
+export const ProjectedContentChipMode: Story = {
+  args: {
+    mode: SegmentedControlMode.compactChip,
+    selectedIndex: 0,
+    size: 'md',
+  },
+  render: (args) => ({
+    props: args,
+    template: `
+      <kirby-segmented-control
+        [selectedIndex]="selectedIndex"
+        [mode]="mode"
+        [size]="size"
+      >
+        <li><div>1D</div><div class="negative">-1.54%</div></li>
+        <li><div>1M</div><div class="negative">-15.54%</div></li>
+        <li><div>3M</div><div class="positive">+8.54%</div></li>
+        <li><div>6M</div><div class="positive">+7.54%</div></li>
+        <li><div>ÅTD</div><div class="positive">+17.54%</div></li>
+        <li><div>1ÅR</div><div class="positive">+107.54%</div></li>
+        <li><div>Maks</div><div class="positive">+141.54%</div></li>
+        <li><div class="calendar"><button kirby-button attentionLevel="3"><kirby-icon name="calendar"></kirby-icon></button></div></li>
+      </kirby-segmented-control>
+      
+    `,
+    styles: [
+      `
+        ::ng-deep .positive {
+          color: green;
+        }
+
+        ::ng-deep .negative {
+          color: red;
+        }
+      `,
+    ],
   }),
 };
