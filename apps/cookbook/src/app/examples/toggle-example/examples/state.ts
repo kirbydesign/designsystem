0e43d8fb0512d001941b3d015e23a5c338e8cfd9
loujuu <lojuujen@gmail.com>
2026-05-26 10:19:55 +0200

    cookbook adjustments

--- apps/cookbook/src/app/examples/toggle-example/examples/state.ts
+++ apps/cookbook/src/app/examples/toggle-example/examples/state.ts
@@ -3,9 +3,9 @@ import { ToggleComponent } from '@kirbydesign/designsystem/toggle';
 
 const config = {
   selector: 'cookbook-toggle-state-example',
-  template: `<kirby-toggle slot="end"></kirby-toggle>
-<kirby-toggle checked="true" slot="end"></kirby-toggle>
-<kirby-toggle disabled="true" slot="end"></kirby-toggle>
+  template: `<kirby-toggle slot="end" aria-label="Default toggle"></kirby-toggle>
+<kirby-toggle checked="true" slot="end" aria-label="Checked toggle"></kirby-toggle>
+<kirby-toggle disabled="true" slot="end" aria-label="Disabled toggle"></kirby-toggle>
 `,
 };
 
