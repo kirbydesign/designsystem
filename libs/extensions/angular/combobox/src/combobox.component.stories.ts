7b21a7732ffa60ea615949761c4100ee8718d1d1
Rasmus <erasmus1212@hotmail.com>
2026-05-30 12:06:00 +0200

    Fix: remove hardcoded selectedItem from combobox story

--- libs/extensions/angular/combobox/src/combobox.component.stories.ts
+++ libs/extensions/angular/combobox/src/combobox.component.stories.ts
@@ -158,7 +158,6 @@ export const Default: Story = {
     itemTextProperty: 'text',
     itemIdProperty: 'id',
     noSearchResultsText: 'No results found.',
-    selectedItem: 'USD',
     size: InputSize.medium,
     expand: undefined,
     popout: HorizontalDirection.right,
@@ -175,7 +174,6 @@ export const Default: Story = {
         [itemTextProperty]="itemTextProperty"
         [itemIdProperty]="itemIdProperty"
         [noSearchResultsText]="noSearchResultsText"
-        [selectedItem]="selectedItem"
         [size]="size"
         [expand]="expand"
         [popout]="popout"
