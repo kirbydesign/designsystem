4049482bbf940ff4a9f936964857090848f1d65c
loujuu <lojuujen@gmail.com>
2026-06-22 11:07:42 +0200

    dd disclosure on time

--- apps/cookbook/src/app/examples/item-example/examples/complex-labels.ts
+++ apps/cookbook/src/app/examples/item-example/examples/complex-labels.ts
@@ -6,17 +6,19 @@ import { AvatarComponent, IconComponent } from '@kirbydesign/designsystem';
 const config = {
   selector: 'cookbook-item-example-complex-labels',
   template: `<kirby-item>
+  <kirby-badge slot="outside" themeColor="danger" size="sm"></kirby-badge>
   <kirby-avatar slot="start" overlay="true" imageSrc="/assets/images/woman.png">
-    <kirby-badge>
-      <kirby-icon name="attach"></kirby-icon>
-    </kirby-badge>
   </kirby-avatar>
-  <kirby-label>
-    <kirby-label direction="horizontal">
+   <kirby-label>
+     <kirby-label direction="horizontal">
       <p class="kirby-item-title">Fusce id neque suscipit, finibus urna convallis, auctor arcu.</p>
-      <time class="kirby-item-detail">20.12.2017</time>
+      <p class="kirby-item-detail">
+        <time>20.12.2017</time>
+          <kirby-icon name="arrow-more"></kirby-icon>
+        </p>
     </kirby-label>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non neque vitae felis ultricies imperdiet in ut orci.</p>
+    <p class="kirby-item-subtitle">Subtitle</p>
+    <p class="kirby-item-detail">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non neque vitae felis ultricies imperdiet in ut orci. Ut non neque vitae felis ultricies imperdiet in ut orci.</p>
   </kirby-label>
 </kirby-item>`,
 };
