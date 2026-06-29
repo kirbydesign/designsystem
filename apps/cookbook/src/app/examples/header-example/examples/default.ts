40279649ee073702e1d1194cc0a6ad75af8c9700
loujuu <lojuujen@gmail.com>
2026-06-29 14:25:37 +0200

    subtitle resize wrap two lines

--- apps/cookbook/src/app/examples/header-example/examples/default.ts
+++ apps/cookbook/src/app/examples/header-example/examples/default.ts
@@ -3,7 +3,7 @@ import { HeaderComponent } from '@kirbydesign/designsystem/header';
 
 const config = {
   selector: 'cookbook-header-example-default',
-  template: `<kirby-header [title]="'Title'" subtitle1="Subtitle one" subtitle2="Subtitle two">
+  template: `<kirby-header [title]="'Title'" subtitle1="Subtitle One. This is a very long example of a very long subtitle. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non neque vitae felis ultricies imperdiet in ut orci." subtitle2="Subtitle two">
 </kirby-header>`,
 };
 
