74c6a64f69646388f9657fe3f4a3704bde412b8f
vindnn <109983719+VindNN@users.noreply.github.com>
2026-07-03 11:58:41 +0200

    Sidebar: allow custom loading menu content in sidebar component (#4508)
    Co-authored-by: bducrv <crv@bankdata.dk>
    Co-authored-by: Jonas Plesner <75488623+Jonaythaz@users.noreply.github.com>
    Co-authored-by: Søren Oest Balmer <oestbalmer@gmail.com>

--- libs/extensions/angular/sidebar-menu/src/index.ts
+++ libs/extensions/angular/sidebar-menu/src/index.ts
@@ -2,4 +2,6 @@ export * from './models';
 export * from './public-components/sidebar';
 export * from './public-components/sidebar-footer';
 export * from './public-components/sidebar-header';
+export * from './public-components/sidebar-menu';
+export * from './public-components/sidebar-menu-loader';
 export * from './sidebar.module';
