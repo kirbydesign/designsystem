54ee1375d441de5c1385d43f14103b9e576f7c0b
loujuu <lojuujen@gmail.com>
2026-08-05 09:13:12 +0200

    add md text to default

--- apps/cookbook/src/app/examples/progress-circle-example/examples/default.ts
+++ apps/cookbook/src/app/examples/progress-circle-example/examples/default.ts
@@ -3,12 +3,13 @@ import { ProgressCircleComponent } from '@kirbydesign/designsystem/progress-circ
 
 const config = {
   selector: 'cookbook-progress-circle-example-default',
-  template: `<kirby-progress-circle aria-label="Your investment savings" value="33"></kirby-progress-circle>`,
+  template: `<kirby-progress-circle aria-label="Your investment savings" value="33"><p>md</p></kirby-progress-circle>`,
 };
 
 @Component({
   selector: config.selector,
   template: config.template,
+  styleUrls: ['./progress-circle-examples.shared.scss'],
   imports: [ProgressCircleComponent],
 })
 export class ProgressCircleExampleDefaultComponent {
