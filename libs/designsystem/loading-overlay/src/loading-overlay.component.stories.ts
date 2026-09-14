22a1910694640c76a3d7b5348ebca6602a0a5777
loujuu <lojuujen@gmail.com>
2026-09-14 10:48:59 +0200

    chromatic test

--- libs/designsystem/loading-overlay/src/loading-overlay.component.stories.ts
+++ libs/designsystem/loading-overlay/src/loading-overlay.component.stories.ts
@@ -15,3 +15,20 @@ export const LoadingOverlay: Story = {
     hideContent: false,
   },
 };
+
+export const ScrollableContent: Story = {
+  render: () => ({
+    template: `
+      <kirby-loading-overlay [isLoading]="true" [showBackdrop]="true" [hideContent]="false">
+        <div style="height: 800px; overflow-y: auto;">
+          @for (item of items; track item) {
+            <p>Scrollable content line {{ item }}</p>
+          }
+        </div>
+      </kirby-loading-overlay>
+    `,
+    props: {
+      items: Array.from({ length: 50 }, (_, index) => index + 1),
+    },
+  }),
+};
