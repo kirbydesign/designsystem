dd0387c8a4cfb2bca0a6282ef25435a2d70c45d0
loujuu <lojuujen@gmail.com>
2026-09-17 09:29:06 +0200

    add icon size xs and selectable item

--- apps/cookbook/src/app/examples/item-example/examples/complex-labels.ts
+++ apps/cookbook/src/app/examples/item-example/examples/complex-labels.ts
@@ -5,14 +5,14 @@ import { AvatarComponent, IconComponent } from '@kirbydesign/designsystem';
 
 const config = {
   selector: 'cookbook-item-example-complex-labels',
-  template: `<kirby-item>
+  template: `<kirby-item [selectable]="true">
   <kirby-badge slot="outside" themeColor="danger" size="sm"></kirby-badge>
   <kirby-avatar slot="start" overlay="true" imageSrc="/assets/images/woman.png"></kirby-avatar>
    <kirby-label>
      <kirby-label direction="horizontal">
       <p class="kirby-item-title">Fusce id neque suscipit, finibus urna convallis, auctor arcu.</p>
       <p class="kirby-item-disclosure">
-        <time>20.12.2017</time><kirby-icon name="arrow-more"></kirby-icon>
+        <time>20.12.2017</time><kirby-icon name="arrow-more" size="xs"></kirby-icon>
       </p>
     </kirby-label>
       <p class="kirby-item-subtitle">Subtitle will wrap if necessary in two lines and truncate with ellipsis if it overflows.</p>
